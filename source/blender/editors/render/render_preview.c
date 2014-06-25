Pagecran/blender

commit ee5f43247b0446467f015b665a8c84cf05c62de7

Fix T40436: No preview in Brush with Cycles
More like a feature request, but after i've implemented preview for
icons it was rather simple to support nodes previews.

source/blender/editors/render/render_preview.c
@@ -299,10 +299,10 @@ static Scene *preview_prepare_scene(Scene *scene, ID *id, int id_type, ShaderPre
 
 		sce->r.cfra = scene->r.cfra;
 
-		if (id_type == ID_TE && sp->pr_method == PR_ICON_RENDER) {
-			/* force blender internal for texture icons render,
+		if (id_type == ID_TE && ELEM(sp->pr_method, PR_ICON_RENDER, PR_NODE_RENDER)) {
+			/* Force blender internal for texture icons and nodes render,
 			 * seems commonly used render engines does not support
-			 * such kind of rendering
+			 * such kind of rendering.
 			 */
 			BLI_strncpy(sce->r.engine, "BLENDER_RENDER", sizeof(sce->r.engine));
 		}
@@ -1138,10 +1138,6 @@ void ED_preview_shader_job(const bContext *C, void *owner, ID *id, ID *parent, M
 	ShaderPreview *sp;
 	Scene *scene = CTX_data_scene(C);
 
-	/* node previews not supported for cycles */
-	if (BKE_scene_use_new_shading_nodes(scene) && method == PR_NODE_RENDER)
-		return;
-
 	wm_job = WM_jobs_get(CTX_wm_manager(C), CTX_wm_window(C), owner, "Shader Preview",
 	                    WM_JOB_EXCL_RENDER, WM_JOB_TYPE_RENDER_PREVIEW);
 	sp = MEM_callocN(sizeof(ShaderPreview), "shader preview");
@@ -1158,7 +1154,7 @@ void ED_preview_shader_job(const bContext *C, void *owner, ID *id, ID *parent, M
 
 	/* hardcoded preview .blend for cycles/internal, this should be solved
 	 * once with custom preview .blend path for external engines */
-	if (BKE_scene_use_new_shading_nodes(scene))
+	if (BKE_scene_use_new_shading_nodes(scene) && method != PR_NODE_RENDER)
 		sp->pr_main = G_pr_main_cycles;
 	else
 		sp->pr_main = G_pr_main;

commit 05f5d8e982a8a99d498d0aad8522a02a80bb3cc6

Fix T40800: Crash when editing material nodes
it was caused by ee5f432. Not sure why exactly blender runs into
threading conflicts here since material preview works.

Let's keep it simple for now and disable material nodes preview,
because it's not supported by the interface anyway.

This is to be included to the final release.

source/blender/editors/render/render_preview.c
@@ -1138,6 +1138,12 @@ void ED_preview_shader_job(const bContext *C, void *owner, ID *id, ID *parent, M
 	ShaderPreview *sp;
 	Scene *scene = CTX_data_scene(C);
 	short id_type = GS(id->name);
+	bool use_new_shading = BKE_scene_use_new_shading_nodes(scene);
+
+	/* Only texture node preview is supported with Cycles. */
+	if (use_new_shading && method == PR_NODE_RENDER && id_type != ID_TE) {
+		return;
+	}
 
 	wm_job = WM_jobs_get(CTX_wm_manager(C), CTX_wm_window(C), owner, "Shader Preview",
 	                    WM_JOB_EXCL_RENDER, WM_JOB_TYPE_RENDER_PREVIEW);
@@ -1155,10 +1161,7 @@ void ED_preview_shader_job(const bContext *C, void *owner, ID *id, ID *parent, M
 
 	/* hardcoded preview .blend for cycles/internal, this should be solved
 	 * once with custom preview .blend path for external engines */
-	if ((method != PR_NODE_RENDER) &&
-	    id_type != ID_TE &&
-	    BKE_scene_use_new_shading_nodes(scene))
-	{
+	if ((method != PR_NODE_RENDER) && id_type != ID_TE && use_new_shading) {
 		sp->pr_main = G_pr_main_cycles;
 	}
 	else {