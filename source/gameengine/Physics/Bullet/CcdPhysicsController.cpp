Pagecran/blender

commit b90de0331df6c92af909b20a3e183596ff3511e4

BGE: Cleaning up the BGE's physics code and removing KX_IPhysicsController and KX_BulletPhysicsController. Instead, we just use PHY_IPhysicsController, which removes a lot of duplicate code.
This is a squashed commit of the following:
    BGE Physics Cleanup: Fix crashes with LibLoading and replication. Also fixing some memory leaks.
    BGE Physics Cleanup: Removing KX_IPhysicsController and KX_BulletPhysicsController.
    BGE Physics Cleanup: Moving the replication code outside of KX_BlenderBulletController and switching KX_ConvertPhysicsObjects to create a CcdPhysicsController instead of a KX_BlenderBulletController.
    BGE Physics Cleanup: Getting rid of an unsued KX_BulletPhysicsController.h include in KX_Scene.cpp.
    BGE Physics Cleanup: Removing unused KX_IPhysicsController and KX_BulletPhysicsController includes.
    BGE Physics Cleanup: Removing m_pPhysicsController1 and GetPhysicsController1() from KX_GameObject.
    BGE Physics Cleanup: Remove SetRigidBody() from KX_IPhysicsController and remove GetName() from CcdPhysicsController.
    BGE Physics Cleanup: Moving Add/RemoveCompoundChild() from KX_IPhysicsController to PHY_IPhysicsController.
    BGE Physics Cleanup: Removing GetLocalInertia() from KX_IPhysicsController.
    BGE Physics Cleanup: Making BlenderBulletCharacterController derive from PHY_ICharacter and removing CharacterWrapper from CcdPhysicsEnvironment.cpp. Also removing the character functions from KX_IPhysicsController.
    BGE Physics Cleanup: Removing GetOrientation(), SetOrientation(), SetPosition(), SetScaling(), and GetRadius() from KX_IPhysicsController.
    BGE Physics Cleanup: Removing GetReactionForce() since all implementations returned (0, 0, 0). The Python interface for KX_GameObject still has reaction force code, but it still also returns (0, 0, 0). This can probably be removed as well, but removing it can break scripts, so I'll leave it for now.

source/gameengine/Physics/Bullet/CcdPhysicsController.h
@@ -33,6 +33,7 @@ subject to the following restrictions:
 #include "LinearMath/btTransform.h"
 
 #include "PHY_IMotionState.h"
+#include "PHY_ICharacter.h"
 
 extern float gDeactivationTime;
 extern float gLinearSleepingTreshold;
@@ -266,6 +267,7 @@ struct CcdConstructionInfo
 		m_soft_kSHR(1.0f),
 		m_soft_kAHR(0.7f),
 		m_collisionFlags(0),
+	    m_bDyna(false),
 		m_bRigid(false),
 		m_bSoft(false),
 		m_bSensor(false),
@@ -349,6 +351,7 @@ struct CcdConstructionInfo
 
 
 	int			m_collisionFlags;
+	bool		m_bDyna;
 	bool		m_bRigid;
 	bool		m_bSoft;
 	bool		m_bSensor;
@@ -396,7 +399,7 @@ class btCollisionObject;
 class btSoftBody;
 class btPairCachingGhostObject;
 
-class BlenderBulletCharacterController : public btKinematicCharacterController
+class BlenderBulletCharacterController : public btKinematicCharacterController, public PHY_ICharacter
 {
 private:
 	btMotionState* m_motionState;
@@ -419,6 +422,25 @@ public:
 	virtual void jump();
 
 	const btVector3& getWalkDirection();
+
+	// PHY_ICharacter interface
+	virtual void Jump()	{ jump(); }
+	virtual bool OnGround(){ return onGround(); }
+	virtual float GetGravity() { return getGravity(); }
+	virtual void SetGravity(float gravity) { setGravity(gravity); }
+	virtual int GetMaxJumps() { return getMaxJumps(); }
+	virtual void SetMaxJumps(int maxJumps) { setMaxJumps(maxJumps); }
+	virtual int GetJumpCount() { return getJumpCount(); }
+	virtual void SetWalkDirection(const MT_Vector3& dir)
+	{
+		btVector3 vec = btVector3(dir[0], dir[1], dir[2]);
+		setWalkDirection(vec);
+	}
+	virtual MT_Vector3 GetWalkDirection()
+	{
+		btVector3 vec = getWalkDirection();
+		return MT_Vector3(vec[0], vec[1], vec[2]);
+	}
 };
 
 ///CcdPhysicsController is a physics object that supports continuous collision detection and time of impact based physics resolution.
@@ -433,6 +455,7 @@ protected:
 	btMotionState* 	m_bulletMotionState;
 	class btCollisionShape*	m_collisionShape;
 	class CcdShapeConstructionInfo* m_shapeInfo;
+	btCollisionShape* m_bulletChildShape;
 
 	friend class CcdPhysicsEnvironment;	// needed when updating the controller
 
@@ -449,6 +472,10 @@ protected:
 
 	CcdPhysicsController* m_parentCtrl;
 
+	int m_savedCollisionFlags;
+	MT_Scalar m_savedMass;
+	bool m_suspended;
+
 	void GetWorldOrientation(btMatrix3x3& mat);
 
 	void CreateRigidbody();
@@ -462,8 +489,8 @@ protected:
 		return (--m_registerCount == 0) ? true : false;
 	}
 
-	void setWorldOrientation(const btMatrix3x3& mat);
-	void forceWorldTransform(const btMatrix3x3& mat, const btVector3& pos);
+	void SetWorldOrientation(const btMatrix3x3& mat);
+	void ForceWorldTransform(const btMatrix3x3& mat, const btVector3& pos);
 
 	public:
 	
@@ -477,11 +504,11 @@ protected:
 
 		virtual ~CcdPhysicsController();
 
-		CcdConstructionInfo& getConstructionInfo()
+		CcdConstructionInfo& GetConstructionInfo()
 		{
 			return m_cci;
 		}
-		const CcdConstructionInfo& getConstructionInfo() const
+		const CcdConstructionInfo& GetConstructionInfo() const
 		{
 			return m_cci;
 		}
@@ -518,42 +545,51 @@ protected:
 		virtual void		SetPhysicsEnvironment(class PHY_IPhysicsEnvironment *env);
 
 		// kinematic methods
-		virtual void		RelativeTranslate(float dlocX,float dlocY,float dlocZ,bool local);
-		virtual void		SetWalkDirection(float dirX,float dirY,float dirZ,bool local);
-		virtual void		RelativeRotate(const float drot[9],bool local);
-		virtual	void		getOrientation(float &quatImag0,float &quatImag1,float &quatImag2,float &quatReal);
-		virtual	void		setOrientation(float quatImag0,float quatImag1,float quatImag2,float quatReal);
-		virtual	void		setPosition(float posX,float posY,float posZ);
-		virtual	void 		getPosition(MT_Vector3&	pos) const;
+		virtual void		RelativeTranslate(const MT_Vector3& dloc,bool local);
+		virtual void		RelativeRotate(const MT_Matrix3x3&rotval, bool local);
+		virtual	MT_Matrix3x3	GetOrientation();
+		virtual	void		SetOrientation(const MT_Matrix3x3& orn);
+		virtual	void		SetPosition(const MT_Vector3& pos);
+		virtual	void 		GetPosition(MT_Vector3&	pos) const;
+		virtual	void		SetScaling(const MT_Vector3& scale);
+		virtual void		SetTransform();
 
-		virtual	void		setScaling(float scaleX,float scaleY,float scaleZ);
+		virtual	MT_Scalar	GetMass();
+		virtual void	SetMass(MT_Scalar newmass);
 		
 		// physics methods
-		virtual void		ApplyTorque(float torqueX,float torqueY,float torqueZ,bool local);
-		virtual void		ApplyForce(float forceX,float forceY,float forceZ,bool local);
-		virtual void		SetAngularVelocity(float ang_velX,float ang_velY,float ang_velZ,bool local);
-		virtual void		SetLinearVelocity(float lin_velX,float lin_velY,float lin_velZ,bool local);
-		virtual void		applyImpulse(float attachX,float attachY,float attachZ, float impulseX,float impulseY,float impulseZ);
+		virtual void		ApplyImpulse(const MT_Point3& attach, const MT_Vector3& impulsein);
+		virtual void		ApplyTorque(const MT_Vector3& torque,bool local);
+		virtual void		ApplyForce(const MT_Vector3& force,bool local);
+		virtual void		SetAngularVelocity(const MT_Vector3& ang_vel,bool local);
+		virtual void		SetLinearVelocity(const MT_Vector3& lin_vel,bool local);
 		virtual void		Jump();
 		virtual void		SetActive(bool active);
 
 		// reading out information from physics
-		virtual void		GetLinearVelocity(float& linvX,float& linvY,float& linvZ);
-		virtual void		GetAngularVelocity(float& angVelX,float& angVelY,float& angVelZ);
-		virtual void		GetVelocity(const float posX,const float posY,const float posZ,float& linvX,float& linvY,float& linvZ); 
-		virtual	void		getReactionForce(float& forceX,float& forceY,float& forceZ);
-		virtual void		GetWalkDirection(float& dirX,float& dirY,float& dirZ);
+		virtual MT_Vector3	GetLinearVelocity();
+		virtual MT_Vector3	GetAngularVelocity();
+		virtual MT_Vector3	GetVelocity(const MT_Point3& posin);
+		virtual	MT_Vector3	GetLocalInertia();
 
 		// dyna's that are rigidbody are free in orientation, dyna's with non-rigidbody are restricted 
-		virtual	void		setRigidBody(bool rigid);
+		virtual	void		SetRigidBody(bool rigid);
 
 		
-		virtual void		resolveCombinedVelocities(float linvelX,float linvelY,float linvelZ,float angVelX,float angVelY,float angVelZ);
+		virtual void		ResolveCombinedVelocities(float linvelX,float linvelY,float linvelZ,float angVelX,float angVelY,float angVelZ);
+
+		virtual void		SuspendDynamics(bool ghost);
+		virtual void		RestoreDynamics();
+
+		// Shape control
+		virtual void    AddCompoundChild(PHY_IPhysicsController* child);
+		virtual void    RemoveCompoundChild(PHY_IPhysicsController* child);
 
 		// clientinfo for raycasts for example
-		virtual	void*				getNewClientInfo();
-		virtual	void				setNewClientInfo(void* clientinfo);
+		virtual	void*				GetNewClientInfo();
+		virtual	void				SetNewClientInfo(void* clientinfo);
 		virtual PHY_IPhysicsController*	GetReplica();
+		virtual PHY_IPhysicsController* GetReplicaForSensors();
 		
 		///There should be no 'SetCollisionFilterGroup' method, as changing this during run-time is will result in errors
 		short int	GetCollisionFilterGroup() const
@@ -566,7 +602,7 @@ protected:
 			return m_cci.m_collisionFilterMask;
 		}
 
-		virtual void calcXform() {}
+		virtual void CalcXform() {}
 		virtual void SetMargin(float margin) 
 		{
 			if (m_collisionShape)
@@ -609,7 +645,7 @@ protected:
 			return m_cci.m_clamp_vel_max;
 		}
 
-		bool	wantsSleeping();
+		bool	WantsSleeping();
 
 		void	UpdateDeactivation(float timeStep);
 
@@ -635,24 +671,29 @@ protected:
 			return m_cci.m_physicsEnv;
 		}
 
-		void	setParentCtrl(CcdPhysicsController* parentCtrl)
+		void	SetParentCtrl(CcdPhysicsController* parentCtrl)
 		{
 			m_parentCtrl = parentCtrl;
 		}
 
-		CcdPhysicsController*	getParentCtrl()
+		CcdPhysicsController*	GetParentCtrl()
 		{
 			return m_parentCtrl;
 		}
 
-		const CcdPhysicsController*	getParentCtrl() const
+		const CcdPhysicsController*	GetParentCtrl() const
 		{
 			return m_parentCtrl;
 		}
 
-		virtual const char* getName()
+		virtual bool IsDynamic()
 		{
-			return 0;
+			return GetConstructionInfo().m_bDyna;
+		}
+
+		virtual bool IsCompound()
+		{
+			return GetConstructionInfo().m_shapeInfo->m_shapeType == PHY_SHAPE_COMPOUND;
 		}
 
 #ifdef WITH_CXX_GUARDEDALLOC
@@ -672,16 +713,16 @@ class	DefaultMotionState : public PHY_IMotionState
 
 		virtual ~DefaultMotionState();
 
-		virtual void	getWorldPosition(float& posX,float& posY,float& posZ);
-		virtual void	getWorldScaling(float& scaleX,float& scaleY,float& scaleZ);
-		virtual void	getWorldOrientation(float& quatIma0,float& quatIma1,float& quatIma2,float& quatReal);
+		virtual void	GetWorldPosition(float& posX,float& posY,float& posZ);
+		virtual void	GetWorldScaling(float& scaleX,float& scaleY,float& scaleZ);
+		virtual void	GetWorldOrientation(float& quatIma0,float& quatIma1,float& quatIma2,float& quatReal);
 		
-		virtual void	setWorldPosition(float posX,float posY,float posZ);
-		virtual	void	setWorldOrientation(float quatIma0,float quatIma1,float quatIma2,float quatReal);
-		virtual void	getWorldOrientation(float* ori);
-		virtual void	setWorldOrientation(const float* ori);
+		virtual void	SetWorldPosition(float posX,float posY,float posZ);
+		virtual	void	SetWorldOrientation(float quatIma0,float quatIma1,float quatIma2,float quatReal);
+		virtual void	GetWorldOrientation(float* ori);
+		virtual void	SetWorldOrientation(const float* ori);
 		
-		virtual	void	calculateWorldTransformations();
+		virtual	void	CalculateWorldTransformations();
 		
 		btTransform	m_worldTransform;
 		btVector3		m_localScaling;

source/gameengine/Converter/BL_BlenderDataConversion.cpp
@@ -1419,12 +1419,12 @@ static void BL_CreateGraphicObjectNew(KX_GameObject* gameobj,
 				PHY_IMotionState* motionstate = new KX_MotionState(gameobj->GetSGNode());
 				CcdGraphicController* ctrl = new CcdGraphicController(env, motionstate);
 				gameobj->SetGraphicController(ctrl);
-				ctrl->setNewClientInfo(gameobj->getClientInfo());
-				ctrl->setLocalAabb(localAabbMin, localAabbMax);
+				ctrl->SetNewClientInfo(gameobj->getClientInfo());
+				ctrl->SetLocalAabb(localAabbMin, localAabbMax);
 				if (isActive) {
 					// add first, this will create the proxy handle, only if the object is visible
 					if (gameobj->GetVisible())
-						env->addCcdGraphicController(ctrl);
+						env->AddCcdGraphicController(ctrl);
 					// update the mesh if there is a deformer, this will also update the bounding box for modifiers
 					RAS_Deformer* deformer = gameobj->GetDeformer();
 					if (deformer)
@@ -2027,22 +2027,8 @@ static void UNUSED_FUNCTION(RBJconstraints)(Object *ob)//not used
 }
 
 #include "PHY_IPhysicsEnvironment.h"
-#include "KX_IPhysicsController.h"
 #include "PHY_DynamicTypes.h"
 
-#if 0  /* UNUSED */
-static KX_IPhysicsController* getPhId(CListValue* sumolist,STR_String busc) {//not used
-
-	for (int j=0;j<sumolist->GetCount();j++)
-	{
-		KX_GameObject* gameobje = (KX_GameObject*) sumolist->GetValue(j);
-		if (gameobje->GetName()==busc)
-			return gameobje->GetPhysicsController();
-	}
-
-	return 0;
-}
-#endif
 
 static KX_GameObject* getGameOb(STR_String busc,CListValue* sumolist)
 {
@@ -2585,7 +2571,7 @@ void BL_ConvertBlenderObjects(struct Main* maggie,
 			kxscene->SetDbvtOcclusionRes(blenderscene->gm.occlusionRes);
 	}
 	if (blenderscene->world)
-		kxscene->GetPhysicsEnvironment()->setNumTimeSubSteps(blenderscene->gm.physubstep);
+		kxscene->GetPhysicsEnvironment()->SetNumTimeSubSteps(blenderscene->gm.physubstep);
 
 	// now that the scenegraph is complete, let's instantiate the deformers.
 	// We need that to create reusable derived mesh and physic shapes
@@ -2676,12 +2662,12 @@ void BL_ConvertBlenderObjects(struct Main* maggie,
 						{
 							KX_GameObject *gotar=getGameOb(dat->tar->id.name+2,sumolist);
 							if (gotar && ((gotar->GetLayer()&activeLayerBitInfo)!=0) && gotar->GetPhysicsController())
-								physctr2 = (PHY_IPhysicsController*) gotar->GetPhysicsController()->GetUserData();
+								physctr2 = gotar->GetPhysicsController();
 						}
 
 						if (gameobj->GetPhysicsController())
 						{
-							PHY_IPhysicsController* physctrl = (PHY_IPhysicsController*) gameobj->GetPhysicsController()->GetUserData();
+							PHY_IPhysicsController* physctrl = gameobj->GetPhysicsController();
 							//we need to pass a full constraint frame, not just axis
 
 							//localConstraintFrameBasis
@@ -2690,7 +2676,7 @@ void BL_ConvertBlenderObjects(struct Main* maggie,
 							MT_Vector3 axis1 = localCFrame.getColumn(1);
 							MT_Vector3 axis2 = localCFrame.getColumn(2);
 								
-							int constraintId = kxscene->GetPhysicsEnvironment()->createConstraint(physctrl,physctr2,(PHY_ConstraintType)dat->type,(float)dat->pivX,
+							int constraintId = kxscene->GetPhysicsEnvironment()->CreateConstraint(physctrl,physctr2,(PHY_ConstraintType)dat->type,(float)dat->pivX,
 								(float)dat->pivY,(float)dat->pivZ,
 								(float)axis0.x(),(float)axis0.y(),(float)axis0.z(),
 								(float)axis1.x(),(float)axis1.y(),(float)axis1.z(),
@@ -2706,11 +2692,11 @@ void BL_ConvertBlenderObjects(struct Main* maggie,
 									{
 										if (dat->flag & dofbit)
 										{
-											kxscene->GetPhysicsEnvironment()->setConstraintParam(constraintId,dof,dat->minLimit[dof],dat->maxLimit[dof]);
+											kxscene->GetPhysicsEnvironment()->SetConstraintParam(constraintId,dof,dat->minLimit[dof],dat->maxLimit[dof]);
 										} else
 										{
 											//minLimit > maxLimit means free(disabled limit) for this degree of freedom
-											kxscene->GetPhysicsEnvironment()->setConstraintParam(constraintId,dof,1,-1);
+											kxscene->GetPhysicsEnvironment()->SetConstraintParam(constraintId,dof,1,-1);
 										}
 										dofbit<<=1;
 									}
@@ -2724,12 +2710,12 @@ void BL_ConvertBlenderObjects(struct Main* maggie,
 									{
 										if (dat->flag & dofbit)
 										{
-											kxscene->GetPhysicsEnvironment()->setConstraintParam(constraintId,dof,dat->minLimit[dof],dat->maxLimit[dof]);
+											kxscene->GetPhysicsEnvironment()->SetConstraintParam(constraintId,dof,dat->minLimit[dof],dat->maxLimit[dof]);
 										}
 										else
 										{
 											//maxLimit < 0 means free(disabled limit) for this degree of freedom
-											kxscene->GetPhysicsEnvironment()->setConstraintParam(constraintId,dof,1,-1);
+											kxscene->GetPhysicsEnvironment()->SetConstraintParam(constraintId,dof,1,-1);
 										}
 										dofbit<<=1;
 									}
@@ -2741,12 +2727,12 @@ void BL_ConvertBlenderObjects(struct Main* maggie,
 									
 									if (dat->flag & dofbit)
 									{
-										kxscene->GetPhysicsEnvironment()->setConstraintParam(constraintId,dof,
+										kxscene->GetPhysicsEnvironment()->SetConstraintParam(constraintId,dof,
 												dat->minLimit[dof],dat->maxLimit[dof]);
 									} else
 									{
 										//minLimit > maxLimit means free(disabled limit) for this degree of freedom
-										kxscene->GetPhysicsEnvironment()->setConstraintParam(constraintId,dof,1,-1);
+										kxscene->GetPhysicsEnvironment()->SetConstraintParam(constraintId,dof,1,-1);
 									}
 								}
 							}

source/gameengine/Converter/BL_ModifierDeformer.cpp
@@ -192,7 +192,7 @@ bool BL_ModifierDeformer::Update(void)
 				float min_r[3], max_r[3];
 				INIT_MINMAX(min_r, max_r);
 				m_dm->getMinMax(m_dm, min_r, max_r);
-				ctrl->setLocalAabb(min_r, max_r);
+				ctrl->SetLocalAabb(min_r, max_r);
 			}
 		}
 		m_lastModifierUpdate=m_gameobj->GetLastFrame();

source/gameengine/Converter/KX_BlenderSceneConverter.cpp
@@ -42,7 +42,6 @@
 #include "PHY_IPhysicsEnvironment.h"
 #include "KX_KetsjiEngine.h"
 #include "KX_PythonInit.h" // So we can handle adding new text datablocks for Python to import
-#include "KX_IPhysicsController.h"
 #include "BL_Material.h"
 #include "BL_ActionActuator.h"
 #include "KX_BlenderMaterial.h"
@@ -355,15 +354,15 @@ void KX_BlenderSceneConverter::ConvertScene(class KX_Scene* destinationscene,
 		case UseBullet:
 			{
 				CcdPhysicsEnvironment* ccdPhysEnv = new CcdPhysicsEnvironment(useDbvtCulling);
-				ccdPhysEnv->setDebugDrawer(new BlenderDebugDraw());
-				ccdPhysEnv->setDeactivationLinearTreshold(blenderscene->gm.lineardeactthreshold);
-				ccdPhysEnv->setDeactivationAngularTreshold(blenderscene->gm.angulardeactthreshold);
-				ccdPhysEnv->setDeactivationTime(blenderscene->gm.deactivationtime);
+				ccdPhysEnv->SetDebugDrawer(new BlenderDebugDraw());
+				ccdPhysEnv->SetDeactivationLinearTreshold(blenderscene->gm.lineardeactthreshold);
+				ccdPhysEnv->SetDeactivationAngularTreshold(blenderscene->gm.angulardeactthreshold);
+				ccdPhysEnv->SetDeactivationTime(blenderscene->gm.deactivationtime);
 
 				SYS_SystemHandle syshandle = SYS_GetSystem(); /*unused*/
 				int visualizePhysics = SYS_GetCommandLineInt(syshandle,"show_physics",0);
 				if (visualizePhysics)
-					ccdPhysEnv->setDebugMode(btIDebugDraw::DBG_DrawWireframe|btIDebugDraw::DBG_DrawAabb|btIDebugDraw::DBG_DrawContactPoints|btIDebugDraw::DBG_DrawText|btIDebugDraw::DBG_DrawConstraintLimits|btIDebugDraw::DBG_DrawConstraints);
+					ccdPhysEnv->SetDebugMode(btIDebugDraw::DBG_DrawWireframe|btIDebugDraw::DBG_DrawAabb|btIDebugDraw::DBG_DrawContactPoints|btIDebugDraw::DBG_DrawText|btIDebugDraw::DBG_DrawConstraintLimits|btIDebugDraw::DBG_DrawConstraints);
 		
 				//todo: get a button in blender ?
 				//disable / enable debug drawing (contact points, aabb's etc)
@@ -709,7 +708,6 @@ void	KX_BlenderSceneConverter::ResetPhysicsObjectsAnimationIpo(bool clearIpo)
 			KX_GameObject* gameObj = (KX_GameObject*)parentList->GetValue(g);
 			if (gameObj->IsDynamic())
 			{
-				//KX_IPhysicsController* physCtrl = gameObj->GetPhysicsController();
 				
 				Object* blenderObject = gameObj->GetBlenderObject();
 				if (blenderObject)
@@ -825,7 +823,6 @@ void	KX_BlenderSceneConverter::WritePhysicsObjectToAnimationIpo(int frameNumber)
 			Object* blenderObject = gameObj->GetBlenderObject();
 			if (blenderObject && blenderObject->parent==NULL && gameObj->IsDynamic())
 			{
-				//KX_IPhysicsController* physCtrl = gameObj->GetPhysicsController();
 
 				if (blenderObject->adt==NULL)
 					BKE_id_add_animdata(&blenderObject->id);
@@ -944,7 +941,6 @@ void	KX_BlenderSceneConverter::TestHandlesPhysicsObjectToAnimationIpo()
 			KX_GameObject* gameObj = (KX_GameObject*)parentList->GetValue(g);
 			if (gameObj->IsDynamic())
 			{
-				//KX_IPhysicsController* physCtrl = gameObj->GetPhysicsController();
 				
 #if 0
 				Object* blenderObject = gameObj->GetBlenderObject();

source/gameengine/Converter/KX_SoftBodyDeformer.cpp
@@ -48,7 +48,6 @@
 #include "CcdPhysicsController.h"
 #include "BulletSoftBody/btSoftBody.h"
 
-#include "KX_BulletPhysicsController.h"
 #include "btBulletDynamicsCommon.h"
 
 void KX_SoftBodyDeformer::Relink(CTR_Map<class CTR_HashedPtr, void*>*map)
@@ -66,7 +65,7 @@ void KX_SoftBodyDeformer::Relink(CTR_Map<class CTR_HashedPtr, void*>*map)
 
 bool KX_SoftBodyDeformer::Apply(class RAS_IPolyMaterial *polymat)
 {
-	KX_BulletPhysicsController* ctrl = (KX_BulletPhysicsController*) m_gameobj->GetPhysicsController();
+	CcdPhysicsController* ctrl = (CcdPhysicsController*) m_gameobj->GetPhysicsController();
 	if (!ctrl)
 		return false;
 

source/gameengine/Ketsji/BL_Action.cpp
@@ -34,6 +34,8 @@
 #include "KX_IpoConvert.h"
 #include "KX_GameObject.h"
 
+#include "SG_Controller.h"
+
 // These three are for getting the action from the logic manager
 #include "KX_Scene.h"
 #include "SCA_LogicManager.h"

source/gameengine/Ketsji/CMakeLists.txt
@@ -71,7 +71,6 @@ set(SRC
 	BL_Texture.cpp
 	KX_ArmatureSensor.cpp
 	KX_BlenderMaterial.cpp
-	KX_BulletPhysicsController.cpp
 	KX_Camera.cpp
 	KX_CameraActuator.cpp
 	KX_CameraIpoSGController.cpp
@@ -86,7 +85,6 @@ set(SRC
 	KX_GameObject.cpp
 	KX_IpoConvert.cpp
 	KX_IPO_SGController.cpp
-	KX_IPhysicsController.cpp
 	KX_IpoActuator.cpp
 	KX_KetsjiEngine.cpp
 	KX_Light.cpp
@@ -146,7 +144,6 @@ set(SRC
 	BL_Texture.h
 	KX_ArmatureSensor.h
 	KX_BlenderMaterial.h
-	KX_BulletPhysicsController.h
 	KX_Camera.h
 	KX_CameraActuator.h
 	KX_CameraIpoSGController.h
@@ -164,7 +161,6 @@ set(SRC
 	KX_IpoConvert.h
 	KX_IPOTransform.h
 	KX_IPO_SGController.h
-	KX_IPhysicsController.h
 	KX_IScalarInterpolator.h
 	KX_ISceneConverter.h
 	KX_ISystem.h

commit 69b314cd38eb378771c3eaac95f4ba5eb74c229a

BGE Fix T39026: No collisions detected with parented rigid body object
Fix for T39026. Preliminary investigation by blenderscripter on D446.
Also it solves bug T41285 (Parenting object leave collission box).

Refactor commit b90de0331df6c92af909b20a3e183596ff3511e4 introduced a
regression bug.

Reviewers: sergof, campbellbarton, moguri

Reviewed By: moguri

Subscribers: blueprintrandom, blenderscripter

Differential Revision: https://developer.blender.org/D1064

source/gameengine/Physics/Bullet/CcdPhysicsController.cpp
@@ -148,6 +148,7 @@ CcdPhysicsController::CcdPhysicsController (const CcdConstructionInfo& ci)
 	m_savedCollisionFilterGroup = 0;
 	m_savedCollisionFilterMask = 0;
 	m_savedMass = 0.0;
+	m_savedDyna = false;
 	m_suspended = false;
 	
 	CreateRigidbody();
@@ -1068,6 +1069,7 @@ void	CcdPhysicsController::SuspendDynamics(bool ghost)
 
 		m_savedCollisionFlags = body->getCollisionFlags();
 		m_savedMass = GetMass();
+		m_savedDyna = m_cci.m_bDyna;
 		m_savedCollisionFilterGroup = handle->m_collisionFilterGroup;
 		m_savedCollisionFilterMask = handle->m_collisionFilterMask;
 		m_suspended = true;
@@ -1076,6 +1078,7 @@ void	CcdPhysicsController::SuspendDynamics(bool ghost)
 			btCollisionObject::CF_STATIC_OBJECT|((ghost)?btCollisionObject::CF_NO_CONTACT_RESPONSE:(m_savedCollisionFlags&btCollisionObject::CF_NO_CONTACT_RESPONSE)),
 			btBroadphaseProxy::StaticFilter,
 			btBroadphaseProxy::AllFilter ^ btBroadphaseProxy::StaticFilter);
+		m_cci.m_bDyna = false;
 	}
 }
 
@@ -1092,6 +1095,7 @@ void	CcdPhysicsController::RestoreDynamics()
 			m_savedCollisionFilterGroup,
 			m_savedCollisionFilterMask);
 		body->activate();
+		m_cci.m_bDyna = m_savedDyna;
 		m_suspended = false;
 	}
 }

source/gameengine/Physics/Bullet/CcdPhysicsController.h
@@ -480,6 +480,7 @@ protected:
 	short m_savedCollisionFilterGroup;
 	short m_savedCollisionFilterMask;
 	MT_Scalar m_savedMass;
+	bool m_savedDyna;
 	bool m_suspended;
 
 