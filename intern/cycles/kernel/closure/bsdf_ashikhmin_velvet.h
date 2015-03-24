Pagecran/blender

commit 01ee21ffb930d1e75c8501da44b06d63b6d46704

Fix T43926: Volume scatter: intersecting objects GPU rendering artifacts Fix T44007: Cycles Volumetrics: block artifacts with overlapping volumes
The issue was caused by uninitialized parameters of some closures, which
lead to unpredictable behavior of shader_merge_closures().

intern/cycles/kernel/closure/bsdf_ashikhmin_velvet.h
@@ -39,6 +39,7 @@ ccl_device int bsdf_ashikhmin_velvet_setup(ShaderClosure *sc)
 {
 	float sigma = fmaxf(sc->data0, 0.01f);
 	sc->data0 = 1.0f/(sigma * sigma); /* m_invsigma2 */
+	sc->data1 = 0.0f;
 	
 	sc->type = CLOSURE_BSDF_ASHIKHMIN_VELVET_ID;
 

intern/cycles/kernel/closure/bsdf_diffuse.h
@@ -40,6 +40,8 @@ CCL_NAMESPACE_BEGIN
 ccl_device int bsdf_diffuse_setup(ShaderClosure *sc)
 {
 	sc->type = CLOSURE_BSDF_DIFFUSE_ID;
+	sc->data0 = 0.0f;
+	sc->data1 = 0.0f;
 	return SD_BSDF|SD_BSDF_HAS_EVAL;
 }
 
@@ -83,6 +85,8 @@ ccl_device int bsdf_diffuse_sample(const ShaderClosure *sc, float3 Ng, float3 I,
 ccl_device int bsdf_translucent_setup(ShaderClosure *sc)
 {
 	sc->type = CLOSURE_BSDF_TRANSLUCENT_ID;
+	sc->data0 = 0.0f;
+	sc->data1 = 0.0f;
 	return SD_BSDF|SD_BSDF_HAS_EVAL;
 }
 

intern/cycles/kernel/closure/bsdf_reflection.h
@@ -40,6 +40,8 @@ CCL_NAMESPACE_BEGIN
 ccl_device int bsdf_reflection_setup(ShaderClosure *sc)
 {
 	sc->type = CLOSURE_BSDF_REFLECTION_ID;
+	sc->data0 = 0.0f;
+	sc->data1 = 0.0f;
 	return SD_BSDF;
 }
 

intern/cycles/kernel/closure/bsdf_refraction.h
@@ -40,6 +40,8 @@ CCL_NAMESPACE_BEGIN
 ccl_device int bsdf_refraction_setup(ShaderClosure *sc)
 {
 	sc->type = CLOSURE_BSDF_REFRACTION_ID;
+	sc->data0 = 0.0f;
+	sc->data1 = 0.0f;
 	return SD_BSDF;
 }
 

intern/cycles/kernel/closure/bsdf_transparent.h
@@ -38,6 +38,8 @@ CCL_NAMESPACE_BEGIN
 ccl_device int bsdf_transparent_setup(ShaderClosure *sc)
 {
 	sc->type = CLOSURE_BSDF_TRANSPARENT_ID;
+	sc->data0 = 0.0f;
+	sc->data1 = 0.0f;
 	return SD_BSDF|SD_TRANSPARENT;
 }
 

intern/cycles/kernel/closure/bssrdf.h
@@ -30,6 +30,7 @@ ccl_device int bssrdf_setup(ShaderClosure *sc, ClosureType type)
 		return flag;
 	}
 	else {
+		sc->data0 = 0.0f;
 		sc->data1 = clamp(sc->data1, 0.0f, 1.0f); /* texture blur */
 		sc->T.x = clamp(sc->T.x, 0.0f, 1.0f); /* sharpness */
 		sc->type = type;

intern/cycles/kernel/closure/volume.h
@@ -35,6 +35,7 @@ ccl_device int volume_henyey_greenstein_setup(ShaderClosure *sc)
 	
 	/* clamp anisotropy to avoid delta function */
 	sc->data0 = signf(sc->data0) * min(fabsf(sc->data0), 1.0f - 1e-3f);
+	sc->data1 = 0.0f;
 
 	return SD_SCATTER;
 }

intern/cycles/kernel/closure/bsdf_diffuse_ramp.h
@@ -52,6 +52,8 @@ ccl_device float3 bsdf_diffuse_ramp_get_color(const ShaderClosure *sc, const flo
 ccl_device int bsdf_diffuse_ramp_setup(ShaderClosure *sc)
 {
 	sc->type = CLOSURE_BSDF_DIFFUSE_RAMP_ID;
+	sc->data0 = 0.0f;
+	sc->data1 = 0.0f;
 	return SD_BSDF|SD_BSDF_HAS_EVAL;
 }
 

commit 02224cb95775206333ec8d3ae4c5fb765eab7ed4

Fix T44123: Cycles SSS renders black in recent builds
Issue was introduced in 01ee21f where i didn't notice *_setup()
function only doing partial initialization, and some of parameters
are expected to be initialized by callee function.

This was hitting only some setups, so tests with benchmark scenes
didn't unleash issues. Now it should all be fine.

This is to go to the 2.74 branch and we actually might re-AHOY.

intern/cycles/kernel/closure/bsdf_ashikhmin_velvet.h
@@ -39,7 +39,6 @@ ccl_device int bsdf_ashikhmin_velvet_setup(ShaderClosure *sc)
 {
 	float sigma = fmaxf(sc->data0, 0.01f);
 	sc->data0 = 1.0f/(sigma * sigma); /* m_invsigma2 */
-	sc->data1 = 0.0f;
 	
 	sc->type = CLOSURE_BSDF_ASHIKHMIN_VELVET_ID;
 

intern/cycles/kernel/closure/bsdf_diffuse.h
@@ -40,8 +40,6 @@ CCL_NAMESPACE_BEGIN
 ccl_device int bsdf_diffuse_setup(ShaderClosure *sc)
 {
 	sc->type = CLOSURE_BSDF_DIFFUSE_ID;
-	sc->data0 = 0.0f;
-	sc->data1 = 0.0f;
 	return SD_BSDF|SD_BSDF_HAS_EVAL;
 }
 
@@ -85,8 +83,6 @@ ccl_device int bsdf_diffuse_sample(const ShaderClosure *sc, float3 Ng, float3 I,
 ccl_device int bsdf_translucent_setup(ShaderClosure *sc)
 {
 	sc->type = CLOSURE_BSDF_TRANSLUCENT_ID;
-	sc->data0 = 0.0f;
-	sc->data1 = 0.0f;
 	return SD_BSDF|SD_BSDF_HAS_EVAL;
 }
 

intern/cycles/kernel/closure/bsdf_reflection.h
@@ -40,8 +40,6 @@ CCL_NAMESPACE_BEGIN
 ccl_device int bsdf_reflection_setup(ShaderClosure *sc)
 {
 	sc->type = CLOSURE_BSDF_REFLECTION_ID;
-	sc->data0 = 0.0f;
-	sc->data1 = 0.0f;
 	return SD_BSDF;
 }
 

intern/cycles/kernel/closure/bsdf_refraction.h
@@ -40,8 +40,6 @@ CCL_NAMESPACE_BEGIN
 ccl_device int bsdf_refraction_setup(ShaderClosure *sc)
 {
 	sc->type = CLOSURE_BSDF_REFRACTION_ID;
-	sc->data0 = 0.0f;
-	sc->data1 = 0.0f;
 	return SD_BSDF;
 }
 

intern/cycles/kernel/closure/bsdf_transparent.h
@@ -38,8 +38,6 @@ CCL_NAMESPACE_BEGIN
 ccl_device int bsdf_transparent_setup(ShaderClosure *sc)
 {
 	sc->type = CLOSURE_BSDF_TRANSPARENT_ID;
-	sc->data0 = 0.0f;
-	sc->data1 = 0.0f;
 	return SD_BSDF|SD_TRANSPARENT;
 }
 

intern/cycles/kernel/closure/bssrdf.h
@@ -30,7 +30,6 @@ ccl_device int bssrdf_setup(ShaderClosure *sc, ClosureType type)
 		return flag;
 	}
 	else {
-		sc->data0 = 0.0f;
 		sc->data1 = clamp(sc->data1, 0.0f, 1.0f); /* texture blur */
 		sc->T.x = clamp(sc->T.x, 0.0f, 1.0f); /* sharpness */
 		sc->type = type;

intern/cycles/kernel/closure/volume.h
@@ -35,7 +35,6 @@ ccl_device int volume_henyey_greenstein_setup(ShaderClosure *sc)
 	
 	/* clamp anisotropy to avoid delta function */
 	sc->data0 = signf(sc->data0) * min(fabsf(sc->data0), 1.0f - 1e-3f);
-	sc->data1 = 0.0f;
 
 	return SD_SCATTER;
 }

intern/cycles/kernel/kernel_shader.h
@@ -798,6 +798,8 @@ ccl_device void shader_eval_surface(KernelGlobals *kg, ShaderData *sd,
 #else
 		sd->closure->weight = make_float3(0.8f, 0.8f, 0.8f);
 		sd->closure->N = sd->N;
+		sd->closyre->data0 = 0.0f;
+		sd->closyre->data1 = 0.0f;
 		sd->flag |= bsdf_diffuse_setup(&sd->closure);
 #endif
 	}

intern/cycles/kernel/osl/osl_shader.cpp
@@ -267,6 +267,7 @@ static void flatten_surface_closure_tree(ShaderData *sd, int path_flag,
 						if(fabsf(weight.x) > 0.0f) {
 							sc.weight = make_float3(weight.x, 0.0f, 0.0f);
 							sc.data0 = bssrdf->radius.x;
+							sc.data1 = 0.0f;
 							sd->flag |= bssrdf_setup(&sc, sc.type);
 							sd->closure[sd->num_closure++] = sc;
 						}
@@ -274,6 +275,7 @@ static void flatten_surface_closure_tree(ShaderData *sd, int path_flag,
 						if(fabsf(weight.y) > 0.0f) {
 							sc.weight = make_float3(0.0f, weight.y, 0.0f);
 							sc.data0 = bssrdf->radius.y;
+							sc.data1 = 0.0f;
 							sd->flag |= bssrdf_setup(&sc, sc.type);
 							sd->closure[sd->num_closure++] = sc;
 						}
@@ -281,6 +283,7 @@ static void flatten_surface_closure_tree(ShaderData *sd, int path_flag,
 						if(fabsf(weight.z) > 0.0f) {
 							sc.weight = make_float3(0.0f, 0.0f, weight.z);
 							sc.data0 = bssrdf->radius.z;
+							sc.data1 = 0.0f;
 							sd->flag |= bssrdf_setup(&sc, sc.type);
 							sd->closure[sd->num_closure++] = sc;
 						}

intern/cycles/kernel/svm/svm_closure.h
@@ -27,8 +27,11 @@ ccl_device void svm_node_glass_setup(ShaderData *sd, ShaderClosure *sc, int type
 			sc->data2 = 0.0f;
 			sd->flag |= bsdf_refraction_setup(sc);
 		}
-		else
+		else {
+			sc->data0 = 0.0f;
+			sc->data1 = 0.0f;
 			sd->flag |= bsdf_reflection_setup(sc);
+		}
 	}
 	else if(type == CLOSURE_BSDF_MICROFACET_BECKMANN_GLASS_ID) {
 		sc->data0 = roughness;
@@ -384,6 +387,8 @@ ccl_device void svm_node_closure_bsdf(KernelGlobals *kg, ShaderData *sd, float *
 					 * spawned by transmission from the front */
 					sc->weight = make_float3(1.0f, 1.0f, 1.0f);
 					sc->N = N;
+					sc->data0 = 0.0f;
+					sc->data1 = 0.0f;
 					sd->flag |= bsdf_transparent_setup(sc);
 				}
 			}
@@ -529,6 +534,7 @@ ccl_device void svm_node_closure_volume(KernelGlobals *kg, ShaderData *sd, float
 
 			if(sc) {
 				sc->data0 = param2; /* g */
+				sc->data1 = 0.0f;
 				sd->flag |= volume_henyey_greenstein_setup(sc);
 			}
 			break;