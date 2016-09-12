Pagecran/blender

commit e672e7a070e698c819a9520f7db4f3d57508c5c2

Small tweak in event debug printing:
- missing print for case event gets handled AND passed on (selecting)

source/blender/windowmanager/intern/wm_event_system.c
@@ -1765,6 +1765,10 @@ static int wm_handlers_do_intern(bContext *C, wmEvent *event, ListBase *handlers
 								break;
 							}
 							else {
+								if (action & WM_HANDLER_HANDLED)
+									if (G.debug & (G_DEBUG_EVENTS | G_DEBUG_HANDLERS))
+										printf("%s:       handled - and pass on! '%s'\n", __func__, kmi->idname);
+								
 #ifndef NDEBUG
 								if (do_debug_handler) {
 									printf("%s:       un-handled '%s'...", __func__, kmi->idname);

commit 0cc43e2ce2f522bbd2725b32d4f96315da1ca879

Fix error in --debug-handlers printing
Was missing else block (and also missing newline character). Caused by e672e7a070e698.

source/blender/windowmanager/intern/wm_event_system.c
@@ -2032,11 +2032,13 @@ static int wm_handlers_do_intern(bContext *C, wmEvent *event, ListBase *handlers
 								break;
 							}
 							else {
-								if (action & WM_HANDLER_HANDLED)
+								if (action & WM_HANDLER_HANDLED) {
 									if (G.debug & (G_DEBUG_EVENTS | G_DEBUG_HANDLERS))
 										printf("%s:       handled - and pass on! '%s'\n", __func__, kmi->idname);
-								
-									PRINT("%s:       un-handled '%s'...", __func__, kmi->idname);
+								}
+								else {
+									PRINT("%s:       un-handled '%s'\n", __func__, kmi->idname);
+								}
 							}
 						}
 					}