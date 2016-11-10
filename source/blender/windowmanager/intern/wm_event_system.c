Pagecran/blender

commit 4811b2d3565cf72a08d8bc55d1717e71d18f7df1

Proper fix for crash when joining areas that doesn't break manipulators
Turns out CTX_wm_region returns mostly NULL in wm_manipulatormaps_handled_modal_update. Now propertly unsetting area/region data of handlers when deleting area/region.

source/blender/windowmanager/intern/wm_event_system.c
@@ -2717,6 +2717,33 @@ wmEventHandler *WM_event_add_modal_handler(bContext *C, wmOperator *op)
 	return handler;
 }
 
+/**
+ * Modal handlers store a pointer to an area which might be freed while the handler runs.
+ * Use this function to NULL all handler pointers to \a old_area.
+ */
+void WM_event_modal_handler_area_replace(wmWindow *win, const ScrArea *old_area, ScrArea *new_area)
+{
+	for (wmEventHandler *handler = win->modalhandlers.first; handler; handler = handler->next) {
+		if (handler->op_area == old_area) {
+			handler->op_area = new_area;
+		}
+	}
+}
+
+/**
+ * Modal handlers store a pointer to a region which might be freed while the handler runs.
+ * Use this function to NULL all handler pointers to \a old_region.
+ */
+void WM_event_modal_handler_region_replace(wmWindow *win, const ARegion *old_region, ARegion *new_region)
+{
+	for (wmEventHandler *handler = win->modalhandlers.first; handler; handler = handler->next) {
+		if (handler->op_region == old_region) {
+			handler->op_region = new_region;
+			handler->op_region_type = new_region ? new_region->regiontype : RGN_TYPE_WINDOW;
+		}
+	}
+}
+
 wmEventHandler *WM_event_add_keymap_handler(ListBase *handlers, wmKeyMap *keymap)
 {
 	wmEventHandler *handler;

source/blender/editors/screen/screen_edit.c
@@ -1277,25 +1277,28 @@ void ED_screens_initialize(wmWindowManager *wm)
 void ED_region_exit(bContext *C, ARegion *ar)
 {
 	wmWindowManager *wm = CTX_wm_manager(C);
+	wmWindow *win = CTX_wm_window(C);
 	ARegion *prevar = CTX_wm_region(C);
 
 	if (ar->type && ar->type->exit)
 		ar->type->exit(wm, ar);
 
 	CTX_wm_region_set(C, ar);
+
 	WM_event_remove_handlers(C, &ar->handlers);
+	WM_event_modal_handler_region_replace(win, ar, NULL);
 	if (ar->swinid) {
-		wm_subwindow_close(CTX_wm_window(C), ar->swinid);
+		wm_subwindow_close(win, ar->swinid);
 		ar->swinid = 0;
 	}
-	
+
 	if (ar->headerstr) {
 		MEM_freeN(ar->headerstr);
 		ar->headerstr = NULL;
 	}
 	
 	if (ar->regiontimer) {
-		WM_event_remove_timer(CTX_wm_manager(C), CTX_wm_window(C), ar->regiontimer);
+		WM_event_remove_timer(wm, win, ar->regiontimer);
 		ar->regiontimer = NULL;
 	}
 
@@ -1305,6 +1308,7 @@ void ED_region_exit(bContext *C, ARegion *ar)
 void ED_area_exit(bContext *C, ScrArea *sa)
 {
 	wmWindowManager *wm = CTX_wm_manager(C);
+	wmWindow *win = CTX_wm_window(C);
 	ScrArea *prevsa = CTX_wm_area(C);
 	ARegion *ar;
 
@@ -1312,10 +1316,13 @@ void ED_area_exit(bContext *C, ScrArea *sa)
 		sa->type->exit(wm, sa);
 
 	CTX_wm_area_set(C, sa);
+
 	for (ar = sa->regionbase.first; ar; ar = ar->next)
 		ED_region_exit(C, ar);
 
 	WM_event_remove_handlers(C, &sa->handlers);
+	WM_event_modal_handler_area_replace(win, sa, NULL);
+
 	CTX_wm_area_set(C, prevsa);
 }
 

source/blender/windowmanager/WM_api.h
@@ -67,6 +67,7 @@ struct wmDrag;
 struct ImBuf;
 struct ImageFormatData;
 struct ARegion;
+struct ScrArea;
 
 #ifdef WITH_INPUT_NDOF
 struct wmNDOFMotionData;
@@ -177,6 +178,9 @@ void WM_event_free_ui_handler_all(
         wmUIHandlerFunc ui_handle, wmUIHandlerRemoveFunc ui_remove);
 
 struct wmEventHandler *WM_event_add_modal_handler(struct bContext *C, struct wmOperator *op);
+void WM_event_modal_handler_area_replace(wmWindow *win, const struct ScrArea *old_area, struct ScrArea *new_area);
+void WM_event_modal_handler_region_replace(wmWindow *win, const struct ARegion *old_region, struct ARegion *new_region);
+
 void		WM_event_remove_handlers(struct bContext *C, ListBase *handlers);
 
 /* handler flag */

source/blender/windowmanager/manipulators/intern/wm_manipulatormap.c
@@ -394,12 +394,10 @@ void wm_manipulatormaps_handled_modal_update(
         bContext *C, wmEvent *event, wmEventHandler *handler,
         const wmOperatorType *ot)
 {
-	ScrArea *area = CTX_wm_area(C);
-	ARegion *region = CTX_wm_region(C);
 	const bool modal_running = (handler->op != NULL);
 
 	/* happens on render or when joining areas */
-	if (!region || !region->manipulator_map)
+	if (!handler->op_region || !handler->op_region->manipulator_map)
 		return;
 
 	/* hide operator manipulators */
@@ -407,8 +405,10 @@ void wm_manipulatormaps_handled_modal_update(
 		ot->mgrouptype->op = NULL;
 	}
 
-	wmManipulatorMap *mmap = region->manipulator_map;
+	wmManipulatorMap *mmap = handler->op_region->manipulator_map;
 	wmManipulator *manipulator = wm_manipulatormap_get_active_manipulator(mmap);
+	ScrArea *area = CTX_wm_area(C);
+	ARegion *region = CTX_wm_region(C);
 
 	wm_manipulatormap_handler_context(C, handler);
 

commit 26a8eecc989132f8bf2b60fb4551d7fdf9b6f192

Fix T49961: Blender 2.8 Crashes on saving an image
Caused by 4811b2d3565cf which caused the event handler hack that is used to fire up the file browser from other operators to fail. Basically the context from before the file browser is opened gets stored and used later for executing the actual file read/write operation (in this case, saving image). This context storage is cleared when exiting an editor since 4811b2d3565cf, which is technically correct, but causes usage of NULLed context data in this case, because the file browser is exited before the file read/write operation is executed.

For now I solved this by moving the fileselect handler to list of normal handlers, instead of modal ones. 4811b2d3565cf only touches list of modal handlers so we avoid the crash. Ideally we'd completely refactor how the file browser opening works to get rid of these event handler hacks.

Note that I wouldn't be suprised if this causes other regressions, but I couldn't find one so worth a try.

source/blender/windowmanager/intern/wm_event_system.c
@@ -2641,7 +2641,7 @@ void WM_event_add_fileselect(bContext *C, wmOperator *op)
 	wmWindow *win = CTX_wm_window(C);
 
 	/* only allow 1 file selector open per window */
-	for (handler = win->modalhandlers.first; handler; handler = handlernext) {
+	for (handler = win->handlers.first; handler; handler = handlernext) {
 		handlernext = handler->next;
 		
 		if (handler->type == WM_HANDLER_FILESELECT) {
@@ -2655,7 +2655,7 @@ void WM_event_add_fileselect(bContext *C, wmOperator *op)
 
 					if (sfile->op == handler->op) {
 						CTX_wm_area_set(C, sa);
-						wm_handler_fileselect_do(C, &win->modalhandlers, handler, EVT_FILESELECT_CANCEL);
+						wm_handler_fileselect_do(C, &win->handlers, handler, EVT_FILESELECT_CANCEL);
 						break;
 					}
 				}
@@ -2663,7 +2663,7 @@ void WM_event_add_fileselect(bContext *C, wmOperator *op)
 
 			/* if not found we stop the handler without changing the screen */
 			if (!sa)
-				wm_handler_fileselect_do(C, &win->modalhandlers, handler, EVT_FILESELECT_EXTERNAL_CANCEL);
+				wm_handler_fileselect_do(C, &win->handlers, handler, EVT_FILESELECT_EXTERNAL_CANCEL);
 		}
 	}
 	
@@ -2674,7 +2674,7 @@ void WM_event_add_fileselect(bContext *C, wmOperator *op)
 	handler->op_area = CTX_wm_area(C);
 	handler->op_region = CTX_wm_region(C);
 	
-	BLI_addhead(&win->modalhandlers, handler);
+	BLI_addhead(&win->handlers, handler);
 	
 	/* check props once before invoking if check is available
 	 * ensures initial properties are valid */

source/blender/windowmanager/wm_event_system.h
@@ -52,7 +52,7 @@ typedef struct wmEventHandler {
 	wmKeyMap *keymap;                   /* pointer to builtin/custom keymaps */
 	const rcti *bblocal, *bbwin;              /* optional local and windowspace bb */
 
-	/* modal operator handler */
+	/* modal operator handler and WM_HANDLER_FILESELECT */
 	wmOperator *op;                     /* for derived/modal handlers */
 	struct ScrArea *op_area;            /* for derived/modal handlers */
 	struct ARegion *op_region;          /* for derived/modal handlers */