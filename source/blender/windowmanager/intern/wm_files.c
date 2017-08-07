Pagecran/blender

commit 7398b3b71b48ca80867cb899494d28790821846e

WM: don't search for app-template when its empty
Changing from a loaded app-template back to Default (none set)
would scan paths for a file with no name. Harmless but silly.

source/blender/windowmanager/intern/wm_files.c
@@ -724,7 +724,7 @@ int wm_homefile_read(
 	if (filepath_startup_override != NULL) {
 		/* pass */
 	}
-	else if (app_template_override) {
+	else if (app_template_override && app_template_override[0]) {
 		app_template = app_template_override;
 	}
 	else if (!use_factory_settings && U.app_template[0]) {

commit 1b0432aea9a8a98be506688e877bba31f592fb38

Fix T52278: 'Default' application template fails
Own error in 7398b3b7

source/blender/windowmanager/intern/wm_files.c
@@ -721,20 +721,20 @@ int wm_homefile_read(
 	if (filepath_startup_override != NULL) {
 		/* pass */
 	}
-	else if (app_template_override && app_template_override[0]) {
+	else if (app_template_override) {
+		/* This may be clearing the current template by setting to an empty string. */
 		app_template = app_template_override;
 	}
 	else if (!use_factory_settings && U.app_template[0]) {
 		app_template = U.app_template;
 	}
 
-	if (app_template != NULL) {
+	if ((app_template != NULL) && (app_template[0] != '\0')) {
 		BKE_appdir_app_template_id_search(app_template, app_template_system, sizeof(app_template_system));
 		BLI_path_join(app_template_config, sizeof(app_template_config), cfgdir, app_template, NULL);
-	}
 
-	/* insert template name into startup file */
-	if (app_template != NULL) {
+		/* Insert template name into startup file. */
+
 		/* note that the path is being set even when 'use_factory_settings == true'
 		 * this is done so we can load a templates factory-settings */
 		if (!use_factory_settings) {