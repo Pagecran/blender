Pagecran/blender

commit 345c6298e995ea618c34282ba6d7ab5af032f191

Object Mode: move to workspace struct
- Read-only access can often use EvaluationContext.object_mode
- Write access to go to WorkSpace.object_mode.
- Some TODO's remain (marked as "TODO/OBMODE")
- Add-ons will need updating
  (context.active_object.mode -> context.workspace.object_mode)
- There will be small/medium issues that still need resolving
  this does work on a basic level though.

See D3037

source/blender/makesrna/intern/rna_layer.c
@@ -64,6 +64,7 @@ const EnumPropertyItem rna_enum_collection_type_items[] = {
 
 #include "DNA_group_types.h"
 #include "DNA_object_types.h"
+#include "DNA_workspace_types.h"
 
 #include "RNA_access.h"
 
@@ -72,6 +73,7 @@ const EnumPropertyItem rna_enum_collection_type_items[] = {
 #include "BKE_node.h"
 #include "BKE_scene.h"
 #include "BKE_mesh.h"
+#include "BKE_workspace.h"
 
 #include "DEG_depsgraph_build.h"
 #include "DEG_depsgraph_query.h"
@@ -833,6 +835,23 @@ static void rna_LayerObjects_active_object_set(PointerRNA *ptr, PointerRNA value
 		view_layer->basact = BKE_view_layer_base_find(view_layer, (Object *)value.data);
 	else
 		view_layer->basact = NULL;
+}
+
+static void rna_LayerObjects_active_object_update(struct bContext *C, PointerRNA *ptr)
+{
+	wmWindow *win = CTX_wm_window(C);
+	Scene *scene = WM_window_get_active_scene(win);
+
+	if (scene != ptr->id.data) {
+		return;
+	}
+
+	WorkSpace *workspace = WM_window_get_active_workspace(win);
+	ViewLayer *view_layer = BKE_workspace_view_layer_get(workspace, scene);
+	if (scene->obedit) {
+		ED_object_editmode_exit(C, EM_FREEDATA);
+	}
+	ED_object_base_activate(C, view_layer->basact);
 }
 
 static IDProperty *rna_ViewLayer_idprops(PointerRNA *ptr, bool create)
@@ -2129,11 +2148,11 @@ static void rna_def_layer_objects(BlenderRNA *brna, PropertyRNA *cprop)
 	prop = RNA_def_property(srna, "active", PROP_POINTER, PROP_NONE);
 	RNA_def_property_struct_type(prop, "Object");
 	RNA_def_property_pointer_funcs(prop, "rna_LayerObjects_active_object_get", "rna_LayerObjects_active_object_set", NULL, NULL);
-	RNA_def_property_flag(prop, PROP_EDITABLE | PROP_NEVER_UNLINK);
+	RNA_def_property_flag(prop, PROP_EDITABLE | PROP_NEVER_UNLINK | PROP_CONTEXT_UPDATE);
 	RNA_def_property_ui_text(prop, "Active Object", "Active object for this layer");
 	/* Could call: ED_object_base_activate(C, rl->basact);
 	 * but would be a bad level call and it seems the notifier is enough */
-	RNA_def_property_update(prop, NC_SCENE | ND_OB_ACTIVE, NULL);
+	RNA_def_property_update(prop, NC_SCENE | ND_OB_ACTIVE, "rna_LayerObjects_active_object_update");
 
 	prop = RNA_def_property(srna, "selected", PROP_COLLECTION, PROP_NONE);
 	RNA_def_property_collection_sdna(prop, NULL, "object_bases", NULL);

commit 1374cedb31f84a50b486b30c8b1a85975f6cc524

Fix T54032: Adding torus test fails
rna_LayerObjects_active_object_update used wrong viewlayer.

Regression in 345c6298e99

source/blender/makesrna/intern/rna_layer.c
@@ -846,8 +846,7 @@ static void rna_LayerObjects_active_object_update(struct bContext *C, PointerRNA
 		return;
 	}
 
-	WorkSpace *workspace = WM_window_get_active_workspace(win);
-	ViewLayer *view_layer = BKE_workspace_view_layer_get(workspace, scene);
+	ViewLayer *view_layer = (ViewLayer *)ptr->data;
 	if (scene->obedit) {
 		ED_object_editmode_exit(C, EM_FREEDATA);
 	}