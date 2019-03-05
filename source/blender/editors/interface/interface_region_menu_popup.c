Pagecran/blender

commit 4dd575e5ab55e153f8b745360bed6a9e065ae899

UI: enum property menus now expand down, consistent with other menus.
Differential Revision: https://developer.blender.org/D4359

source/blender/editors/interface/interface_region_menu_popup.c
@@ -193,19 +193,12 @@ static uiBlock *ui_block_func_POPUP(bContext *C, uiPopupBlockHandle *handle, voi
 			minwidth = UI_MENU_WIDTH_MIN;
 		}
 
-		/* settings (typically rna-enum-popups) show above the button,
-		 * menu's like file-menu, show below */
 		if (pup->block->direction != 0) {
 			/* allow overriding the direction from menu_func */
 			direction = pup->block->direction;
 		}
-		else if ((pup->but->type == UI_BTYPE_PULLDOWN) ||
-		         (UI_but_menutype_get(pup->but) != NULL))
-		{
-			direction = UI_DIR_DOWN;
-		}
 		else {
-			direction = UI_DIR_UP;
+			direction = UI_DIR_DOWN;
 		}
 	}
 	else {
@@ -228,6 +221,13 @@ static uiBlock *ui_block_func_POPUP(bContext *C, uiPopupBlockHandle *handle, voi
 	UI_block_layout_resolve(block, &width, &height);
 
 	UI_block_flag_enable(block, UI_BLOCK_MOVEMOUSE_QUIT);
+
+	/* Flip layout because rna enum list ordered in reverse. */
+	if ((pup->but && pup->but->type != UI_BTYPE_PULLDOWN) &&
+	    (UI_but_menutype_get(pup->but) == NULL))
+	{
+		UI_block_order_flip(block);
+	}
 
 	if (pup->popup) {
 		uiBut *bt;

commit 1617465919d2c4c81fe09cdffa6659ae0bc02f89

Fix upside down tool menus
Regression from 4dd575e5ab55e153f8

source/blender/editors/interface/interface_region_menu_popup.c
@@ -224,7 +224,8 @@ static uiBlock *ui_block_func_POPUP(bContext *C, uiPopupBlockHandle *handle, voi
 
 	/* Flip layout because rna enum list ordered in reverse. */
 	if ((pup->but && pup->but->type != UI_BTYPE_PULLDOWN) &&
-	    (UI_but_menutype_get(pup->but) == NULL))
+	    (UI_but_menutype_get(pup->but) == NULL) &&
+	    (UI_but_is_tool(pup->but) == false))
 	{
 		UI_block_order_flip(block);
 	}