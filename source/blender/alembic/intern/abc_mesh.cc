Pagecran/blender

commit 4337bc2e6303dbd5f295878f3e7490995a62713a

Fix T65901: Alembic crash on out-of-bounds UV indices
An Alembic file saved by 3DS Max caused Blender to crash when importing.
Either the UV indices in the file are out of bounds or they are written
in a way we don't expect. In either case, this now no longer causes Blender
to crash.

source/blender/alembic/intern/abc_mesh.cc
@@ -830,6 +830,8 @@ static void read_mpolys(CDStreamConfig &config, const AbcMeshData &mesh_data)
   const Int32ArraySamplePtr &face_indices = mesh_data.face_indices;
   const Int32ArraySamplePtr &face_counts = mesh_data.face_counts;
   const V2fArraySamplePtr &uvs = mesh_data.uvs;
+  const size_t uvs_size = uvs->size();
+
   const UInt32ArraySamplePtr &uvs_indices = mesh_data.uvs_indices;
   const N3fArraySamplePtr &normals = mesh_data.face_normals;
 
@@ -861,6 +863,12 @@ static void read_mpolys(CDStreamConfig &config, const AbcMeshData &mesh_data)
         MLoopUV &loopuv = mloopuvs[rev_loop_index];
 
         uv_index = (*uvs_indices)[loop_index];
+
+        /* Some Alembic files are broken (or at least export UVs in a way we don't expect). */
+        if (uv_index >= uvs_size) {
+          continue;
+        }
+
         loopuv.uv[0] = (*uvs)[uv_index][0];
         loopuv.uv[1] = (*uvs)[uv_index][1];
       }

commit 4f6e5846730273de2f8fc145592c510dacf8e007

Fix: Alembic import segfault when importing mesh with null UVs
This fixes an issue introduced in 4337bc2e6303dbd5f295878f3e7490995a62713a.

source/blender/alembic/intern/abc_mesh.cc
@@ -830,7 +830,7 @@ static void read_mpolys(CDStreamConfig &config, const AbcMeshData &mesh_data)
   const Int32ArraySamplePtr &face_indices = mesh_data.face_indices;
   const Int32ArraySamplePtr &face_counts = mesh_data.face_counts;
   const V2fArraySamplePtr &uvs = mesh_data.uvs;
-  const size_t uvs_size = uvs->size();
+  const size_t uvs_size = uvs == nullptr ? 0 : uvs->size();
 
   const UInt32ArraySamplePtr &uvs_indices = mesh_data.uvs_indices;
   const N3fArraySamplePtr &normals = mesh_data.face_normals;