Pagecran/blender

commit aa919f3e82020a4d9e953ec826a390bf2837cadc

UI: Info Editor Visual Changes
Changes to Info Editor making it easier to read. Only visual changes, no functional changes.

Differential Revision: https://developer.blender.org/D6491

Reviewed by Julian Eisel

source/blender/editors/space_console/console_draw.c
@@ -42,22 +42,33 @@
 
 #include "../space_info/textview.h"
 
-static void console_line_color(unsigned char fg[3], int type)
+static int console_line_data(struct TextViewContext *tvc,
+                                      unsigned char fg[4],
+                                      unsigned char UNUSED(bg[4]),
+                                      int *UNUSED(icon),
+                                      unsigned char UNUSED(icon_fg[4]),
+                                      unsigned char UNUSED(icon_bg[4]))
 {
-  switch (type) {
+  ConsoleLine *cl_iter = (ConsoleLine *)tvc->iter;
+  int fg_id = TH_TEXT;
+
+  switch (cl_iter->type) {
     case CONSOLE_LINE_OUTPUT:
-      UI_GetThemeColor3ubv(TH_CONSOLE_OUTPUT, fg);
+      fg_id = TH_CONSOLE_OUTPUT;
       break;
     case CONSOLE_LINE_INPUT:
-      UI_GetThemeColor3ubv(TH_CONSOLE_INPUT, fg);
+      fg_id = TH_CONSOLE_INPUT;
       break;
     case CONSOLE_LINE_INFO:
-      UI_GetThemeColor3ubv(TH_CONSOLE_INFO, fg);
+      fg_id = TH_CONSOLE_INFO;
       break;
     case CONSOLE_LINE_ERROR:
-      UI_GetThemeColor3ubv(TH_CONSOLE_ERROR, fg);
+      fg_id = TH_CONSOLE_ERROR;
       break;
   }
+
+  UI_GetThemeColor4ubv(fg_id, fg);
+  return TVC_LINE_FG;
 }
 
 void console_scrollback_prompt_begin(struct SpaceConsole *sc, ConsoleLine *cl_dummy)
@@ -137,47 +148,36 @@ static void console_cursor_wrap_offset(
   return;
 }
 
-static int console_textview_line_color(struct TextViewContext *tvc,
-                                       unsigned char fg[3],
-                                       unsigned char UNUSED(bg[3]))
+static void console_textview_draw_cursor(struct TextViewContext *tvc)
 {
-  ConsoleLine *cl_iter = (ConsoleLine *)tvc->iter;
+  const SpaceConsole *sc = (SpaceConsole *)tvc->arg1;
+  const ConsoleLine *cl = (ConsoleLine *)sc->history.last;
+  int offl = 0, offc = 0;
+  int xy[2] = {tvc->draw_rect.xmin, tvc->draw_rect.ymin};
+  int pen[2];
+  GPUVertFormat *format = immVertexFormat();
+  uint pos = GPU_vertformat_attr_add(format, "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
+  xy[1] += tvc->lheight * 0.35f;
 
-  /* annoying hack, to draw the prompt */
-  if (tvc->iter_index == 0) {
-    const SpaceConsole *sc = (SpaceConsole *)tvc->arg1;
-    const ConsoleLine *cl = (ConsoleLine *)sc->history.last;
-    int offl = 0, offc = 0;
-    int xy[2] = {tvc->draw_rect.xmin, tvc->draw_rect.ymin};
-    int pen[2];
-    GPUVertFormat *format = immVertexFormat();
-    uint pos = GPU_vertformat_attr_add(format, "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
-    xy[1] += tvc->lheight / 6;
+  console_cursor_wrap_offset(sc->prompt, tvc->columns, &offl, &offc, NULL);
+  console_cursor_wrap_offset(cl->line, tvc->columns, &offl, &offc, cl->line + cl->cursor);
+  pen[0] = tvc->cwidth * (offc + tvc->margin_left_chars);
+  pen[1] = -2 - tvc->lheight * offl;
 
-    console_cursor_wrap_offset(sc->prompt, tvc->columns, &offl, &offc, NULL);
-    console_cursor_wrap_offset(cl->line, tvc->columns, &offl, &offc, cl->line + cl->cursor);
-    pen[0] = tvc->cwidth * offc;
-    pen[1] = -2 - tvc->lheight * offl;
+  console_cursor_wrap_offset(cl->line + cl->cursor, tvc->columns, &offl, &offc, NULL);
+  pen[1] += tvc->lheight * offl;
 
-    console_cursor_wrap_offset(cl->line + cl->cursor, tvc->columns, &offl, &offc, NULL);
-    pen[1] += tvc->lheight * offl;
+  /* cursor */
+  immBindBuiltinProgram(GPU_SHADER_2D_UNIFORM_COLOR);
+  immUniformThemeColor(TH_CONSOLE_CURSOR);
 
-    /* cursor */
-    immBindBuiltinProgram(GPU_SHADER_2D_UNIFORM_COLOR);
-    immUniformThemeColor(TH_CONSOLE_CURSOR);
+  immRectf(pos,
+           (xy[0] + pen[0]) - U.pixelsize,
+           (xy[1] + pen[1]),
+           (xy[0] + pen[0]) + U.pixelsize,
+           (xy[1] + pen[1] + tvc->lheight));
 
-    immRectf(pos,
-             (xy[0] + pen[0]) - U.pixelsize,
-             (xy[1] + pen[1]),
-             (xy[0] + pen[0]) + U.pixelsize,
-             (xy[1] + pen[1] + tvc->lheight));
-
-    immUnbindProgram();
-  }
-
-  console_line_color(fg, cl_iter->type);
-
-  return TVC_LINE_FG;
+  immUnbindProgram();
 }
 
 static void console_textview_const_colors(TextViewContext *UNUSED(tvc), unsigned char bg_sel[4])
@@ -187,11 +187,9 @@ static void console_textview_const_colors(TextViewContext *UNUSED(tvc), unsigned
 
 static void console_textview_draw_rect_calc(const ARegion *ar, rcti *draw_rect)
 {
-  const int margin = 4 * UI_DPI_FAC;
-  draw_rect->xmin = margin;
-  draw_rect->xmax = ar->winx - (margin + V2D_SCROLL_WIDTH);
-  draw_rect->ymin = margin;
-  /* No margin at the top (allow text to scroll off the window). */
+  draw_rect->xmin = 0;
+  draw_rect->xmax = ar->winx;
+  draw_rect->ymin = 0;
   draw_rect->ymax = ar->winy;
 }
 
@@ -214,7 +212,8 @@ static int console_textview_main__internal(struct SpaceConsole *sc,
 
   tvc.step = console_textview_step;
   tvc.line_get = console_textview_line_get;
-  tvc.line_color = console_textview_line_color;
+  tvc.line_data = console_line_data;
+  tvc.draw_cursor = console_textview_draw_cursor;
   tvc.const_colors = console_textview_const_colors;
 
   tvc.arg1 = sc;
@@ -223,7 +222,9 @@ static int console_textview_main__internal(struct SpaceConsole *sc,
   /* view */
   tvc.sel_start = sc->sel_start;
   tvc.sel_end = sc->sel_end;
-  tvc.lheight = sc->lheight * UI_DPI_FAC;
+  tvc.lheight = sc->lheight * 1.2f * UI_DPI_FAC;
+  tvc.margin_left_chars = 1;
+  tvc.margin_right_chars = 2;
   tvc.scroll_ymin = v2d->cur.ymin;
   tvc.scroll_ymax = v2d->cur.ymax;
 

source/blender/editors/space_info/info_draw.c
@@ -42,48 +42,78 @@
 #include "textview.h"
 #include "GPU_framebuffer.h"
 
-/* complicates things a bit, so leaving in old simple code */
-#define USE_INFO_NEWLINE
-
-static void info_report_color(unsigned char *fg,
-                              unsigned char *bg,
-                              Report *report,
-                              const short do_tint)
+static int report_line_data(struct TextViewContext *tvc,
+                                     unsigned char fg[4],
+                                     unsigned char bg[4],
+                                     int *icon,
+                                     unsigned char icon_fg[4],
+                                     unsigned char icon_bg[4])
 {
-  int bg_id = TH_BACK, fg_id = TH_TEXT;
-  int shade = do_tint ? 0 : -6;
+  Report *report = (Report *)tvc->iter;
 
-  if (report->flag & SELECT) {
-    bg_id = TH_INFO_SELECTED;
-    fg_id = TH_INFO_SELECTED_TEXT;
-  }
-  else if (report->type & RPT_ERROR_ALL) {
-    bg_id = TH_INFO_ERROR;
-    fg_id = TH_INFO_ERROR_TEXT;
+  /* Same text color no matter what type of report. */
+  UI_GetThemeColor4ubv((report->flag & SELECT) ? TH_INFO_SELECTED_TEXT : TH_TEXT, fg);
+
+  /* Zebra striping for background. */
+  int bg_id = (report->flag & SELECT) ? TH_INFO_SELECTED : TH_BACK;
+  int shade = tvc->iter_tmp % 2 ? 4 : -4;
+  UI_GetThemeColorShade4ubv(bg_id, shade, bg);
+
+  /* Icon color and backgound depend of report type. */
+
+  int icon_fg_id;
+  int icon_bg_id;
+
+  if (report->type & RPT_ERROR_ALL) {
+    icon_fg_id = TH_INFO_ERROR_TEXT;
+    icon_bg_id = TH_INFO_ERROR;
+    *icon = ICON_CANCEL;
   }
   else if (report->type & RPT_WARNING_ALL) {
-    bg_id = TH_INFO_WARNING;
-    fg_id = TH_INFO_WARNING_TEXT;
+    icon_fg_id = TH_INFO_WARNING_TEXT;
+    icon_bg_id = TH_INFO_WARNING;
+    *icon = ICON_ERROR;
   }
   else if (report->type & RPT_INFO_ALL) {
-    bg_id = TH_INFO_INFO;
-    fg_id = TH_INFO_INFO_TEXT;
+    icon_fg_id = TH_INFO_INFO_TEXT;
+    icon_bg_id = TH_INFO_INFO;
+    *icon = ICON_INFO;
   }
   else if (report->type & RPT_DEBUG_ALL) {
-    bg_id = TH_INFO_DEBUG;
-    fg_id = TH_INFO_DEBUG_TEXT;
+    icon_fg_id = TH_INFO_DEBUG_TEXT;
+    icon_bg_id = TH_INFO_DEBUG;
+    *icon = ICON_SYSTEM;
+  }
+  else if (report->type & RPT_PROPERTY) {
+    icon_fg_id = TH_INFO_PROPERTY_TEXT;
+    icon_bg_id = TH_INFO_PROPERTY;
+    *icon = ICON_OPTIONS;
+  }
+  else if (report->type & RPT_OPERATOR) {
+    icon_fg_id = TH_INFO_OPERATOR_TEXT;
+    icon_bg_id = TH_INFO_OPERATOR;
+    *icon = ICON_CHECKMARK;
   }
   else {
-    bg_id = TH_BACK;
-    fg_id = TH_TEXT;
+    *icon = ICON_NONE;
   }
 
-  UI_GetThemeColorShade3ubv(bg_id, shade, bg);
-  UI_GetThemeColor3ubv(fg_id, fg);
+  if (report->flag & SELECT) {
+    icon_fg_id = TH_INFO_SELECTED;
+    icon_bg_id = TH_INFO_SELECTED_TEXT;
+  }
+
+  if (*icon != ICON_NONE) {
+    UI_GetThemeColor4ubv(icon_fg_id, icon_fg);
+    UI_GetThemeColor4ubv(icon_bg_id, icon_bg);
+    return TVC_LINE_FG | TVC_LINE_BG | TVC_LINE_ICON | TVC_LINE_ICON_FG | TVC_LINE_ICON_BG;
+  }
+  else {
+    return TVC_LINE_FG | TVC_LINE_BG;
+  }
 }
 
 /* reports! */
-#ifdef USE_INFO_NEWLINE
 static void report_textview_init__internal(TextViewContext *tvc)
 {
   Report *report = (Report *)tvc->iter;
@@ -108,14 +138,11 @@ static int report_textview_skip__internal(TextViewContext *tvc)
   return (tvc->iter != NULL);
 }
 
-#endif  // USE_INFO_NEWLINE
-
 static int report_textview_begin(TextViewContext *tvc)
 {
-  // SpaceConsole *sc = (SpaceConsole *)tvc->arg1;
   ReportList *reports = (ReportList *)tvc->arg2;
 
-  tvc->lheight = 14 * UI_DPI_FAC;  // sc->lheight;
+  tvc->lheight = 14 * UI_DPI_FAC;
   tvc->sel_start = 0;
   tvc->sel_end = 0;
 
@@ -125,7 +152,6 @@ static int report_textview_begin(TextViewContext *tvc)
   UI_ThemeClearColor(TH_BACK);
   GPU_clear(GPU_COLOR_BIT);
 
-#ifdef USE_INFO_NEWLINE
   tvc->iter_tmp = 0;
   if (tvc->iter && report_textview_skip__internal(tvc)) {
     /* init the newline iterator */
@@ -137,9 +163,6 @@ static int report_textview_begin(TextViewContext *tvc)
   else {
     return false;
   }
-#else
-  return (tvc->iter != NULL);
-#endif
 }
 
 static void report_textview_end(TextViewContext *UNUSED(tvc))
@@ -147,7 +170,6 @@ static void report_textview_end(TextViewContext *UNUSED(tvc))
   /* pass */
 }
 
-#ifdef USE_INFO_NEWLINE
 static int report_textview_step(TextViewContext *tvc)
 {
   /* simple case, but no newline support */
@@ -184,57 +206,11 @@ static int report_textview_line_get(struct TextViewContext *tvc, const char **li
   return 1;
 }
 
-static int report_textview_line_color(struct TextViewContext *tvc,
-                                      unsigned char fg[3],
-                                      unsigned char bg[3])
-{
-  Report *report = (Report *)tvc->iter;
-  info_report_color(fg, bg, report, tvc->iter_tmp % 2);
-  return TVC_LINE_FG | TVC_LINE_BG;
-}
-
-#else  // USE_INFO_NEWLINE
-
-static int report_textview_step(TextViewContext *tvc)
-{
-  SpaceInfo *sinfo = (SpaceInfo *)tvc->arg1;
-  const int report_mask = info_report_mask(sinfo);
-  do {
-    tvc->iter = (void *)((Link *)tvc->iter)->prev;
-  } while (tvc->iter && (((Report *)tvc->iter)->type & report_mask) == 0);
-
-  return (tvc->iter != NULL);
-}
-
-static int report_textview_line_get(struct TextViewContext *tvc, const char **line, int *len)
-{
-  Report *report = (Report *)tvc->iter;
-  *line = report->message;
-  *len = report->len;
-
-  return 1;
-}
-
-static int report_textview_line_color(struct TextViewContext *tvc,
-                                      unsigned char fg[3],
-                                      unsigned char bg[3])
-{
-  Report *report = (Report *)tvc->iter;
-  info_report_color(fg, bg, report, tvc->iter_tmp % 2);
-  return TVC_LINE_FG | TVC_LINE_BG;
-}
-
-#endif  // USE_INFO_NEWLINE
-
-#undef USE_INFO_NEWLINE
-
 static void info_textview_draw_rect_calc(const ARegion *ar, rcti *draw_rect)
 {
-  const int margin = 4 * UI_DPI_FAC;
-  draw_rect->xmin = margin;
-  draw_rect->xmax = ar->winx - (V2D_SCROLL_WIDTH + margin);
-  draw_rect->ymin = margin;
-  /* No margin at the top (allow text to scroll off the window). */
+  draw_rect->xmin = 0;
+  draw_rect->xmax = ar->winx;
+  draw_rect->ymin = 0;
   draw_rect->ymax = ar->winy;
 }
 
@@ -256,7 +232,7 @@ static int info_textview_main__internal(struct SpaceInfo *sinfo,
 
   tvc.step = report_textview_step;
   tvc.line_get = report_textview_line_get;
-  tvc.line_color = report_textview_line_color;
+  tvc.line_data = report_line_data;
   tvc.const_colors = NULL;
 
   tvc.arg1 = sinfo;
@@ -265,7 +241,10 @@ static int info_textview_main__internal(struct SpaceInfo *sinfo,
   /* view */
   tvc.sel_start = 0;
   tvc.sel_end = 0;
-  tvc.lheight = 14 * UI_DPI_FAC;  // sc->lheight;
+  tvc.lheight = 17 * UI_DPI_FAC;
+  tvc.row_vpadding = 0.4 * tvc.lheight;
+  tvc.margin_left_chars = 5;
+  tvc.margin_right_chars = 2;
   tvc.scroll_ymin = v2d->cur.ymin;
   tvc.scroll_ymax = v2d->cur.ymax;
 

commit 0f46aa0f232d51ecaa8fca597a366b148e511b25

Fix T73784: Python console: incorrect wrapped line cursor position
Regression in aa919f3e82020

Remove character margins, it complicated drawing & picking
to have one margin in pixels and a second margin in characters.

Replace this with an outer pixel-margin for drawing background colors.

source/blender/editors/space_console/console_draw.c
@@ -161,7 +161,7 @@ static void console_textview_draw_cursor(struct TextViewContext *tvc)
 
   console_cursor_wrap_offset(sc->prompt, tvc->columns, &offl, &offc, NULL);
   console_cursor_wrap_offset(cl->line, tvc->columns, &offl, &offc, cl->line + cl->cursor);
-  pen[0] = tvc->cwidth * (offc + tvc->margin_left_chars);
+  pen[0] = tvc->cwidth * offc;
   pen[1] = -2 - tvc->lheight * offl;
 
   console_cursor_wrap_offset(cl->line + cl->cursor, tvc->columns, &offl, &offc, NULL);
@@ -185,12 +185,21 @@ static void console_textview_const_colors(TextViewContext *UNUSED(tvc), unsigned
   UI_GetThemeColor4ubv(TH_CONSOLE_SELECT, bg_sel);
 }
 
-static void console_textview_draw_rect_calc(const ARegion *ar, rcti *draw_rect)
+static void console_textview_draw_rect_calc(const ARegion *ar,
+                                            rcti *r_draw_rect,
+                                            rcti *r_draw_rect_outer)
 {
-  draw_rect->xmin = 0;
-  draw_rect->xmax = ar->winx;
-  draw_rect->ymin = 0;
-  draw_rect->ymax = ar->winy;
+  const int margin = 4 * UI_DPI_FAC;
+  r_draw_rect->xmin = margin;
+  r_draw_rect->xmax = ar->winx - V2D_SCROLL_WIDTH;
+  r_draw_rect->ymin = margin;
+  /* No margin at the top (allow text to scroll off the window). */
+  r_draw_rect->ymax = ar->winy;
+
+  r_draw_rect_outer->xmin = 0;
+  r_draw_rect_outer->xmax = ar->winx;
+  r_draw_rect_outer->ymin = 0;
+  r_draw_rect_outer->ymax = ar->winy;
 }
 
 static int console_textview_main__internal(struct SpaceConsole *sc,
@@ -223,12 +232,10 @@ static int console_textview_main__internal(struct SpaceConsole *sc,
   tvc.sel_start = sc->sel_start;
   tvc.sel_end = sc->sel_end;
   tvc.lheight = sc->lheight * 1.2f * UI_DPI_FAC;
-  tvc.margin_left_chars = 1;
-  tvc.margin_right_chars = 2;
   tvc.scroll_ymin = v2d->cur.ymin;
   tvc.scroll_ymax = v2d->cur.ymax;
 
-  console_textview_draw_rect_calc(ar, &tvc.draw_rect);
+  console_textview_draw_rect_calc(ar, &tvc.draw_rect, &tvc.draw_rect_outer);
 
   console_scrollback_prompt_begin(sc, &cl_dummy);
   ret = textview_draw(&tvc, do_draw, mval, r_mval_pick_item, r_mval_pick_offset);
@@ -253,9 +260,6 @@ int console_char_pick(struct SpaceConsole *sc, const ARegion *ar, const int mval
 {
   int r_mval_pick_offset = 0;
   void *mval_pick_item = NULL;
-
-  rcti draw_rect;
-  console_textview_draw_rect_calc(ar, &draw_rect);
 
   console_textview_main__internal(sc, ar, false, mval, &mval_pick_item, &r_mval_pick_offset);
   return r_mval_pick_offset;

source/blender/editors/space_info/info_draw.c
@@ -206,12 +206,21 @@ static int report_textview_line_get(struct TextViewContext *tvc, const char **li
   return 1;
 }
 
-static void info_textview_draw_rect_calc(const ARegion *ar, rcti *draw_rect)
+static void info_textview_draw_rect_calc(const ARegion *ar,
+                                         rcti *r_draw_rect,
+                                         rcti *r_draw_rect_outer)
 {
-  draw_rect->xmin = 0;
-  draw_rect->xmax = ar->winx;
-  draw_rect->ymin = 0;
-  draw_rect->ymax = ar->winy;
+  const int margin = 0.45f * U.widget_unit;
+  r_draw_rect->xmin = margin + UI_UNIT_X;
+  r_draw_rect->xmax = ar->winx - V2D_SCROLL_WIDTH;
+  r_draw_rect->ymin = margin;
+  r_draw_rect->ymax = ar->winy;
+  /* No margin at the top (allow text to scroll off the window). */
+
+  r_draw_rect_outer->xmin = 0;
+  r_draw_rect_outer->xmax = ar->winx;
+  r_draw_rect_outer->ymin = 0;
+  r_draw_rect_outer->ymax = ar->winy;
 }
 
 static int info_textview_main__internal(struct SpaceInfo *sinfo,
@@ -243,12 +252,10 @@ static int info_textview_main__internal(struct SpaceInfo *sinfo,
   tvc.sel_end = 0;
   tvc.lheight = 17 * UI_DPI_FAC;
   tvc.row_vpadding = 0.4 * tvc.lheight;
-  tvc.margin_left_chars = 5;
-  tvc.margin_right_chars = 2;
   tvc.scroll_ymin = v2d->cur.ymin;
   tvc.scroll_ymax = v2d->cur.ymax;
 
-  info_textview_draw_rect_calc(ar, &tvc.draw_rect);
+  info_textview_draw_rect_calc(ar, &tvc.draw_rect, &tvc.draw_rect_outer);
 
   ret = textview_draw(&tvc, do_draw, mval, r_mval_pick_item, r_mval_pick_offset);
 

source/blender/editors/space_info/textview.c
@@ -53,12 +53,13 @@ typedef struct TextViewDrawState {
   int lheight;
   /** Text vertical offset per line. */
   int lofs;
-  int margin_left_chars;
-  int margin_right_chars;
   int row_vpadding;
   /** Number of characters that fit into the width of the console (fixed width). */
   int columns;
+  /** For drawing text. */
   const rcti *draw_rect;
+  /** For drawing backgrounds colors which may extend beyond text. */
+  const rcti *draw_rect_outer;
   int scroll_ymin, scroll_ymax;
   int *xy;   // [2]
   int *sel;  // [2]
@@ -85,9 +86,8 @@ static void console_draw_sel(const char *str,
   const int lheight = tds->lheight;
 
   if (sel[0] <= str_len_draw && sel[1] >= 0) {
-    const int sta = BLI_str_utf8_offset_to_column(str, max_ii(sel[0], 0)) + tds->margin_left_chars;
-    const int end = BLI_str_utf8_offset_to_column(str, min_ii(sel[1], str_len_draw)) +
-                    tds->margin_left_chars;
+    const int sta = BLI_str_utf8_offset_to_column(str, max_ii(sel[0], 0));
+    const int end = BLI_str_utf8_offset_to_column(str, min_ii(sel[1], str_len_draw));
 
     GPU_blend(true);
     GPU_blend_set_func_separate(
@@ -154,11 +154,7 @@ static bool console_draw_string(TextViewDrawState *tds,
   int tot_lines; /* Total number of lines for wrapping. */
   int *offsets;  /* Offsets of line beginnings for wrapping. */
 
-  str_len = console_wrap_offsets(str,
-                                 str_len,
-                                 tds->columns - (tds->margin_left_chars + tds->margin_right_chars),
-                                 &tot_lines,
-                                 &offsets);
+  str_len = console_wrap_offsets(str, str_len, tds->columns, &tot_lines, &offsets);
 
   int line_height = (tot_lines * tds->lheight) + (tds->row_vpadding * 2);
   int line_bottom = tds->xy[1];
@@ -223,15 +219,15 @@ static bool console_draw_string(TextViewDrawState *tds,
     uint pos = GPU_vertformat_attr_add(format, "pos", GPU_COMP_I32, 2, GPU_FETCH_INT_TO_FLOAT);
     immBindBuiltinProgram(GPU_SHADER_2D_UNIFORM_COLOR);
     immUniformColor4ubv(bg);
-    immRecti(pos, 0, line_bottom, tds->draw_rect->xmax, line_top);
+    immRecti(pos, tds->draw_rect_outer->xmin, line_bottom, tds->draw_rect_outer->xmax, line_top);
     immUnbindProgram();
   }
 
   if (icon_bg) {
     float col[4];
-    int bg_size = 20 * UI_DPI_FAC;
+    int bg_size = UI_DPI_ICON_SIZE * 1.2;
     float vpadding = (tds->lheight + (tds->row_vpadding * 2) - bg_size) / 2;
-    float hpadding = ((tds->margin_left_chars * tds->cwidth) - bg_size) / 2;
+    float hpadding = tds->draw_rect->xmin - (bg_size * 1.2f);
 
     rgba_uchar_to_float(col, icon_bg);
     UI_draw_roundbox_corner_set(UI_CNR_ALL);
@@ -246,7 +242,7 @@ static bool console_draw_string(TextViewDrawState *tds,
 
   if (icon) {
     int vpadding = (tds->lheight + (tds->row_vpadding * 2) - UI_DPI_ICON_SIZE) / 2;
-    int hpadding = ((tds->margin_left_chars * tds->cwidth) - UI_DPI_ICON_SIZE) / 2;
+    int hpadding = tds->draw_rect->xmin - (UI_DPI_ICON_SIZE * 1.3f);
 
     GPU_blend(true);
     UI_icon_draw_ex(hpadding,
@@ -266,10 +262,7 @@ static bool console_draw_string(TextViewDrawState *tds,
   const int final_offset = offsets[tot_lines - 1];
   len = str_len - final_offset;
   s = str + final_offset;
-  BLF_position(tds->font_id,
-               tds->xy[0] + (tds->margin_left_chars * tds->cwidth),
-               tds->lofs + line_bottom + tds->row_vpadding,
-               0);
+  BLF_position(tds->font_id, tds->xy[0], tds->lofs + line_bottom + tds->row_vpadding, 0);
   BLF_color4ubv(tds->font_id, fg);
   BLF_draw_mono(tds->font_id, s, len, tds->cwidth);
 
@@ -287,10 +280,7 @@ static bool console_draw_string(TextViewDrawState *tds,
     len = offsets[i] - offsets[i - 1];
     s = str + offsets[i - 1];
 
-    BLF_position(tds->font_id,
-                 tds->xy[0] + (tds->margin_left_chars * tds->cwidth),
-                 tds->lofs + tds->xy[1],
-                 0);
+    BLF_position(tds->font_id, tds->xy[0], tds->lofs + tds->xy[1], 0);
     BLF_draw_mono(tds->font_id, s, len, tds->cwidth);
 
     if (tds->sel[0] != tds->sel[1]) {
@@ -364,8 +354,6 @@ int textview_draw(TextViewContext *tvc,
   tds.cwidth = (int)BLF_fixed_width(font_id);
   BLI_assert(tds.cwidth > 0);
   tds.lheight = tvc->lheight;
-  tds.margin_left_chars = tvc->margin_left_chars;
-  tds.margin_right_chars = tvc->margin_right_chars;
   tds.row_vpadding = tvc->row_vpadding;
   tds.lofs = -BLF_descender(font_id);
   /* Note, scroll bar must be already subtracted. */
@@ -375,6 +363,7 @@ int textview_draw(TextViewContext *tvc,
     tds.columns = 1;
   }
   tds.draw_rect = &tvc->draw_rect;
+  tds.draw_rect_outer = &tvc->draw_rect_outer;
   tds.scroll_ymin = tvc->scroll_ymin;
   tds.scroll_ymax = tvc->scroll_ymax;
   tds.xy = xy;

source/blender/editors/space_info/textview.h
@@ -32,11 +32,11 @@ typedef struct TextViewContext {
   int columns; /* shouldnt be needed! */
 
   int row_vpadding;
-  int margin_left_chars;
-  int margin_right_chars;
 
-  /** Area to draw: (0, 0, winx, winy) with a margin applied and scroll-bar subtracted. */
+  /** Area to draw text: (0, 0, winx, winy) with a margin applied and scroll-bar subtracted. */
   rcti draw_rect;
+  /** Area to draw text background colors (extending beyond text in some cases). */
+  rcti draw_rect_outer;
 
   /** Scroll offset in pixels. */
   int scroll_ymin, scroll_ymax;