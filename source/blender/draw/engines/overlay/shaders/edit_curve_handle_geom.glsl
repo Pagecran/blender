Pagecran/blender

commit 49f59092e7c8c652df015bdfd27bae2480e51d01

Curves: Implement Handles for selected points only
When editing a complex curve is very annoying to have all handles at a time. Also, this is a requirement for the current GSoC Edit Grease Pencil using curves.
I have seen that this improvement can be used in any other area of blender, so I have decided to publish the option in the overlay panel..

Reviewed By: fclem, #user_interface, billreynish, Severin

Differential Revision: https://developer.blender.org/D7754

source/blender/draw/engines/overlay/shaders/edit_curve_handle_geom.glsl
@@ -4,10 +4,15 @@
 #define EVEN_U_BIT 1 << 4
 #define COLOR_SHIFT 5
 
+/* Keep the same value in `handle_display` in `DNA_view3d_types.h` */
+#define CURVE_HANDLE_SELECTED 0
+#define CURVE_HANDLE_ALL 1
+
 layout(lines) in;
 layout(triangle_strip, max_vertices = 10) out;
 
 uniform bool showCurveHandles;
+uniform int curveHandleDisplay;
 
 flat in int vertFlag[];
 
@@ -46,6 +51,17 @@ void main()
   }
 
   bool edge_selected = (((vertFlag[1] | vertFlag[0]) & VERT_SELECTED) != 0);
+  bool handle_selected = (showCurveHandles &&
+                          (((vertFlag[1] | vertFlag[0]) & HANDLE_SELECTED) != 0));
+
+  /* If handle type is only selected and the edge is not selected, don't show. */
+  if ((curveHandleDisplay != CURVE_HANDLE_ALL) && (!handle_selected)) {
+    /* Nurbs must show the handles always. */
+    bool is_u_segment = (((vertFlag[1] ^ vertFlag[0]) & EVEN_U_BIT) != 0);
+    if (!is_u_segment) {
+      return;
+    }
+  }
 
   vec4 inner_color;
   if (color_id == 0) {

source/blender/draw/engines/overlay/shaders/edit_curve_point_vert.glsl
@@ -2,7 +2,11 @@
 /* Keep the same value of `BEZIER_HANDLE` in `draw_cache_imp_curve.c` */
 #define BEZIER_HANDLE 1 << 3
 
+/* Keep the same value in `handle_display` in `DNA_view3d_types.h` */
+#define CURVE_HANDLE_SELECTED 0
+
 uniform bool showCurveHandles;
+uniform int curveHandleDisplay;
 
 in vec3 pos;
 in int data;
@@ -32,7 +36,12 @@ void main()
   world_clip_planes_calc_clip_distance(world_pos);
 #endif
 
-  if (!showCurveHandles && ((data & BEZIER_HANDLE) != 0)) {
+  bool show_handle = showCurveHandles;
+  if ((curveHandleDisplay == CURVE_HANDLE_SELECTED) && ((data & HANDLE_SELECTED) == 0)) {
+    show_handle = false;
+  }
+
+  if (!show_handle && ((data & BEZIER_HANDLE) != 0)) {
     /* We set the vertex at the camera origin to generate 0 fragments. */
     gl_Position = vec4(0.0, 0.0, -3e36, 0.0);
   }

source/blender/draw/intern/draw_cache_impl.h
@@ -208,6 +208,7 @@ enum {
   VFLAG_EDGE_SEAM = 1 << 4,
   VFLAG_EDGE_SHARP = 1 << 5,
   VFLAG_EDGE_FREESTYLE = 1 << 6,
+  VFLAG_HANDLE_SELECTED = 1 << 7,
   /* Beware to not go over 1 << 7 (it's a byte flag)
    * (see gpu_shader_edit_mesh_overlay_geom.glsl) */
 };

source/blender/draw/intern/draw_cache_impl_curve.c
@@ -685,14 +685,20 @@ static void curve_create_edit_curves_nor(CurveRenderData *rdata, GPUVertBuf *vbo
   BLI_assert(vbo_len_used == verts_len_capacity);
 }
 
-static char beztriple_vflag_get(
-    CurveRenderData *rdata, char flag, char col_id, int v_idx, int nu_id, bool handle_point)
+static char beztriple_vflag_get(CurveRenderData *rdata,
+                                char flag,
+                                char col_id,
+                                int v_idx,
+                                int nu_id,
+                                bool handle_point,
+                                const bool handle_selected)
 {
   char vflag = 0;
   SET_FLAG_FROM_TEST(vflag, (flag & SELECT), VFLAG_VERT_SELECTED);
   SET_FLAG_FROM_TEST(vflag, (v_idx == rdata->actvert && nu_id == rdata->actnu), VFLAG_VERT_ACTIVE);
   SET_FLAG_FROM_TEST(vflag, (nu_id == rdata->actnu), ACTIVE_NURB);
   SET_FLAG_FROM_TEST(vflag, handle_point, BEZIER_HANDLE);
+  SET_FLAG_FROM_TEST(vflag, handle_selected, VFLAG_HANDLE_SELECTED);
   /* handle color id */
   vflag |= col_id << COLOR_SHIFT;
   return vflag;
@@ -754,11 +760,13 @@ static void curve_create_edit_data_and_handles(CurveRenderData *rdata,
   for (Nurb *nu = rdata->nurbs->first; nu; nu = nu->next, nu_id++) {
     const BezTriple *bezt = nu->bezt;
     const BPoint *bp = nu->bp;
+
     if (bezt) {
       for (int a = 0; a < nu->pntsu; a++, bezt++) {
         if (bezt->hide == true) {
           continue;
         }
+        const bool handle_selected = BEZT_ISSEL_ANY(bezt);
 
         if (elbp_verts) {
           GPU_indexbuf_add_point_vert(elbp_verts, vbo_len_used + 0);
@@ -771,9 +779,9 @@ static void curve_create_edit_data_and_handles(CurveRenderData *rdata,
         }
         if (vbo_data) {
           const char vflag[3] = {
-              beztriple_vflag_get(rdata, bezt->f1, bezt->h1, a, nu_id, true),
-              beztriple_vflag_get(rdata, bezt->f2, bezt->h1, a, nu_id, false),
-              beztriple_vflag_get(rdata, bezt->f3, bezt->h2, a, nu_id, true),
+              beztriple_vflag_get(rdata, bezt->f1, bezt->h1, a, nu_id, true, handle_selected),
+              beztriple_vflag_get(rdata, bezt->f2, bezt->h1, a, nu_id, false, handle_selected),
+              beztriple_vflag_get(rdata, bezt->f3, bezt->h2, a, nu_id, true, handle_selected),
           };
           for (int j = 0; j < 3; j++) {
             GPU_vertbuf_attr_set(vbo_data, attr_id.data, vbo_len_used + j, &vflag[j]);

source/blender/draw/intern/shaders/common_globals_lib.glsl
@@ -140,3 +140,4 @@ layout(std140) uniform globalsBlock
 #define EDGE_SEAM (1 << 4)
 #define EDGE_SHARP (1 << 5)
 #define EDGE_FREESTYLE (1 << 6)
+#define HANDLE_SELECTED (1 << 7)

release/scripts/startup/bl_ui/space_view3d.py
@@ -6379,7 +6379,7 @@ class VIEW3D_PT_overlay_edit_curve(Panel):
         col.active = display_all
 
         row = col.row()
-        row.prop(overlay, "show_curve_handles", text="Handles")
+        row.prop(overlay, "display_handle", text="Handles")
 
         row = col.row()
         row.prop(overlay, "show_curve_normals", text="")

source/blender/blenkernel/BKE_curve.h
@@ -173,7 +173,8 @@ void BKE_nurbList_handles_recalculate(struct ListBase *editnurb,
                                       const char flag);
 
 void BKE_nurbList_handles_autocalc(ListBase *editnurb, int flag);
-void BKE_nurbList_flag_set(ListBase *editnurb, short flag);
+void BKE_nurbList_flag_set(ListBase *editnurb, short flag, bool set);
+bool BKE_nurbList_flag_set_from_flag(ListBase *editnurb, short from_flag, short flag);
 
 void BKE_nurb_free(struct Nurb *nu);
 struct Nurb *BKE_nurb_duplicate(const struct Nurb *nu);

source/blender/blenkernel/intern/curve.c
@@ -4485,7 +4485,7 @@ void BKE_nurbList_handles_recalculate(ListBase *editnurb, const bool calc_length
   }
 }
 
-void BKE_nurbList_flag_set(ListBase *editnurb, short flag)
+void BKE_nurbList_flag_set(ListBase *editnurb, short flag, bool set)
 {
   Nurb *nu;
   BezTriple *bezt;
@@ -4497,7 +4497,16 @@ void BKE_nurbList_flag_set(ListBase *editnurb, short flag)
       a = nu->pntsu;
       bezt = nu->bezt;
       while (a--) {
-        bezt->f1 = bezt->f2 = bezt->f3 = flag;
+        if (set) {
+          bezt->f1 |= flag;
+          bezt->f2 |= flag;
+          bezt->f3 |= flag;
+        }
+        else {
+          bezt->f1 &= ~flag;
+          bezt->f2 &= ~flag;
+          bezt->f3 &= ~flag;
+        }
         bezt++;
       }
     }
@@ -4505,11 +4514,45 @@ void BKE_nurbList_flag_set(ListBase *editnurb, short flag)
       a = nu->pntsu * nu->pntsv;
       bp = nu->bp;
       while (a--) {
-        bp->f1 = flag;
+        SET_FLAG_FROM_TEST(bp->f1, set, flag);
         bp++;
       }
     }
   }
+}
+
+/**
+ * Set \a flag for every point that already has \a from_flag set.
+ */
+bool BKE_nurbList_flag_set_from_flag(ListBase *editnurb, short from_flag, short flag)
+{
+  bool changed = false;
+
+  for (Nurb *nu = editnurb->first; nu; nu = nu->next) {
+    if (nu->type == CU_BEZIER) {
+      for (int i = 0; i < nu->pntsu; i++) {
+        BezTriple *bezt = &nu->bezt[i];
+        int old_f1 = bezt->f1, old_f2 = bezt->f2, old_f3 = bezt->f3;
+
+        SET_FLAG_FROM_TEST(bezt->f1, bezt->f1 & from_flag, flag);
+        SET_FLAG_FROM_TEST(bezt->f2, bezt->f2 & from_flag, flag);
+        SET_FLAG_FROM_TEST(bezt->f3, bezt->f3 & from_flag, flag);
+
+        changed |= (old_f1 != bezt->f1) || (old_f2 != bezt->f2) || (old_f3 != bezt->f3);
+      }
+    }
+    else {
+      for (int i = 0; i < nu->pntsu * nu->pntsv; i++) {
+        BPoint *bp = &nu->bp[i];
+        int old_f1 = bp->f1;
+
+        SET_FLAG_FROM_TEST(bp->f1, bp->f1 & from_flag, flag);
+        changed |= (old_f1 != bp->f1);
+      }
+    }
+  }
+
+  return changed;
 }
 
 void BKE_nurb_direction_switch(Nurb *nu)

commit 2cb532b29a7d9ef390bed0b35b7b290e3d490b54

Fix curve handle color ID display
Regression in 49f59092e7c8c caused all handles to display using
the 'aligned' theme color.

Arrange flags to fix this, add assert to avoid this happening again.

Also rename flag so it's use is clearer.

source/blender/draw/engines/overlay/shaders/edit_curve_handle_geom.glsl
@@ -52,7 +52,7 @@ void main()
 
   bool edge_selected = (((vertFlag[1] | vertFlag[0]) & VERT_SELECTED) != 0);
   bool handle_selected = (showCurveHandles &&
-                          (((vertFlag[1] | vertFlag[0]) & HANDLE_SELECTED) != 0));
+                          (((vertFlag[1] | vertFlag[0]) & VERT_SELECTED_BEZT_HANDLE) != 0));
 
   /* If handle type is only selected and the edge is not selected, don't show. */
   if ((curveHandleDisplay != CURVE_HANDLE_ALL) && (!handle_selected)) {

source/blender/draw/engines/overlay/shaders/edit_curve_point_vert.glsl
@@ -37,7 +37,7 @@ void main()
 #endif
 
   bool show_handle = showCurveHandles;
-  if ((curveHandleDisplay == CURVE_HANDLE_SELECTED) && ((data & HANDLE_SELECTED) == 0)) {
+  if ((curveHandleDisplay == CURVE_HANDLE_SELECTED) && ((data & VERT_SELECTED_BEZT_HANDLE) == 0)) {
     show_handle = false;
   }
 

source/blender/draw/intern/draw_cache_impl.h
@@ -201,18 +201,19 @@ struct GPUBatch *DRW_mesh_batch_cache_get_edit_mesh_analysis(struct Mesh *me);
 
 int DRW_mesh_material_count_get(struct Mesh *me);
 
+/* See 'common_globals_lib.glsl' for duplicate defines. */
+
 /* Edit mesh bitflags (is this the right place?) */
 enum {
   VFLAG_VERT_ACTIVE = 1 << 0,
   VFLAG_VERT_SELECTED = 1 << 1,
-  VFLAG_EDGE_ACTIVE = 1 << 2,
-  VFLAG_EDGE_SELECTED = 1 << 3,
-  VFLAG_EDGE_SEAM = 1 << 4,
-  VFLAG_EDGE_SHARP = 1 << 5,
-  VFLAG_EDGE_FREESTYLE = 1 << 6,
-  VFLAG_HANDLE_SELECTED = 1 << 7,
-  /* Beware to not go over 1 << 7 (it's a byte flag)
-   * (see gpu_shader_edit_mesh_overlay_geom.glsl) */
+  VFLAG_VERT_SELECTED_BEZT_HANDLE = 1 << 2,
+  VFLAG_EDGE_ACTIVE = 1 << 3,
+  VFLAG_EDGE_SELECTED = 1 << 4,
+  VFLAG_EDGE_SEAM = 1 << 5,
+  VFLAG_EDGE_SHARP = 1 << 6,
+  VFLAG_EDGE_FREESTYLE = 1 << 7,
+  /* Beware to not go over 1 << 7 (it's a byte flag). */
 };
 
 enum {
@@ -224,8 +225,7 @@ enum {
   VFLAG_EDGE_UV_SELECT = 1 << 5,
   VFLAG_FACE_UV_ACTIVE = 1 << 6,
   VFLAG_FACE_UV_SELECT = 1 << 7,
-  /* Beware to not go over 1 << 7 (it's a byte flag)
-   * (see gpu_shader_edit_mesh_overlay_geom.glsl) */
+  /* Beware to not go over 1 << 7 (it's a byte flag). */
 };
 
 /* Particles */

source/blender/draw/intern/draw_cache_impl_curve.c
@@ -47,6 +47,7 @@
 
 #include "draw_cache_impl.h" /* own include */
 
+/* See: edit_curve_point_vert.glsl for duplicate includes. */
 #define SELECT 1
 #define ACTIVE_NURB 1 << 2
 #define BEZIER_HANDLE 1 << 3
@@ -698,7 +699,9 @@ static char beztriple_vflag_get(CurveRenderData *rdata,
   SET_FLAG_FROM_TEST(vflag, (v_idx == rdata->actvert && nu_id == rdata->actnu), VFLAG_VERT_ACTIVE);
   SET_FLAG_FROM_TEST(vflag, (nu_id == rdata->actnu), ACTIVE_NURB);
   SET_FLAG_FROM_TEST(vflag, handle_point, BEZIER_HANDLE);
-  SET_FLAG_FROM_TEST(vflag, handle_selected, VFLAG_HANDLE_SELECTED);
+  SET_FLAG_FROM_TEST(vflag, handle_selected, VFLAG_VERT_SELECTED_BEZT_HANDLE);
+  /* Setting flags that overlap with will cause the color id not to work properly. */
+  BLI_assert((vflag >> COLOR_SHIFT) == 0);
   /* handle color id */
   vflag |= col_id << COLOR_SHIFT;
   return vflag;
@@ -711,6 +714,8 @@ static char bpoint_vflag_get(CurveRenderData *rdata, char flag, int v_idx, int n
   SET_FLAG_FROM_TEST(vflag, (v_idx == rdata->actvert && nu_id == rdata->actnu), VFLAG_VERT_ACTIVE);
   SET_FLAG_FROM_TEST(vflag, (nu_id == rdata->actnu), ACTIVE_NURB);
   SET_FLAG_FROM_TEST(vflag, ((u % 2) == 0), EVEN_U_BIT);
+  /* Setting flags that overlap with will cause the color id not to work properly. */
+  BLI_assert((vflag >> COLOR_SHIFT) == 0);
   vflag |= COLOR_NURB_ULINE_ID << COLOR_SHIFT;
   return vflag;
 }

source/blender/draw/intern/shaders/common_globals_lib.glsl
@@ -123,6 +123,8 @@ layout(std140) uniform globalsBlock
 
 #define sizeViewportInv (sizeViewport.zw)
 
+/* See: 'draw_cache_impl.h' for matching includes. */
+
 /* data[0] (1st byte flags) */
 #define FACE_ACTIVE (1 << 0)
 #define FACE_SELECTED (1 << 1)
@@ -135,9 +137,9 @@ layout(std140) uniform globalsBlock
 /* data[1] (2st byte flags) */
 #define VERT_ACTIVE (1 << 0)
 #define VERT_SELECTED (1 << 1)
-#define EDGE_ACTIVE (1 << 2)
-#define EDGE_SELECTED (1 << 3)
-#define EDGE_SEAM (1 << 4)
-#define EDGE_SHARP (1 << 5)
-#define EDGE_FREESTYLE (1 << 6)
-#define HANDLE_SELECTED (1 << 7)
+#define VERT_SELECTED_BEZT_HANDLE (1 << 2)
+#define EDGE_ACTIVE (1 << 3)
+#define EDGE_SELECTED (1 << 4)
+#define EDGE_SEAM (1 << 5)
+#define EDGE_SHARP (1 << 6)
+#define EDGE_FREESTYLE (1 << 7)