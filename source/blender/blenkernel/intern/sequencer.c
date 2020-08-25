Pagecran/blender

commit 70500121b457d1bb7ce6aeab39eb3c4c30a6ffe3

Cleanup: rename iterators over sequences to be more clear about what they do.
No functional changes expected.

source/blender/blenkernel/intern/sequencer.c
@@ -507,11 +507,12 @@ void BKE_sequencer_editing_free(Scene *scene, const bool do_id_user)
   BKE_sequencer_prefetch_free(scene);
   BKE_sequencer_cache_destruct(scene);
 
-  SEQ_BEGIN (ed, seq) {
+  SEQ_ALL_BEGIN(ed, seq)
+  {
     /* handle cache freeing above */
     BKE_sequence_free_ex(scene, seq, false, do_id_user, false);
   }
-  SEQ_END;
+  SEQ_ALL_END;
 
   BLI_freelistN(&ed->metastack);
   MEM_freeN(ed);
@@ -662,7 +663,7 @@ void BKE_sequencer_new_render_data(Main *bmain,
 
 /* ************************* iterator ************************** */
 /* *************** (replaces old WHILE_SEQ) ********************* */
-/* **************** use now SEQ_BEGIN () SEQ_END ***************** */
+/* **************** use now SEQ_ALL_BEGIN () SEQ_ALL_END ***************** */
 
 /* sequence strip iterator:
  * - builds a full array, recursively into meta strips
@@ -697,7 +698,10 @@ static void seq_build_array(ListBase *seqbase, Sequence ***array, int depth)
   }
 }
 
-static void seq_array(Editing *ed, Sequence ***seqarray, int *tot, bool use_pointer)
+static void seq_array(Editing *ed,
+                      Sequence ***seqarray,
+                      int *tot,
+                      const bool use_current_sequences)
 {
   Sequence **array;
 
@@ -708,7 +712,7 @@ static void seq_array(Editing *ed, Sequence ***seqarray, int *tot, bool use_poin
     return;
   }
 
-  if (use_pointer) {
+  if (use_current_sequences) {
     seq_count(ed->seqbasep, tot);
   }
   else {
@@ -720,7 +724,7 @@ static void seq_array(Editing *ed, Sequence ***seqarray, int *tot, bool use_poin
   }
 
   *seqarray = array = MEM_mallocN(sizeof(Sequence *) * (*tot), "SeqArray");
-  if (use_pointer) {
+  if (use_current_sequences) {
     seq_build_array(ed->seqbasep, &array, 0);
   }
   else {
@@ -728,10 +732,10 @@ static void seq_array(Editing *ed, Sequence ***seqarray, int *tot, bool use_poin
   }
 }
 
-void BKE_sequence_iterator_begin(Editing *ed, SeqIterator *iter, bool use_pointer)
+void BKE_sequence_iterator_begin(Editing *ed, SeqIterator *iter, const bool use_current_sequences)
 {
   memset(iter, 0, sizeof(*iter));
-  seq_array(ed, &iter->array, &iter->tot, use_pointer);
+  seq_array(ed, &iter->array, &iter->tot, use_current_sequences);
 
   if (iter->tot) {
     iter->cur = 0;
@@ -6168,7 +6172,8 @@ void BKE_sequencer_check_uuids_unique_and_report(const Scene *scene)
       BLI_session_uuid_ghash_hash, BLI_session_uuid_ghash_compare, "sequencer used uuids");
 
   const Sequence *sequence;
-  SEQ_BEGIN (scene->ed, sequence) {
+  SEQ_ALL_BEGIN(scene->ed, sequence)
+  {
     const SessionUUID *session_uuid = &sequence->runtime.session_uuid;
     if (!BLI_session_uuid_is_generated(session_uuid)) {
       printf("Sequence %s does not have UUID generated.\n", sequence->name);
@@ -6182,7 +6187,7 @@ void BKE_sequencer_check_uuids_unique_and_report(const Scene *scene)
 
     BLI_gset_insert(used_uuids, (void *)session_uuid);
   }
-  SEQ_END;
+  SEQ_ALL_END;
 
   BLI_gset_free(used_uuids, NULL);
 }

source/blender/blenloader/intern/versioning_legacy.c
@@ -1251,12 +1251,13 @@ void blo_do_versions_pre250(FileData *fd, Library *lib, Main *bmain)
     while (sce) {
       ed = sce->ed;
       if (ed) {
-        SEQ_BEGIN (sce->ed, seq) {
+        SEQ_ALL_BEGIN(sce->ed, seq)
+        {
           if (seq->type == SEQ_TYPE_IMAGE || seq->type == SEQ_TYPE_MOVIE) {
             seq->alpha_mode = SEQ_ALPHA_STRAIGHT;
           }
         }
-        SEQ_END;
+        SEQ_ALL_END;
       }
 
       sce = sce->id.next;
@@ -2442,12 +2443,13 @@ void blo_do_versions_pre250(FileData *fd, Library *lib, Main *bmain)
     Sequence *seq;
 
     for (sce = bmain->scenes.first; sce; sce = sce->id.next) {
-      SEQ_BEGIN (sce->ed, seq) {
+      SEQ_ALL_BEGIN(sce->ed, seq)
+      {
         if (seq->blend_mode == 0) {
           seq->blend_opacity = 100.0f;
         }
       }
-      SEQ_END;
+      SEQ_ALL_END;
     }
   }
 
@@ -2595,12 +2597,13 @@ void blo_do_versions_pre250(FileData *fd, Library *lib, Main *bmain)
     while (sce) {
       ed = sce->ed;
       if (ed) {
-        SEQP_BEGIN (ed, seq) {
+        SEQ_CURRENT_BEGIN(ed, seq)
+        {
           if (seq->strip && seq->strip->proxy) {
             seq->strip->proxy->quality = 90;
           }
         }
-        SEQ_END;
+        SEQ_CURRENT_END;
       }
 
       sce = sce->id.next;

source/blender/editors/space_sequencer/sequencer_edit.c
@@ -216,7 +216,8 @@ static void seq_proxy_build_job(const bContext *C, ReportList *reports)
   file_list = BLI_gset_new(BLI_ghashutil_strhash_p, BLI_ghashutil_strcmp, "file list");
   bool selected = false; /* Check for no selected strips */
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     if (!ELEM(seq->type, SEQ_TYPE_MOVIE, SEQ_TYPE_IMAGE, SEQ_TYPE_META) ||
         (seq->flag & SELECT) == 0) {
       continue;
@@ -239,7 +240,7 @@ static void seq_proxy_build_job(const bContext *C, ReportList *reports)
       BKE_reportf(reports, RPT_WARNING, "Overwrite is not checked for %s, skipping", seq->name);
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
   if (!selected) {
     BKE_reportf(reports, RPT_WARNING, "Select movie or image strips");
@@ -482,10 +483,11 @@ void ED_sequencer_deselect_all(Scene *scene)
     return;
   }
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     seq->flag &= ~SEQ_ALLSEL;
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 }
 
 void recurs_sel_seq(Sequence *seqm)
@@ -1030,7 +1032,7 @@ static void set_filter_seq(Scene *scene)
     return;
   }
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     if (seq->flag & SELECT) {
       if (seq->type == SEQ_TYPE_MOVIE) {
         seq->flag |= SEQ_FILTERY;
@@ -1039,7 +1041,7 @@ static void set_filter_seq(Scene *scene)
       }
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 }
 #endif
 
@@ -1065,7 +1067,8 @@ static void UNUSED_FUNCTION(seq_remap_paths)(Scene *scene)
     return;
   }
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     if (seq->flag & SELECT) {
       if (STREQLEN(seq->strip->dir, from, strlen(from))) {
         printf("found %s\n", seq->strip->dir);
@@ -1080,7 +1083,7 @@ static void UNUSED_FUNCTION(seq_remap_paths)(Scene *scene)
       }
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 }
 
 /** \} */
@@ -1308,7 +1311,7 @@ static int sequencer_snap_exec(bContext *C, wmOperator *op)
   }
 
   /* Test for effects and overlap.
-   * Don't use SEQP_BEGIN since that would be recursive. */
+   * Don't use SEQ_CURRENT_BEGIN since that would be recursive. */
   for (seq = ed->seqbasep->first; seq; seq = seq->next) {
     if (seq->flag & SELECT && !(seq->depth == 0 && seq->flag & SEQ_LOCK)) {
       seq->flag &= ~SEQ_OVERLAP;
@@ -2294,25 +2297,28 @@ static int sequencer_split_exec(bContext *C, wmOperator *op)
     Sequence *seq;
     if (ignore_selection) {
       if (use_cursor_position) {
-        SEQP_BEGIN (ed, seq) {
+        SEQ_CURRENT_BEGIN(ed, seq)
+        {
           if (seq->enddisp == split_frame && seq->machine == split_channel) {
             seq_selected = seq->flag & SEQ_ALLSEL;
           }
         }
-        SEQ_END;
+        SEQ_CURRENT_END;
         if (!seq_selected) {
-          SEQP_BEGIN (ed, seq) {
+          SEQ_CURRENT_BEGIN(ed, seq)
+          {
             if (seq->startdisp == split_frame && seq->machine == split_channel) {
               seq->flag &= ~SEQ_ALLSEL;
             }
           }
-          SEQ_END;
+          SEQ_CURRENT_END;
         }
       }
     }
     else {
       if (split_side != SEQ_SIDE_BOTH) {
-        SEQP_BEGIN (ed, seq) {
+        SEQ_CURRENT_BEGIN(ed, seq)
+        {
           if (split_side == SEQ_SIDE_LEFT) {
             if (seq->startdisp >= split_frame) {
               seq->flag &= ~SEQ_ALLSEL;
@@ -2324,15 +2330,16 @@ static int sequencer_split_exec(bContext *C, wmOperator *op)
             }
           }
         }
-        SEQ_END;
+        SEQ_CURRENT_END;
       }
     }
-    SEQP_BEGIN (ed, seq) {
+    SEQ_CURRENT_BEGIN(ed, seq)
+    {
       if (seq->seq1 || seq->seq2 || seq->seq3) {
         BKE_sequence_calc(scene, seq);
       }
     }
-    SEQ_END;
+    SEQ_CURRENT_END;
 
     BKE_sequencer_sort(scene);
   }
@@ -2521,12 +2528,13 @@ static int sequencer_delete_exec(bContext *C, wmOperator *UNUSED(op))
 
   BKE_sequencer_prefetch_stop(scene);
 
-  SEQP_BEGIN (scene->ed, seq) {
+  SEQ_CURRENT_BEGIN(scene->ed, seq)
+  {
     if (seq->flag & SELECT) {
       BKE_sequencer_flag_for_removal(scene, ed->seqbasep, seq);
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
   BKE_sequencer_remove_flagged_sequences(scene, ed->seqbasep);
 
   DEG_id_tag_update(&scene->id, ID_RECALC_SEQUENCER_STRIPS);
@@ -2952,7 +2960,7 @@ static int sequencer_meta_separate_exec(bContext *C, wmOperator *UNUSED(op))
   recurs_del_seq_flag(scene, ed->seqbasep, SEQ_FLAG_DELETE, 0);
 
   /* Test for effects and overlap
-   * don't use SEQP_BEGIN since that would be recursive. */
+   * don't use SEQ_CURRENT_BEGIN since that would be recursive. */
   for (seq = ed->seqbasep->first; seq; seq = seq->next) {
     if (seq->flag & SELECT) {
       seq->flag &= ~SEQ_OVERLAP;
@@ -3476,7 +3484,8 @@ static int sequencer_rebuild_proxy_exec(bContext *C, wmOperator *UNUSED(op))
 
   file_list = BLI_gset_new(BLI_ghashutil_strhash_p, BLI_ghashutil_strcmp, "file list");
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     if ((seq->flag & SELECT)) {
       ListBase queue = {NULL, NULL};
       LinkData *link;
@@ -3493,7 +3502,7 @@ static int sequencer_rebuild_proxy_exec(bContext *C, wmOperator *UNUSED(op))
       BKE_sequencer_free_imbuf(scene, &ed->seqbase, false);
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
   BLI_gset_free(file_list, MEM_freeN);
 
@@ -3544,7 +3553,8 @@ static int sequencer_enable_proxies_exec(bContext *C, wmOperator *op)
     turnon = false;
   }
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     if ((seq->flag & SELECT)) {
       if (ELEM(seq->type, SEQ_TYPE_MOVIE, SEQ_TYPE_IMAGE, SEQ_TYPE_META)) {
         BKE_sequencer_proxy_set(seq, turnon);
@@ -3589,7 +3599,7 @@ static int sequencer_enable_proxies_exec(bContext *C, wmOperator *op)
       }
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
   WM_event_add_notifier(C, NC_SCENE | ND_SEQUENCER, scene);
 
@@ -3966,12 +3976,13 @@ static int sequencer_export_subtitles_exec(bContext *C, wmOperator *op)
     return OPERATOR_CANCELLED;
   }
 
-  SEQ_BEGIN (ed, seq) {
+  SEQ_ALL_BEGIN(ed, seq)
+  {
     if (seq->type == SEQ_TYPE_TEXT) {
       BLI_addtail(&text_seq, MEM_dupallocN(seq));
     }
   }
-  SEQ_END;
+  SEQ_ALL_END;
 
   if (BLI_listbase_is_empty(&text_seq)) {
     BKE_report(op->reports, RPT_ERROR, "No subtitles (text strips) to export");

source/blender/editors/space_sequencer/sequencer_modifier.c
@@ -232,7 +232,8 @@ static int strip_modifier_copy_exec(bContext *C, wmOperator *op)
     return OPERATOR_CANCELLED;
   }
 
-  SEQP_BEGIN (ed, seq_iter) {
+  SEQ_CURRENT_BEGIN(ed, seq_iter)
+  {
     if (seq_iter->flag & SELECT) {
       if (seq_iter == seq) {
         continue;
@@ -254,7 +255,7 @@ static int strip_modifier_copy_exec(bContext *C, wmOperator *op)
       BKE_sequence_modifier_list_copy(seq_iter, seq);
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
   BKE_sequence_invalidate_cache_preprocessed(scene, seq);
   WM_event_add_notifier(C, NC_SCENE | ND_SEQUENCER, scene);

source/blender/editors/space_sequencer/sequencer_select.c
@@ -415,14 +415,15 @@ static int sequencer_select_exec(bContext *C, wmOperator *op)
 
     const float x = UI_view2d_region_to_view_x(v2d, mval[0]);
 
-    SEQP_BEGIN (ed, seq) {
+    SEQ_CURRENT_BEGIN(ed, seq)
+    {
       if (((x < CFRA) && (seq->enddisp <= CFRA)) || ((x >= CFRA) && (seq->startdisp >= CFRA))) {
         /* Select left or right. */
         seq->flag |= SELECT;
         recurs_sel_seq(seq);
       }
     }
-    SEQ_END;
+    SEQ_CURRENT_END;
 
     {
       SpaceSeq *sseq = CTX_wm_space_seq(C);
@@ -975,7 +976,8 @@ static int sequencer_select_side_of_frame_exec(bContext *C, wmOperator *op)
     ED_sequencer_deselect_all(scene);
   }
   const int cfra = CFRA;
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     bool test = false;
     switch (side) {
       case -1:
@@ -994,7 +996,7 @@ static int sequencer_select_side_of_frame_exec(bContext *C, wmOperator *op)
       recurs_sel_seq(seq);
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
   ED_outliner_select_sync_from_sequence_tag(C);
 
@@ -1282,13 +1284,14 @@ static bool select_grouped_type(Editing *ed, Sequence *actseq, const int channel
   Sequence *seq;
   bool changed = false;
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     if (SEQ_CHANNEL_CHECK(seq, channel) && seq->type == actseq->type) {
       seq->flag |= SELECT;
       changed = true;
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
   return changed;
 }
@@ -1299,13 +1302,14 @@ static bool select_grouped_type_basic(Editing *ed, Sequence *actseq, const int c
   bool changed = false;
   const bool is_sound = SEQ_IS_SOUND(actseq);
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     if (SEQ_CHANNEL_CHECK(seq, channel) && (is_sound ? SEQ_IS_SOUND(seq) : !SEQ_IS_SOUND(seq))) {
       seq->flag |= SELECT;
       changed = true;
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
   return changed;
 }
@@ -1316,14 +1320,15 @@ static bool select_grouped_type_effect(Editing *ed, Sequence *actseq, const int 
   bool changed = false;
   const bool is_effect = SEQ_IS_EFFECT(actseq);
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     if (SEQ_CHANNEL_CHECK(seq, channel) &&
         (is_effect ? SEQ_IS_EFFECT(seq) : !SEQ_IS_EFFECT(seq))) {
       seq->flag |= SELECT;
       changed = true;
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
   return changed;
 }
@@ -1339,45 +1344,49 @@ static bool select_grouped_data(Editing *ed, Sequence *actseq, const int channel
   }
 
   if (SEQ_HAS_PATH(actseq) && dir) {
-    SEQP_BEGIN (ed, seq) {
+    SEQ_CURRENT_BEGIN(ed, seq)
+    {
       if (SEQ_CHANNEL_CHECK(seq, channel) && SEQ_HAS_PATH(seq) && seq->strip &&
           STREQ(seq->strip->dir, dir)) {
         seq->flag |= SELECT;
         changed = true;
       }
     }
-    SEQ_END;
+    SEQ_CURRENT_END;
   }
   else if (actseq->type == SEQ_TYPE_SCENE) {
     Scene *sce = actseq->scene;
-    SEQP_BEGIN (ed, seq) {
+    SEQ_CURRENT_BEGIN(ed, seq)
+    {
       if (SEQ_CHANNEL_CHECK(seq, channel) && seq->type == SEQ_TYPE_SCENE && seq->scene == sce) {
         seq->flag |= SELECT;
         changed = true;
       }
     }
-    SEQ_END;
+    SEQ_CURRENT_END;
   }
   else if (actseq->type == SEQ_TYPE_MOVIECLIP) {
     MovieClip *clip = actseq->clip;
-    SEQP_BEGIN (ed, seq) {
+    SEQ_CURRENT_BEGIN(ed, seq)
+    {
       if (SEQ_CHANNEL_CHECK(seq, channel) && seq->type == SEQ_TYPE_MOVIECLIP &&
           seq->clip == clip) {
         seq->flag |= SELECT;
         changed = true;
       }
     }
-    SEQ_END;
+    SEQ_CURRENT_END;
   }
   else if (actseq->type == SEQ_TYPE_MASK) {
     struct Mask *mask = actseq->mask;
-    SEQP_BEGIN (ed, seq) {
+    SEQ_CURRENT_BEGIN(ed, seq)
+    {
       if (SEQ_CHANNEL_CHECK(seq, channel) && seq->type == SEQ_TYPE_MASK && seq->mask == mask) {
         seq->flag |= SELECT;
         changed = true;
       }
     }
-    SEQ_END;
+    SEQ_CURRENT_END;
   }
 
   return changed;
@@ -1394,15 +1403,17 @@ static bool select_grouped_effect(Editing *ed, Sequence *actseq, const int chann
     effects[i] = false;
   }
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     if (SEQ_CHANNEL_CHECK(seq, channel) && (seq->type & SEQ_TYPE_EFFECT) &&
         ELEM(actseq, seq->seq1, seq->seq2, seq->seq3)) {
       effects[seq->type] = true;
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     if (SEQ_CHANNEL_CHECK(seq, channel) && effects[seq->type]) {
       if (seq->seq1) {
         seq->seq1->flag |= SELECT;
@@ -1416,7 +1427,7 @@ static bool select_grouped_effect(Editing *ed, Sequence *actseq, const int chann
       changed = true;
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
   return changed;
 }
@@ -1426,13 +1437,14 @@ static bool select_grouped_time_overlap(Editing *ed, Sequence *actseq)
   Sequence *seq;
   bool changed = false;
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     if (seq->startdisp < actseq->enddisp && seq->enddisp > actseq->startdisp) {
       seq->flag |= SELECT;
       changed = true;
     }
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
   return changed;
 }
@@ -1447,10 +1459,11 @@ static bool select_grouped_effect_link(Editing *ed, Sequence *actseq, const int 
   int machine = actseq->machine;
   SeqIterator iter;
 
-  SEQP_BEGIN (ed, seq) {
+  SEQ_CURRENT_BEGIN(ed, seq)
+  {
     seq->tmp = NULL;
   }
-  SEQ_END;
+  SEQ_CURRENT_END;
 
   actseq->tmp = POINTER_FROM_INT(true);
 
@@ -1523,11 +1536,12 @@ static int sequencer_select_grouped_exec(bContext *C, wmOperator *op)
   bool changed = false;
 
   if (!extend) {
-    SEQP_BEGIN (ed, seq) {
+    SEQ_CURRENT_BEGIN(ed, seq)
+    {
       seq->flag &= ~SELECT;
       changed = true;
     }
-    SEQ_END;
+    SEQ_CURRENT_END;
   }
 
   switch (type) {

commit 259324b6d83be566c419db6195b9448c002eb1e8

Cleanup: add SEQ_ALL_BEGIN, SEQ_CURRENT_BEGIN to .clang-format
These were missed in 70500121b457d which caused reformatting.

.clang-format
@@ -253,8 +253,8 @@ ForEachMacros:
   - RNA_STRUCT_BEGIN_SKIP_RNA_TYPE
   - SCULPT_VERTEX_DUPLICATES_AND_NEIGHBORS_ITER_BEGIN
   - SCULPT_VERTEX_NEIGHBORS_ITER_BEGIN
-  - SEQP_BEGIN
-  - SEQ_BEGIN
+  - SEQ_ALL_BEGIN
+  - SEQ_CURRENT_BEGIN
   - SURFACE_QUAD_ITER_BEGIN
   - foreach
   - ED_screen_areas_iter

source/blender/blenkernel/intern/sequencer.c
@@ -507,8 +507,7 @@ void BKE_sequencer_editing_free(Scene *scene, const bool do_id_user)
   BKE_sequencer_prefetch_free(scene);
   BKE_sequencer_cache_destruct(scene);
 
-  SEQ_ALL_BEGIN(ed, seq)
-  {
+  SEQ_ALL_BEGIN (ed, seq) {
     /* handle cache freeing above */
     BKE_sequence_free_ex(scene, seq, false, do_id_user, false);
   }
@@ -6172,8 +6171,7 @@ void BKE_sequencer_check_uuids_unique_and_report(const Scene *scene)
       BLI_session_uuid_ghash_hash, BLI_session_uuid_ghash_compare, "sequencer used uuids");
 
   const Sequence *sequence;
-  SEQ_ALL_BEGIN(scene->ed, sequence)
-  {
+  SEQ_ALL_BEGIN (scene->ed, sequence) {
     const SessionUUID *session_uuid = &sequence->runtime.session_uuid;
     if (!BLI_session_uuid_is_generated(session_uuid)) {
       printf("Sequence %s does not have UUID generated.\n", sequence->name);

source/blender/blenloader/intern/versioning_legacy.c
@@ -1251,8 +1251,7 @@ void blo_do_versions_pre250(FileData *fd, Library *lib, Main *bmain)
     while (sce) {
       ed = sce->ed;
       if (ed) {
-        SEQ_ALL_BEGIN(sce->ed, seq)
-        {
+        SEQ_ALL_BEGIN (sce->ed, seq) {
           if (seq->type == SEQ_TYPE_IMAGE || seq->type == SEQ_TYPE_MOVIE) {
             seq->alpha_mode = SEQ_ALPHA_STRAIGHT;
           }
@@ -2443,8 +2442,7 @@ void blo_do_versions_pre250(FileData *fd, Library *lib, Main *bmain)
     Sequence *seq;
 
     for (sce = bmain->scenes.first; sce; sce = sce->id.next) {
-      SEQ_ALL_BEGIN(sce->ed, seq)
-      {
+      SEQ_ALL_BEGIN (sce->ed, seq) {
         if (seq->blend_mode == 0) {
           seq->blend_opacity = 100.0f;
         }
@@ -2597,8 +2595,7 @@ void blo_do_versions_pre250(FileData *fd, Library *lib, Main *bmain)
     while (sce) {
       ed = sce->ed;
       if (ed) {
-        SEQ_CURRENT_BEGIN(ed, seq)
-        {
+        SEQ_CURRENT_BEGIN (ed, seq) {
           if (seq->strip && seq->strip->proxy) {
             seq->strip->proxy->quality = 90;
           }

source/blender/editors/space_sequencer/sequencer_edit.c
@@ -217,8 +217,7 @@ static void seq_proxy_build_job(const bContext *C, ReportList *reports)
   file_list = BLI_gset_new(BLI_ghashutil_strhash_p, BLI_ghashutil_strcmp, "file list");
   bool selected = false; /* Check for no selected strips */
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     if (!ELEM(seq->type, SEQ_TYPE_MOVIE, SEQ_TYPE_IMAGE, SEQ_TYPE_META) ||
         (seq->flag & SELECT) == 0) {
       continue;
@@ -484,8 +483,7 @@ void ED_sequencer_deselect_all(Scene *scene)
     return;
   }
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     seq->flag &= ~SEQ_ALLSEL;
   }
   SEQ_CURRENT_END;
@@ -1068,8 +1066,7 @@ static void UNUSED_FUNCTION(seq_remap_paths)(Scene *scene)
     return;
   }
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     if (seq->flag & SELECT) {
       if (STREQLEN(seq->strip->dir, from, strlen(from))) {
         printf("found %s\n", seq->strip->dir);
@@ -2298,16 +2295,14 @@ static int sequencer_split_exec(bContext *C, wmOperator *op)
     Sequence *seq;
     if (ignore_selection) {
       if (use_cursor_position) {
-        SEQ_CURRENT_BEGIN(ed, seq)
-        {
+        SEQ_CURRENT_BEGIN (ed, seq) {
           if (seq->enddisp == split_frame && seq->machine == split_channel) {
             seq_selected = seq->flag & SEQ_ALLSEL;
           }
         }
         SEQ_CURRENT_END;
         if (!seq_selected) {
-          SEQ_CURRENT_BEGIN(ed, seq)
-          {
+          SEQ_CURRENT_BEGIN (ed, seq) {
             if (seq->startdisp == split_frame && seq->machine == split_channel) {
               seq->flag &= ~SEQ_ALLSEL;
             }
@@ -2318,8 +2313,7 @@ static int sequencer_split_exec(bContext *C, wmOperator *op)
     }
     else {
       if (split_side != SEQ_SIDE_BOTH) {
-        SEQ_CURRENT_BEGIN(ed, seq)
-        {
+        SEQ_CURRENT_BEGIN (ed, seq) {
           if (split_side == SEQ_SIDE_LEFT) {
             if (seq->startdisp >= split_frame) {
               seq->flag &= ~SEQ_ALLSEL;
@@ -2334,8 +2328,7 @@ static int sequencer_split_exec(bContext *C, wmOperator *op)
         SEQ_CURRENT_END;
       }
     }
-    SEQ_CURRENT_BEGIN(ed, seq)
-    {
+    SEQ_CURRENT_BEGIN (ed, seq) {
       if (seq->seq1 || seq->seq2 || seq->seq3) {
         BKE_sequence_calc(scene, seq);
       }
@@ -2552,8 +2545,7 @@ static int sequencer_delete_exec(bContext *C, wmOperator *UNUSED(op))
 
   BKE_sequencer_prefetch_stop(scene);
 
-  SEQ_CURRENT_BEGIN(scene->ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (scene->ed, seq) {
     if (seq->flag & SELECT) {
       BKE_sequencer_flag_for_removal(scene, ed->seqbasep, seq);
     }
@@ -3508,8 +3500,7 @@ static int sequencer_rebuild_proxy_exec(bContext *C, wmOperator *UNUSED(op))
 
   file_list = BLI_gset_new(BLI_ghashutil_strhash_p, BLI_ghashutil_strcmp, "file list");
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     if ((seq->flag & SELECT)) {
       ListBase queue = {NULL, NULL};
       LinkData *link;
@@ -3577,8 +3568,7 @@ static int sequencer_enable_proxies_exec(bContext *C, wmOperator *op)
     turnon = false;
   }
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     if ((seq->flag & SELECT)) {
       if (ELEM(seq->type, SEQ_TYPE_MOVIE, SEQ_TYPE_IMAGE, SEQ_TYPE_META)) {
         BKE_sequencer_proxy_set(seq, turnon);
@@ -4000,8 +3990,7 @@ static int sequencer_export_subtitles_exec(bContext *C, wmOperator *op)
     return OPERATOR_CANCELLED;
   }
 
-  SEQ_ALL_BEGIN(ed, seq)
-  {
+  SEQ_ALL_BEGIN (ed, seq) {
     if (seq->type == SEQ_TYPE_TEXT) {
       BLI_addtail(&text_seq, MEM_dupallocN(seq));
     }

source/blender/editors/space_sequencer/sequencer_modifier.c
@@ -232,8 +232,7 @@ static int strip_modifier_copy_exec(bContext *C, wmOperator *op)
     return OPERATOR_CANCELLED;
   }
 
-  SEQ_CURRENT_BEGIN(ed, seq_iter)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq_iter) {
     if (seq_iter->flag & SELECT) {
       if (seq_iter == seq) {
         continue;

source/blender/editors/space_sequencer/sequencer_select.c
@@ -415,8 +415,7 @@ static int sequencer_select_exec(bContext *C, wmOperator *op)
 
     const float x = UI_view2d_region_to_view_x(v2d, mval[0]);
 
-    SEQ_CURRENT_BEGIN(ed, seq)
-    {
+    SEQ_CURRENT_BEGIN (ed, seq) {
       if (((x < CFRA) && (seq->enddisp <= CFRA)) || ((x >= CFRA) && (seq->startdisp >= CFRA))) {
         /* Select left or right. */
         seq->flag |= SELECT;
@@ -976,8 +975,7 @@ static int sequencer_select_side_of_frame_exec(bContext *C, wmOperator *op)
     ED_sequencer_deselect_all(scene);
   }
   const int cfra = CFRA;
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     bool test = false;
     switch (side) {
       case -1:
@@ -1284,8 +1282,7 @@ static bool select_grouped_type(Editing *ed, Sequence *actseq, const int channel
   Sequence *seq;
   bool changed = false;
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     if (SEQ_CHANNEL_CHECK(seq, channel) && seq->type == actseq->type) {
       seq->flag |= SELECT;
       changed = true;
@@ -1302,8 +1299,7 @@ static bool select_grouped_type_basic(Editing *ed, Sequence *actseq, const int c
   bool changed = false;
   const bool is_sound = SEQ_IS_SOUND(actseq);
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     if (SEQ_CHANNEL_CHECK(seq, channel) && (is_sound ? SEQ_IS_SOUND(seq) : !SEQ_IS_SOUND(seq))) {
       seq->flag |= SELECT;
       changed = true;
@@ -1320,8 +1316,7 @@ static bool select_grouped_type_effect(Editing *ed, Sequence *actseq, const int 
   bool changed = false;
   const bool is_effect = SEQ_IS_EFFECT(actseq);
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     if (SEQ_CHANNEL_CHECK(seq, channel) &&
         (is_effect ? SEQ_IS_EFFECT(seq) : !SEQ_IS_EFFECT(seq))) {
       seq->flag |= SELECT;
@@ -1344,8 +1339,7 @@ static bool select_grouped_data(Editing *ed, Sequence *actseq, const int channel
   }
 
   if (SEQ_HAS_PATH(actseq) && dir) {
-    SEQ_CURRENT_BEGIN(ed, seq)
-    {
+    SEQ_CURRENT_BEGIN (ed, seq) {
       if (SEQ_CHANNEL_CHECK(seq, channel) && SEQ_HAS_PATH(seq) && seq->strip &&
           STREQ(seq->strip->dir, dir)) {
         seq->flag |= SELECT;
@@ -1356,8 +1350,7 @@ static bool select_grouped_data(Editing *ed, Sequence *actseq, const int channel
   }
   else if (actseq->type == SEQ_TYPE_SCENE) {
     Scene *sce = actseq->scene;
-    SEQ_CURRENT_BEGIN(ed, seq)
-    {
+    SEQ_CURRENT_BEGIN (ed, seq) {
       if (SEQ_CHANNEL_CHECK(seq, channel) && seq->type == SEQ_TYPE_SCENE && seq->scene == sce) {
         seq->flag |= SELECT;
         changed = true;
@@ -1367,8 +1360,7 @@ static bool select_grouped_data(Editing *ed, Sequence *actseq, const int channel
   }
   else if (actseq->type == SEQ_TYPE_MOVIECLIP) {
     MovieClip *clip = actseq->clip;
-    SEQ_CURRENT_BEGIN(ed, seq)
-    {
+    SEQ_CURRENT_BEGIN (ed, seq) {
       if (SEQ_CHANNEL_CHECK(seq, channel) && seq->type == SEQ_TYPE_MOVIECLIP &&
           seq->clip == clip) {
         seq->flag |= SELECT;
@@ -1379,8 +1371,7 @@ static bool select_grouped_data(Editing *ed, Sequence *actseq, const int channel
   }
   else if (actseq->type == SEQ_TYPE_MASK) {
     struct Mask *mask = actseq->mask;
-    SEQ_CURRENT_BEGIN(ed, seq)
-    {
+    SEQ_CURRENT_BEGIN (ed, seq) {
       if (SEQ_CHANNEL_CHECK(seq, channel) && seq->type == SEQ_TYPE_MASK && seq->mask == mask) {
         seq->flag |= SELECT;
         changed = true;
@@ -1403,8 +1394,7 @@ static bool select_grouped_effect(Editing *ed, Sequence *actseq, const int chann
     effects[i] = false;
   }
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     if (SEQ_CHANNEL_CHECK(seq, channel) && (seq->type & SEQ_TYPE_EFFECT) &&
         ELEM(actseq, seq->seq1, seq->seq2, seq->seq3)) {
       effects[seq->type] = true;
@@ -1412,8 +1402,7 @@ static bool select_grouped_effect(Editing *ed, Sequence *actseq, const int chann
   }
   SEQ_CURRENT_END;
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     if (SEQ_CHANNEL_CHECK(seq, channel) && effects[seq->type]) {
       if (seq->seq1) {
         seq->seq1->flag |= SELECT;
@@ -1437,8 +1426,7 @@ static bool select_grouped_time_overlap(Editing *ed, Sequence *actseq)
   Sequence *seq;
   bool changed = false;
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     if (seq->startdisp < actseq->enddisp && seq->enddisp > actseq->startdisp) {
       seq->flag |= SELECT;
       changed = true;
@@ -1459,8 +1447,7 @@ static bool select_grouped_effect_link(Editing *ed, Sequence *actseq, const int 
   int machine = actseq->machine;
   SeqIterator iter;
 
-  SEQ_CURRENT_BEGIN(ed, seq)
-  {
+  SEQ_CURRENT_BEGIN (ed, seq) {
     seq->tmp = NULL;
   }
   SEQ_CURRENT_END;
@@ -1536,8 +1523,7 @@ static int sequencer_select_grouped_exec(bContext *C, wmOperator *op)
   bool changed = false;
 
   if (!extend) {
-    SEQ_CURRENT_BEGIN(ed, seq)
-    {
+    SEQ_CURRENT_BEGIN (ed, seq) {
       seq->flag &= ~SELECT;
       changed = true;
     }

source/blender/makesrna/intern/rna_color.c
@@ -623,8 +623,7 @@ static void rna_ColorManagedColorspaceSettings_reload_update(Main *bmain,
       bool seq_found = false;
 
       if (&scene->sequencer_colorspace_settings != colorspace_settings) {
-        SEQ_ALL_BEGIN(scene->ed, seq)
-        {
+        SEQ_ALL_BEGIN (scene->ed, seq) {
           if (seq->strip && &seq->strip->colorspace_settings == colorspace_settings) {
             seq_found = true;
             break;
@@ -644,8 +643,7 @@ static void rna_ColorManagedColorspaceSettings_reload_update(Main *bmain,
         BKE_sequence_invalidate_cache_preprocessed(scene, seq);
       }
       else {
-        SEQ_ALL_BEGIN(scene->ed, seq)
-        {
+        SEQ_ALL_BEGIN (scene->ed, seq) {
           BKE_sequence_free_anim(seq);
         }
         SEQ_ALL_END;