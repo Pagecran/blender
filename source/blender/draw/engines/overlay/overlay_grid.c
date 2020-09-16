Pagecran/blender

commit a3fc3265691eea8d8871802a652871ff7a08b381

Use DrawManager for Image/UV Editor
This project moves the current UV/Image editor drawing to the draw manager.
Why would we do this:

**Performance**:

Current implementation would draw each texel per time. Multiple texels could be
drawn per pixel what would overwrite the previous result. You can notice this
when working with large textures. Repeat image drawing made this visible by
drawing for a small period of time and stop drawing the rest. Now the rendering
is fast and all repeated images are drawn.

**Alpha drawing**:

Current implementation would draw directly in display space. Giving incorrect
results when displaying alpha transparent images.
This addresses {T52680}, {T74709}, {T79518}
The image editor now can show emission only colors. See {D8234} for
examples.

**Current Limitations**

Using images that are larger than supported by your GPU are resized (eg larger
than 16000x16000 are resized to 8k). This leaves some blurring artifacts. It is
a low priority to add support back of displaying individual pixels of huge
images. There is a design task {T80113} with more detail.

**Implementation overview**
Introduced an Image Engine in the draw module. this engine is responsible for
drawing the texture in the main area of the UV/Image editor. The overlay engine
has a edit_uv overlay which is responsible to draw the UV's, shadows and
overlays specifically for the UV Image editor. The background + checker pattern
is drawn by the overlay_background.

The patch will allow us to share overlays between the 3d viewport and UV/Image
editor more easily. In most cases we just need to switch the `pos` with the `u`
attribute in the vertex shader.

The project can be activated in the user preferences as experimental features.
In a later commit this will be reversed.

Reviewed By: Clément Foucault

Differential Revision: https://developer.blender.org/D8234

source/blender/draw/engines/overlay/overlay_grid.c
@@ -26,6 +26,7 @@
 
 #include "DEG_depsgraph_query.h"
 
+#include "ED_image.h"
 #include "ED_view3d.h"
 
 #include "overlay_private.h"
@@ -42,14 +43,31 @@ enum {
   CLIP_ZNEG = (1 << 8),
   GRID_BACK = (1 << 9),
   GRID_CAMERA = (1 << 10),
+  PLANE_IMAGE = (1 << 11),
 };
 
 void OVERLAY_grid_init(OVERLAY_Data *vedata)
 {
   OVERLAY_PrivateData *pd = vedata->stl->pd;
   OVERLAY_ShadingData *shd = &pd->shdata;
-
   const DRWContextState *draw_ctx = DRW_context_state_get();
+
+  shd->grid_flag = 0;
+  shd->zneg_flag = 0;
+  shd->zpos_flag = 0;
+  shd->grid_line_size = max_ff(0.0f, U.pixelsize - 1.0f) * 0.5f;
+
+  if (pd->is_image_editor) {
+    SpaceImage *sima = (SpaceImage *)draw_ctx->space_data;
+    shd->grid_flag = ED_space_image_has_buffer(sima) ? 0 : PLANE_IMAGE | SHOW_GRID;
+    shd->grid_distance = 1.0f;
+    shd->grid_mesh_size = 1.0f;
+    for (int step = 0; step < 8; step++) {
+      shd->grid_steps[step] = powf(4, step) * (1.0f / 16.0f);
+    }
+    return;
+  }
+
   View3D *v3d = draw_ctx->v3d;
   Scene *scene = draw_ctx->scene;
   RegionView3D *rv3d = draw_ctx->rv3d;
@@ -59,10 +77,6 @@ void OVERLAY_grid_init(OVERLAY_Data *vedata)
   const bool show_axis_z = (pd->v3d_gridflag & V3D_SHOW_Z) != 0;
   const bool show_floor = (pd->v3d_gridflag & V3D_SHOW_FLOOR) != 0;
   const bool show_ortho_grid = (pd->v3d_gridflag & V3D_SHOW_ORTHO_GRID) != 0;
-
-  shd->grid_flag = 0;
-  shd->zneg_flag = 0;
-  shd->zpos_flag = 0;
 
   if (pd->hide_overlays || !(pd->v3d_gridflag & (V3D_SHOW_X | V3D_SHOW_Y | V3D_SHOW_Z |
                                                  V3D_SHOW_FLOOR | V3D_SHOW_ORTHO_GRID))) {
@@ -163,14 +177,16 @@ void OVERLAY_grid_init(OVERLAY_Data *vedata)
   }
 
   shd->grid_distance = dist / 2.0f;
-  shd->grid_line_size = max_ff(0.0f, U.pixelsize - 1.0f) * 0.5f;
 
   ED_view3d_grid_steps(scene, v3d, rv3d, shd->grid_steps);
 }
 
 void OVERLAY_grid_cache_init(OVERLAY_Data *vedata)
 {
-  OVERLAY_ShadingData *shd = &vedata->stl->pd->shdata;
+  OVERLAY_StorageList *stl = vedata->stl;
+  OVERLAY_PrivateData *pd = stl->pd;
+  OVERLAY_ShadingData *shd = &pd->shdata;
+
   OVERLAY_PassList *psl = vedata->psl;
   DefaultTextureList *dtxl = DRW_viewport_texture_list_get();
 
@@ -182,12 +198,29 @@ void OVERLAY_grid_cache_init(OVERLAY_Data *vedata)
 
   DRWState state = DRW_STATE_WRITE_COLOR | DRW_STATE_BLEND_ALPHA;
   DRW_PASS_CREATE(psl->grid_ps, state);
-
-  GPUShader *sh = OVERLAY_shader_grid();
+  DRWShadingGroup *grp;
+  GPUShader *sh;
   struct GPUBatch *geom = DRW_cache_grid_get();
 
+  if (pd->is_image_editor) {
+    /* add quad background */
+    sh = OVERLAY_shader_grid_image();
+    grp = DRW_shgroup_create(sh, psl->grid_ps);
+    DRW_shgroup_call(grp, DRW_cache_quad_get(), NULL);
+    float color_back[4];
+    interp_v4_v4v4(color_back, G_draw.block.colorBackground, G_draw.block.colorGrid, 0.5);
+    DRW_shgroup_uniform_vec4_copy(grp, "color", color_back);
+
+    /* add wire border */
+    grp = DRW_shgroup_create(sh, psl->grid_ps);
+    DRW_shgroup_call(grp, DRW_cache_quad_wires_get(), NULL);
+    DRW_shgroup_uniform_vec4_copy(grp, "color", G_draw.block.colorGrid);
+  }
+
+  sh = OVERLAY_shader_grid();
+
   /* Create 3 quads to render ordered transparency Z axis */
-  DRWShadingGroup *grp = DRW_shgroup_create(sh, psl->grid_ps);
+  grp = DRW_shgroup_create(sh, psl->grid_ps);
   DRW_shgroup_uniform_int(grp, "gridFlag", &shd->zneg_flag, 1);
   DRW_shgroup_uniform_vec3(grp, "planeAxes", shd->zplane_axes, 1);
   DRW_shgroup_uniform_float(grp, "gridDistance", &shd->grid_distance, 1);

source/blender/draw/engines/overlay/overlay_private.h
@@ -34,6 +34,9 @@ extern "C" {
 #  define USE_GEOM_SHADER_WORKAROUND 0
 #endif
 
+/* Needed for eSpaceImage_UVDT_Stretch */
+#include "DNA_space_types.h"
+
 typedef struct OVERLAY_FramebufferList {
   struct GPUFrameBuffer *overlay_default_fb;
   struct GPUFrameBuffer *overlay_line_fb;
@@ -54,6 +57,14 @@ typedef struct OVERLAY_TextureList {
 
 #define NOT_IN_FRONT 0
 #define IN_FRONT 1
+
+typedef enum OVERLAY_UVLineStyle {
+  OVERLAY_UV_LINE_STYLE_OUTLINE = 0,
+  OVERLAY_UV_LINE_STYLE_DASH = 1,
+  OVERLAY_UV_LINE_STYLE_BLACK = 2,
+  OVERLAY_UV_LINE_STYLE_WHITE = 3,
+  OVERLAY_UV_LINE_STYLE_SHADOW = 4,
+} OVERLAY_UVLineStyle;
 
 typedef struct OVERLAY_PassList {
   DRWPass *antialiasing_ps;
@@ -78,6 +89,11 @@ typedef struct OVERLAY_PassList {
   DRWPass *edit_text_overlay_ps;
   DRWPass *edit_text_darken_ps;
   DRWPass *edit_text_wire_ps[2];
+  DRWPass *edit_uv_edges_ps;
+  DRWPass *edit_uv_verts_ps;
+  DRWPass *edit_uv_faces_ps;
+  DRWPass *edit_uv_stretching_ps;
+  DRWPass *edit_uv_tiled_image_borders_ps;
   DRWPass *extra_ps[2];
   DRWPass *extra_blend_ps;
   DRWPass *extra_centers_ps;
@@ -244,6 +260,12 @@ typedef struct OVERLAY_PrivateData {
   DRWShadingGroup *edit_particle_point_grp;
   DRWShadingGroup *edit_text_overlay_grp;
   DRWShadingGroup *edit_text_wire_grp[2];
+  DRWShadingGroup *edit_uv_verts_grp;
+  DRWShadingGroup *edit_uv_edges_grp;
+  DRWShadingGroup *edit_uv_shadow_edges_grp;
+  DRWShadingGroup *edit_uv_faces_grp;
+  DRWShadingGroup *edit_uv_face_dots_grp;
+  DRWShadingGroup *edit_uv_stretching_grp;
   DRWShadingGroup *extra_grid_grp;
   DRWShadingGroup *facing_grp[2];
   DRWShadingGroup *motion_path_lines_grp;
@@ -286,6 +308,7 @@ typedef struct OVERLAY_PrivateData {
 
   View3DOverlay overlay;
   enum eContextObjectMode ctx_mode;
+  bool is_image_editor;
   bool clear_in_front;
   bool use_in_front;
   bool wireframe_mode;
@@ -324,6 +347,29 @@ typedef struct OVERLAY_PrivateData {
     bool use_weight;
     int select_mode;
   } edit_particle;
+  struct {
+    bool do_uv_overlay;
+    bool do_uv_shadow_overlay;
+    bool do_uv_stretching_overlay;
+    bool do_tiled_image_overlay;
+
+    bool do_faces;
+    bool do_face_dots;
+
+    float uv_opacity;
+    /* edge drawing */
+    OVERLAY_UVLineStyle line_style;
+    float dash_length;
+    int do_smooth_wire;
+
+    /* stretching overlay */
+    float aspect[2];
+    eSpaceImage_UVDT_Stretch draw_type;
+    ListBase totals;
+    float total_area_ratio;
+    float total_area_ratio_inv;
+
+  } edit_uv;
   struct {
     bool transparent;
     bool show_relations;
@@ -472,6 +518,11 @@ void OVERLAY_edit_particle_cache_init(OVERLAY_Data *vedata);
 void OVERLAY_edit_particle_cache_populate(OVERLAY_Data *vedata, Object *ob);
 void OVERLAY_edit_particle_draw(OVERLAY_Data *vedata);
 
+void OVERLAY_edit_uv_init(OVERLAY_Data *vedata);
+void OVERLAY_edit_uv_cache_init(OVERLAY_Data *vedata);
+void OVERLAY_edit_uv_cache_populate(OVERLAY_Data *vedata, Object *ob);
+void OVERLAY_edit_uv_draw(OVERLAY_Data *vedata);
+
 void OVERLAY_extra_cache_init(OVERLAY_Data *vedata);
 void OVERLAY_extra_cache_populate(OVERLAY_Data *vedata, Object *ob);
 void OVERLAY_extra_blend_draw(OVERLAY_Data *vedata);
@@ -570,6 +621,7 @@ void OVERLAY_wireframe_cache_populate(OVERLAY_Data *vedata,
 void OVERLAY_wireframe_draw(OVERLAY_Data *vedata);
 void OVERLAY_wireframe_in_front_draw(OVERLAY_Data *vedata);
 
+void OVERLAY_shader_library_ensure(void);
 GPUShader *OVERLAY_shader_antialiasing(void);
 GPUShader *OVERLAY_shader_armature_degrees_of_freedom_wire(void);
 GPUShader *OVERLAY_shader_armature_degrees_of_freedom_solid(void);
@@ -599,6 +651,13 @@ GPUShader *OVERLAY_shader_edit_mesh_skin_root(void);
 GPUShader *OVERLAY_shader_edit_mesh_vert(void);
 GPUShader *OVERLAY_shader_edit_particle_strand(void);
 GPUShader *OVERLAY_shader_edit_particle_point(void);
+GPUShader *OVERLAY_shader_edit_uv_edges_get(void);
+GPUShader *OVERLAY_shader_edit_uv_face_get(void);
+GPUShader *OVERLAY_shader_edit_uv_face_dots_get(void);
+GPUShader *OVERLAY_shader_edit_uv_verts_get(void);
+GPUShader *OVERLAY_shader_edit_uv_stretching_area_get(void);
+GPUShader *OVERLAY_shader_edit_uv_stretching_angle_get(void);
+GPUShader *OVERLAY_shader_edit_uv_tiled_image_borders_get(void);
 GPUShader *OVERLAY_shader_extra(bool is_select);
 GPUShader *OVERLAY_shader_extra_groundline(void);
 GPUShader *OVERLAY_shader_extra_wire(bool use_object, bool is_select);
@@ -607,6 +666,7 @@ GPUShader *OVERLAY_shader_extra_point(void);
 GPUShader *OVERLAY_shader_facing(void);
 GPUShader *OVERLAY_shader_gpencil_canvas(void);
 GPUShader *OVERLAY_shader_grid(void);
+GPUShader *OVERLAY_shader_grid_image(void);
 GPUShader *OVERLAY_shader_image(void);
 GPUShader *OVERLAY_shader_motion_path_line(void);
 GPUShader *OVERLAY_shader_motion_path_vert(void);

source/blender/draw/engines/overlay/shaders/grid_vert.glsl
@@ -12,7 +12,7 @@ uniform int gridFlag;
 #define PLANE_YZ (1 << 6)
 #define CLIP_Z_POS (1 << 7)
 #define CLIP_Z_NEG (1 << 8)
-
+#define PLANE_IMAGE (1 << 11)
 in vec3 pos;
 
 out vec3 local_pos;
@@ -28,8 +28,11 @@ void main()
   else if ((gridFlag & PLANE_XZ) != 0) {
     vert_pos = vec3(pos.x, 0.0, pos.y);
   }
-  else {
+  else if ((gridFlag & PLANE_YZ) != 0) {
     vert_pos = vec3(0.0, pos.x, pos.y);
+  }
+  else /* PLANE_IMAGE */ {
+    vert_pos = vec3(pos.xy * 0.5 + 0.5, 0.0);
   }
 
   local_pos = vert_pos;

release/datafiles/userdef/userdef_default_theme.c
@@ -714,6 +714,7 @@ const bTheme U_theme_default = {
     .paint_curve_pivot = RGBA(0xff7f7f7f),
     .paint_curve_handle = RGBA(0x7fff7f7f),
     .metadatatext = RGBA(0xffffffff),
+    .grid = RGBA(0x505050ff),
   },
   .space_text = {
     .back = RGBA(0x30303000),

release/scripts/startup/bl_ui/space_userpref.py
@@ -2176,6 +2176,7 @@ class USERPREF_PT_experimental_prototypes(ExperimentalPanel, Panel):
         self._draw_items(
             context, (
                 ({"property": "use_new_hair_type"}, "T68981"),
+                ({"property": "use_drw_image_editor"}, "T67530"),
             ),
         )
 

source/blender/blenkernel/intern/image_gpu.c
@@ -272,6 +272,13 @@ static GPUTexture *image_get_gpu_texture(Image *ima,
    * context and might as well ensure we have as much space free as possible. */
   gpu_free_unused_buffers();
 
+  /* Free GPU textures when requesting a different render pass/layer. */
+  if (ima->gpu_pass != iuser->pass || ima->gpu_layer != iuser->layer) {
+    ima->gpu_pass = iuser->pass;
+    ima->gpu_layer = iuser->layer;
+    ima->gpuflag |= IMA_GPU_REFRESH;
+  }
+
   /* currently, gpu refresh tagging is used by ima sequences */
   if (ima->gpuflag & IMA_GPU_REFRESH) {
     image_free_gpu(ima, true);
@@ -282,7 +289,10 @@ static GPUTexture *image_get_gpu_texture(Image *ima,
   BKE_image_tag_time(ima);
 
   /* Test if we already have a texture. */
-  GPUTexture **tex = get_image_gpu_texture_ptr(ima, textarget, iuser ? iuser->multiview_eye : 0);
+  const int current_view = iuser ? ((iuser->flag & IMA_SHOW_STEREO) != 0 ? iuser->multiview_eye :
+                                                                           iuser->view) :
+                                   0;
+  GPUTexture **tex = get_image_gpu_texture_ptr(ima, textarget, current_view);
   if (*tex) {
     return *tex;
   }

source/blender/blenloader/intern/versioning_userdef.c
@@ -233,6 +233,8 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
      * the outliner's, and it's less disruptive to just copy them. */
     copy_v4_v4_uchar(btheme->space_file.back, btheme->space_outliner.back);
     copy_v4_v4_uchar(btheme->space_file.row_alternate, btheme->space_outliner.row_alternate);
+
+    FROM_DEFAULT_V4_UCHAR(space_image.grid);
   }
 
 #undef FROM_DEFAULT_V4_UCHAR

source/blender/compositor/operations/COM_ViewerOperation.cpp
@@ -190,7 +190,7 @@ void ViewerOperation::updateImage(rcti *rect)
                                     rect->ymin,
                                     rect->xmax,
                                     rect->ymax);
-
+  this->m_image->gpuflag |= IMA_GPU_REFRESH;
   this->updateDraw();
 }
 

commit 083bd709118f3bfe4801e7b448f68e314fdbc7f4

Fix T80825: UV Editor UV Grid
Regression introduced by D8234.

source/blender/draw/engines/overlay/overlay_grid.c
@@ -29,6 +29,8 @@
 #include "ED_image.h"
 #include "ED_view3d.h"
 
+#include "UI_resources.h"
+
 #include "overlay_private.h"
 
 enum {
@@ -61,7 +63,8 @@ void OVERLAY_grid_init(OVERLAY_Data *vedata)
     SpaceImage *sima = (SpaceImage *)draw_ctx->space_data;
     shd->grid_flag = ED_space_image_has_buffer(sima) ? 0 : PLANE_IMAGE | SHOW_GRID;
     shd->grid_distance = 1.0f;
-    shd->grid_mesh_size = 1.0f;
+    copy_v3_fl3(
+        shd->grid_size, (float)sima->tile_grid_shape[0], (float)sima->tile_grid_shape[1], 1.0f);
     for (int step = 0; step < 8; step++) {
       shd->grid_steps[step] = powf(4, step) * (1.0f / 16.0f);
     }
@@ -169,11 +172,11 @@ void OVERLAY_grid_init(OVERLAY_Data *vedata)
   }
 
   if (winmat[3][3] == 0.0f) {
-    shd->grid_mesh_size = dist;
+    copy_v3_fl(shd->grid_size, dist);
   }
   else {
     float viewdist = 1.0f / min_ff(fabsf(winmat[0][0]), fabsf(winmat[1][1]));
-    shd->grid_mesh_size = viewdist * dist;
+    copy_v3_fl(shd->grid_size, viewdist * dist);
   }
 
   shd->grid_distance = dist / 2.0f;
@@ -203,18 +206,19 @@ void OVERLAY_grid_cache_init(OVERLAY_Data *vedata)
   struct GPUBatch *geom = DRW_cache_grid_get();
 
   if (pd->is_image_editor) {
+    float mat[4][4];
+
     /* add quad background */
     sh = OVERLAY_shader_grid_image();
     grp = DRW_shgroup_create(sh, psl->grid_ps);
-    DRW_shgroup_call(grp, DRW_cache_quad_get(), NULL);
     float color_back[4];
     interp_v4_v4v4(color_back, G_draw.block.colorBackground, G_draw.block.colorGrid, 0.5);
     DRW_shgroup_uniform_vec4_copy(grp, "color", color_back);
-
-    /* add wire border */
-    grp = DRW_shgroup_create(sh, psl->grid_ps);
-    DRW_shgroup_call(grp, DRW_cache_quad_wires_get(), NULL);
-    DRW_shgroup_uniform_vec4_copy(grp, "color", G_draw.block.colorGrid);
+    unit_m4(mat);
+    mat[0][0] = shd->grid_size[0];
+    mat[1][1] = shd->grid_size[1];
+    mat[2][2] = shd->grid_size[2];
+    DRW_shgroup_call_obmat(grp, DRW_cache_quad_get(), mat);
   }
 
   sh = OVERLAY_shader_grid();
@@ -225,7 +229,7 @@ void OVERLAY_grid_cache_init(OVERLAY_Data *vedata)
   DRW_shgroup_uniform_vec3(grp, "planeAxes", shd->zplane_axes, 1);
   DRW_shgroup_uniform_float(grp, "gridDistance", &shd->grid_distance, 1);
   DRW_shgroup_uniform_float_copy(grp, "lineKernel", shd->grid_line_size);
-  DRW_shgroup_uniform_float_copy(grp, "meshSize", shd->grid_mesh_size);
+  DRW_shgroup_uniform_vec3(grp, "gridSize", shd->grid_size, 1);
   DRW_shgroup_uniform_block(grp, "globalsBlock", G_draw.block_ubo);
   DRW_shgroup_uniform_texture_ref(grp, "depthBuffer", &dtxl->depth);
   if (shd->zneg_flag) {
@@ -249,6 +253,26 @@ void OVERLAY_grid_cache_init(OVERLAY_Data *vedata)
   DRW_shgroup_uniform_texture_ref(grp, "depthBuffer", &dtxl->depth);
   if (shd->zpos_flag) {
     DRW_shgroup_call(grp, geom, NULL);
+  }
+
+  if (pd->is_image_editor) {
+    float theme_color[4];
+    UI_GetThemeColorShade4fv(TH_BACK, 60, theme_color);
+    srgb_to_linearrgb_v4(theme_color, theme_color);
+
+    float mat[4][4];
+    /* add wire border */
+    sh = OVERLAY_shader_grid_image();
+    grp = DRW_shgroup_create(sh, psl->grid_ps);
+    DRW_shgroup_uniform_vec4_copy(grp, "color", theme_color);
+    unit_m4(mat);
+    for (int x = 0; x < shd->grid_size[0]; x++) {
+      mat[3][0] = x;
+      for (int y = 0; y < shd->grid_size[1]; y++) {
+        mat[3][1] = y;
+        DRW_shgroup_call_obmat(grp, DRW_cache_quad_wires_get(), mat);
+      }
+    }
   }
 }
 

source/blender/draw/engines/overlay/overlay_private.h
@@ -127,7 +127,7 @@ typedef struct OVERLAY_PassList {
 typedef struct OVERLAY_ShadingData {
   /** Grid */
   float grid_axes[3], grid_distance;
-  float zplane_axes[3], grid_mesh_size;
+  float zplane_axes[3], grid_size[3];
   float grid_steps[8];
   float inv_viewport_size[2];
   float grid_line_size;

source/blender/draw/engines/overlay/shaders/grid_frag.glsl
@@ -10,7 +10,7 @@ out vec4 FragColor;
 
 uniform vec3 planeAxes;
 uniform float gridDistance;
-uniform float meshSize;
+uniform vec3 gridSize;
 uniform float lineKernel = 0.0;
 uniform sampler2D depthBuffer;
 
@@ -73,7 +73,7 @@ vec3 get_axes(vec3 co, vec3 fwidthCos, float line_size)
 
 void main()
 {
-  vec3 wPos = local_pos * meshSize;
+  vec3 wPos = local_pos * gridSize;
   vec3 dFdxPos = dFdx(wPos);
   vec3 dFdyPos = dFdy(wPos);
   vec3 fwidthPos = abs(dFdxPos) + abs(dFdyPos);

source/blender/draw/engines/overlay/shaders/grid_vert.glsl
@@ -3,7 +3,7 @@
  * Clément Foucault */
 
 uniform vec3 planeAxes;
-uniform float meshSize;
+uniform vec3 gridSize;
 
 uniform int gridFlag;
 
@@ -37,7 +37,7 @@ void main()
 
   local_pos = vert_pos;
 
-  vec3 real_pos = cameraPos * planeAxes + vert_pos * meshSize;
+  vec3 real_pos = cameraPos * planeAxes + vert_pos * gridSize;
 
   /* Used for additional Z axis */
   if ((gridFlag & CLIP_Z_POS) != 0) {