Pagecran/blender

commit 127b5423d6203d521acb2b96b7de5534e8dbe79a

Another fix for bpy.props getters/setters: PyC_AsArray does a refcount decrement internally on errors, need to skip Py_DECREF in that case to avoid negative refcounts.

source/blender/python/intern/bpy_props.c
@@ -388,14 +388,17 @@ static void bpy_prop_boolean_array_get_cb(struct PointerRNA *ptr, struct Propert
 			values[i] = false;
 	}
 	else {
-		if (ret && PyC_AsArray(values, ret, len, &PyBool_Type, false, "BoolVectorProperty get") < 0) {
+		if (PyC_AsArray(values, ret, len, &PyBool_Type, false, "BoolVectorProperty get") < 0) {
 			printf_func_error(py_func);
 
 			for (i = 0; i < len; ++i)
 				values[i] = false;
-		}
 
-		Py_DECREF(ret);
+			/* PyC_AsArray decrements refcount internally on error */
+		}
+		else {
+			Py_DECREF(ret);
+		}
 	}
 
 	if (use_gil)
@@ -619,14 +622,17 @@ static void bpy_prop_int_array_get_cb(struct PointerRNA *ptr, struct PropertyRNA
 			values[i] = 0;
 	}
 	else {
-		if (ret && PyC_AsArray(values, ret, len, &PyLong_Type, false, "IntVectorProperty get") < 0) {
+		if (PyC_AsArray(values, ret, len, &PyLong_Type, false, "IntVectorProperty get") < 0) {
 			printf_func_error(py_func);
 
 			for (i = 0; i < len; ++i)
 				values[i] = 0;
-		}
 
-		Py_DECREF(ret);
+			/* PyC_AsArray decrements refcount internally on error */
+		}
+		else {
+			Py_DECREF(ret);
+		}
 	}
 
 	if (use_gil)
@@ -850,14 +856,17 @@ static void bpy_prop_float_array_get_cb(struct PointerRNA *ptr, struct PropertyR
 			values[i] = 0.0f;
 	}
 	else {
-		if (ret && PyC_AsArray(values, ret, len, &PyFloat_Type, false, "FloatVectorProperty get") < 0) {
+		if (PyC_AsArray(values, ret, len, &PyFloat_Type, false, "FloatVectorProperty get") < 0) {
 			printf_func_error(py_func);
 
 			for (i = 0; i < len; ++i)
 				values[i] = 0.0f;
-		}
 
-		Py_DECREF(ret);
+			/* PyC_AsArray decrements refcount internally on error */
+		}
+		else {
+			Py_DECREF(ret);
+		}
 	}
 
 	if (use_gil)

commit 401a9baf481af93aef362d71f05fca467e8dfa92

Fix memory leak with Python RNA property get callback errors
Failure to return a list of the expected size & type wasn't
decrementing the value, leaking a reference.

Caused by 127b5423d6203d521acb2b96b7de5534e8dbe79a a workaround for the
real error that was fixed f5e020a7a6ad6451fcaf075ae14f7014b8a4faea.

source/blender/python/intern/bpy_props.c
@@ -726,12 +726,8 @@ static void bpy_prop_boolean_array_get_fn(struct PointerRNA *ptr,
       for (i = 0; i < len; i++) {
         values[i] = false;
       }
-
-      /* PyC_AsArray decrements refcount internally on error */
     }
-    else {
-      Py_DECREF(ret);
-    }
+    Py_DECREF(ret);
   }
 
   if (use_gil) {
@@ -977,12 +973,8 @@ static void bpy_prop_int_array_get_fn(struct PointerRNA *ptr,
       for (i = 0; i < len; i++) {
         values[i] = 0;
       }
-
-      /* PyC_AsArray decrements refcount internally on error */
     }
-    else {
-      Py_DECREF(ret);
-    }
+    Py_DECREF(ret);
   }
 
   if (use_gil) {
@@ -1228,12 +1220,8 @@ static void bpy_prop_float_array_get_fn(struct PointerRNA *ptr,
       for (i = 0; i < len; i++) {
         values[i] = 0.0f;
       }
-
-      /* PyC_AsArray decrements refcount internally on error */
     }
-    else {
-      Py_DECREF(ret);
-    }
+    Py_DECREF(ret);
   }
 
   if (use_gil) {