Pagecran/blender

commit 4d7274b7f43e572e3d63ec1aac47646415beeea0

Cleanup: Use C++ types for crazyspace deform storage
Use float3, float3x3, and Array for data used for mesh crazyspace
calculation. Propagate the change wherever necessary to not add
more casting to the old C types.

Because `ObjectRuntime` (and therefore `DEGObjectIterData`) became
non-trivial structs, the code that swaps iterators for RNA depsgraph
object iteration had to be changed a bit to be more friendly to C++
memory semantics.

Pull Request: https://projects.blender.org/blender/blender/pulls/114998

source/blender/makesrna/intern/rna_depsgraph.cc
@@ -6,6 +6,7 @@
  * \ingroup RNA
  */
 
+#include <array>
 #include <cstdlib>
 
 #include "BLI_math_matrix.h"
@@ -312,8 +313,7 @@ static void rna_Depsgraph_update(Depsgraph *depsgraph, Main *bmain, ReportList *
 static void rna_Depsgraph_objects_begin(CollectionPropertyIterator *iter, PointerRNA *ptr)
 {
   iter->internal.custom = MEM_callocN(sizeof(BLI_Iterator), __func__);
-  DEGObjectIterData *data = static_cast<DEGObjectIterData *>(
-      MEM_callocN(sizeof(DEGObjectIterData), __func__));
+  DEGObjectIterData *data = MEM_new<DEGObjectIterData>(__func__);
   DEGObjectIterSettings *deg_iter_settings = static_cast<DEGObjectIterSettings *>(
       MEM_callocN(sizeof(DEGObjectIterSettings), __func__));
   deg_iter_settings->depsgraph = (Depsgraph *)ptr->data;
@@ -341,7 +341,7 @@ static void rna_Depsgraph_objects_end(CollectionPropertyIterator *iter)
   DEG_iterator_objects_end(static_cast<BLI_Iterator *>(iter->internal.custom));
   MEM_freeN(data->settings);
   MEM_freeN(((BLI_Iterator *)iter->internal.custom)->data);
-  MEM_freeN(iter->internal.custom);
+  MEM_delete(data);
 }
 
 static PointerRNA rna_Depsgraph_objects_get(CollectionPropertyIterator *iter)
@@ -368,8 +368,7 @@ struct RNA_Depsgraph_Instances_Iterator {
 
 static void rna_Depsgraph_object_instances_begin(CollectionPropertyIterator *iter, PointerRNA *ptr)
 {
-  RNA_Depsgraph_Instances_Iterator *di_it = static_cast<RNA_Depsgraph_Instances_Iterator *>(
-      MEM_callocN(sizeof(*di_it), __func__));
+  RNA_Depsgraph_Instances_Iterator *di_it = MEM_new<RNA_Depsgraph_Instances_Iterator>(__func__);
   iter->internal.custom = di_it;
   DEGObjectIterSettings *deg_iter_settings = static_cast<DEGObjectIterSettings *>(
       MEM_callocN(sizeof(DEGObjectIterSettings), __func__));
@@ -395,9 +394,7 @@ static void rna_Depsgraph_object_instances_next(CollectionPropertyIterator *iter
 
   /* We need to copy current iterator status to next one being worked on. */
   di_it->iterators[(di_it->counter + 1) % 2].iter = di_it->iterators[di_it->counter % 2].iter;
-  memcpy(static_cast<void *>(&di_it->deg_data[(di_it->counter + 1) % 2]),
-         static_cast<void *>(&di_it->deg_data[di_it->counter % 2]),
-         sizeof(DEGObjectIterData));
+  di_it->deg_data[(di_it->counter + 1) % 2] = di_it->deg_data[di_it->counter % 2];
   di_it->counter++;
 
   di_it->iterators[di_it->counter % 2].iter.data = &di_it->deg_data[di_it->counter % 2];
@@ -436,7 +433,7 @@ static void rna_Depsgraph_object_instances_end(CollectionPropertyIterator *iter)
 #  endif
   }
 
-  MEM_freeN(di_it);
+  MEM_delete(di_it);
 }
 
 static PointerRNA rna_Depsgraph_object_instances_get(CollectionPropertyIterator *iter)

source/blender/blenkernel/BKE_DerivedMesh.hh
@@ -48,6 +48,8 @@
  */
 
 #include "BLI_compiler_attrs.h"
+#include "BLI_math_vector_types.hh"
+#include "BLI_span.hh"
 
 #include "DNA_customdata_types.h"
 
@@ -223,7 +225,7 @@ void DM_interp_vert_data(const DerivedMesh *source,
                          int count,
                          int dest_index);
 
-void mesh_get_mapped_verts_coords(Mesh *me_eval, float (*r_cos)[3], int totcos);
+void mesh_get_mapped_verts_coords(Mesh *me_eval, blender::MutableSpan<blender::float3> r_cos);
 
 /**
  * Same as above but won't use render settings.

source/blender/blenkernel/BKE_crazyspace.hh
@@ -8,6 +8,7 @@
 
 #pragma once
 
+#include "BLI_array.hh"
 #include "BLI_math_matrix.hh"
 #include "BLI_span.hh"
 
@@ -69,36 +70,38 @@ GeometryDeformation get_evaluated_grease_pencil_drawing_deformation(const Depsgr
 /**
  * Disable subdivision-surface temporal, get mapped coordinates, and enable it.
  */
-float (*BKE_crazyspace_get_mapped_editverts(Depsgraph *depsgraph, Object *obedit))[3];
+blender::Array<blender::float3> BKE_crazyspace_get_mapped_editverts(Depsgraph *depsgraph,
+                                                                    Object *obedit);
 void BKE_crazyspace_set_quats_editmesh(BMEditMesh *em,
-                                       float (*origcos)[3],
-                                       float (*mappedcos)[3],
+                                       blender::Span<blender::float3> origcos,
+                                       blender::Span<blender::float3> mappedcos,
                                        float (*quats)[4],
                                        bool use_select);
 void BKE_crazyspace_set_quats_mesh(Mesh *me,
-                                   float (*origcos)[3],
-                                   float (*mappedcos)[3],
+                                   blender::Span<blender::float3> origcos,
+                                   blender::Span<blender::float3> mappedcos,
                                    float (*quats)[4]);
 /**
  * Returns an array of deform matrices for crazy-space correction,
  * and the number of modifiers left.
  */
-int BKE_crazyspace_get_first_deform_matrices_editbmesh(Depsgraph *depsgraph,
-                                                       Scene *,
-                                                       Object *,
-                                                       BMEditMesh *em,
-                                                       float (**deformmats)[3][3],
-                                                       float (**deformcos)[3]);
+int BKE_crazyspace_get_first_deform_matrices_editbmesh(
+    Depsgraph *depsgraph,
+    Scene *,
+    Object *,
+    BMEditMesh *em,
+    blender::Array<blender::float3x3, 0> &deformmats,
+    blender::Array<blender::float3, 0> &deformcos);
 int BKE_sculpt_get_first_deform_matrices(Depsgraph *depsgraph,
                                          Scene *scene,
                                          Object *ob,
-                                         float (**deformmats)[3][3],
-                                         float (**deformcos)[3]);
+                                         blender::Array<blender::float3x3, 0> &deformmats,
+                                         blender::Array<blender::float3, 0> &deformcos);
 void BKE_crazyspace_build_sculpt(Depsgraph *depsgraph,
                                  Scene *scene,
                                  Object *ob,
-                                 float (**deformmats)[3][3],
-                                 float (**deformcos)[3]);
+                                 blender::Array<blender::float3x3, 0> &deformmats,
+                                 blender::Array<blender::float3, 0> &deformcos);
 
 /* -------------------------------------------------------------------- */
 /** \name Crazy-Space API

source/blender/blenkernel/BKE_mesh_fair.hh
@@ -10,6 +10,8 @@
  * https://github.com/fedackb/mesh-fairing.
  */
 
+#include "BLI_math_vector_types.hh"
+#include "BLI_span.hh"
 #include "BLI_utildefines.h"
 
 struct BMesh;
@@ -35,6 +37,6 @@ void BKE_bmesh_prefair_and_fair_verts(BMesh *bm, bool *affect_verts, eMeshFairin
  * fairing result. When NULL, the function will use mesh positions directly.
  */
 void BKE_mesh_prefair_and_fair_verts(Mesh *mesh,
-                                     float (*deform_vert_positions)[3],
+                                     blender::MutableSpan<blender::float3> deform_vert_positions,
                                      bool *affect_verts,
                                      eMeshFairingDepth depth);

source/blender/blenkernel/BKE_mesh_wrapper.hh
@@ -7,6 +7,9 @@
  * \ingroup bke
  */
 
+#include "BLI_math_vector_types.hh"
+#include "BLI_span.hh"
+
 struct BMEditMesh;
 struct CustomData_MeshMasks;
 struct Mesh;
@@ -37,8 +40,7 @@ const float (*BKE_mesh_wrapper_face_normals(Mesh *mesh))[3];
 void BKE_mesh_wrapper_tag_positions_changed(Mesh *mesh);
 
 void BKE_mesh_wrapper_vert_coords_copy(const Mesh *me,
-                                       float (*vert_coords)[3],
-                                       int vert_coords_len);
+                                       blender::MutableSpan<blender::float3> positions);
 void BKE_mesh_wrapper_vert_coords_copy_with_mat4(const Mesh *me,
                                                  float (*vert_coords)[3],
                                                  int vert_coords_len,

source/blender/blenkernel/BKE_object_types.hh
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include "BLI_array.hh"
+#include "BLI_math_matrix_types.hh"
 #include "BLI_math_vector_types.hh"
 
 #include "DNA_customdata_types.h" /* #CustomData_MeshMasks. */
@@ -25,43 +27,43 @@ struct ObjectRuntime {
    * The custom data layer mask that was last used
    * to calculate data_eval and mesh_deform_eval.
    */
-  CustomData_MeshMasks last_data_mask;
+  CustomData_MeshMasks last_data_mask = {};
 
   /** Did last modifier stack generation need mapping support? */
-  char last_need_mapping;
+  char last_need_mapping = false;
 
   /** Only used for drawing the parent/child help-line. */
-  float parent_display_origin[3];
+  float3 parent_display_origin;
 
   /**
    * Selection id of this object. It might differ between an evaluated and its original object,
    * when the object is being instanced.
    */
-  int select_id;
+  int select_id = -1;
 
   /**
    * Denotes whether the evaluated data is owned by this object or is referenced and owned by
    * somebody else.
    */
-  char is_data_eval_owned;
+  char is_data_eval_owned = false;
 
   /** Start time of the mode transfer overlay animation. */
-  double overlay_mode_transfer_start_time;
+  double overlay_mode_transfer_start_time = 0.0f;
 
   /** Axis aligned bound-box (in local-space). */
-  BoundBox *bb;
+  BoundBox *bb = nullptr;
 
   /**
    * Original data pointer, before object->data was changed to point
    * to data_eval.
    * Is assigned by dependency graph's copy-on-write evaluation.
    */
-  ID *data_orig;
+  ID *data_orig = nullptr;
   /**
    * Object data structure created during object evaluation. It has all modifiers applied.
    * The type is determined by the type of the original object.
    */
-  ID *data_eval;
+  ID *data_eval = nullptr;
 
   /**
    * Objects can evaluate to a geometry set instead of a single ID. In those cases, the evaluated
@@ -69,34 +71,34 @@ struct ObjectRuntime {
    * #geometry_set_eval might reference the ID pointed to by #data_eval as well, but does not own
    * the data.
    */
-  GeometrySet *geometry_set_eval;
+  GeometrySet *geometry_set_eval = nullptr;
 
   /**
    * Mesh structure created during object evaluation.
    * It has deformation only modifiers applied on it.
    */
-  Mesh *mesh_deform_eval;
+  Mesh *mesh_deform_eval = nullptr;
 
   /* Evaluated mesh cage in edit mode. */
-  Mesh *editmesh_eval_cage;
+  Mesh *editmesh_eval_cage = nullptr;
 
   /**
    * Original grease pencil bGPdata pointer, before object->data was changed to point
    * to gpd_eval.
    * Is assigned by dependency graph's copy-on-write evaluation.
    */
-  bGPdata *gpd_orig;
+  bGPdata *gpd_orig = nullptr;
   /**
    * bGPdata structure created during object evaluation.
    * It has all modifiers applied.
    */
-  bGPdata *gpd_eval;
+  bGPdata *gpd_eval = nullptr;
 
   /**
    * This is a mesh representation of corresponding object.
    * It created when Python calls `object.to_mesh()`.
    */
-  Mesh *object_as_temp_mesh;
+  Mesh *object_as_temp_mesh = nullptr;
 
   /**
    * Backup of the object's pose (might be a subset, i.e. not contain all bones).
@@ -104,22 +106,21 @@ struct ObjectRuntime {
    * Created by `BKE_pose_backup_create_on_object()`. This memory is owned by the Object.
    * It is freed along with the object, or when `BKE_pose_backup_clear()` is called.
    */
-  PoseBackup *pose_backup;
+  PoseBackup *pose_backup = nullptr;
 
   /**
    * This is a curve representation of corresponding object.
    * It created when Python calls `object.to_curve()`.
    */
-  ::Curve *object_as_temp_curve;
+  ::Curve *object_as_temp_curve = nullptr;
 
   /** Runtime evaluated curve-specific data, not stored in the file. */
-  CurveCache *curve_cache;
+  CurveCache *curve_cache = nullptr;
 
-  unsigned short local_collections_bits;
+  unsigned short local_collections_bits = 0;
 
-  float (*crazyspace_deform_imats)[3][3];
-  float (*crazyspace_deform_cos)[3];
-  int crazyspace_verts_num;
+  Array<float3x3, 0> crazyspace_deform_imats;
+  Array<float3, 0> crazyspace_deform_cos;
 };
 
 }  // namespace blender::bke

source/blender/blenkernel/BKE_paint.hh
@@ -11,6 +11,7 @@
 #include "BLI_array.hh"
 #include "BLI_bitmap.h"
 #include "BLI_compiler_compat.h"
+#include "BLI_math_matrix_types.hh"
 #include "BLI_math_vector_types.hh"
 #include "BLI_offset_indices.hh"
 #include "BLI_ordered_edge.hh"
@@ -625,11 +626,14 @@ struct SculptSession {
   /* PBVH acceleration structure */
   PBVH *pbvh;
 
-  /* Painting on deformed mesh */
-  bool deform_modifiers_active; /* Object is deformed with some modifiers. */
-  float (*orig_cos)[3];         /* Coords of un-deformed mesh. */
-  float (*deform_cos)[3];       /* Coords of deformed mesh but without stroke displacement. */
-  float (*deform_imats)[3][3];  /* Crazy-space deformation matrices. */
+  /* Object is deformed with some modifiers. */
+  bool deform_modifiers_active;
+  /* Coords of un-deformed mesh. */
+  blender::Array<blender::float3> orig_cos;
+  /* Coords of deformed mesh but without stroke displacement. */
+  blender::Array<blender::float3, 0> deform_cos;
+  /* Crazy-space deformation matrices. */
+  blender::Array<blender::float3x3, 0> deform_imats;
 
   /* Pool for texture evaluations. */
   ImagePool *tex_pool;

source/blender/blenkernel/BKE_pbvh_api.hh
@@ -474,7 +474,7 @@ void BKE_pbvh_update_hide_attributes_from_mesh(PBVH *pbvh);
 
 /* Vertex Deformer. */
 
-void BKE_pbvh_vert_coords_apply(PBVH *pbvh, const float (*vertCos)[3], int totvert);
+void BKE_pbvh_vert_coords_apply(PBVH *pbvh, blender::Span<blender::float3> vert_positions);
 bool BKE_pbvh_is_deformed(PBVH *pbvh);
 
 /* Vertex Iterator. */
@@ -638,7 +638,7 @@ void BKE_pbvh_parallel_range_settings(TaskParallelSettings *settings,
                                       bool use_threading,
                                       int totnode);
 
-float (*BKE_pbvh_get_vert_positions(const PBVH *pbvh))[3];
+blender::MutableSpan<blender::float3> BKE_pbvh_get_vert_positions(const PBVH *pbvh);
 const float (*BKE_pbvh_get_vert_normals(const PBVH *pbvh))[3];
 const bool *BKE_pbvh_get_vert_hide(const PBVH *pbvh);
 bool *BKE_pbvh_get_vert_hide_for_write(PBVH *pbvh);

commit 3fc4ca18db8e5b75039db0fa99bb7e6c7da0296e

Fix: Incorrect freeing in depsgraph RNA objects iterator
Caused by 4d7274b7f43e572e3d63ec1aac47646415beeea0.
The depsgraph iterator was freed twice, and the "custom"
iterator wasn't freed at all. Thanks to Jesse for investigating this.

source/blender/makesrna/intern/rna_depsgraph.cc
@@ -340,8 +340,8 @@ static void rna_Depsgraph_objects_end(CollectionPropertyIterator *iter)
   DEGObjectIterData *data = (DEGObjectIterData *)((BLI_Iterator *)iter->internal.custom)->data;
   DEG_iterator_objects_end(static_cast<BLI_Iterator *>(iter->internal.custom));
   MEM_freeN(data->settings);
-  MEM_freeN(((BLI_Iterator *)iter->internal.custom)->data);
   MEM_delete(data);
+  MEM_freeN(iter->internal.custom);
 }
 
 static PointerRNA rna_Depsgraph_objects_get(CollectionPropertyIterator *iter)