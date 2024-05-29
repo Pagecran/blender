Pagecran/blender

commit 32259aa0b6e080230de6ce5bf3bef9956a045d6e

Fix: EEVEE-Next: Versioning older file to remove indirect volume light
Make sure older file have indirect volume lighting disabled to
avoid this new feature washing out the volume object lighting.

source/blender/blenloader/intern/versioning_400.cc
@@ -3334,6 +3334,9 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
 
     LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
       scene->eevee.clamp_surface_indirect = 10.0f;
+      /* Make contribution of indirect lighting very small (but non-null) to avoid world lighting
+       * and volume lightprobe changing the appearance of volume objects. */
+      scene->eevee.clamp_surface_indirect = 1e-8f;
     }
   }
 

commit 3f965492a7a1283ec1dfefc71b857b9b8271828d

Fix: EEVEE-Next: Wrong variable in versioning code
Caused by 32259aa0b6e080230de6ce5bf3bef9956a045d6e

source/blender/blenloader/intern/versioning_400.cc
@@ -3336,7 +3336,7 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
       scene->eevee.clamp_surface_indirect = 10.0f;
       /* Make contribution of indirect lighting very small (but non-null) to avoid world lighting
        * and volume lightprobe changing the appearance of volume objects. */
-      scene->eevee.clamp_surface_indirect = 1e-8f;
+      scene->eevee.clamp_volume_indirect = 1e-8f;
     }
   }
 