Pagecran/blender

commit b295df73456e6f8bba874b8f6985d7167d268438

Refactor: Overlay: Port "next" shaders declarations to static CreateInfos
Use static CreateInfos for Overlay-Next shaders using a similar approach to Workbench shader variations.

Remove unused infos and shader sources.
Remove the `gpu_shader_create_info_get_unfinalized_copy` workaround.

Pull Request: https://projects.blender.org/blender/blender/pulls/131514

source/blender/draw/engines/overlay/shaders/overlay_wireframe_frag.glsl
@@ -32,7 +32,7 @@ void main()
   fragColor = vec4(mix(final_front_col, rim_col, saturate(fac)), 1.0);
   fragColor *= fragColor;
 
-#elif !defined(SELECT_EDGES)
+#elif defined(SELECT_ENABLE)
   lineOutput = pack_line_data(gl_FragCoord.xy, edgeStart, edgePos);
   fragColor = finalColor;
 

commit 0e27017775d211a4c9afca79af53916f5635e1de

Fix #131846: Overlay: Wireframe missing
Regression from b295df7345
Fix fragment/selection logic

source/blender/draw/engines/overlay/shaders/overlay_wireframe_frag.glsl
@@ -32,7 +32,7 @@ void main()
   fragColor = vec4(mix(final_front_col, rim_col, saturate(fac)), 1.0);
   fragColor *= fragColor;
 
-#elif defined(SELECT_ENABLE)
+#elif !defined(SELECT_ENABLE)
   lineOutput = pack_line_data(gl_FragCoord.xy, edgeStart, edgePos);
   fragColor = finalColor;
 