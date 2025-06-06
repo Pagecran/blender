Pagecran/blender

commit 582c7d94b8fbfd2720e95fd89dc7b882175400ef

Paint: Scale brush size in preparation for planned 5.0 changes
This commit, in conjunction with !139561, is used to ensure consistent
values for the brush size while we transition it from representing
radius to diameter.

Related to #134204

Pull Request: https://projects.blender.org/blender/blender/pulls/139813

source/blender/blenkernel/intern/brush.cc
@@ -210,6 +210,11 @@ static void brush_foreach_id(ID *id, LibraryForeachIDData *data)
 static void brush_blend_write(BlendWriter *writer, ID *id, const void *id_address)
 {
   Brush *brush = reinterpret_cast<Brush *>(id);
+  /* In 5.0 we intend to change the brush.size value from representing radius to representing
+   * diameter. This and the corresponding code in `brush_blend_read_data` should be removed once
+   * that transition is complete. */
+  brush->size *= 2;
+  brush->unprojected_radius *= 2.0;
 
   BLO_write_id_struct(writer, Brush, id_address, &brush->id);
   BKE_id_blend_write(writer, &brush->id);

source/blender/blenkernel/intern/scene.cc
@@ -1016,6 +1016,12 @@ static void scene_blend_write(BlendWriter *writer, ID *id, const void *id_addres
   ToolSettings *tos = sce->toolsettings;
   BLO_write_struct(writer, ToolSettings, tos);
 
+  /* In 5.0 we intend to change the brush.size value from representing radius to representing
+   * diameter. This and the corresponding code in `brush_blend_read_data` should be removed once
+   * that transition is complete. */
+  tos->unified_paint_settings.size *= 2;
+  tos->unified_paint_settings.unprojected_radius *= 2.0f;
+
   if (tos->unified_paint_settings.curve_rand_hue) {
     BKE_curvemapping_blend_write(writer, tos->unified_paint_settings.curve_rand_hue);
   }

commit 2643213c53b48d8d44dd8932c5609981ba39a0ea

Fix #139913: Brush size doubles each time you draw
In `brush_blend_write` and `scene_blend_write` the brush/tool settings
brush size are doubled for 5.0 to store diameter instead of radius, it
should be divided back to original value after writing (for undo),
otherwise your will see your brush size doubles everytime an undo push
is involved.

The doubling was caused by 582c7d94b8fbfd2, now it should not
happen.

Pull Request: https://projects.blender.org/blender/blender/pulls/139914

source/blender/blenkernel/intern/brush.cc
@@ -212,7 +212,10 @@ static void brush_blend_write(BlendWriter *writer, ID *id, const void *id_addres
   Brush *brush = reinterpret_cast<Brush *>(id);
   /* In 5.0 we intend to change the brush.size value from representing radius to representing
    * diameter. This and the corresponding code in `brush_blend_read_data` should be removed once
-   * that transition is complete. */
+   * that transition is complete. Note that we do not need to restore these values, because `id`
+   * is a shallow copy of the original, but any child data that's owned by the id is not copied,
+   * which means for `scene_blend_write` where it writes brush size from `tool_settings`, that
+   * value will need to be restored. See `scene_blend_write` from `blenkernel/intern/scene.cc`. */
   brush->size *= 2;
   brush->unprojected_radius *= 2.0;
 

source/blender/blenkernel/intern/scene.cc
@@ -1178,6 +1178,14 @@ static void scene_blend_write(BlendWriter *writer, ID *id, const void *id_addres
 
   /* Freed on `do_versions()`. */
   BLI_assert(sce->layer_properties == nullptr);
+
+  /* Restore original values after writing, so current working file is not affected. Note that this
+   * is only needed because scene `id` is a shallow copy here, and `tool_settings` is not copied.
+   * In the case of `brush_blend_write`, the size value does not need restoring after writing
+   * because that one was directly under `Brush` id which is copied. See `brush_blend_write` in
+   * `blenkernel/intern/brush.cc`. */
+  tos->unified_paint_settings.size /= 2;
+  tos->unified_paint_settings.unprojected_radius /= 2.0f;
 }
 
 static void direct_link_paint_helper(BlendDataReader *reader, const Scene *scene, Paint **paint)