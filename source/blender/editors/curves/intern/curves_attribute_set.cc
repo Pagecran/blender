Pagecran/blender

commit 1f92fd75771ea0c8fe98d3c40161e41e198d2d16

Refactor: Use AttrType instead of CustomData type in attribute API
Change `eCustomDataType` to `bke::AttrType` for uses of the attribute
API (the `AttributeAccessor` one anyway). I didn't touch any values that
might be saved in files; those should be handled on a case by case basis.

Part of #122398

Pull Request: https://projects.blender.org/blender/blender/pulls/141301

source/blender/editors/curves/intern/curves_attribute_set.cc
@@ -97,8 +97,8 @@ static wmOperatorStatus set_attribute_exec(bContext *C, wmOperator *op)
   const StringRef name = *BKE_attributes_active_name_get(active_owner);
   const bke::AttributeMetaData active_meta_data =
       *active_curves_id.geometry.wrap().attributes().lookup_meta_data(name);
-  const eCustomDataType active_type = active_meta_data.data_type;
-  const CPPType &type = *bke::custom_data_type_to_cpp_type(active_type);
+  const bke::AttrType active_type = active_meta_data.data_type;
+  const CPPType &type = bke::attribute_type_to_cpp_type(active_type);
 
   BUFFER_FOR_CPP_TYPE_VALUE(type, buffer);
   BLI_SCOPED_DEFER([&]() { type.destruct(buffer); });
@@ -162,7 +162,7 @@ static wmOperatorStatus set_attribute_invoke(bContext *C, wmOperator *op, const 
   const CPPType &type = attribute.varray.type();
 
   PropertyRNA *prop = geometry::rna_property_for_type(*op->ptr,
-                                                      bke::cpp_type_to_custom_data_type(type));
+                                                      bke::cpp_type_to_attribute_type(type));
   if (RNA_property_is_set(op->ptr, prop)) {
     return WM_operator_props_popup(C, op, event);
   }

source/blender/editors/mesh/editmesh_attribute.cc
@@ -9,6 +9,7 @@
 #include "BLI_generic_pointer.hh"
 
 #include "BKE_attribute.hh"
+#include "BKE_attribute_legacy_convert.hh"
 #include "BKE_context.hh"
 #include "BKE_editmesh.hh"
 #include "BKE_layer.hh"
@@ -142,8 +143,9 @@ static wmOperatorStatus mesh_set_attribute_exec(bContext *C, wmOperator *op)
   const StringRef name = *BKE_attributes_active_name_get(active_owner);
   CustomDataLayer *active_layer = BKE_attribute_search_for_write(
       active_owner, name, CD_MASK_PROP_ALL, ATTR_DOMAIN_MASK_ALL);
-  const eCustomDataType active_type = eCustomDataType(active_layer->type);
-  const CPPType &type = *bke::custom_data_type_to_cpp_type(active_type);
+  const bke::AttrType active_type = *bke::custom_data_type_to_attr_type(
+      eCustomDataType(active_layer->type));
+  const CPPType &type = bke::attribute_type_to_cpp_type(active_type);
 
   BUFFER_FOR_CPP_TYPE_VALUE(type, buffer);
   BLI_SCOPED_DEFER([&]() { type.destruct(buffer); });
@@ -217,7 +219,8 @@ static wmOperatorStatus mesh_set_attribute_invoke(bContext *C,
   const StringRef name = *BKE_attributes_active_name_get(owner);
   CustomDataLayer *layer = BKE_attribute_search_for_write(
       owner, name, CD_MASK_PROP_ALL, ATTR_DOMAIN_MASK_ALL);
-  const eCustomDataType data_type = eCustomDataType(layer->type);
+  const bke::AttrType data_type = *bke::custom_data_type_to_attr_type(
+      eCustomDataType(layer->type));
   const bke::AttrDomain domain = BKE_attribute_domain(owner, layer);
   const BMElem *active_elem = BM_mesh_active_elem_get(bm);
   if (!active_elem) {
@@ -230,7 +233,7 @@ static wmOperatorStatus mesh_set_attribute_invoke(bContext *C,
     return WM_operator_props_popup(C, op, event);
   }
 
-  const CPPType &type = *bke::custom_data_type_to_cpp_type(data_type);
+  const CPPType &type = bke::attribute_type_to_cpp_type(data_type);
   const GPointer active_value(type, POINTER_OFFSET(active_elem->head.data, layer->offset));
 
   PropertyRNA *prop = geometry::rna_property_for_type(*op->ptr, data_type);
@@ -252,7 +255,7 @@ static void mesh_set_attribute_ui(bContext *C, wmOperator *op)
   const StringRef name = *BKE_attributes_active_name_get(owner);
   CustomDataLayer *layer = BKE_attribute_search_for_write(
       owner, name, CD_MASK_PROP_ALL, ATTR_DOMAIN_MASK_ALL);
-  const eCustomDataType active_type = eCustomDataType(layer->type);
+  const bke::AttrType active_type = bke::AttrType(layer->type);
   const StringRefNull prop_name = geometry::rna_property_name_for_type(active_type);
   layout->prop(op->ptr, prop_name, UI_ITEM_NONE, name, ICON_NONE);
 }

source/blender/editors/pointcloud/intern/attribute_set.cc
@@ -84,8 +84,8 @@ static wmOperatorStatus set_attribute_exec(bContext *C, wmOperator *op)
   AttributeOwner active_owner = AttributeOwner::from_id(&active_pointcloud.id);
   const StringRef name = *BKE_attributes_active_name_get(active_owner);
   const bke::AttributeMetaData meta_data = *active_pointcloud.attributes().lookup_meta_data(name);
-  const eCustomDataType active_type = meta_data.data_type;
-  const CPPType &type = *bke::custom_data_type_to_cpp_type(active_type);
+  const bke::AttrType active_type = meta_data.data_type;
+  const CPPType &type = bke::attribute_type_to_cpp_type(active_type);
 
   BUFFER_FOR_CPP_TYPE_VALUE(type, buffer);
   BLI_SCOPED_DEFER([&]() { type.destruct(buffer); });
@@ -146,7 +146,7 @@ static wmOperatorStatus set_attribute_invoke(bContext *C, wmOperator *op, const 
   const CPPType &type = attribute.varray.type();
 
   PropertyRNA *prop = geometry::rna_property_for_type(*op->ptr,
-                                                      bke::cpp_type_to_custom_data_type(type));
+                                                      bke::cpp_type_to_attribute_type(type));
   if (RNA_property_is_set(op->ptr, prop)) {
     return WM_operator_props_popup(C, op, event);
   }
@@ -179,7 +179,7 @@ static void set_attribute_ui(bContext *C, wmOperator *op)
   AttributeOwner owner = AttributeOwner::from_id(&pointcloud.id);
   const StringRef name = *BKE_attributes_active_name_get(owner);
   const bke::AttributeMetaData meta_data = *pointcloud.attributes().lookup_meta_data(name);
-  const eCustomDataType active_type = eCustomDataType(meta_data.data_type);
+  const bke::AttrType active_type = bke::AttrType(meta_data.data_type);
   const StringRefNull prop_name = geometry::rna_property_name_for_type(active_type);
   layout->prop(op->ptr, prop_name, UI_ITEM_NONE, name, ICON_NONE);
 }

commit 93dd58887be088c95a07fe52c2c2e41619e748cf

Fix #141355: Set attribute operator broken for meshes
Mistake in 1f92fd75771ea0c8fe98d3c40161e41e198d2d16

Also unify the code a bit between geometry types.

source/blender/editors/curves/intern/curves_attribute_set.cc
@@ -195,8 +195,7 @@ static void set_attribute_ui(bContext *C, wmOperator *op)
   AttributeOwner owner = AttributeOwner::from_id(&curves_id.id);
   const StringRef name = *BKE_attributes_active_name_get(owner);
   const bke::CurvesGeometry &curves = curves_id.geometry.wrap();
-  const bke::AttributeAccessor attributes = curves.attributes();
-  const bke::AttributeMetaData meta_data = *attributes.lookup_meta_data(name);
+  const bke::AttributeMetaData meta_data = *curves.attributes().lookup_meta_data(name);
   const StringRefNull prop_name = geometry::rna_property_name_for_type(meta_data.data_type);
   layout->prop(op->ptr, prop_name, UI_ITEM_NONE, name, ICON_NONE);
 }

source/blender/editors/mesh/editmesh_attribute.cc
@@ -255,7 +255,8 @@ static void mesh_set_attribute_ui(bContext *C, wmOperator *op)
   const StringRef name = *BKE_attributes_active_name_get(owner);
   CustomDataLayer *layer = BKE_attribute_search_for_write(
       owner, name, CD_MASK_PROP_ALL, ATTR_DOMAIN_MASK_ALL);
-  const bke::AttrType active_type = bke::AttrType(layer->type);
+  const bke::AttrType active_type = *bke::custom_data_type_to_attr_type(
+      eCustomDataType(layer->type));
   const StringRefNull prop_name = geometry::rna_property_name_for_type(active_type);
   layout->prop(op->ptr, prop_name, UI_ITEM_NONE, name, ICON_NONE);
 }

source/blender/editors/pointcloud/intern/attribute_set.cc
@@ -179,8 +179,7 @@ static void set_attribute_ui(bContext *C, wmOperator *op)
   AttributeOwner owner = AttributeOwner::from_id(&pointcloud.id);
   const StringRef name = *BKE_attributes_active_name_get(owner);
   const bke::AttributeMetaData meta_data = *pointcloud.attributes().lookup_meta_data(name);
-  const bke::AttrType active_type = bke::AttrType(meta_data.data_type);
-  const StringRefNull prop_name = geometry::rna_property_name_for_type(active_type);
+  const StringRefNull prop_name = geometry::rna_property_name_for_type(meta_data.data_type);
   layout->prop(op->ptr, prop_name, UI_ITEM_NONE, name, ICON_NONE);
 }
 