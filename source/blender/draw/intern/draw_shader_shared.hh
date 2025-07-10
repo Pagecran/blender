Pagecran/blender

commit 58a81f5c3a920e3451a4f3cc2480cd0b892f949f

Fix #141725: EEVEE View Layers Functionality Broken
The ENUM_OPERATORS macro was outdated.
Update it an move it next to the enum declaration.

source/blender/draw/intern/draw_shader_shared.hh
@@ -154,6 +154,7 @@ enum eObjectInfoFlag : uint32_t {
   /* Avoid skipped info to change culling. */
   OBJECT_NO_INFO = ~OBJECT_HOLDOUT
 };
+ENUM_OPERATORS(eObjectInfoFlag, OBJECT_ACTIVE_EDIT_MODE);
 
 struct ObjectInfos {
   /** Uploaded as center + size. Converted to mul+bias to local coord. */

source/blender/draw/intern/draw_resource.hh
@@ -59,8 +59,6 @@ inline std::ostream &operator<<(std::ostream &stream, const ObjectMatrices &matr
 /** \name ObjectInfos
  * \{ */
 
-ENUM_OPERATORS(eObjectInfoFlag, OBJECT_NEGATIVE_SCALE)
-
 inline void ObjectInfos::sync()
 {
   object_attrs_len = 0;

commit 11f135bb1a487b1105df1f0198120fa8b36ec239

Fix 141742: Crash on startup due to failed shader compilation
Vulkan only.
Regression caused by 58a81f5c3a.

source/blender/draw/intern/draw_shader_shared.hh
@@ -154,7 +154,10 @@ enum eObjectInfoFlag : uint32_t {
   /* Avoid skipped info to change culling. */
   OBJECT_NO_INFO = ~OBJECT_HOLDOUT
 };
+
+#if !defined(GPU_SHADER) && defined(__cplusplus)
 ENUM_OPERATORS(eObjectInfoFlag, OBJECT_ACTIVE_EDIT_MODE);
+#endif
 
 struct ObjectInfos {
   /** Uploaded as center + size. Converted to mul+bias to local coord. */