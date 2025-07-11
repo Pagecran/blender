Pagecran/blender

commit a12bce039fcdca88e7bf9119e39c2ac30d78d837

Lights: Add temperature property
Similar to other renderers, this adds a temperature property to set the
light color using blackbody emission. This can be more convenient than
using nodes, and can improve interop with other software.

This is supported in Cycles, EEVEE, Hydra, USD, COLLADA and FBX.

Pull Request: https://projects.blender.org/blender/blender/pulls/134303

scripts/addons_core/io_scene_fbx/export_fbx_bin.py
@@ -603,12 +603,18 @@ def fbx_data_light_elements(root, lamp, scene_data):
     elem_data_single_int32(light, b"GeometryVersion", FBX_GEOMETRY_VERSION)  # Sic...
 
     intensity = lamp.energy * 100.0 * pow(2.0, lamp.exposure)
+    color = lamp.color
+    if lamp.use_temperature:
+        temperature_color = lamp.temperature_color
+        color[0] *= temperature_color[0]
+        color[1] *= temperature_color[1]
+        color[2] *= temperature_color[2]
 
     tmpl = elem_props_template_init(scene_data.templates, b"Light")
     props = elem_properties(light)
     elem_props_template_set(tmpl, props, "p_enum", b"LightType", FBX_LIGHT_TYPES[lamp.type])
     elem_props_template_set(tmpl, props, "p_bool", b"CastLight", do_light)
-    elem_props_template_set(tmpl, props, "p_color", b"Color", lamp.color)
+    elem_props_template_set(tmpl, props, "p_color", b"Color", color)
     elem_props_template_set(tmpl, props, "p_number", b"Intensity", intensity)
     elem_props_template_set(tmpl, props, "p_enum", b"DecayType", FBX_LIGHT_DECAY_TYPES['INVERSE_SQUARE'])
     elem_props_template_set(tmpl, props, "p_double", b"DecayStart", 25.0 * gscale)  # 25 is old Blender default

intern/cycles/blender/addon/ui.py
@@ -1533,8 +1533,30 @@ class CYCLES_LIGHT_PT_light(CyclesButtonsPanel, Panel):
             layout.row().prop(light, "type")
 
         col = layout.column()
+        heading = col.column(align=True, heading="Temperature")
+        row = heading.column(align=True).row(align=True)
+        row.prop(light, "use_temperature", text="")
+        # Don't show color preview for now, it is grayed out so the color
+        # is not accurate. Would not a change in the UI code to allow
+        # non-editable colors to be displayed as is.
+        if False:  # light.use_temperature:
+            sub = row.split(factor=0.7, align=True)
+            sub.active = light.use_temperature
+            sub.prop(light, "temperature", text="")
+            sub.prop(light, "temperature_color", text="")
+        else:
+            sub = row.row()
+            sub.active = light.use_temperature
+            sub.prop(light, "temperature", text="")
 
-        col.prop(light, "color")
+        if light.use_temperature:
+            col.prop(light, "color", text="Tint")
+        else:
+            col.prop(light, "color", text="Color")
+
+        layout.separator()
+
+        col = layout.column()
         col.prop(light, "energy")
         col.prop(light, "exposure")
 
@@ -1555,6 +1577,7 @@ class CYCLES_LIGHT_PT_light(CyclesButtonsPanel, Panel):
             elif light.shape in {'RECTANGLE', 'ELLIPSE'}:
                 sub.prop(light, "size", text="Size X")
                 sub.prop(light, "size_y", text="Y")
+
 
 class CYCLES_LIGHT_PT_settings(CyclesButtonsPanel, Panel):
     bl_label = "Settings"

intern/cycles/blender/light.cpp
@@ -4,6 +4,10 @@
 
 #include "scene/light.h"
 
+#include "DNA_light_types.h"
+
+#include "IMB_colormanagement.hh"
+
 #include "blender/sync.h"
 #include "blender/util.h"
 #include "scene/object.h"
@@ -74,9 +78,14 @@ void BlenderSync::sync_light(BObjectInfo &b_ob_info, Light *light)
     }
   }
 
-  /* strength */
-  const float3 strength = get_float3(b_light.color()) *
-                          (BL::PointLight(b_light).energy() * exp2f(b_light.exposure()));
+  /* Color and strength. */
+  float3 light_color = get_float3(b_light.color());
+  if (b_light.use_temperature()) {
+    light_color *= get_float3(b_light.temperature_color());
+  }
+
+  const float3 strength = light_color * BL::PointLight(b_light).energy() *
+                          exp2f(b_light.exposure());
   light->set_strength(strength);
 
   /* shadow */

scripts/addons_core/hydra_storm/ui.py
@@ -167,8 +167,19 @@ class STORM_HYDRA_LIGHT_PT_light(Panel):
         layout.use_property_decorate = False
 
         main_col = layout.column()
+        heading = main_col.column(align=True, heading="Temperature")
+        row = heading.column(align=True).row(align=True)
+        row.prop(light, "use_temperature", text="")
+        sub = row.row()
+        sub.active = light.use_temperature
+        sub.prop(light, "temperature", text="")
 
-        main_col.prop(light, "color")
+        if light.use_temperature:
+            main_col.prop(light, "color", text="Tint")
+        else:
+            main_col.prop(light, "color", text="Color")
+
+        main_col = layout.column()
         main_col.prop(light, "energy")
         main_col.prop(light, "exposure")
         main_col.separator()

scripts/startup/bl_ui/properties_data_light.py
@@ -89,7 +89,30 @@ class DATA_PT_EEVEE_light(DataButtonsPanel, Panel):
             layout.row().prop(light, "type")
 
         col = layout.column()
-        col.prop(light, "color")
+        heading = col.column(align=True, heading="Temperature")
+        row = heading.column(align=True).row(align=True)
+        row.prop(light, "use_temperature", text="")
+        # Don't show color preview for now, it is grayed out so the color
+        # is not accurate. Would not a change in the UI code to allow
+        # non-editable colors to be displayed as is.
+        if False:  # light.use_temperature:
+            sub = row.split(factor=0.7, align=True)
+            sub.active = light.use_temperature
+            sub.prop(light, "temperature", text="")
+            sub.prop(light, "temperature_color", text="")
+        else:
+            sub = row.row()
+            sub.active = light.use_temperature
+            sub.prop(light, "temperature", text="")
+
+        if light.use_temperature:
+            col.prop(light, "color", text="Tint")
+        else:
+            col.prop(light, "color", text="Color")
+
+        layout.separator()
+
+        col = layout.column()
         col.prop(light, "energy")
         col.prop(light, "exposure")
 

source/blender/blenkernel/BKE_light.h
@@ -10,11 +10,15 @@
  */
 
 #include "BLI_compiler_attrs.h"
+#include "BLI_math_vector_types.hh"
 
 struct Depsgraph;
 struct Light;
 struct Main;
 
-struct Light *BKE_light_add(struct Main *bmain, const char *name) ATTR_WARN_UNUSED_RESULT;
+Light *BKE_light_add(Main *bmain, const char *name) ATTR_WARN_UNUSED_RESULT;
 
-void BKE_light_eval(struct Depsgraph *depsgraph, struct Light *la);
+void BKE_light_eval(Depsgraph *depsgraph, Light *la);
+
+float BKE_light_power(const Light &light);
+blender::float3 BKE_light_color(const Light &light);

source/blender/blenkernel/intern/light.cc
@@ -33,6 +33,8 @@
 
 #include "DEG_depsgraph.hh"
 
+#include "IMB_colormanagement.hh"
+
 #include "BLO_read_write.hh"
 
 static void light_init_data(ID *id)
@@ -198,4 +200,22 @@ Light *BKE_light_add(Main *bmain, const char *name)
 void BKE_light_eval(Depsgraph *depsgraph, Light *la)
 {
   DEG_debug_print_eval(depsgraph, __func__, la->id.name, la);
+}
+
+float BKE_light_power(const Light &light)
+{
+  return light.energy * exp2f(light.exposure);
+}
+
+blender::float3 BKE_light_color(const Light &light)
+{
+  blender::float3 color(&light.r);
+
+  if (light.mode & LA_USE_TEMPERATURE) {
+    float temperature_color[4];
+    IMB_colormanagement_blackbody_temperature_to_rgb(temperature_color, light.temperature);
+    color *= blender::float3(temperature_color);
+  }
+
+  return color;
 }

source/blender/blenloader/intern/versioning_450.cc
@@ -13,6 +13,7 @@
 #include "DNA_anim_types.h"
 #include "DNA_brush_types.h"
 #include "DNA_defaults.h"
+#include "DNA_light_types.h"
 #include "DNA_mesh_types.h"
 #include "DNA_sequence_types.h"
 
@@ -5555,6 +5556,14 @@ void blo_do_versions_450(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
       }
     }
     FOREACH_NODETREE_END;
+  }
+
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 405, 76)) {
+    LISTBASE_FOREACH (Light *, light, &bmain->lights) {
+      if (light->temperature == 0.0f) {
+        light->temperature = 6500.0f;
+      }
+    }
   }
 
   /* Always run this versioning (keep at the bottom of the function). Meshes are written with the

commit 1f3c0d549bf9a8d18b17df4d533d17650d987c4f

Fix #141760: FBX export inadvertently modifies color property on lights
Broken with commit a12bce039fcdca88e7bf9119e39c2ac30d78d837

The Blender `Color` type is treated as a reference in Python. Make a
`.copy` to ensure that changes are not made to the actual scene.

Pull Request: https://projects.blender.org/blender/blender/pulls/141762

scripts/addons_core/io_scene_fbx/export_fbx_bin.py
@@ -603,7 +603,7 @@ def fbx_data_light_elements(root, lamp, scene_data):
     elem_data_single_int32(light, b"GeometryVersion", FBX_GEOMETRY_VERSION)  # Sic...
 
     intensity = lamp.energy * 100.0 * pow(2.0, lamp.exposure)
-    color = lamp.color
+    color = lamp.color.copy()
     if lamp.use_temperature:
         temperature_color = lamp.temperature_color
         color[0] *= temperature_color[0]