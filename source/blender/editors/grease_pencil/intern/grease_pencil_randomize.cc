Pagecran/blender

commit 96e549c09239cd592b68919e9a1fe2e5a8e81ba8

Paint: Add color jitter support
Add ability to randomize color of each stroke for or use perlin noise
randomization during the stroke. Works similarly to grease pencil stroke
color randomization. This feature is available for all mesh color
painting modes (Sculpt Paint brush, Texture Paint and Vertex Paint)

This setting is available both as a per-brush setting and a unified
setting on the scene level. Additionally, the equivalent Grease Pencil
options have been migrated to these more generic flags and values.

Based on this [1] RCS request.

[1]: https://blender.community/c/rightclickselect/mwgbbc/

Pull Request: https://projects.blender.org/blender/blender/pulls/128953

source/blender/editors/grease_pencil/intern/grease_pencil_randomize.cc
@@ -6,6 +6,8 @@
  * \ingroup edgreasepencil
  */
 
+#include "BKE_brush.hh"
+#include "BKE_paint.hh"
 #include "BLI_noise.hh"
 #include "BLI_rand.hh"
 
@@ -141,82 +143,25 @@ ColorGeometry4f randomize_color(const BrushGpencilSettings &settings,
   {
     return color;
   }
-  /* TODO: This should be exposed as a setting to scale the noise along the stroke. */
-  constexpr float noise_scale = 1 / 20.0f;
 
-  float random_hue = 0.0f;
-  if ((settings.flag2 & GP_BRUSH_USE_HUE_AT_STROKE) == 0) {
-    random_hue = noise::perlin_signed(float2(distance * noise_scale, stroke_hue_factor));
-  }
-  else {
-    random_hue = stroke_hue_factor;
-  }
+  BrushColorJitterSettings jitter_settings = {
+      settings.color_jitter_flag,
+      settings.random_hue,
+      settings.random_saturation,
+      settings.random_value,
 
-  float random_saturation = 0.0f;
-  if ((settings.flag2 & GP_BRUSH_USE_SAT_AT_STROKE) == 0) {
-    random_saturation = noise::perlin_signed(
-        float2(distance * noise_scale, stroke_saturation_factor));
-  }
-  else {
-    random_saturation = stroke_saturation_factor;
-  }
+      settings.curve_rand_hue,
+      settings.curve_rand_saturation,
+      settings.curve_rand_value,
+  };
 
-  float random_value = 0.0f;
-  if ((settings.flag2 & GP_BRUSH_USE_VAL_AT_STROKE) == 0) {
-    random_value = noise::perlin_signed(float2(distance * noise_scale, stroke_value_factor));
-  }
-  else {
-    random_value = stroke_value_factor;
-  }
+  blender::float3 initial_hsv_jitter = {
+      stroke_hue_factor, stroke_saturation_factor, stroke_value_factor};
 
-  if ((settings.flag2 & GP_BRUSH_USE_HUE_RAND_PRESS) != 0) {
-    random_hue *= BKE_curvemapping_evaluateF(settings.curve_rand_hue, 0, pressure);
-  }
-  if ((settings.flag2 & GP_BRUSH_USE_SAT_RAND_PRESS) != 0) {
-    random_saturation *= BKE_curvemapping_evaluateF(settings.curve_rand_saturation, 0, pressure);
-  }
-  if ((settings.flag2 & GP_BRUSH_USE_VAL_RAND_PRESS) != 0) {
-    random_value *= BKE_curvemapping_evaluateF(settings.curve_rand_value, 0, pressure);
-  }
+  blender::float3 jittered = BKE_paint_randomize_color(
+      jitter_settings, initial_hsv_jitter, distance, pressure, {color.r, color.g, color.b});
 
-  float3 hsv;
-  linearrgb_to_srgb_v3_v3(hsv, color);
-  rgb_to_hsv_v(hsv, hsv);
-
-  hsv[0] += random_hue * settings.random_hue;
-
-  const float absolute_brightness = hsv[2] + random_value * settings.random_value;
-
-  /*
-   * To match relative brightness we want the ratio of the original to modified Value to not
-   * depend on the brightness of the input Value, Exp is used because we need a function that
-   * is positive for all 'x' and has the property that 'f(-x) = 1/f(x)' this is so that
-   * we make the Value on average growth and shrink by the same amount. To detriment the rate
-   * of the Exponential we set slope to match addition for small random values at an arbitrary
-   * Base Value.
-   */
-  constexpr float base_value = 0.5f;
-  const float relative_brightness = hsv[2] *
-                                    math::exp(random_value * settings.random_value / base_value);
-
-  /* Use the fourth power. */
-  const float blend_factor = math::pow(settings.random_value, 4.0f);
-  /* Use relative brightness at low randomness, and switch to absolute at high. */
-  hsv[2] = math::interpolate(relative_brightness, absolute_brightness, blend_factor);
-
-  /* Multiply by the current saturation to prevent grays from becoming red. */
-  hsv[1] += random_saturation * settings.random_saturation * 2.0f * hsv[1];
-
-  /* Wrap hue, clamp saturation and value. */
-  hsv[0] = math::fract(hsv[0]);
-  hsv[1] = math::clamp(hsv[1], 0.0f, 1.0f);
-  hsv[2] = math::clamp(hsv[2], 0.0f, 1.0f);
-
-  ColorGeometry4f random_color;
-  hsv_to_rgb_v(hsv, random_color);
-  srgb_to_linearrgb_v3_v3(random_color, random_color);
-  random_color.a = color.a;
-  return random_color;
+  return {jittered[0], jittered[1], jittered[2], 1};
 }
 
 }  // namespace blender::ed::greasepencil

scripts/startup/bl_ui/properties_paint_common.py
@@ -1178,6 +1178,43 @@ def brush_shared_settings(layout, context, brush, popover=False):
         layout.row().prop(brush, "direction", expand=True)
 
 
+def color_jitter_panel(layout, context, brush):
+    mode = UnifiedPaintPanel.get_brush_mode(context)
+    ups = context.scene.tool_settings.unified_paint_settings
+
+    is_sculpt_paint_mode = mode == 'SCULPT' and brush.sculpt_capabilities.has_color
+    if mode in {'PAINT_TEXTURE', 'PAINT_2D', 'PAINT_VERTEX'} or is_sculpt_paint_mode:
+        prop_owner = ups if ups.use_unified_color else brush
+        layout.use_property_split = False
+
+        header, panel = layout.panel("color_jitter_panel", default_closed=True)
+        header.prop(prop_owner, "use_color_jitter", text="Randomize Color")
+        if panel:
+            panel.use_property_split = True
+            panel.use_property_decorate = False
+
+            col = panel.column(align=True)
+            col.use_property_split = True
+
+            row = col.row(align=True)
+            row.enabled = prop_owner.use_color_jitter
+            row.prop(prop_owner, "hue_jitter", slider=True, text="Hue")
+            row.prop(prop_owner, "use_stroke_random_hue", text="", icon='GP_SELECT_STROKES')
+            row.prop(prop_owner, "use_random_press_hue", text="", icon='STYLUS_PRESSURE')
+
+            row = col.row(align=True)
+            row.enabled = prop_owner.use_color_jitter
+            row.prop(prop_owner, "saturation_jitter", slider=True, text="Saturation")
+            row.prop(prop_owner, "use_stroke_random_sat", text="", icon='GP_SELECT_STROKES')
+            row.prop(prop_owner, "use_random_press_sat", text="", icon='STYLUS_PRESSURE')
+
+            row = col.row(align=True)
+            row.enabled = prop_owner.use_color_jitter
+            row.prop(prop_owner, "value_jitter", slider=True, text="Value")
+            row.prop(prop_owner, "use_stroke_random_val", text="", icon='GP_SELECT_STROKES')
+            row.prop(prop_owner, "use_random_press_val", text="", icon='STYLUS_PRESSURE')
+
+
 def brush_settings_advanced(layout, context, settings, brush, popover=False):
     """Draw advanced brush settings for Sculpt, Texture/Vertex/Weight Paint modes."""
 
@@ -1357,6 +1394,9 @@ def brush_settings_advanced(layout, context, settings, brush, popover=False):
     if use_frontface:
         layout.prop(brush, "use_frontface", text="Front Faces Only")
 
+    if popover:
+        color_jitter_panel(layout, context, brush)
+
     # Brush modes
     header, panel = layout.panel("modes", default_closed=True)
     header.label(text="Modes")
@@ -1400,6 +1440,9 @@ def draw_color_settings(context, layout, brush, color_type=False):
         row.separator()
         row.operator("paint.brush_colors_flip", icon='FILE_REFRESH', text="", emboss=False)
         row.prop(ups, "use_unified_color", text="", icon='BRUSHES_ALL')
+
+        color_jitter_panel(layout, context, brush)
+
     # Gradient
     elif brush.color_type == 'GRADIENT':
         layout.template_color_ramp(brush, "gradient", expand=True)

source/blender/blenkernel/BKE_blender_version.h
@@ -27,7 +27,7 @@
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 83
+#define BLENDER_FILE_SUBVERSION 84
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenkernel/BKE_brush.hh
@@ -10,6 +10,8 @@
  * General operations for brushes.
  */
 
+#include <optional>
+
 #include "BLI_span.hh"
 
 #include "DNA_brush_enums.h"
@@ -164,7 +166,22 @@ ImBuf *BKE_brush_gen_radial_control_imbuf(Brush *br, bool secondary, bool displa
 
 /* Unified strength size and color. */
 
+struct BrushColorJitterSettings {
+  int flag;
+  /** Jitter amounts */
+  float hue;
+  float saturation;
+  float value;
+
+  /** Jitter pressure curves. */
+  struct CurveMapping *curve_hue_jitter;
+  struct CurveMapping *curve_sat_jitter;
+  struct CurveMapping *curve_val_jitter;
+};
+
 const float *BKE_brush_color_get(const Scene *scene, const Paint *paint, const Brush *brush);
+const std::optional<BrushColorJitterSettings> BKE_brush_color_jitter_get_settings(
+    const Scene *scene, const Paint *paint, const Brush *brush);
 const float *BKE_brush_secondary_color_get(const Scene *scene,
                                            const Paint *paint,
                                            const Brush *brush);

source/blender/blenkernel/BKE_paint.hh
@@ -31,6 +31,7 @@ struct BMesh;
 struct BlendDataReader;
 struct BlendWriter;
 struct Brush;
+struct BrushColorJitterSettings;
 struct CurveMapping;
 struct Depsgraph;
 struct EnumPropertyItem;
@@ -314,6 +315,10 @@ void BKE_paint_face_set_overlay_color_get(int face_set, int seed, uchar r_color[
 
 /* Stroke related. */
 
+/* Random values are generated on each new stroke so each stroke
+ * gets a different starting point in the perlin noise. */
+blender::float3 seed_hsv_jitter();
+
 bool paint_calculate_rake_rotation(UnifiedPaintSettings &ups,
                                    const Brush &brush,
                                    const float mouse_pos[2],
@@ -324,6 +329,12 @@ void paint_update_brush_rake_rotation(UnifiedPaintSettings &ups,
                                       float rotation);
 
 void BKE_paint_stroke_get_average(const Scene *scene, const Object *ob, float stroke[3]);
+
+blender::float3 BKE_paint_randomize_color(const BrushColorJitterSettings &color_jitter,
+                                          const blender::float3 &initial_hsv_jitter,
+                                          const float distance,
+                                          const float pressure,
+                                          const blender::float3 &color);
 
 /* .blend I/O */
 
@@ -660,3 +671,4 @@ bool BKE_paint_canvas_image_get(PaintModeSettings *settings,
 int BKE_paint_canvas_uvmap_layer_index_get(const PaintModeSettings *settings, Object *ob);
 void BKE_sculpt_check_cavity_curves(Sculpt *sd);
 CurveMapping *BKE_sculpt_default_cavity_curve();
+CurveMapping *BKE_paint_default_curve();

source/blender/blenkernel/intern/brush.cc
@@ -86,6 +86,10 @@ static void brush_copy_data(Main * /*bmain*/,
   brush_dst->curve = BKE_curvemapping_copy(brush_src->curve);
   brush_dst->automasking_cavity_curve = BKE_curvemapping_copy(brush_src->automasking_cavity_curve);
 
+  brush_dst->curve_rand_hue = BKE_curvemapping_copy(brush_src->curve_rand_hue);
+  brush_dst->curve_rand_saturation = BKE_curvemapping_copy(brush_src->curve_rand_saturation);
+  brush_dst->curve_rand_value = BKE_curvemapping_copy(brush_src->curve_rand_value);
+
   if (brush_src->gpencil_settings != nullptr) {
     brush_dst->gpencil_settings = MEM_dupallocN<BrushGpencilSettings>(
         __func__, *(brush_src->gpencil_settings));
@@ -128,6 +132,10 @@ static void brush_free_data(ID *id)
   }
   BKE_curvemapping_free(brush->curve);
   BKE_curvemapping_free(brush->automasking_cavity_curve);
+
+  BKE_curvemapping_free(brush->curve_rand_hue);
+  BKE_curvemapping_free(brush->curve_rand_saturation);
+  BKE_curvemapping_free(brush->curve_rand_value);
 
   if (brush->gpencil_settings != nullptr) {
     BKE_curvemapping_free(brush->gpencil_settings->curve_sensitivity);
@@ -229,6 +237,16 @@ static void brush_blend_write(BlendWriter *writer, ID *id, const void *id_addres
     BKE_curvemapping_blend_write(writer, brush->automasking_cavity_curve);
   }
 
+  if (brush->curve_rand_hue) {
+    BKE_curvemapping_blend_write(writer, brush->curve_rand_hue);
+  }
+  if (brush->curve_rand_saturation) {
+    BKE_curvemapping_blend_write(writer, brush->curve_rand_saturation);
+  }
+  if (brush->curve_rand_value) {
+    BKE_curvemapping_blend_write(writer, brush->curve_rand_value);
+  }
+
   if (brush->gpencil_settings) {
     BLO_write_struct(writer, BrushGpencilSettings, brush->gpencil_settings);
 
@@ -293,6 +311,30 @@ static void brush_blend_read_data(BlendDataReader *reader, ID *id)
   }
   else {
     brush->automasking_cavity_curve = BKE_sculpt_default_cavity_curve();
+  }
+
+  BLO_read_struct(reader, CurveMapping, &brush->curve_rand_hue);
+  if (brush->curve_rand_hue) {
+    BKE_curvemapping_blend_read(reader, brush->curve_rand_hue);
+  }
+  else {
+    brush->curve_rand_hue = BKE_paint_default_curve();
+  }
+
+  BLO_read_struct(reader, CurveMapping, &brush->curve_rand_saturation);
+  if (brush->curve_rand_saturation) {
+    BKE_curvemapping_blend_read(reader, brush->curve_rand_saturation);
+  }
+  else {
+    brush->curve_rand_saturation = BKE_paint_default_curve();
+  }
+
+  BLO_read_struct(reader, CurveMapping, &brush->curve_rand_value);
+  if (brush->curve_rand_value) {
+    BKE_curvemapping_blend_read(reader, brush->curve_rand_value);
+  }
+  else {
+    brush->curve_rand_value = BKE_paint_default_curve();
   }
 
   /* grease pencil */
@@ -1082,6 +1124,42 @@ const float *BKE_brush_color_get(const Scene *scene, const Paint *paint, const B
     return scene->toolsettings->unified_paint_settings.rgb;
   }
   return brush->rgb;
+}
+
+/** Get color jitter settings if enabled. */
+const std::optional<BrushColorJitterSettings> BKE_brush_color_jitter_get_settings(
+    const Scene *scene, const Paint *paint, const Brush *brush)
+{
+  if (BKE_paint_use_unified_color(scene->toolsettings, paint)) {
+    if ((scene->toolsettings->unified_paint_settings.flag & UNIFIED_PAINT_COLOR_JITTER) == 0) {
+      return std::nullopt;
+    }
+
+    const UnifiedPaintSettings settings = scene->toolsettings->unified_paint_settings;
+    return BrushColorJitterSettings{
+        settings.color_jitter_flag,
+        settings.hsv_jitter[0],
+        settings.hsv_jitter[1],
+        settings.hsv_jitter[2],
+        settings.curve_rand_hue,
+        settings.curve_rand_saturation,
+        settings.curve_rand_value,
+    };
+  }
+
+  if ((brush->flag2 & BRUSH_JITTER_COLOR) == 0) {
+    return std::nullopt;
+  }
+
+  return BrushColorJitterSettings{
+      brush->color_jitter_flag,
+      brush->hsv_jitter[0],
+      brush->hsv_jitter[1],
+      brush->hsv_jitter[2],
+      brush->curve_rand_hue,
+      brush->curve_rand_saturation,
+      brush->curve_rand_value,
+  };
 }
 
 const float *BKE_brush_secondary_color_get(const Scene *scene,

source/blender/blenkernel/intern/paint.cc
@@ -32,6 +32,7 @@
 #include "BLI_math_color.h"
 #include "BLI_math_matrix.hh"
 #include "BLI_math_vector.h"
+#include "BLI_noise.hh"
 #include "BLI_string.h"
 #include "BLI_utildefines.h"
 #include "BLI_vector.hh"
@@ -1846,6 +1847,62 @@ void BKE_paint_stroke_get_average(const Scene *scene, const Object *ob, float st
   else {
     copy_v3_v3(stroke, ob->object_to_world().location());
   }
+}
+
+blender::float3 BKE_paint_randomize_color(const BrushColorJitterSettings &color_jitter,
+                                          const blender::float3 &initial_hsv_jitter,
+                                          const float distance,
+                                          const float pressure,
+                                          const blender::float3 &color)
+{
+  constexpr float noise_scale = 1 / 20.0f;
+
+  const float random_hue = (color_jitter.flag & BRUSH_COLOR_JITTER_USE_HUE_AT_STROKE) ?
+                               initial_hsv_jitter[0] :
+                               blender::noise::perlin(blender::float2(
+                                   distance * noise_scale, initial_hsv_jitter[0] * 100));
+
+  const float random_sat = (color_jitter.flag & BRUSH_COLOR_JITTER_USE_SAT_AT_STROKE) ?
+                               initial_hsv_jitter[1] :
+                               blender::noise::perlin(blender::float2(
+                                   distance * noise_scale, initial_hsv_jitter[1] * 100));
+
+  const float random_val = (color_jitter.flag & BRUSH_COLOR_JITTER_USE_VAL_AT_STROKE) ?
+                               initial_hsv_jitter[2] :
+                               blender::noise::perlin(blender::float2(
+                                   distance * noise_scale, initial_hsv_jitter[2] * 100));
+
+  float hue_jitter_scale = color_jitter.hue;
+  if ((color_jitter.flag & BRUSH_COLOR_JITTER_USE_HUE_RAND_PRESS)) {
+    hue_jitter_scale *= BKE_curvemapping_evaluateF(color_jitter.curve_hue_jitter, 0, pressure);
+  }
+  float sat_jitter_scale = color_jitter.saturation;
+  if ((color_jitter.flag & BRUSH_COLOR_JITTER_USE_SAT_RAND_PRESS)) {
+    sat_jitter_scale *= BKE_curvemapping_evaluateF(color_jitter.curve_sat_jitter, 0, pressure);
+  }
+  float val_jitter_scale = color_jitter.value;
+  if ((color_jitter.flag & BRUSH_COLOR_JITTER_USE_VAL_RAND_PRESS)) {
+    val_jitter_scale *= BKE_curvemapping_evaluateF(color_jitter.curve_val_jitter, 0, pressure);
+  }
+
+  blender::float3 hsv;
+  rgb_to_hsv_v(color, hsv);
+
+  hsv[0] += blender::math::interpolate(0.5f, random_hue, hue_jitter_scale) - 0.5f;
+  /* Wrap hue. */
+  if (hsv[0] > 1.0f) {
+    hsv[0] -= 1.0f;
+  }
+  else if (hsv[0] < 0.0f) {
+    hsv[0] += 1.0f;
+  }
+
+  hsv[1] *= blender::math::interpolate(1.0f, random_sat * 2.0f, sat_jitter_scale);
+  hsv[2] *= blender::math::interpolate(1.0f, random_val * 2.0f, val_jitter_scale);
+
+  blender::float3 random_color;
+  hsv_to_rgb_v(hsv, random_color);
+  return random_color;
 }
 
 void BKE_paint_blend_write(BlendWriter *writer, Paint *paint)

source/blender/blenkernel/intern/scene.cc
@@ -137,6 +137,15 @@ CurveMapping *BKE_sculpt_default_cavity_curve()
   return cumap;
 }
 
+CurveMapping *BKE_paint_default_curve()
+{
+  CurveMapping *cumap = BKE_curvemapping_add(1, 0, 0, 1, 1);
+  BKE_curvemap_reset(cumap->cm, &cumap->clipr, CURVE_PRESET_LINE, CURVEMAP_SLOPE_POSITIVE);
+  BKE_curvemapping_init(cumap);
+
+  return cumap;
+}
+
 void BKE_sculpt_check_cavity_curves(Sculpt *sd)
 {
   if (!sd->automasking_cavity_curve) {
@@ -172,6 +181,10 @@ static void scene_init_data(ID *id)
   scene->toolsettings = DNA_struct_default_alloc(ToolSettings);
 
   scene->toolsettings->autokey_mode = uchar(U.autokey_mode);
+
+  scene->toolsettings->unified_paint_settings.curve_rand_hue = BKE_paint_default_curve();
+  scene->toolsettings->unified_paint_settings.curve_rand_saturation = BKE_paint_default_curve();
+  scene->toolsettings->unified_paint_settings.curve_rand_value = BKE_paint_default_curve();
 
   /* Grease pencil multi-frame falloff curve. */
   scene->toolsettings->gp_sculpt.cur_falloff = BKE_curvemapping_add(1, 0.0f, 0.0f, 1.0f, 1.0f);
@@ -1013,6 +1026,22 @@ static void scene_blend_write(BlendWriter *writer, ID *id, const void *id_addres
   /* direct data */
   ToolSettings *tos = sce->toolsettings;
   BLO_write_struct(writer, ToolSettings, tos);
+
+  BLO_write_struct(writer, CurveMapping, &tos->unified_paint_settings.curve_rand_hue);
+  if (tos->unified_paint_settings.curve_rand_hue) {
+    BKE_curvemapping_blend_write(writer, tos->unified_paint_settings.curve_rand_hue);
+  }
+
+  BLO_write_struct(writer, CurveMapping, &tos->unified_paint_settings.curve_rand_saturation);
+  if (tos->unified_paint_settings.curve_rand_saturation) {
+    BKE_curvemapping_blend_write(writer, tos->unified_paint_settings.curve_rand_saturation);
+  }
+
+  BLO_write_struct(writer, CurveMapping, &tos->unified_paint_settings.curve_rand_value);
+  if (tos->unified_paint_settings.curve_rand_value) {
+    BKE_curvemapping_blend_write(writer, tos->unified_paint_settings.curve_rand_value);
+  }
+
   if (tos->vpaint) {
     BLO_write_struct(writer, VPaint, tos->vpaint);
     BKE_paint_blend_write(writer, &tos->vpaint->paint);
@@ -1208,6 +1237,24 @@ static void scene_blend_read_data(BlendDataReader *reader, ID *id)
     UnifiedPaintSettings *ups = &sce->toolsettings->unified_paint_settings;
     zero_v3(ups->last_location);
     ups->last_hit = 0;
+
+    BLO_read_struct(reader, CurveMapping, &ups->curve_rand_hue);
+    if (ups->curve_rand_hue) {
+      BKE_curvemapping_blend_read(reader, ups->curve_rand_hue);
+      BKE_curvemapping_init(ups->curve_rand_hue);
+    }
+
+    BLO_read_struct(reader, CurveMapping, &ups->curve_rand_saturation);
+    if (ups->curve_rand_saturation) {
+      BKE_curvemapping_blend_read(reader, ups->curve_rand_saturation);
+      BKE_curvemapping_init(ups->curve_rand_saturation);
+    }
+
+    BLO_read_struct(reader, CurveMapping, &ups->curve_rand_value);
+    if (ups->curve_rand_value) {
+      BKE_curvemapping_blend_read(reader, ups->curve_rand_value);
+      BKE_curvemapping_init(ups->curve_rand_value);
+    }
 
     direct_link_paint_helper(reader, sce, (Paint **)&sce->toolsettings->sculpt);
     direct_link_paint_helper(reader, sce, (Paint **)&sce->toolsettings->vpaint);
@@ -1657,6 +1704,14 @@ ToolSettings *BKE_toolsettings_copy(ToolSettings *toolsettings, const int flag)
     BKE_paint_copy(&ts->curves_sculpt->paint, &ts->curves_sculpt->paint, flag);
   }
 
+  /* Color jitter curves in unified paint settings. */
+  ts->unified_paint_settings.curve_rand_hue = BKE_curvemapping_copy(
+      ts->unified_paint_settings.curve_rand_hue);
+  ts->unified_paint_settings.curve_rand_saturation = BKE_curvemapping_copy(
+      ts->unified_paint_settings.curve_rand_saturation);
+  ts->unified_paint_settings.curve_rand_value = BKE_curvemapping_copy(
+      ts->unified_paint_settings.curve_rand_value);
+
   BKE_paint_copy(&ts->imapaint.paint, &ts->imapaint.paint, flag);
   ts->particle.paintcursor = nullptr;
   ts->particle.scene = nullptr;
@@ -1722,6 +1777,17 @@ void BKE_toolsettings_free(ToolSettings *toolsettings)
     MEM_freeN(toolsettings->curves_sculpt);
   }
   BKE_paint_free(&toolsettings->imapaint.paint);
+
+  /* Color jitter curves in unified paint settings. */
+  if (toolsettings->unified_paint_settings.curve_rand_hue) {
+    BKE_curvemapping_free(toolsettings->unified_paint_settings.curve_rand_hue);
+  }
+  if (toolsettings->unified_paint_settings.curve_rand_saturation) {
+    BKE_curvemapping_free(toolsettings->unified_paint_settings.curve_rand_saturation);
+  }
+  if (toolsettings->unified_paint_settings.curve_rand_value) {
+    BKE_curvemapping_free(toolsettings->unified_paint_settings.curve_rand_value);
+  }
 
   /* free Grease Pencil interpolation curve */
   if (toolsettings->gp_interpolate.custom_ipo) {

commit 2dec2e7c493dab45b72424a22dec416c56c26d6a

Fix #142006: Color Jitter produces black points in Material mode
This fixes a bug caused by 96e549c092. Before the change, the
`randomize_color` function passed through the `color.a` value in the
returned rgba color. The alpha is `1` when the color mode is Color
Attribute and `0` when it's Material. After the patch, it was hard
coded to `1`. This fixes it by passing the `color.a` back through
again.

Pull Request: https://projects.blender.org/blender/blender/pulls/142642

source/blender/editors/grease_pencil/intern/grease_pencil_randomize.cc
@@ -148,7 +148,7 @@ ColorGeometry4f randomize_color(const BrushGpencilSettings &settings,
   blender::float3 jittered = BKE_paint_randomize_color(
       *jitter, initial_hsv_jitter, distance, pressure, {color.r, color.g, color.b});
 
-  return {jittered[0], jittered[1], jittered[2], 1};
+  return {jittered[0], jittered[1], jittered[2], color.a};
 }
 
 }  // namespace blender::ed::greasepencil