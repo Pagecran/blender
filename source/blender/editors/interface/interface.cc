Pagecran/blender

commit ecd8de4bea8eb54ce72f3329988fd2fee216412b

Cleanup: UI: Remove unnecessary button arguments
Passing the current enum value as a float to the "min" argument
doesn't seem to do anything. It's also the only place that the min
and max arguments to `uiDefIconTextBut` are used.

source/blender/editors/interface/interface.cc
@@ -4691,7 +4691,7 @@ static void ui_def_but_rna__menu(bContext *C, uiLayout *layout, void *but_p)
                                      UI_UNIT_X * 5,
                                      UI_UNIT_Y,
                                      &handle->retvalue,
-                                     item->value,
+                                     0.0f,
                                      0.0,
                                      description_static);
       }

source/blender/editors/space_image/image_buttons.cc
@@ -111,7 +111,7 @@ static void ui_imageuser_slot_menu(bContext *C, uiLayout *layout, void *image_p)
                       UI_UNIT_X * 5,
                       UI_UNIT_X,
                       &image->render_slot,
-                      float(slot_id),
+                      0.0f,
                       0.0,
                       "");
   }

commit 6116b3d9fdbb184e4ce27557478ca459890b3598

Fix: Some enum item buttons don't function after recent cleanup
Caused by ecd8de4bea8eb54ce72f3329988fd2fee216412b

source/blender/editors/interface/interface.cc
@@ -4711,6 +4711,9 @@ static void ui_def_but_rna__menu(bContext *C, uiLayout *layout, void *but_p)
         item_but->flag |= UI_SELECT_DRAW;
       }
 
+      /* "hardmin" is used to store the value of the enum item. */
+      item_but->hardmin = float(item->value);
+
       if (use_enum_copy_description) {
         if (item->description && item->description[0]) {
           char *description_copy = BLI_strdup(item->description);