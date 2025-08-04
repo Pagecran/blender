Pagecran/blender

commit 5b73f798d0a413a65a0a169c3a16ed07772c1723

Refactor: Nodes: allow passing already unique name/identifier to node copy function
This is useful when the src node name is not unique, but the caller can provide
a unique name more efficiently then if the function has to compute the name
automatically.

source/blender/blenkernel/BKE_node.hh
@@ -964,14 +964,15 @@ void node_rebuild_id_vector(bNodeTree &node_tree);
 
 /**
  * \note keeps socket list order identical, for copying links.
- * \param use_unique: If true, make sure the node's identifier and name are unique in the new
- * tree. Must be *true* if the \a dst_tree had nodes that weren't in the source node's tree.
- * Must be *false* when simply copying a node tree, so that identifiers don't change.
+ * \param dst_name: The name of the copied node. This is expected to be unique in the destination
+ * tree if provided. If not provided, the src name is used and made unique.
+ * \param dst_identifier: Same ad dst_name, but for the identifier.
  */
 bNode *node_copy_with_mapping(bNodeTree *dst_tree,
                               const bNode &node_src,
                               int flag,
-                              bool use_unique,
+                              std::optional<StringRefNull> dst_unique_name,
+                              std::optional<int> dst_unique_identifier,
                               Map<const bNodeSocket *, bNodeSocket *> &new_socket_map);
 
 bNode *node_copy(bNodeTree *dst_tree, const bNode &src_node, int flag, bool use_unique);

source/blender/blenkernel/intern/node.cc
@@ -162,7 +162,7 @@ static void ntree_copy_data(Main * /*bmain*/,
   LISTBASE_FOREACH_INDEX (const bNode *, src_node, &ntree_src->nodes, i) {
     /* Don't find a unique name for every node, since they should have valid names already. */
     bNode *new_node = node_copy_with_mapping(
-        ntree_dst, *src_node, flag_subdata, false, socket_map);
+        ntree_dst, *src_node, flag_subdata, src_node->name, src_node->identifier, socket_map);
     dst_runtime.nodes_by_id.add_new(new_node);
     new_node->runtime->index_in_tree = i;
   }
@@ -3353,20 +3353,28 @@ static void node_socket_copy(bNodeSocket *sock_dst, const bNodeSocket *sock_src,
 bNode *node_copy_with_mapping(bNodeTree *dst_tree,
                               const bNode &node_src,
                               const int flag,
-                              const bool use_unique,
+                              const std::optional<StringRefNull> dst_unique_name,
+                              const std::optional<int> dst_unique_identifier,
                               Map<const bNodeSocket *, bNodeSocket *> &socket_map)
 {
   bNode *node_dst = MEM_mallocN<bNode>(__func__);
   *node_dst = node_src;
-
   node_dst->runtime = MEM_new<bNodeRuntime>(__func__);
+  if (dst_unique_name) {
+    STRNCPY_UTF8(node_dst->name, dst_unique_name->c_str());
+  }
+  else if (dst_tree) {
+    node_unique_name(*dst_tree, *node_dst);
+  }
+  if (dst_unique_identifier) {
+    node_dst->identifier = *dst_unique_identifier;
+  }
+  else if (dst_tree) {
+    node_unique_id(*dst_tree, *node_dst);
+  }
 
   /* Can be called for nodes outside a node tree (e.g. clipboard). */
   if (dst_tree) {
-    if (use_unique) {
-      node_unique_name(*dst_tree, *node_dst);
-      node_unique_id(*dst_tree, *node_dst);
-    }
     BLI_addtail(&dst_tree->nodes, node_dst);
   }
 
@@ -3566,7 +3574,13 @@ void node_socket_move_default_value(Main & /*bmain*/,
 bNode *node_copy(bNodeTree *dst_tree, const bNode &src_node, const int flag, const bool use_unique)
 {
   Map<const bNodeSocket *, bNodeSocket *> socket_map;
-  return node_copy_with_mapping(dst_tree, src_node, flag, use_unique, socket_map);
+  return node_copy_with_mapping(
+      dst_tree,
+      src_node,
+      flag,
+      use_unique ? std::nullopt : std::make_optional<StringRefNull>(src_node.name),
+      use_unique ? std::nullopt : std::make_optional(src_node.identifier),
+      socket_map);
 }
 
 static int node_count_links(const bNodeTree *ntree, const bNodeSocket *socket)

source/blender/editors/space_node/node_group.cc
@@ -531,7 +531,8 @@ static bool node_group_separate_selected(
   for (bNode *node : nodes_to_move) {
     bNode *newnode;
     if (make_copy) {
-      newnode = bke::node_copy_with_mapping(&ntree, *node, LIB_ID_COPY_DEFAULT, true, socket_map);
+      newnode = bke::node_copy_with_mapping(
+          &ntree, *node, LIB_ID_COPY_DEFAULT, std::nullopt, std::nullopt, socket_map);
       node_identifier_map.add(node->identifier, newnode->identifier);
     }
     else {

source/blender/editors/space_node/clipboard.cc
@@ -227,8 +227,13 @@ struct NodeClipboard {
   {
     /* No ID reference-counting, this node is virtual,
      * detached from any actual Blender data currently. */
-    bNode *new_node = bke::node_copy_with_mapping(
-        nullptr, node, LIB_ID_CREATE_NO_USER_REFCOUNT | LIB_ID_CREATE_NO_MAIN, false, socket_map);
+    bNode *new_node = bke::node_copy_with_mapping(nullptr,
+                                                  node,
+                                                  LIB_ID_CREATE_NO_USER_REFCOUNT |
+                                                      LIB_ID_CREATE_NO_MAIN,
+                                                  node.name,
+                                                  node.identifier,
+                                                  socket_map);
     node_map.add_new(&node, new_node);
 
     /* Find a new valid ID pointer for all ID usages in given node. */
@@ -387,7 +392,7 @@ static wmOperatorStatus node_clipboard_paste_exec(bContext *C, wmOperator *op)
     /* Do not access referenced ID pointers here, as they are still the old ones, which may be
      * invalid. */
     bNode *new_node = bke::node_copy_with_mapping(
-        &tree, node, LIB_ID_CREATE_NO_USER_REFCOUNT, true, socket_map);
+        &tree, node, LIB_ID_CREATE_NO_USER_REFCOUNT, std::nullopt, std::nullopt, socket_map);
     /* Update the newly copied node's ID references. */
     clipboard.paste_update_node_id_references(*new_node);
     /* Reset socket shape in case a node is copied to a different tree type. */

source/blender/editors/space_node/node_edit.cc
@@ -1425,7 +1425,7 @@ static wmOperatorStatus node_duplicate_exec(bContext *C, wmOperator *op)
 
   for (bNode *node : get_selected_nodes(*ntree)) {
     bNode *new_node = bke::node_copy_with_mapping(
-        ntree, *node, LIB_ID_COPY_DEFAULT, true, socket_map);
+        ntree, *node, LIB_ID_COPY_DEFAULT, std::nullopt, std::nullopt, socket_map);
     node_map.add_new(node, new_node);
 
     if (node->id && dupli_node_tree) {

commit 6dd4280f5ab2513c065b95772c61acfbba6c8aeb

Fix: crash due to wrong duplicate node name handling
Generally, node names are expected to be unique within a node tree. However,
`ntree_shader_copy_branch` currently does not enforce this for performance
reasons. This was broken by recent changes in 5b73f798d0 and 9fd877e174 which
allows for passing already unique names and identifiers to the node copy
function.

The main problem here was that the `use_unique` parameter of the `node_copy`
function was not well defined. Now, this function is completely removed in favor
of using `node_copy_with_mapping` directly. This also has a new
`allow_duplicate_names` parameter now which makes the expected behavior more
explicit.

Pull Request: https://projects.blender.org/blender/blender/pulls/143951

source/blender/blenkernel/BKE_node.hh
@@ -965,7 +965,8 @@ void node_rebuild_id_vector(bNodeTree &node_tree);
 /**
  * \note keeps socket list order identical, for copying links.
  * \param dst_name: The name of the copied node. This is expected to be unique in the destination
- * tree if provided. If not provided, the src name is used and made unique.
+ *   tree if provided. If not provided, the src name is used and is made unique unless
+ *   allow_duplicate_names is true.
  * \param dst_identifier: Same ad dst_name, but for the identifier.
  */
 bNode *node_copy_with_mapping(bNodeTree *dst_tree,
@@ -973,9 +974,8 @@ bNode *node_copy_with_mapping(bNodeTree *dst_tree,
                               int flag,
                               std::optional<StringRefNull> dst_unique_name,
                               std::optional<int> dst_unique_identifier,
-                              Map<const bNodeSocket *, bNodeSocket *> &new_socket_map);
-
-bNode *node_copy(bNodeTree *dst_tree, const bNode &src_node, int flag, bool use_unique);
+                              Map<const bNodeSocket *, bNodeSocket *> &new_socket_map,
+                              bool allow_duplicate_names = false);
 
 /**
  * Move socket default from \a src (input socket) to locations specified by \a dst (output socket).

source/blender/blenkernel/intern/node.cc
@@ -3356,7 +3356,8 @@ bNode *node_copy_with_mapping(bNodeTree *dst_tree,
                               const int flag,
                               const std::optional<StringRefNull> dst_unique_name,
                               const std::optional<int> dst_unique_identifier,
-                              Map<const bNodeSocket *, bNodeSocket *> &socket_map)
+                              Map<const bNodeSocket *, bNodeSocket *> &socket_map,
+                              const bool allow_duplicate_names)
 {
   bNode *node_dst = MEM_mallocN<bNode>(__func__);
   *node_dst = node_src;
@@ -3366,7 +3367,9 @@ bNode *node_copy_with_mapping(bNodeTree *dst_tree,
     STRNCPY_UTF8(node_dst->name, dst_unique_name->c_str());
   }
   else if (dst_tree) {
-    node_unique_name(*dst_tree, *node_dst);
+    if (!allow_duplicate_names) {
+      node_unique_name(*dst_tree, *node_dst);
+    }
   }
   if (dst_unique_identifier) {
     node_dst->identifier = *dst_unique_identifier;
@@ -3574,18 +3577,6 @@ void node_socket_move_default_value(Main & /*bmain*/,
   {
     src_type.value_initialize(src_value);
   }
-}
-
-bNode *node_copy(bNodeTree *dst_tree, const bNode &src_node, const int flag, const bool use_unique)
-{
-  Map<const bNodeSocket *, bNodeSocket *> socket_map;
-  return node_copy_with_mapping(
-      dst_tree,
-      src_node,
-      flag,
-      use_unique ? std::nullopt : std::make_optional<StringRefNull>(src_node.name),
-      use_unique ? std::nullopt : std::make_optional(src_node.identifier),
-      socket_map);
 }
 
 static int node_count_links(const bNodeTree *ntree, const bNodeSocket *socket)

source/blender/editors/space_node/node_group.cc
@@ -1331,7 +1331,10 @@ static bNodeTree *node_group_make_wrapper(const bContext &C,
   /* Add the node that make up the wrapper node group. */
   bNode &input_node = *bke::node_add_static_node(&C, *dst_group, NODE_GROUP_INPUT);
   bNode &output_node = *bke::node_add_static_node(&C, *dst_group, NODE_GROUP_OUTPUT);
-  bNode &inner_node = *bke::node_copy(dst_group, src_node, 0, true);
+
+  Map<const bNodeSocket *, bNodeSocket *> inner_node_socket_mapping;
+  bNode &inner_node = *bke::node_copy_with_mapping(
+      dst_group, src_node, 0, std::nullopt, std::nullopt, inner_node_socket_mapping);
 
   /* Position nodes. */
   input_node.location[0] = -300 - input_node.width;

source/blender/nodes/shader/node_shader_tree.cc
@@ -647,11 +647,15 @@ static void ntree_shader_copy_branch(bNodeTree *ntree,
       int id = node->runtime->tmp_flag;
       /* Avoid creating unique names in the new tree, since it is very slow.
        * The names on the new nodes will be invalid. */
-      nodes_copy[id] = blender::bke::node_copy(
-          ntree, *node, LIB_ID_CREATE_NO_USER_REFCOUNT | LIB_ID_CREATE_NO_MAIN, false);
-      /* But identifiers must be created for the `bNodeTree::all_nodes()` vector,
-       * so they won't match the original. */
-      blender::bke::node_unique_id(*ntree, *nodes_copy[id]);
+      blender::Map<const bNodeSocket *, bNodeSocket *> socket_map;
+      nodes_copy[id] = blender::bke::node_copy_with_mapping(ntree,
+                                                            *node,
+                                                            LIB_ID_CREATE_NO_USER_REFCOUNT |
+                                                                LIB_ID_CREATE_NO_MAIN,
+                                                            std::nullopt,
+                                                            std::nullopt,
+                                                            socket_map,
+                                                            true);
 
       bNode *copy = nodes_copy[id];
       copy->runtime->tmp_flag = -2; /* Copy */