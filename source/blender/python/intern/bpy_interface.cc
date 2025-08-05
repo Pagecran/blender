Pagecran/blender

commit 9f056204e5128d91665b46b8657b39593cbeaa23

Cleanup: use UTF8 string functions in source/blender/python
Use UTF8 aware functions unless raw bytes are expected.

source/blender/python/intern/bpy_interface.cc
@@ -21,7 +21,7 @@
 #include "CLG_log.h"
 
 #include "BLI_path_utils.hh"
-#include "BLI_string.h"
+#include "BLI_string_utf8.h"
 #include "BLI_threads.h"
 #include "BLI_utildefines.h"
 
@@ -723,7 +723,7 @@ void BPY_modules_load_user(bContext *C)
       if (!(G.f & G_FLAG_SCRIPT_AUTOEXEC)) {
         if (!(G.f & G_FLAG_SCRIPT_AUTOEXEC_FAIL_QUIET)) {
           G.f |= G_FLAG_SCRIPT_AUTOEXEC_FAIL;
-          SNPRINTF(G.autoexec_fail, RPT_("Text '%s'"), text->id.name + 2);
+          SNPRINTF_UTF8(G.autoexec_fail, RPT_("Text '%s'"), text->id.name + 2);
 
           printf("scripts disabled for \"%s\", skipping '%s'\n",
                  BKE_main_blendfile_path(bmain),

source/blender/python/generic/py_capi_utils.cc
@@ -24,7 +24,7 @@
 #ifndef MATH_STANDALONE
 #  include "MEM_guardedalloc.h"
 
-#  include "BLI_string.h"
+#  include "BLI_string_utf8.h"
 #endif
 
 #ifdef _WIN32
@@ -591,7 +591,7 @@ void PyC_ObSpitStr(char *result, size_t result_maxncpy, PyObject *var)
   /* No name, creator of string can manage that. */
   const char *null_str = "<null>";
   if (var == nullptr) {
-    BLI_strncpy(result, null_str, result_maxncpy);
+    BLI_strncpy_utf8(result, null_str, result_maxncpy);
   }
   else {
     const PyTypeObject *type = Py_TYPE(var);
@@ -601,13 +601,13 @@ void PyC_ObSpitStr(char *result, size_t result_maxncpy, PyObject *var)
        * but this may be used for generating errors - so don't for now. */
       PyErr_Clear();
     }
-    BLI_snprintf(result,
-                 result_maxncpy,
-                 " ref=%d, ptr=%p, type=%s, value=%.200s",
-                 int(var->ob_refcnt),
-                 (void *)var,
-                 type ? type->tp_name : null_str,
-                 var_str ? PyUnicode_AsUTF8(var_str) : "<error>");
+    BLI_snprintf_utf8(result,
+                      result_maxncpy,
+                      " ref=%d, ptr=%p, type=%s, value=%.200s",
+                      int(var->ob_refcnt),
+                      (void *)var,
+                      type ? type->tp_name : null_str,
+                      var_str ? PyUnicode_AsUTF8(var_str) : "<error>");
     if (var_str != nullptr) {
       Py_DECREF(var_str);
     }

source/blender/python/gpu/gpu_py_offscreen.cc
@@ -14,7 +14,7 @@
 
 #include <Python.h>
 
-#include "BLI_string.h"
+#include "BLI_string_utf8.h"
 
 #include "BKE_global.hh"
 #include "BKE_lib_id.hh" /* For #BKE_id_is_in_global_main. */
@@ -315,7 +315,7 @@ static PyObject *pygpu_offscreen__tp_new(PyTypeObject * /*self*/, PyObject *args
                                err_out);
   }
   else {
-    STRNCPY(err_out, "No active GPU context found");
+    STRNCPY_UTF8(err_out, "No active GPU context found");
   }
 
   if (ofs == nullptr) {

source/blender/python/gpu/gpu_py_texture.cc
@@ -14,7 +14,7 @@
 #include <Python.h>
 
 #include "BLI_math_base.h"
-#include "BLI_string.h"
+#include "BLI_string_utf8.h"
 
 #include "DNA_image_types.h"
 
@@ -212,17 +212,18 @@ static PyObject *pygpu_texture__tp_new(PyTypeObject * /*self*/, PyObject *args, 
 
   blender::gpu::Texture *tex = nullptr;
   if (is_cubemap && len != 1) {
-    STRNCPY(err_out,
-            "In cubemaps the same dimension represents height, width and depth. No tuple needed");
+    STRNCPY_UTF8(
+        err_out,
+        "In cubemaps the same dimension represents height, width and depth. No tuple needed");
   }
   else if (size[0] < 1 || size[1] < 1 || size[2] < 1) {
-    STRNCPY(err_out, "Values less than 1 are not allowed in dimensions");
+    STRNCPY_UTF8(err_out, "Values less than 1 are not allowed in dimensions");
   }
   else if (layers && len == 3) {
-    STRNCPY(err_out, "3D textures have no layers");
+    STRNCPY_UTF8(err_out, "3D textures have no layers");
   }
   else if (!GPU_context_active_get()) {
-    STRNCPY(err_out, "No active GPU context found");
+    STRNCPY_UTF8(err_out, "No active GPU context found");
   }
   else {
     const char *name = "python_texture";

source/blender/python/gpu/gpu_py_uniformbuffer.cc
@@ -13,7 +13,7 @@
 
 #include <Python.h>
 
-#include "BLI_string.h"
+#include "BLI_string_utf8.h"
 
 #include "GPU_context.hh"
 #include "GPU_uniform_buffer.hh"
@@ -79,7 +79,7 @@ static PyObject *pygpu_uniformbuffer__tp_new(PyTypeObject * /*self*/,
   }
 
   if (!GPU_context_active_get()) {
-    STRNCPY(err_out, "No active GPU context found");
+    STRNCPY_UTF8(err_out, "No active GPU context found");
   }
   else {
     Py_buffer pybuffer;
@@ -89,7 +89,7 @@ static PyObject *pygpu_uniformbuffer__tp_new(PyTypeObject * /*self*/,
     }
 
     if ((pybuffer.len % 16) != 0) {
-      STRNCPY(err_out, "UBO is not padded to size of vec4");
+      STRNCPY_UTF8(err_out, "UBO is not padded to size of vec4");
     }
     else {
       ubo = GPU_uniformbuf_create_ex(pybuffer.len, pybuffer.buf, "python_uniformbuffer");

source/blender/python/intern/bpy_app_translations.cc
@@ -275,7 +275,7 @@ std::optional<StringRefNull> BPY_app_translations_py_pgettext(const StringRef ms
     /* This function may be called from C (i.e. outside of python interpreter 'context'). */
     PyGILState_STATE _py_state = PyGILState_Ensure();
 
-    STRNCPY(locale, tmp);
+    STRNCPY_UTF8(locale, tmp);
 
     /* Locale changed or cache does not exist, refresh the whole cache! */
     _build_translations_cache(_translations->py_messages, locale);

source/blender/python/intern/bpy_driver.cc
@@ -15,7 +15,7 @@
 #include "DNA_anim_types.h"
 
 #include "BLI_listbase.h"
-#include "BLI_string.h"
+#include "BLI_string_utf8.h"
 
 #include "BKE_animsys.h"
 #include "BKE_fcurve_driver.h"
@@ -556,7 +556,7 @@ float BPY_driver_exec(PathResolvedRNA *anim_rna,
   if (!(G.f & G_FLAG_SCRIPT_AUTOEXEC)) {
     if (!(G.f & G_FLAG_SCRIPT_AUTOEXEC_FAIL_QUIET)) {
       G.f |= G_FLAG_SCRIPT_AUTOEXEC_FAIL;
-      SNPRINTF(G.autoexec_fail, "Driver '%s'", expr);
+      SNPRINTF_UTF8(G.autoexec_fail, "Driver '%s'", expr);
 
       printf("skipping driver '%s', automatic scripts are disabled\n", expr);
     }
@@ -718,7 +718,7 @@ float BPY_driver_exec(PathResolvedRNA *anim_rna,
       {
         if (!(G.f & G_FLAG_SCRIPT_AUTOEXEC_FAIL_QUIET)) {
           G.f |= G_FLAG_SCRIPT_AUTOEXEC_FAIL;
-          SNPRINTF(G.autoexec_fail, "Driver '%s'", expr);
+          SNPRINTF_UTF8(G.autoexec_fail, "Driver '%s'", expr);
         }
 
         Py_DECREF(expr_code);

source/blender/python/intern/bpy_interface_run.cc
@@ -16,6 +16,7 @@
 #include "BLI_listbase.h"
 #include "BLI_path_utils.hh"
 #include "BLI_string.h"
+#include "BLI_string_utils.hh"
 
 #include "BKE_context.hh"
 #include "BKE_library.hh"
@@ -57,12 +58,8 @@ static void bpy_text_filepath_get(char *filepath,
                                   const Main *bmain,
                                   const Text *text)
 {
-  BLI_snprintf(filepath,
-               filepath_maxncpy,
-               "%s%c%s",
-               ID_BLEND_PATH(bmain, &text->id),
-               SEP,
-               text->id.name + 2);
+  BLI_string_join_by_sep_char(
+      filepath, filepath_maxncpy, SEP, ID_BLEND_PATH(bmain, &text->id), text->id.name + 2);
 }
 
 /**

commit 43d5d8ce5bebc68a71ef319332ab17097209864e

Build: unbreak WITH_PYTHON_MODULE=ON
Caused by 9f056204e5128d91665b46b8657b39593cbeaa23.

source/blender/python/intern/bpy_interface.cc
@@ -24,6 +24,9 @@
 #include "BLI_string_utf8.h"
 #include "BLI_threads.h"
 #include "BLI_utildefines.h"
+#ifdef WITH_PYTHON_MODULE
+#  include "BLI_string.h"
+#endif
 
 #include "BLT_translation.hh"
 