Pagecran/blender

commit a43359eb88e898c8e1960564705bf80104b60295

Anim: Store pose bone visibility flag on pose bone
This PR adds a flag to the pose bone that determines its visibility.
Doing so allows to hide a pose bone and
* don't affect other instances of the same armature
* save the visibility even if the rig is linked

The visibility of the bone in object mode is now also determined by the
pose bone whereas before it was the `Bone`.

**This breaks backwards compatibility** on the Python API since the visibility property,
on the `Bone` behaves differently now as it hides the edit bone instead of the pose bone.

In order to remove all active uses of `BONE_HIDDEN_P` the changes in `armature_skinning.cc` are required.

Part of #138482

Pull Request: https://projects.blender.org/blender/blender/pulls/139167

source/blender/makesrna/intern/rna_armature.cc
@@ -720,11 +720,9 @@ static void rna_Bone_hide_update(Main * /*bmain*/, Scene * /*scene*/, PointerRNA
 {
   bArmature *arm = (bArmature *)ptr->owner_id;
   Bone *bone = (Bone *)ptr->data;
-
-  if (bone->flag & (BONE_HIDDEN_P | BONE_UNSELECTABLE)) {
+  if (bone->flag & (BONE_HIDDEN_A | BONE_UNSELECTABLE)) {
     bone->flag &= ~(BONE_SELECTED | BONE_TIPSEL | BONE_ROOTSEL);
   }
-
   WM_main_add_notifier(NC_OBJECT | ND_POSE, arm);
   DEG_id_tag_update(&arm->id, ID_RECALC_SYNC_TO_EVAL);
 }
@@ -1778,16 +1776,11 @@ static void rna_def_bone(BlenderRNA *brna)
 
   RNA_define_lib_overridable(true);
 
-  /* XXX should we define this in PoseChannel wrapping code instead?
-   *     But PoseChannels directly get some of their flags from here... */
   prop = RNA_def_property(srna, "hide", PROP_BOOLEAN, PROP_NONE);
-  RNA_def_property_boolean_sdna(prop, nullptr, "flag", BONE_HIDDEN_P);
-  RNA_def_property_ui_text(
-      prop,
-      "Hide",
-      "Bone is not visible when it is not in Edit Mode (i.e. in Object or Pose Modes)");
+  RNA_def_property_boolean_sdna(prop, nullptr, "flag", BONE_HIDDEN_A);
+  RNA_def_property_ui_text(prop, "Hide", "Bone is not visible when it is in Edit Mode");
   RNA_def_property_ui_icon(prop, ICON_RESTRICT_VIEW_OFF, -1);
-  RNA_def_property_update(prop, 0, "rna_Bone_hide_update");
+  RNA_def_property_update(prop, 0, "rna_EditBone_hide_update");
 
   prop = RNA_def_property(srna, "select", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "flag", BONE_SELECTED);

scripts/startup/bl_ui/properties_data_bone.py
@@ -323,17 +323,18 @@ class BONE_PT_display(BoneButtonsPanel, Panel):
         bone = context.bone
 
         col = layout.column()
-        col.prop(bone, "hide", text="Hide", toggle=False)
+        # Figure out the pose bone.
+        ob = context.object
+        pose_bone = ob and ob.pose.bones[bone.name]
         hide_select_sub = col.column()
-        hide_select_sub.active = not bone.hide
+        if pose_bone:
+            col.prop(pose_bone, "hide", text="Hide", toggle=False)
+            hide_select_sub.active = not pose_bone.hide
         hide_select_sub.prop(bone, "hide_select", invert_checkbox=True)
         col.prop(bone, "display_type", text="Display As")
 
-        # Figure out the pose bone.
-        ob = context.object
-        if not ob:
+        if not pose_bone:
             return
-        pose_bone = ob.pose.bones[bone.name]
 
         # Allow the layout to use the space normally occupied by the 'set a key' diamond.
         layout.use_property_decorate = False

source/blender/animrig/ANIM_armature.hh
@@ -27,7 +27,8 @@ inline bool bone_is_visible(const bArmature *armature, const Bone *bone)
 
 inline bool bone_is_visible_pchan(const bArmature *armature, const bPoseChannel *pchan)
 {
-  return bone_is_visible(armature, pchan->bone);
+  const bool bone_itself_visible = (pchan->drawflag & PCHAN_DRAW_HIDDEN) == 0;
+  return bone_itself_visible && ANIM_bone_in_visible_collection(armature, pchan->bone);
 }
 
 inline bool bone_is_visible_editbone(const bArmature *armature, const EditBone *ebone)
@@ -54,5 +55,13 @@ inline bool bone_is_selected(const bArmature *armature, const EditBone *ebone)
 {
   return (ebone->flag & BONE_SELECTED) && bone_is_visible_editbone(armature, ebone);
 }
+
+/**
+ * Iterates all descendents of the given pose bone including the bone itself. Iterates breadth
+ * first.
+ */
+void pose_bone_descendent_iterator(bPose &pose,
+                                   bPoseChannel &pose_bone,
+                                   FunctionRef<void(bPoseChannel &child_bone)> callback);
 
 }  // namespace blender::animrig

source/blender/animrig/CMakeLists.txt
@@ -24,6 +24,7 @@ set(SRC
   intern/action_selection.cc
   intern/anim_rna.cc
   intern/animdata.cc
+  intern/armature.cc
   intern/bone_collections.cc
   intern/bonecolor.cc
   intern/driver.cc

source/blender/animrig/intern/armature.cc
@@ -1,0 +1,41 @@
+/* SPDX-FileCopyrightText: 2025 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup animrig
+ */
+
+#include <deque>
+
+#include "ANIM_armature.hh"
+#include "BKE_action.hh"
+#include "BLI_listbase.h"
+
+namespace blender::animrig {
+
+void pose_bone_descendent_iterator(bPose &pose,
+                                   bPoseChannel &pose_bone,
+                                   FunctionRef<void(bPoseChannel &child_bone)> callback)
+{
+  /* Needed for fast name lookups. */
+  BKE_pose_channels_hash_ensure(&pose);
+
+  std::deque<bPoseChannel *> to_visit = {&pose_bone};
+  while (!to_visit.empty()) {
+    bPoseChannel *descendant = to_visit.front();
+    to_visit.pop_front();
+    callback(*descendant);
+    LISTBASE_FOREACH (Bone *, child_bone, &descendant->bone->childbase) {
+      bPoseChannel *child_pose_bone = BKE_pose_channel_find_name(&pose, child_bone->name);
+      if (!child_pose_bone) {
+        /* Can happen if the pose is not rebuilt. */
+        BLI_assert_unreachable();
+        continue;
+      }
+      to_visit.push_back(child_pose_bone);
+    }
+  }
+};
+
+}  // namespace blender::animrig

source/blender/blenkernel/BKE_armature.hh
@@ -576,7 +576,7 @@ void BKE_pchan_bbone_deform_segment_index(const bPoseChannel *pchan,
   for (bPoseChannel *_pchan = (bPoseChannel *)(_ob)->pose->chanbase.first; _pchan; \
        _pchan = _pchan->next) \
   { \
-    if (blender::animrig::bone_is_visible(((bArmature *)(_ob)->data), (_pchan)->bone) && \
+    if (blender::animrig::bone_is_visible_pchan(((bArmature *)(_ob)->data), _pchan) && \
         ((_pchan)->bone->flag & BONE_SELECTED)) \
     {
 #define FOREACH_PCHAN_SELECTED_IN_OBJECT_END \
@@ -588,7 +588,7 @@ void BKE_pchan_bbone_deform_segment_index(const bPoseChannel *pchan,
   for (bPoseChannel *_pchan = (bPoseChannel *)(_ob)->pose->chanbase.first; _pchan; \
        _pchan = _pchan->next) \
   { \
-    if (blender::animrig::bone_is_visible(((bArmature *)(_ob)->data), (_pchan)->bone)) {
+    if (blender::animrig::bone_is_visible_pchan(((bArmature *)(_ob)->data), _pchan)) {
 #define FOREACH_PCHAN_VISIBLE_IN_OBJECT_END \
   } \
   } \

source/blender/blenloader/intern/versioning_500.cc
@@ -1585,6 +1585,22 @@ void do_versions_after_linking_500(FileData *fd, Main *bmain)
     FOREACH_NODETREE_END;
   }
 
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 54)) {
+    LISTBASE_FOREACH (Object *, object, &bmain->objects) {
+      if (object->type != OB_ARMATURE) {
+        continue;
+      }
+      LISTBASE_FOREACH (bPoseChannel *, pose_bone, &object->pose->chanbase) {
+        if (pose_bone->bone->flag & BONE_HIDDEN_P) {
+          pose_bone->drawflag |= PCHAN_DRAW_HIDDEN;
+        }
+        else {
+          pose_bone->drawflag &= ~PCHAN_DRAW_HIDDEN;
+        }
+      }
+    }
+  }
+
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.

source/blender/draw/engines/overlay/overlay_armature.cc
@@ -2022,11 +2022,11 @@ void Armatures::draw_armature_pose(Armatures::DrawContext *ctx)
   for (bPoseChannel *pchan = static_cast<bPoseChannel *>(ob->pose->chanbase.first); pchan;
        pchan = pchan->next, index += 0x10000)
   {
-    Bone *bone = pchan->bone;
-    if (!blender::animrig::bone_is_visible(&arm, bone)) {
+    if (!blender::animrig::bone_is_visible_pchan(&arm, pchan)) {
       continue;
     }
 
+    Bone *bone = pchan->bone;
     const bool draw_dofs = !is_pose_select && ctx->show_relations &&
                            (ctx->draw_mode == ARM_DRAW_MODE_POSE) &&
                            (bone->flag & BONE_SELECTED) &&
@@ -2042,7 +2042,7 @@ void Armatures::draw_armature_pose(Armatures::DrawContext *ctx)
     }
 
     eBone_Flag boneflag = eBone_Flag(bone->flag);
-    if (bone->parent && !blender::animrig::bone_is_visible(&arm, bone->parent)) {
+    if (pchan->parent && !blender::animrig::bone_is_visible_pchan(&arm, pchan->parent)) {
       /* Avoid drawing connection line to hidden parent. */
       boneflag &= ~BONE_CONNECTED;
     }

commit 14982958cad9bd0b81e014c309ae42449725c6e6

Fix #144364: Incorrect RNA update function called for Bones
The issue was that an rna function was called on `Bone`
that expected an `EditBone`. Because the pointer was just casted,
this means it would write into wrong bytes on the struct.

Caused by a43359eb88e898c8e1960564705bf80104b60295

Pull Request: https://projects.blender.org/blender/blender/pulls/144447

source/blender/makesrna/intern/rna_armature.cc
@@ -1780,7 +1780,7 @@ static void rna_def_bone(BlenderRNA *brna)
   RNA_def_property_boolean_sdna(prop, nullptr, "flag", BONE_HIDDEN_A);
   RNA_def_property_ui_text(prop, "Hide", "Bone is not visible when it is in Edit Mode");
   RNA_def_property_ui_icon(prop, ICON_RESTRICT_VIEW_OFF, -1);
-  RNA_def_property_update(prop, 0, "rna_EditBone_hide_update");
+  RNA_def_property_update(prop, 0, "rna_Bone_hide_update");
 
   prop = RNA_def_property(srna, "select", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "flag", BONE_SELECTED);