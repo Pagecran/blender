Pagecran/blender

commit 39990303a023cee65eab26f6d5004f90f251c755

Cleanup: pass "Curve" as a const reference for font evaluation
Remove a redundant `const_cast`.

source/blender/blenkernel/intern/curve.cc
@@ -5098,7 +5098,7 @@ std::optional<blender::Bounds<blender::float3>> BKE_curve_minmax(const Curve *cu
   if (is_font) {
     ListBase temp_nurb_lb{};
     BKE_vfont_to_curve_ex(nullptr,
-                          const_cast<Curve *>(cu),
+                          *cu,
                           FO_EDIT,
                           &temp_nurb_lb,
                           nullptr,

source/blender/blenkernel/BKE_vfont.hh
@@ -135,7 +135,7 @@ int BKE_vfont_cursor_to_text_index(Object *ob, const float cursor_location[2]);
  * \warning Expects to have access to evaluated data (i.e. passed object should be evaluated one).
  */
 bool BKE_vfont_to_curve(Object *ob, eEditFontMode mode);
-void BKE_vfont_char_build(Curve *cu,
+void BKE_vfont_char_build(const Curve &cu,
                           ListBase *nubase,
                           unsigned int charcode,
                           const CharInfo *info,
@@ -147,7 +147,7 @@ void BKE_vfont_char_build(Curve *cu,
                           float fsize);
 
 bool BKE_vfont_to_curve_ex(Object *ob,
-                           Curve *cu,
+                           const Curve &cu,
                            eEditFontMode mode,
                            ListBase *r_nubase,
                            const char32_t **r_text,

source/blender/blenkernel/intern/object_dupli.cc
@@ -827,10 +827,15 @@ static Object *find_family_object(
 
 static void make_duplis_font(const DupliContext *ctx)
 {
+  /* Font dupli-verts not supported inside collections. */
+  if (ctx->collection) {
+    return;
+  }
+
   Object *par = ctx->object;
   GHash *family_gh;
   Object *ob;
-  Curve *cu;
+  Curve *cu = (Curve *)par->data;
   CharTrans *ct, *chartransdata = nullptr;
   float vec[3], obmat[4][4], pmat[4][4], fsize, xof, yof;
   int text_len, a;
@@ -838,30 +843,17 @@ static void make_duplis_font(const DupliContext *ctx)
   const char32_t *text = nullptr;
   bool text_free = false;
 
-  /* Font dupli-verts not supported inside collections. */
-  if (ctx->collection) {
-    return;
-  }
-
   copy_m4_m4(pmat, par->object_to_world().ptr());
 
   /* In `par` the family name is stored, use this to find the other objects. */
 
-  BKE_vfont_to_curve_ex(par,
-                        (Curve *)par->data,
-                        FO_DUPLI,
-                        nullptr,
-                        &text,
-                        &text_len,
-                        &text_free,
-                        &chartransdata,
-                        nullptr);
+  BKE_vfont_to_curve_ex(
+      par, *cu, FO_DUPLI, nullptr, &text, &text_len, &text_free, &chartransdata, nullptr);
 
   if (text == nullptr || chartransdata == nullptr) {
     return;
   }
 
-  cu = (Curve *)par->data;
   fsize = cu->fsize;
   xof = cu->xof;
   yof = cu->yof;

source/blender/editors/object/object_add.cc
@@ -3601,7 +3601,7 @@ static Object *convert_font_to_curve_legacy_generic(Object *ob,
 
   Object *ob_eval = DEG_get_evaluated(info.depsgraph, ob);
   BKE_vfont_to_curve_ex(ob_eval,
-                        static_cast<Curve *>(ob_eval->data),
+                        *static_cast<const Curve *>(ob_eval->data),
                         FO_EDIT,
                         &cu->nurb,
                         nullptr,

source/blender/nodes/geometry/nodes/node_geo_string_to_curves.cc
@@ -207,7 +207,7 @@ static std::optional<TextLayout> get_text_layout(GeoNodeExecParams &params)
   float final_font_size = 0.0f;
   /* Mode FO_DUPLI used because it doesn't create curve splines. */
   BKE_vfont_to_curve_ex(nullptr,
-                        &cu,
+                        cu,
                         FO_DUPLI,
                         nullptr,
                         &r_text,
@@ -279,7 +279,7 @@ static Map<int, int> create_curve_instances(GeoNodeExecParams &params,
     CharInfo charinfo = {0};
     charinfo.mat_nr = 1;
 
-    BKE_vfont_char_build(&cu, &cu.nurb, layout.char_codes[i], &charinfo, false, 0, 0, 0, i, 1);
+    BKE_vfont_char_build(cu, &cu.nurb, layout.char_codes[i], &charinfo, false, 0, 0, 0, i, 1);
     Curves *curves_id = bke::curve_legacy_to_curves(cu);
     if (curves_id == nullptr) {
       if (pivot_required) {

commit 21823de0b0752540806b85cf44706b8e0fbf4dcc

Cleanup: Formatting
Caused by 39990303a023cee65eab26f6d5004f90f251c755.

source/blender/blenkernel/intern/curve.cc
@@ -5097,15 +5097,8 @@ std::optional<blender::Bounds<blender::float3>> BKE_curve_minmax(const Curve *cu
    */
   if (is_font) {
     ListBase temp_nurb_lb{};
-    BKE_vfont_to_curve_ex(nullptr,
-                          *cu,
-                          FO_EDIT,
-                          &temp_nurb_lb,
-                          nullptr,
-                          nullptr,
-                          nullptr,
-                          nullptr,
-                          nullptr);
+    BKE_vfont_to_curve_ex(
+        nullptr, *cu, FO_EDIT, &temp_nurb_lb, nullptr, nullptr, nullptr, nullptr, nullptr);
     BLI_SCOPED_DEFER([&]() { BKE_nurbList_free(&temp_nurb_lb); });
     return calc_nurblist_bounds(&temp_nurb_lb, false);
   }