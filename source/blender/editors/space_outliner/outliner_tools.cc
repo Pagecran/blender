6193ef9478f4c8e66ffdb48c71d666a329435133
Pascal André <pascalandr@gmail.com>
2025-10-12 17:41:37 +0200

    Add Library Override Push Back functionality to Outliner
    Implements a new "Push Back" feature for library overrides, allowing
    users to push property modifications from local overrides back to their
    source .blend library files without opening them in the editor. Previous
    commit was the core of the feature, this one connect the UI

--- source/blender/editors/space_outliner/outliner_tools.cc
+++ source/blender/editors/space_outliner/outliner_tools.cc
@@ -1787,6 +1787,7 @@ enum eOutlinerLibOverrideOpTypes {
   OUTLINER_LIBOVERRIDE_OP_RESYNC_HIERARCHY,
   OUTLINER_LIBOVERRIDE_OP_RESYNC_HIERARCHY_ENFORCE,
   OUTLINER_LIBOVERRIDE_OP_DELETE_HIERARCHY,
+  OUTLINER_LIBOVERRIDE_OP_PUSH_BACK,
 };
 
 static const EnumPropertyItem prop_liboverride_op_types[] = {
@@ -1807,6 +1808,11 @@ static const EnumPropertyItem prop_liboverride_op_types[] = {
      "Clear",
      "Delete the selected local overrides and relink their usages to the linked data-blocks if "
      "possible, else reset them and mark them as non editable"},
+    {OUTLINER_LIBOVERRIDE_OP_PUSH_BACK,
+     "OVERRIDE_LIBRARY_PUSH_BACK",
+     0,
+     "Push Back",
+     "Push modifications from the selected overrides back into their linked library"},
     {0, nullptr, 0, nullptr, nullptr},
 };
 
@@ -1996,6 +2002,14 @@ static wmOperatorStatus outliner_liboverride_operation_exec(bContext *C, wmOpera
       id_override_library_delete_hierarchy_process(C, op->reports, override_data);
 
       ED_undo_push(C, "Delete Overridden Data Hierarchy");
+      break;
+    }
+    case OUTLINER_LIBOVERRIDE_OP_PUSH_BACK: {
+      wmWindowManager *wm = CTX_wm_manager(C);
+      wm->op_undo_depth++;
+      WM_operator_name_call(
+          C, "OBJECT_OT_library_override_pushback", WM_OP_EXEC_DEFAULT, nullptr, nullptr);
+      wm->op_undo_depth--;
       break;
     }
     default:
