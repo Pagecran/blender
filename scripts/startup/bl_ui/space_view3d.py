Pagecran/blender

commit 1fa2be8a9ba825d8c7d0a56370932161664cc20c

Fix #147394: Grease Pencil: Opacity operator missing for the menu
The Grease Pencil operator for changing the Opacity of selected strokes
was missing from the transform menu, which is the where the `Radius`
operator lives.

This prevented the operator from being searched.

4.5 backport candidate.

Pull Request: https://projects.blender.org/blender/blender/pulls/147672

scripts/startup/bl_ui/space_view3d.py
@@ -1291,6 +1291,8 @@ class VIEW3D_MT_transform(VIEW3D_MT_transform_base, Menu):
             layout.operator("transform.skin_resize")
         elif context.mode in {'EDIT_CURVE', 'EDIT_GREASE_PENCIL', 'EDIT_CURVES', 'EDIT_POINTCLOUD'}:
             layout.operator("transform.transform", text="Radius").mode = 'CURVE_SHRINKFATTEN'
+        if context.mode in 'EDIT_GREASE_PENCIL':
+            layout.operator("transform.transform", text="Opacity").mode = 'GPENCIL_OPACITY'
 
         if context.mode != 'EDIT_CURVES' and context.mode != 'EDIT_GREASE_PENCIL':
             layout.separator()

commit 6d79a34d63527bf4683a665ef47ee5f07a786919

Fix: Grease Pencil: UI: Wrong usage of contains operator
Introduced with 1fa2be8a9ba825d8c7d0a56370932161664cc20c.
This should have used `==` instrad of `in`.

scripts/startup/bl_ui/space_view3d.py
@@ -1291,7 +1291,7 @@ class VIEW3D_MT_transform(VIEW3D_MT_transform_base, Menu):
             layout.operator("transform.skin_resize")
         elif context.mode in {'EDIT_CURVE', 'EDIT_GREASE_PENCIL', 'EDIT_CURVES', 'EDIT_POINTCLOUD'}:
             layout.operator("transform.transform", text="Radius").mode = 'CURVE_SHRINKFATTEN'
-        if context.mode in 'EDIT_GREASE_PENCIL':
+        if context.mode == 'EDIT_GREASE_PENCIL':
             layout.operator("transform.transform", text="Opacity").mode = 'GPENCIL_OPACITY'
 
         if context.mode != 'EDIT_CURVES' and context.mode != 'EDIT_GREASE_PENCIL':