Pagecran/blender

commit 1c41815c4d4c20c986b932b9cea0a27020ec1888

Fix: Workbench: Color imprecision for pure black color
The bias for mapping to log color space was wrong and
was assuming `log(2x)` instead of `log2(x)`.

This made pure black colors result in 0.00008 values
after AA resolve.

Using the correct bias ensures 0 stays at 0 during the
AA process and doesn't exhibit any imprecision.

source/blender/draw/engines/workbench/shaders/workbench_effect_smaa_frag.glsl
@@ -34,7 +34,7 @@ void main()
   }
   out_color /= taa_accumulated_weight;
   /* Exit log2 space used for Anti-aliasing. */
-  out_color = exp2(out_color) - 0.5f;
+  out_color = exp2(out_color) - 1.0f;
 
   /* Avoid float precision issue. */
   if (out_color.a > 0.999f) {

source/blender/draw/engines/eevee/shaders/eevee_depth_of_field_accumulator_lib.glsl
@@ -428,7 +428,7 @@ void dof_gather_init(float base_radius,
   /* TODO(fclem) Seems like the default lod selection is too big. Bias to avoid blocky moving out
    * of focus shapes. */
   constexpr float lod_bias = -2.0f;
-  lod = max(floor(log2(base_radius * unit_sample_radius) + 0.5f) + lod_bias, 0.0f);
+  lod = max(floor(log2(base_radius * unit_sample_radius) + 1.0f) + lod_bias, 0.0f);
 
   if (no_gather_mipmaps) {
     lod = 0.0f;

commit 100833d5e24166cdc13e6fdb5765dfdf8d859e30

Fix #147782: Workbench alpha imprecision
Regression from 1c41815c4d4c20c986b932b9cea0a27020ec1888.

The log2/exp2 round-trip can cause imprecisions, which are especially
notable with the alpha channel.
This removes the round-trip conversion for the alpha channel.

Pull Request: https://projects.blender.org/blender/blender/pulls/148218

source/blender/draw/engines/workbench/shaders/workbench_effect_smaa_frag.glsl
@@ -38,7 +38,7 @@ void main()
   }
   out_color /= taa_accumulated_weight;
   /* Exit log2 space used for Anti-aliasing. */
-  out_color = exp2(out_color) - 1.0f;
+  out_color.rgb = exp2(out_color.rgb) - 1.0f;
 
   /* Avoid float precision issue. */
   if (out_color.a > 0.999f) {

source/blender/draw/engines/workbench/shaders/workbench_effect_taa_frag.glsl
@@ -21,7 +21,7 @@ void main()
       /* Clamp infinite inputs (See #112211). */
       color = clamp(color, float4(0.0f), float4(1e10f));
       /* Use log2 space to avoid highlights creating too much aliasing. */
-      color = log2(color + 1.0f);
+      color.rgb = log2(color.rgb + 1.0f);
 
       frag_color += color * samplesWeights[i];
     }