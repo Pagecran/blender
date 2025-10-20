Pagecran/blender

commit 2c435ce8dfa9e042df4ddd93ee82b0230eb2b777

Fix #141469: Geometry Nodes: use safer approach to modifying each instance geometry
Many nodes operate on all the instances that are passed into them. For example,
the Subdivision Surface node subdivides the mesh at the root but also instanced
meshes. This works well for most nodes, but there are a few nodes were the old
`modify_geometry_sets` function was not very well defined and it was tricky to
use correctly.

The fundamental problem was that the behavior is not obvious when a node creates
or modifies instances and how those are integrated with the already existing
instances.

This patch solves this with the following changes:
* Remove the old `GeometrySet::modify_geometry_sets` and related
  `*_during_modify` methods.
* Add a new `blender::geometry::foreach_real_geometry` function that is similar
  to the old `modify_geometry_sets` but has a more well-defined interface:
  * It never passes instances into the callback. So existing instances can't be
    modified with it.
  * The callback is allowed to create new instances. This will automatically be
    merged back with potentially already existing instances. The callback does
    not have to worry about accidentally invalidating existing instances like
    before.
* A few existing usages used `modify_geometry_sets` to actually modify existing
  instances (usually just removing attributes). Those can't use the new
  `foreach_real_geometry`, so they just get a custom simple recursive
  implementation instead of using a generic function.

Pull Request: https://projects.blender.org/blender/blender/pulls/143898

source/blender/nodes/geometry/nodes/node_geo_curve_resample.cc
@@ -2,6 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "GEO_foreach_geometry.hh"
 #include "GEO_resample_curves.hh"
 
 #include "BKE_curves.hh"
@@ -91,7 +92,7 @@ static void node_geo_exec(GeoNodeExecParams params)
   switch (mode) {
     case GEO_NODE_CURVE_RESAMPLE_COUNT: {
       Field<int> count = params.extract_input<Field<int>>("Count");
-      geometry_set.modify_geometry_sets([&](GeometrySet &geometry) {
+      geometry::foreach_real_geometry(geometry_set, [&](GeometrySet &geometry) {
         if (const Curves *src_curves_id = geometry.get_curves()) {
           const bke::CurvesGeometry &src_curves = src_curves_id->geometry.wrap();
           const bke::CurvesFieldContext field_context{*src_curves_id, AttrDomain::Curve};
@@ -123,7 +124,7 @@ static void node_geo_exec(GeoNodeExecParams params)
     }
     case GEO_NODE_CURVE_RESAMPLE_LENGTH: {
       Field<float> length = params.extract_input<Field<float>>("Length");
-      geometry_set.modify_geometry_sets([&](GeometrySet &geometry) {
+      geometry::foreach_real_geometry(geometry_set, [&](GeometrySet &geometry) {
         if (const Curves *src_curves_id = geometry.get_curves()) {
           const bke::CurvesGeometry &src_curves = src_curves_id->geometry.wrap();
           const bke::CurvesFieldContext field_context{*src_curves_id, AttrDomain::Curve};
@@ -153,7 +154,7 @@ static void node_geo_exec(GeoNodeExecParams params)
       break;
     }
     case GEO_NODE_CURVE_RESAMPLE_EVALUATED:
-      geometry_set.modify_geometry_sets([&](GeometrySet &geometry) {
+      geometry::foreach_real_geometry(geometry_set, [&](GeometrySet &geometry) {
         if (const Curves *src_curves_id = geometry.get_curves()) {
           const bke::CurvesGeometry &src_curves = src_curves_id->geometry.wrap();
           const bke::CurvesFieldContext field_context{*src_curves_id, AttrDomain::Curve};

commit cb52010e584353b66376c8c0edd9698f68eb6fc4

Fix #148251: Resample Curve node broken for Grease Pencil
Caused by 2c435ce8dfa9e042df4ddd93ee82b0230eb2b777

source/blender/nodes/geometry/nodes/node_geo_curve_resample.cc
@@ -104,7 +104,7 @@ static void node_geo_exec(GeoNodeExecParams params)
           bke::curves_copy_parameters(*src_curves_id, *dst_curves_id);
           geometry.replace_curves(dst_curves_id);
         }
-        if (GreasePencil *grease_pencil = geometry_set.get_grease_pencil_for_write()) {
+        if (GreasePencil *grease_pencil = geometry.get_grease_pencil_for_write()) {
           using namespace blender::bke::greasepencil;
           for (const int layer_index : grease_pencil->layers().index_range()) {
             Drawing *drawing = grease_pencil->get_eval_drawing(grease_pencil->layer(layer_index));
@@ -136,7 +136,7 @@ static void node_geo_exec(GeoNodeExecParams params)
           bke::curves_copy_parameters(*src_curves_id, *dst_curves_id);
           geometry.replace_curves(dst_curves_id);
         }
-        if (GreasePencil *grease_pencil = geometry_set.get_grease_pencil_for_write()) {
+        if (GreasePencil *grease_pencil = geometry.get_grease_pencil_for_write()) {
           using namespace blender::bke::greasepencil;
           for (const int layer_index : grease_pencil->layers().index_range()) {
             Drawing *drawing = grease_pencil->get_eval_drawing(grease_pencil->layer(layer_index));
@@ -166,7 +166,7 @@ static void node_geo_exec(GeoNodeExecParams params)
           bke::curves_copy_parameters(*src_curves_id, *dst_curves_id);
           geometry.replace_curves(dst_curves_id);
         }
-        if (GreasePencil *grease_pencil = geometry_set.get_grease_pencil_for_write()) {
+        if (GreasePencil *grease_pencil = geometry.get_grease_pencil_for_write()) {
           using namespace blender::bke::greasepencil;
           for (const int layer_index : grease_pencil->layers().index_range()) {
             Drawing *drawing = grease_pencil->get_eval_drawing(grease_pencil->layer(layer_index));