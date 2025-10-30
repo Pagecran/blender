Pagecran/blender

commit 67a03ea03e4362a465decb70c9786e0bb72b2a83

Metal: Rewrite shader interface and source generator
This is a full rewrite which reduces the code
complexity and remove a lot of deprecated code paths.

All arguments to the shader MSL wrapper class are now passed
by reference. This avoid having to use accessor defines for
every resources.

The binding slot assignments is also revisited and made much
more straight-forward. See `mtl_shader_generate.hh` for details.

More detail in the pull request.

Pull Request: https://projects.blender.org/blender/blender/pulls/147010

source/blender/gpu/intern/gpu_batch.cc
@@ -243,7 +243,11 @@ static uint16_t bind_attribute_as_ssbo(const ShaderInterface *interface,
        * But for now, changes are a bit too invasive. Will need to be revisited later on. */
       char uniform_name_len[] = "gpu_attr_0_len";
       uniform_name_len[9] = '0' + input->location;
-      GPU_shader_uniform_1i(shader, uniform_name_len, a->type.comp_len());
+      const int loc = GPU_shader_get_uniform(shader, name);
+      if (loc != -1) {
+        int data = a->type.comp_len();
+        GPU_shader_uniform_int_ex(shader, loc, 1, 1, &data);
+      }
     }
   }
   return bound_attr;

commit 5d3168d0c808e3ef3fb11a5325a1b45e1c8369e7

Fix: GPU: Mistakenly unused variable
The `uniform_name_len` was not used when it should have.

Caused by 67a03ea03e4362a465decb70c9786e0bb72b2a83.

source/blender/gpu/intern/gpu_batch.cc
@@ -243,7 +243,7 @@ static uint16_t bind_attribute_as_ssbo(const ShaderInterface *interface,
        * But for now, changes are a bit too invasive. Will need to be revisited later on. */
       char uniform_name_len[] = "gpu_attr_0_len";
       uniform_name_len[9] = '0' + input->location;
-      const int loc = GPU_shader_get_uniform(shader, name);
+      const int loc = GPU_shader_get_uniform(shader, uniform_name_len);
       if (loc != -1) {
         int data = a->type.comp_len();
         GPU_shader_uniform_int_ex(shader, loc, 1, 1, &data);