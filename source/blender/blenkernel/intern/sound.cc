Pagecran/blender

commit dc3727bd809e8901b1bf7873dae9d02d77c02ad5

Fix #149125: Preserve Pitch on retimed sounds broken when anything is animated
Similar case as was with equalizer modifier recently (#141084):
here the AnimateableTimeStretchPitchScale effect was being
constantly re-created every frame, as soon as anything in VSE
timeline has an animation.

Fix this by checking if Audaspace sequence handle for the sound
is already an instance of AnimateableTimeStretchPitchScale; if it
is then just use the already created one.

Pull Request: https://projects.blender.org/blender/blender/pulls/149127

source/blender/blenkernel/intern/sound.cc
@@ -1535,8 +1535,18 @@ bool BKE_sound_stream_info_get(Main *main,
 }
 
 #  ifdef WITH_RUBBERBAND
-void *BKE_sound_add_time_stretch_effect(void *sound_handle, float fps)
+void *BKE_sound_ensure_time_stretch_effect(void *sound_handle, void *sequence_handle, float fps)
 {
+  /* If sequence handle is already the time stretch effect with the same framerate, use that. */
+  AUD_Sound *cur_seq_sound = sequence_handle ? AUD_SequenceEntry_getSound(sequence_handle) :
+                                               nullptr;
+  if (AUD_Sound_isAnimateableTimeStretchPitchScale(cur_seq_sound) &&
+      AUD_Sound_animateableTimeStretchPitchScale_getFPS(cur_seq_sound) == fps)
+  {
+    return cur_seq_sound;
+  }
+
+  /* Otherwise create the time stretch effect. */
   return AUD_Sound_animateableTimeStretchPitchScale(
       sound_handle, fps, 1.0, 1.0, AUD_STRETCHER_QUALITY_HIGH, false);
 }

extern/audaspace/bindings/C/AUD_Sound.cpp
@@ -855,4 +855,10 @@ AUD_API void AUD_Sound_animateableTimeStretchPitchScale_setFPS(AUD_Sound* sound,
 	dynamic_cast<AnimateableTimeStretchPitchScale*>(sound->get())->setFPS(value);
 }
 
+AUD_API bool AUD_Sound_isAnimateableTimeStretchPitchScale(AUD_Sound* sound)
+{
+	assert(sound);
+	return dynamic_cast<AnimateableTimeStretchPitchScale*>(sound->get()) != nullptr;
+}
+
 #endif

extern/audaspace/bindings/C/AUD_Sound.h
@@ -468,6 +468,11 @@ extern AUD_API AUD_Sound* AUD_Sound_mutable(AUD_Sound* sound);
      * \return The fps of the sound.
      */
     extern AUD_API float AUD_Sound_animateableTimeStretchPitchScale_getFPS(AUD_Sound* sequence);
+
+    /**
+     * Checks if sound is an instance of AnimateableTimeStretchPitchScale effect.
+     */
+    extern AUD_API bool AUD_Sound_isAnimateableTimeStretchPitchScale(AUD_Sound* sound);
 #endif
 
 #ifdef __cplusplus

source/blender/blenkernel/BKE_sound.h
@@ -215,4 +215,4 @@ struct Depsgraph;
 
 void BKE_sound_evaluate(struct Depsgraph *depsgraph, struct Main *bmain, struct bSound *sound);
 
-void *BKE_sound_add_time_stretch_effect(void *sound_handle, float fps);
+void *BKE_sound_ensure_time_stretch_effect(void *sound_handle, void *sequence_handle, float fps);

source/blender/sequencer/intern/strip_retiming.cc
@@ -1102,7 +1102,8 @@ void retiming_sound_animation_data_set(const Scene *scene, const Strip *strip)
   void *sound_handle = strip->sound ? strip->sound->playback_handle : nullptr;
   const float scene_fps = float(scene->r.frs_sec) / float(scene->r.frs_sec_base);
   if (correct_pitch) {
-    sound_handle = BKE_sound_add_time_stretch_effect(sound_handle, scene_fps);
+    sound_handle = BKE_sound_ensure_time_stretch_effect(
+        sound_handle, strip->scene_sound, scene_fps);
     BKE_sound_set_scene_sound_pitch_constant_range(
         strip->scene_sound, 0, strip->start + strip->len, 1.0f);
   }

commit 423a0694da24617d8155e356aaf1a01b71158a38

Fix: Lite build link error
Introduced in dc3727bd809e8

Pull Request: https://projects.blender.org/blender/blender/pulls/149262

source/blender/blenkernel/intern/sound.cc
@@ -1697,7 +1697,9 @@ bool BKE_sound_stream_info_get(Main * /*main*/,
 #endif /* WITH_AUDASPACE */
 
 #if !defined(WITH_AUDASPACE) || !defined(WITH_RUBBERBAND)
-void *BKE_sound_add_time_stretch_effect(void * /*sound_handle*/, float /*fps*/)
+void *BKE_sound_ensure_time_stretch_effect(void * /*sound_handle*/,
+                                           void * /*sequence_handle*/,
+                                           float /*fps*/)
 {
   return nullptr;
 }