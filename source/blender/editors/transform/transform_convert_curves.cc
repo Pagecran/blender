Pagecran/blender

commit 5b6bf4743c94275a4fcdc7e0e0b058f726862f66

Fix #145266: Curves: Radius gets stuck at 0
There are multiple reasons this was not working the same for `Curve` and
`Curves`:

`Curve` is actually setting the `TransData.val` for handles (as opposed
to control points) to `nullptr` (`Curves` doesnt).
This is used in multiple places (and makes sense, since this value is
used for tweaking radius, tilt and GP opacities -- stuff that should not
be able to be tweaked from just handle selection, which was actually
possible...)

Included in the places where this `nullptr` is checked for is the logic
in `initCurveShrinkFatten` which relied on this to spot a selection
including a **control point**.

So this PR fixes these two things (setting nullptr to TransData value
for Curves handles and tweaking the logic in `initCurveShrinkFatten` a
bit so it can handle the different layout/order of `TransData` for
`Curves` vs. `Curve`)

Pull Request: https://projects.blender.org/blender/blender/pulls/145327

source/blender/editors/transform/transform_convert_curves.cc
@@ -645,10 +645,15 @@ void curve_populate_trans_data_structs(const TransInfo &t,
 
           td.extra = extra;
 
-          if (value_attribute) {
+          /* Set #TransData.val to nullptr for handles since those values are only tweaked on
+           * control points. Logic in e.g. #initCurveShrinkFatten() also relies on this. */
+          if (value_attribute && (selection_i == 0)) {
             float *value = &((*value_attribute)[domain_i]);
             td.val = value;
             td.ival = *value;
+          }
+          else {
+            td.val = nullptr;
           }
 
           if (deformation.deform_mats.is_empty()) {

source/blender/editors/transform/transform_mode_curveshrinkfatten.cc
@@ -101,15 +101,46 @@ static void initCurveShrinkFatten(TransInfo *t, wmOperator * /*op*/)
   t->num.unit_type[0] = B_UNIT_NONE;
 
   float scale_factor = 0.0f;
-  if (((t->spacetype == SPACE_VIEW3D) && (t->region->regiontype == RGN_TYPE_WINDOW) &&
-       (t->data_len_all == 1)) ||
-      (t->data_len_all == 3 && TRANS_DATA_CONTAINER_FIRST_OK(t)->data[0].val == nullptr))
-  {
+  if ((t->spacetype == SPACE_VIEW3D) && (t->region->regiontype == RGN_TYPE_WINDOW)) {
     /* For cases where only one point on the curve is being transformed and the radius of that
-     * point is zero, use the factor to multiply the offset of the ratio and allow scaling.
-     * Note that for bezier curves, 3 TransData equals 1 point in most cases. */
-    RegionView3D *rv3d = static_cast<RegionView3D *>(t->region->regiondata);
-    scale_factor = rv3d->pixsize * t->mouse.factor * t->zfac;
+     * point is zero [that is actually only checked for in #applyCurveShrinkFatten()], use the
+     * factor to multiply the offset of the ratio and allow scaling. Note that for bezier curves, 3
+     * TransData equals 1 point in most cases. Handles (as opposed to control points) have their
+     * #TransData.val set to nullptr (set in #createTransCurveVerts() /
+     * #curve_populate_trans_data_structs() since we only want to apply values to _control points_
+     * [this is again checked for in #applyCurveShrinkFatten()]. Only calculate the scale_factor if
+     * we are working on a control point. */
+    bool use_scaling_factor = false;
+    if (t->data_len_all == 1) {
+      /* Either a single control point of a non-bezier curve or single handle of a bezier curve
+       * selected. */
+      use_scaling_factor = TRANS_DATA_CONTAINER_FIRST_OK(t)->data[0].val != nullptr;
+    }
+    if (t->data_len_all == 3) {
+      /* Either a single control point of a bezier curve (or its handles as well) selected, also
+       * true for three individual handles selected. Note the layout/order of TransData is
+       * different for Curve vs. Curves (Curves have the control point first, Curve has it in the
+       * middle), so check this explicitly. */
+      TransDataContainer *tc = TRANS_DATA_CONTAINER_FIRST_OK(t);
+      TransData td_0 = tc->data[0];
+      TransData td_1 = tc->data[1];
+      TransData td_2 = tc->data[2];
+
+      if (t->data_type == &TransConvertType_Curve) {
+        use_scaling_factor = td_0.val == nullptr && td_1.val != nullptr && td_2.val == nullptr;
+      }
+      else if (ELEM(t->data_type,
+                    &curves::TransConvertType_Curves,
+                    &greasepencil::TransConvertType_GreasePencil))
+      {
+        use_scaling_factor = td_0.val != nullptr && td_1.val == nullptr && td_2.val == nullptr;
+      }
+    }
+
+    if (use_scaling_factor) {
+      RegionView3D *rv3d = static_cast<RegionView3D *>(t->region->regiondata);
+      scale_factor = rv3d->pixsize * t->mouse.factor * t->zfac;
+    }
   }
   t->custom.mode.data = POINTER_FROM_UINT(float_as_uint(scale_factor));
 }

commit 7cfdafb6475525ce96f4ba96d6e0a2daa63e2a2f

Fix: Curves: Support transforming radius and opacity by handles.
This fixes a regression caused by
5b6bf4743c94275a4fcdc7e0e0b058f726862f66 where the radius/opacity of
selected Bézier handles could not be transformed.

The ability to change the radius/opacity of points by just having the
handle selected is extremely helpful for the `Pen` Tool.

This effects Curves and Grease Pencil Objects.

Pull Request: https://projects.blender.org/blender/blender/pulls/149234

source/blender/editors/transform/transform_convert_curves.cc
@@ -645,9 +645,7 @@ void curve_populate_trans_data_structs(const TransInfo &t,
 
           td.extra = extra;
 
-          /* Set #TransData.val to nullptr for handles since those values are only tweaked on
-           * control points. Logic in e.g. #initCurveShrinkFatten() also relies on this. */
-          if (value_attribute && (selection_i == 0)) {
+          if (value_attribute) {
             float *value = &((*value_attribute)[domain_i]);
             td.val = value;
             td.ival = *value;

source/blender/editors/transform/transform_mode_curveshrinkfatten.cc
@@ -105,11 +105,7 @@ static void initCurveShrinkFatten(TransInfo *t, wmOperator * /*op*/)
     /* For cases where only one point on the curve is being transformed and the radius of that
      * point is zero [that is actually only checked for in #applyCurveShrinkFatten()], use the
      * factor to multiply the offset of the ratio and allow scaling. Note that for bezier curves, 3
-     * TransData equals 1 point in most cases. Handles (as opposed to control points) have their
-     * #TransData.val set to nullptr (set in #createTransCurveVerts() /
-     * #curve_populate_trans_data_structs() since we only want to apply values to _control points_
-     * [this is again checked for in #applyCurveShrinkFatten()]. Only calculate the scale_factor if
-     * we are working on a control point. */
+     * TransData equals 1 point in most cases. */
     bool use_scaling_factor = false;
     if (t->data_len_all == 1) {
       /* Either a single control point of a non-bezier curve or single handle of a bezier curve
@@ -122,18 +118,15 @@ static void initCurveShrinkFatten(TransInfo *t, wmOperator * /*op*/)
        * different for Curve vs. Curves (Curves have the control point first, Curve has it in the
        * middle), so check this explicitly. */
       TransDataContainer *tc = TRANS_DATA_CONTAINER_FIRST_OK(t);
-      TransData td_0 = tc->data[0];
-      TransData td_1 = tc->data[1];
-      TransData td_2 = tc->data[2];
 
       if (t->data_type == &TransConvertType_Curve) {
-        use_scaling_factor = td_0.val == nullptr && td_1.val != nullptr && td_2.val == nullptr;
+        use_scaling_factor = tc->data[1].val != nullptr;
       }
       else if (ELEM(t->data_type,
                     &curves::TransConvertType_Curves,
                     &greasepencil::TransConvertType_GreasePencil))
       {
-        use_scaling_factor = td_0.val != nullptr && td_1.val == nullptr && td_2.val == nullptr;
+        use_scaling_factor = tc->data[0].val != nullptr;
       }
     }
 