Pagecran/blender

commit b89ccb12286cc980009a1bf588ae1920debc684d

Refactor: GPU: Use the new BLI_thread_queue API for GPUWorker
Use the API from #140214 / #140992.
This should not cause any functional change, it's done for code
deduplication purposes.

Pull Request: https://projects.blender.org/blender/blender/pulls/140993

source/blender/gpu/intern/gpu_shader.cc
@@ -860,6 +860,20 @@ Shader *ShaderCompiler::compile(const shader::ShaderCreateInfo &orig_info,
   return shader;
 }
 
+static ThreadQueueWorkPriority to_work_priority(CompilationPriority priority)
+{
+  switch (priority) {
+    case CompilationPriority::Low:
+      return BLI_THREAD_QUEUE_WORK_PRIORITY_LOW;
+    case CompilationPriority::Medium:
+      return BLI_THREAD_QUEUE_WORK_PRIORITY_NORMAL;
+    case CompilationPriority::High:
+      return BLI_THREAD_QUEUE_WORK_PRIORITY_HIGH;
+  }
+  BLI_assert_unreachable();
+  return BLI_THREAD_QUEUE_WORK_PRIORITY_NORMAL;
+}
+
 ShaderCompiler::ShaderCompiler(uint32_t threads_count,
                                GPUWorker::ContextType context_type,
                                bool support_specializations)
@@ -868,11 +882,7 @@ ShaderCompiler::ShaderCompiler(uint32_t threads_count,
 
   if (!GPU_use_main_context_workaround()) {
     compilation_worker_ = std::make_unique<GPUWorker>(
-        threads_count,
-        context_type,
-        mutex_,
-        [this]() -> void * { return this->pop_work(); },
-        [this](void *work) { this->do_work(work); });
+        threads_count, context_type, do_work_static_cb);
   }
 }
 
@@ -903,10 +913,12 @@ BatchHandle ShaderCompiler::batch_compile(Span<const shader::ShaderCreateInfo *>
 
   if (compilation_worker_) {
     batch->shaders.resize(infos.size(), nullptr);
+    batch->works.reserve(infos.size());
     batch->pending_compilations = infos.size();
     for (int i : infos.index_range()) {
-      compilation_queue_.push({batch, i}, priority);
-      compilation_worker_->wake_up();
+      batch->works.append(std::make_unique<ParallelWork>(ParallelWork{this, batch, i}));
+      batch->works.last()->id = compilation_worker_->push_work(batch->works.last().get(),
+                                                               to_work_priority(priority));
     }
   }
   else {
@@ -924,7 +936,11 @@ void ShaderCompiler::batch_cancel(BatchHandle &handle)
     std::unique_lock lock(mutex_);
 
     Batch *batch = batches_.pop(handle);
-    compilation_queue_.remove_batch(batch);
+    for (std::unique_ptr<ParallelWork> &work : batch->works) {
+      if (compilation_worker_->cancel_work(work->id)) {
+        batch->pending_compilations--;
+      }
+    }
 
     /* If it was already being compiled, wait until it's ready so the calling thread can safely
      * delete the ShaderCreateInfos. */
@@ -972,14 +988,16 @@ SpecializationBatchHandle ShaderCompiler::precompile_specializations(
 
   Batch *batch = MEM_new<Batch>(__func__);
   batch->specializations = specializations;
+  batch->works.reserve(specializations.size());
+  batch->pending_compilations = specializations.size();
 
   BatchHandle handle = next_batch_handle_++;
   batches_.add(handle, batch);
 
-  batch->pending_compilations = specializations.size();
   for (int i : specializations.index_range()) {
-    compilation_queue_.push({batch, i}, priority);
-    compilation_worker_->wake_up();
+    batch->works.append(std::make_unique<ParallelWork>(ParallelWork{this, batch, i}));
+    batch->works.last()->id = compilation_worker_->push_work(batch->works.last().get(),
+                                                             to_work_priority(priority));
   }
 
   return handle;
@@ -998,24 +1016,16 @@ bool ShaderCompiler::specialization_batch_is_ready(SpecializationBatchHandle &ha
   return handle == 0;
 }
 
-void *ShaderCompiler::pop_work()
+void ShaderCompiler::do_work_static_cb(void *payload)
 {
-  /* NOTE: Already under mutex lock when GPUWorker calls this function. */
-
-  if (compilation_queue_.is_empty()) {
-    return nullptr;
-  }
-
-  ParallelWork work = compilation_queue_.pop();
-  return MEM_new<ParallelWork>(__func__, work);
+  ParallelWork *work = reinterpret_cast<ParallelWork *>(payload);
+  work->compiler->do_work(*work);
 }
 
-void ShaderCompiler::do_work(void *work_payload)
+void ShaderCompiler::do_work(ParallelWork &work)
 {
-  ParallelWork *work = reinterpret_cast<ParallelWork *>(work_payload);
-  Batch *batch = work->batch;
-  int shader_index = work->shader_index;
-  MEM_delete(work);
+  Batch *batch = work.batch;
+  int shader_index = work.shader_index;
 
   /* Compile */
   if (!batch->is_specialization_batch()) {
@@ -1043,7 +1053,7 @@ bool ShaderCompiler::is_compiling_impl()
   /* The mutex should be locked before calling this function. */
   BLI_assert(!mutex_.try_lock());
 
-  if (!compilation_queue_.is_empty()) {
+  if (!compilation_worker_->is_empty()) {
     return true;
   }
 
@@ -1064,6 +1074,8 @@ bool ShaderCompiler::is_compiling()
 
 void ShaderCompiler::wait_for_all()
 {
+  /** NOTE: We can't rely on BLI_thread_queue_wait_finish, since that only waits until the queue is
+   * empty, but the works might still being processed. */
   std::unique_lock lock(mutex_);
   BLI_assert(!is_paused_);
   compilation_finished_notification_.wait(lock, [&]() { return !is_compiling_impl(); });

source/blender/blenlib/BLI_threads.h
@@ -189,8 +189,10 @@ uint64_t BLI_thread_queue_push(ThreadQueue *queue, void *work, ThreadQueueWorkPr
 
 /**
  * Remove the corresponding work from the queue.
+ *
+ * \returns true if the work_id was still in the queue.
  */
-void BLI_thread_queue_cancel_work(ThreadQueue *queue, uint64_t work_id);
+bool BLI_thread_queue_cancel_work(ThreadQueue *queue, uint64_t work_id);
 
 /**
  * Pop the oldest, highest priority work from the queue.

source/blender/blenlib/intern/threads.cc
@@ -681,7 +681,7 @@ static void check_finalization(ThreadQueue *queue)
   }
 }
 
-void BLI_thread_queue_cancel_work(ThreadQueue *queue, uint64_t work_id)
+bool BLI_thread_queue_cancel_work(ThreadQueue *queue, uint64_t work_id)
 {
   pthread_mutex_lock(&queue->mutex);
 
@@ -707,6 +707,8 @@ void BLI_thread_queue_cancel_work(ThreadQueue *queue, uint64_t work_id)
   }
 
   pthread_mutex_unlock(&queue->mutex);
+
+  return found;
 }
 
 void *BLI_thread_queue_pop(ThreadQueue *queue)

source/blender/gpu/GPU_worker.hh
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include "BLI_threads.h"
 #include "BLI_vector.hh"
 #include "GPU_context.hh"
 
@@ -16,6 +17,9 @@
 
 namespace blender::gpu {
 
+using WorkCallback = void (*)(void *);
+using WorkID = uint64_t;
+
 /**
  * Abstracts the creation and management of secondary threads with GPU contexts.
  * Must be created from the main thread.
@@ -24,9 +28,8 @@ namespace blender::gpu {
 class GPUWorker {
  private:
   Vector<std::unique_ptr<std::thread>> threads_;
-  std::condition_variable condition_var_;
-  std::mutex &mutex_;
-  bool terminate_ = false;
+  ThreadQueue *work_queue_;
+  WorkCallback callback_;
 
  public:
   enum class ContextType {
@@ -39,32 +42,18 @@ class GPUWorker {
   /**
    * \param threads_count: Number of threads to span.
    * \param context_type: The type of context each thread uses.
-   * \param mutex: Mutex used when trying to acquire the next work
-   *               (and reused internally for termination).
-   * \param pop_work: The callback function that will be called to acquire the next work,
-   *                  should return a void pointer.
-   *                  NOTE: The mutex is locked when this function is called.
-   * \param do_work: The callback function that will be called for each acquired work
+   * \param callback: The callback function that will be called for each acquired work
    *                 (passed as a void pointer).
-   *                 NOTE: The mutex is unlocked when this function is called.
    */
-  GPUWorker(uint32_t threads_count,
-            ContextType context_type,
-            std::mutex &mutex,
-            std::function<void *()> pop_work,
-            std::function<void(void *)> do_work);
+  GPUWorker(uint32_t threads_count, ContextType context_type, WorkCallback callback);
   ~GPUWorker();
 
-  /* Wake up a single thread. */
-  void wake_up()
-  {
-    condition_var_.notify_one();
-  }
+  WorkID push_work(void *work, ThreadQueueWorkPriority priority);
+  bool cancel_work(WorkID id);
+  bool is_empty();
 
  private:
-  void run(std::shared_ptr<GPUSecondaryContext> context,
-           std::function<void *()> pop_work,
-           std::function<void(void *)> do_work);
+  void run(std::shared_ptr<GPUSecondaryContext> context);
 };
 
 }  // namespace blender::gpu

source/blender/gpu/intern/gpu_shader_private.hh
@@ -154,11 +154,21 @@ class ShaderCompiler {
     std::string comp;
   };
 
+  struct Batch;
+  struct ParallelWork {
+    ShaderCompiler *compiler = nullptr;
+    ShaderCompiler::Batch *batch = nullptr;
+    int shader_index = 0;
+    WorkID id = 0;
+  };
+
   struct Batch {
     Vector<Shader *> shaders;
     Vector<const shader::ShaderCreateInfo *> infos;
 
     Vector<ShaderSpecialization> specializations;
+
+    Vector<std::unique_ptr<ParallelWork>> works;
 
     std::atomic<int> pending_compilations = 0;
 
@@ -187,89 +197,12 @@ class ShaderCompiler {
   std::mutex mutex_;
   std::condition_variable compilation_finished_notification_;
 
-  struct ParallelWork {
-    Batch *batch = nullptr;
-    int shader_index = 0;
-  };
-
-  struct CompilationQueue {
-    std::deque<ParallelWork> low_priority;
-    std::deque<ParallelWork> normal_priority;
-    std::deque<ParallelWork> high_priority;
-
-    void push(ParallelWork &&work, CompilationPriority priority)
-    {
-      switch (priority) {
-        case CompilationPriority::Low:
-          low_priority.push_back(work);
-          break;
-        case CompilationPriority::Medium:
-          normal_priority.push_back(work);
-          break;
-        case CompilationPriority::High:
-          high_priority.push_back(work);
-          break;
-        default:
-          BLI_assert_unreachable();
-          break;
-      }
-    }
-
-    ParallelWork pop()
-    {
-      if (!high_priority.empty()) {
-        ParallelWork work = high_priority.front();
-        high_priority.pop_front();
-        return work;
-      }
-      if (!normal_priority.empty()) {
-        ParallelWork work = normal_priority.front();
-        normal_priority.pop_front();
-        return work;
-      }
-      if (!low_priority.empty()) {
-        ParallelWork work = low_priority.front();
-        low_priority.pop_front();
-        return work;
-      }
-      BLI_assert_unreachable();
-      return {};
-    }
-
-    bool is_empty()
-    {
-      return low_priority.empty() && normal_priority.empty() && high_priority.empty();
-    }
-
-    void remove_batch(Batch *batch)
-    {
-      auto remove = [](std::deque<ParallelWork> &queue, Batch *batch) {
-        for (ParallelWork &work : queue) {
-          if (work.batch == batch) {
-            work = {};
-            batch->pending_compilations--;
-          }
-        }
-
-        queue.erase(std::remove_if(queue.begin(),
-                                   queue.end(),
-                                   [](const ParallelWork &work) { return !work.batch; }),
-                    queue.end());
-      };
-
-      remove(low_priority, batch);
-      remove(normal_priority, batch);
-      remove(high_priority, batch);
-    }
-  };
-  CompilationQueue compilation_queue_;
-
   std::unique_ptr<GPUWorker> compilation_worker_;
 
   bool support_specializations_;
 
-  void *pop_work();
-  void do_work(void *work_payload);
+  static void do_work_static_cb(void *payload);
+  void do_work(ParallelWork &work);
 
   BatchHandle next_batch_handle_ = 1;
 

source/blender/gpu/intern/gpu_worker.cc
@@ -6,58 +6,57 @@
 
 namespace blender::gpu {
 
-GPUWorker::GPUWorker(uint32_t threads_count,
-                     ContextType context_type,
-                     std::mutex &mutex,
-                     std::function<void *()> pop_work,
-                     std::function<void(void *)> do_work)
-    : mutex_(mutex)
+GPUWorker::GPUWorker(uint32_t threads_count, ContextType context_type, WorkCallback callback)
+    : callback_(callback)
 {
+  work_queue_ = BLI_thread_queue_init();
+
   for (int i : IndexRange(threads_count)) {
     UNUSED_VARS(i);
     std::shared_ptr<GPUSecondaryContext> thread_context =
         context_type == ContextType::PerThread ? std::make_shared<GPUSecondaryContext>() : nullptr;
-    threads_.append(
-        std::make_unique<std::thread>([=]() { this->run(thread_context, pop_work, do_work); }));
+    threads_.append(std::make_unique<std::thread>([=]() { this->run(thread_context); }));
   }
 }
 
 GPUWorker::~GPUWorker()
 {
-  {
-    std::unique_lock<std::mutex> lock(mutex_);
-    terminate_ = true;
-  }
-  condition_var_.notify_all();
+  /* Any work left should have been cancelled at this point. */
+  BLI_assert(BLI_thread_queue_is_empty(work_queue_));
+  /* Signal background threads to stop waiting for new tasks if none are left. */
+  BLI_thread_queue_nowait(work_queue_);
+  /* But we still wait, in case the above assert fails. */
+  BLI_thread_queue_wait_finish(work_queue_);
   for (std::unique_ptr<std::thread> &thread : threads_) {
     thread->join();
   }
+  BLI_thread_queue_free(work_queue_);
 }
 
-void GPUWorker::run(std::shared_ptr<GPUSecondaryContext> context,
-                    std::function<void *()> pop_work,
-                    std::function<void(void *)> do_work)
+WorkID GPUWorker::push_work(void *work, ThreadQueueWorkPriority priority)
+{
+  return BLI_thread_queue_push(work_queue_, work, priority);
+}
+
+bool GPUWorker::cancel_work(WorkID id)
+{
+  return BLI_thread_queue_cancel_work(work_queue_, id);
+}
+
+bool GPUWorker::is_empty()
+{
+  return BLI_thread_queue_is_empty(work_queue_);
+}
+
+void GPUWorker::run(std::shared_ptr<GPUSecondaryContext> context)
 {
   if (context) {
     context->activate();
   }
 
-  std::unique_lock<std::mutex> lock(mutex_);
-
-  /* Loop until we get the terminate signal. */
-  while (!terminate_) {
-    void *work = pop_work();
-    if (!work) {
-      condition_var_.wait(lock);
-      if (terminate_) {
-        break;
-      }
-      continue;
-    }
-
-    lock.unlock();
-    do_work(work);
-    lock.lock();
+  /* Loop until the queue is cancelled. */
+  while (void *work = BLI_thread_queue_pop(work_queue_)) {
+    callback_(work);
   }
 }
 

commit 72b1e6ef122b61297270eb16c6f027e2221fa81e

Fix: GPU: Missing compilation_worker_ null checks
Regressions from b89ccb1228 and 7605235d8c.
Fixes several issues when `GPU_use_main_context_workaround` is enabled.
Fixes the Windows-AMD builder timeouts.

Pull Request: https://projects.blender.org/blender/blender/pulls/149927

source/blender/gpu/intern/gpu_shader.cc
@@ -921,6 +921,7 @@ AsyncCompilationHandle ShaderCompiler::async_compilation(const shader::ShaderCre
   }
   else {
     compilation->shader = compile(*info, false);
+    compilation->is_ready = true;
   }
 
   return handle;
@@ -932,7 +933,7 @@ void ShaderCompiler::asyc_compilation_cancel(AsyncCompilationHandle &handle)
     std::unique_lock lock(mutex_);
 
     AsyncCompilation *compilation = async_compilations_.pop(handle);
-    if (compilation_worker_->cancel_work(compilation->work->id)) {
+    if (compilation_worker_ && compilation_worker_->cancel_work(compilation->work->id)) {
       compilation->is_ready = true;
     }
 
@@ -1045,6 +1046,10 @@ bool ShaderCompiler::is_compiling_impl()
 {
   /* The mutex should be locked before calling this function. */
   BLI_assert(!mutex_.try_lock());
+
+  if (compilation_worker_ == nullptr) {
+    return false;
+  }
 
   if (!compilation_worker_->is_empty()) {
     return true;