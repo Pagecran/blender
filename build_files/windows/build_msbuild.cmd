cc09b7f36017dafa4c7b38fbe55907d58aebf3f4
pascalandr <pascalandr@gmail.com>
2025-11-26 21:04:03 +0100

    feat: Add CMake configuration for a lite Blender build and a Windows MSBuild script.

--- build_files/windows/build_msbuild.cmd
+++ build_files/windows/build_msbuild.cmd
@@ -1,10 +1,17 @@
 if "%NOBUILD%"=="1" goto EOF
 echo %TIME% > %BUILD_DIR%\buildtime.txt
+
+if exist "%BUILD_DIR%\Blender.slnx" (
+	set SOLUTION_FILE=%BUILD_DIR%\Blender.slnx
+) else (
+	set SOLUTION_FILE=%BUILD_DIR%\Blender.sln
+)
+
 msbuild ^
-	%BUILD_DIR%\Blender.sln ^
+	%SOLUTION_FILE% ^
 	/target:build ^
 	/property:Configuration=%BUILD_TYPE% ^
-	/maxcpucount:2 ^
+	/maxcpucount ^
 	/verbosity:minimal ^
 	/p:platform=%MSBUILD_PLATFORM% ^
 	/flp:Summary;Verbosity=minimal;LogFile=%BUILD_DIR%\Build.log 
