Pagecran/blender

commit f05200667a9d407385238aa444d87ff284ebe359

Fix #148561: Text outline does not match text boundary
Offset strip quad based on the anchor.

Pull Request: https://projects.blender.org/blender/blender/pulls/151292

source/blender/sequencer/intern/strip_transform.cc
@@ -695,6 +695,33 @@ static Array<float2> strip_image_transform_quad_get_ex(const Scene *scene,
       {(-image_size[0] / 2) + crop->left, (image_size[1] / 2) - crop->top},
   };
 
+  if (strip->type == STRIP_TYPE_TEXT) {
+    const TextVars *data = static_cast<TextVars *>(strip->effectdata);
+    float2 offset{(0, 0)};
+
+    switch (data->anchor_x) {
+      case SEQ_TEXT_ALIGN_X_LEFT:
+        offset.x = image_size.x / 2;
+        break;
+      case SEQ_TEXT_ALIGN_X_RIGHT:
+        offset.x = -image_size.x / 2;
+        break;
+    }
+    switch (data->anchor_y) {
+      case SEQ_TEXT_ALIGN_Y_BOTTOM:
+        offset.y = image_size.y / 2;
+        break;
+      case SEQ_TEXT_ALIGN_Y_TOP:
+        offset.y = -image_size.y / 2;
+        break;
+    }
+
+    quad[0] += offset;
+    quad[1] += offset;
+    quad[2] += offset;
+    quad[3] += offset;
+  }
+
   const float3x3 matrix = seq_image_transform_matrix_get_ex(scene, strip, apply_rotation);
   const float2 viewport_pixel_aspect(scene->r.xasp / scene->r.yasp, 1.0f);
   const float2 mirror = image_transform_mirror_factor_get(strip);

commit 7cc9a0c63f4fa6e7ff9a06b93ab9a3015a5b1b21

Fix: Compiler warning
Incorrect value initialization, introduced in f05200667a.

source/blender/sequencer/intern/strip_transform.cc
@@ -697,7 +697,7 @@ static Array<float2> strip_image_transform_quad_get_ex(const Scene *scene,
 
   if (strip->type == STRIP_TYPE_TEXT) {
     const TextVars *data = static_cast<TextVars *>(strip->effectdata);
-    float2 offset{(0, 0)};
+    float2 offset(0, 0);
 
     switch (data->anchor_x) {
       case SEQ_TEXT_ALIGN_X_LEFT: