Pagecran/blender

commit 899ce2e182cd75943c5ac252be695f80a4311076

XR: Arc teleportation ray drawing and functionalities overhaul
This PR rewrites and overhaul the XR Arc Teleportation functionality,
making it closer to the sort of arc ray locomotion typically found in
modern VR video-games and softwares. Main user facing changes include:

- The teleportation ray arc parabola is now pre-computed and fixed,
  allowing for more predictable trajectories and precise aiming.
- New destination pad of cylindrical shape, drawn in world space, to
  clearly show the user destination
- Walls and other non-planar surfaces are now considered inaccessible,
  displayed using a red ray and sphere shaped destination indicator.
- New world ground grid intersection heuristics:
   - The arc teleportation ray will now stop at the world ground
     grid *only* if no accessible geometry exists below, allowing to
     teleport to surfaces below the ground grid.
   - The arc teleportation ray will now stop at the world ground grid in
     the *downward* direction only., allowing to teleport to geometry
     above the ground grid, or to the ground grid itself from below in
     an arc.

Operator specific changes:
- New, less saturated colors for ray valid, invalid and world ground ray
  hits
- The ray arc curve is now fixed to real-world gravity, with the arc
  width now being controlled by two "range" and "force" operator
  parameters
- The "from_viewer" property and logic was dropped, with it only being
  unused for the currently incomplete VR gamepad controller binding.
  This can be re-implemented later as part of a rework of VR gamepad
  support.
- Removed internal curve sampling values from being modifiable from
  operator parameters
- Removed "interpolation", "offset", "axis" and "teleport_axes"
  parameters and their computation function if favor of making for a
  simpler point to point teleportation operation
- Simplified VR actions bindings, now directly using operator parameter
  defaults instead of re-defining values in binding file

source/blender/gpu/GPU_shader_builtin.hh
@@ -91,9 +91,6 @@ enum GPUBuiltinShader {
   /** Draw sequencer zebra pattern (overexposed regions). */
   GPU_SHADER_SEQUENCER_ZEBRA,
 
-  /** Draw XR ray-cast as a ruled spline surface. */
-  GPU_SHADER_XR_RAYCAST,
-
   /** Compute shaders to generate 2d index buffers (mainly for curve drawing). */
   GPU_SHADER_INDEXBUF_POINTS,
   GPU_SHADER_INDEXBUF_LINES,
@@ -137,7 +134,7 @@ enum GPUBuiltinShader {
 
   /**
    * Draw a sRGB color space texture in 3D.
-   * Texture color space is assumed to match the frame-buffer.
+   * Texture color space is assumed to match the framebuffer.
    * Take a 3D position and a 2D texture coordinate for each vertex.
    *
    * \param image: uniform sampler2D

scripts/addons_core/viewport_vr_preview/configs/default.py
@@ -267,13 +267,7 @@ actionconfig_data = [
                         "haptic_amplitude": "0.0",
                         "haptic_mode": "PRESS",
                     },
-                    {
-                        "op_properties": [
-                            ("interpolation", 0.9),
-                            ("hit_color", (0.0, 1.0, 1.0, 1.0)),
-                            ("miss_color", (1.0, 0.0, 0.0, 1.0)),
-                        ],
-                    },
+                    None,
                     {
                         "bindings": [
                             (
@@ -1559,14 +1553,7 @@ actionconfig_data = [
                         "haptic_amplitude": "0.0",
                         "haptic_mode": "PRESS",
                     },
-                    {
-                        "op_properties": [
-                            ("interpolation", 0.9),
-                            ("from_viewer", True),
-                            ("hit_color", (0.0, 1.0, 1.0, 1.0)),
-                            ("miss_color", (1.0, 0.0, 0.0, 1.0)),
-                        ],
-                    },
+                    None,
                     {
                         "bindings": [
                             (

source/blender/gpu/CMakeLists.txt
@@ -500,7 +500,6 @@ set(GLSL_SRC
   shaders/infos/gpu_shader_sequencer_infos.hh
   shaders/infos/gpu_shader_simple_lighting_infos.hh
   shaders/infos/gpu_shader_text_infos.hh
-  shaders/infos/gpu_shader_xr_raycast_infos.hh
   shaders/infos/gpu_srgb_to_framebuffer_space_infos.hh
 
   shaders/gpu_shader_depth_only_frag.glsl
@@ -555,7 +554,6 @@ set(GLSL_SRC
 
   shaders/gpu_shader_text_vert.glsl
   shaders/gpu_shader_text_frag.glsl
-  shaders/gpu_shader_xr_raycast_vert.glsl
   shaders/gpu_shader_keyframe_shape_vert.glsl
   shaders/gpu_shader_keyframe_shape_frag.glsl
 

source/blender/gpu/GPU_xr_defines.hh
@@ -8,4 +8,4 @@
 
 #pragma once
 
-#define XR_MAX_RAYCASTS 8
+#define XR_TELEPORTATION_ARC_CONTROL_POINTS 12

source/blender/gpu/intern/gpu_shader_builtin.cc
@@ -123,8 +123,6 @@ static const char *builtin_shader_create_info_name(GPUBuiltinShader shader)
       return "gpu_shader_index_2d_array_lines";
     case GPU_SHADER_INDEXBUF_TRIS:
       return "gpu_shader_index_2d_array_tris";
-    case GPU_SHADER_XR_RAYCAST:
-      return "gpu_shader_xr_raycast";
     default:
       BLI_assert_unreachable();
       return "";

source/blender/gpu/shaders/gpu_shader_xr_raycast_vert.glsl
@@ -1,43 +1,0 @@
-/* SPDX-FileCopyrightText: 2016-2024 Blender Authors
- *
- * SPDX-License-Identifier: GPL-2.0-or-later */
-
-#include "infos/gpu_shader_xr_raycast_infos.hh"
-
-VERTEX_SHADER_CREATE_INFO(gpu_shader_xr_raycast)
-
-vec3 catmull_rom(vec3 p0, vec3 p1, vec3 p2, vec3 p3, float t)
-{
-  float t2 = t * t;
-  float t3 = t2 * t;
-  return 0.5 * ((2.0 * p1) + (-p0 + p2) * t + (2.0 * p0 - 5.0 * p1 + 4.0 * p2 - p3) * t2 +
-                (-p0 + 3.0 * p1 - 3.0 * p2 + p3) * t3);
-}
-
-vec3 get_control_point(int idx)
-{
-  idx = clamp(idx, 0, control_point_count - 1);
-  return control_points[idx].xyz;
-}
-
-void main()
-{
-  int sample_idx = gl_VertexID >> 1;
-  float side = ((gl_VertexID & 1) != 0) ? -1.0 : 1.0;
-
-  /** Interpolate within the range: [0, segment_count] */
-  float sample_value = float(sample_idx) * float(control_point_count - 1) /
-                       float(sample_count - 1);
-
-  int segment_idx = int(sample_value);
-  float t = sample_value - float(segment_idx);
-
-  vec3 p0 = get_control_point(segment_idx - 1);
-  vec3 p1 = get_control_point(segment_idx + 0);
-  vec3 p2 = get_control_point(segment_idx + 1);
-  vec3 p3 = get_control_point(segment_idx + 2);
-
-  vec3 pos = catmull_rom(p0, p1, p2, p3, t) + 0.5 * width * side * right_vector;
-
-  gl_Position = ModelViewProjectionMatrix * vec4(pos, 1.0);
-}

source/blender/gpu/shaders/infos/gpu_shader_xr_raycast_infos.hh
@@ -1,33 +1,0 @@
-/* SPDX-FileCopyrightText: 2022 Blender Authors
- *
- * SPDX-License-Identifier: GPL-2.0-or-later */
-
-/** \file
- * \ingroup gpu
- */
-
-#ifdef GPU_SHADER
-#  pragma once
-#  include "gpu_shader_compat.hh"
-
-#  include "GPU_shader_shared.hh"
-#endif
-
-#include "GPU_xr_defines.hh"
-#include "gpu_shader_create_info.hh"
-
-GPU_SHADER_CREATE_INFO(gpu_shader_xr_raycast)
-DEFINE_VALUE("XR_MAX_RAYCASTS", STRINGIFY(XR_MAX_RAYCASTS))
-FRAGMENT_OUT(0, float4, fragColor)
-PUSH_CONSTANT_ARRAY(float4, control_points, XR_MAX_RAYCASTS + 1)
-PUSH_CONSTANT(float4x4, ModelViewProjectionMatrix)
-PUSH_CONSTANT(float4, color)
-PUSH_CONSTANT(float3, right_vector)
-PUSH_CONSTANT(float, width)
-PUSH_CONSTANT(int, control_point_count)
-PUSH_CONSTANT(int, sample_count)
-VERTEX_SOURCE("gpu_shader_xr_raycast_vert.glsl")
-FRAGMENT_SOURCE("gpu_shader_uniform_color_frag.glsl")
-ADDITIONAL_INFO(gpu_srgb_to_framebuffer_space)
-DO_STATIC_COMPILATION()
-GPU_SHADER_CREATE_END()

source/blender/windowmanager/xr/intern/wm_xr_draw.cc
@@ -332,17 +332,8 @@ static void wm_xr_controller_model_draw(const XrSessionSettings *settings,
 
 static void wm_xr_controller_aim_draw(const XrSessionSettings *settings, wmXrSessionState *state)
 {
-  bool draw_ray;
-  switch (settings->controller_draw_style) {
-    case XR_CONTROLLER_DRAW_DARK:
-    case XR_CONTROLLER_DRAW_LIGHT:
-      draw_ray = false;
-      break;
-    case XR_CONTROLLER_DRAW_DARK_RAY:
-    case XR_CONTROLLER_DRAW_LIGHT_RAY:
-      draw_ray = !state->is_raycast_shown;
-      break;
-  }
+  const bool draw_ray = (settings->controller_draw_style == XR_CONTROLLER_DRAW_DARK_RAY ||
+                         settings->controller_draw_style == XR_CONTROLLER_DRAW_LIGHT_RAY);
 
   GPUVertFormat *format = immVertexFormat();
   uint pos = GPU_vertformat_attr_add(format, "pos", blender::gpu::VertAttrType::SFLOAT_32_32_32);

commit 322cebec8398c256361b629368306504f2b17628

Cleanup: Spelling: Revert "frame-buffer" typo fix
Small mistake that slipped in during main merge conflict resolution,
commit d1c9ef11 made a spelling cleanup by replacing "framebuffer" with
"frame-buffer", and commit 899ce2e182 inadvertently overwrote it back to
"framebuffer", revert this change.

source/blender/gpu/GPU_shader_builtin.hh
@@ -134,7 +134,7 @@ enum GPUBuiltinShader {
 
   /**
    * Draw a sRGB color space texture in 3D.
-   * Texture color space is assumed to match the framebuffer.
+   * Texture color space is assumed to match the frame-buffer.
    * Take a 3D position and a 2D texture coordinate for each vertex.
    *
    * \param image: uniform sampler2D