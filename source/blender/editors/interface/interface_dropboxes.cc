Pagecran/blender

commit a4f0005149aad17296c5e0cde92f99710147de9c

Refactor: UI: Use std::string for drag & drop disabled hint storage
Avoids having to manually manage memory of the string. Especially useful when
potentially overriding the string.

source/blender/editors/interface/interface_dropboxes.cc
@@ -41,12 +41,12 @@ static bool ui_view_drop_poll(bContext *C, wmDrag *drag, const wmEvent *event)
     return false;
   }
 
-  if (drag->drop_state.free_disabled_info) {
-    MEM_SAFE_FREE(drag->drop_state.disabled_info);
-  }
-  drag->drop_state.free_disabled_info = false;
+  const char *disabled_info = "";
+  const bool can_drop = drop_target->can_drop(*drag, &disabled_info);
 
-  return drop_target->can_drop(*drag, &drag->drop_state.disabled_info);
+  drag->drop_state.disabled_info = disabled_info;
+
+  return can_drop;
 }
 
 static std::string ui_view_drop_tooltip(bContext *C,

source/blender/windowmanager/WM_types.hh
@@ -1329,8 +1329,7 @@ struct wmDragActiveDropState {
    * operator poll fails. Typically the message the operator set with
    * #CTX_wm_operator_poll_msg_set().
    */
-  const char *disabled_info;
-  bool free_disabled_info;
+  std::optional<std::string> disabled_info;
 
   std::string tooltip;
 };

source/blender/windowmanager/intern/wm_dragdrop.cc
@@ -427,9 +427,6 @@ void WM_drag_free(wmDrag *drag)
     WM_drag_data_free(drag->type, drag->poin);
   }
   drag->drop_state.ui_context.reset();
-  if (drag->drop_state.free_disabled_info) {
-    MEM_SAFE_FREE(drag->drop_state.disabled_info);
-  }
   BLI_freelistN(&drag->ids);
   LISTBASE_FOREACH_MUTABLE (wmDragAssetListItem *, asset_item, &drag->asset_items) {
     if (asset_item->is_external) {
@@ -493,7 +490,9 @@ static wmDropBox *dropbox_active(bContext *C,
           const char *disabled_hint = CTX_wm_operator_poll_msg_get(C, &free_disabled_info);
           if (disabled_hint) {
             drag->drop_state.disabled_info = disabled_hint;
-            drag->drop_state.free_disabled_info = free_disabled_info;
+            if (free_disabled_info) {
+              MEM_SAFE_FREE(disabled_hint);
+            }
           }
         }
       }
@@ -511,10 +510,7 @@ static wmDropBox *wm_dropbox_active(bContext *C, wmDrag *drag, const wmEvent *ev
   ScrArea *area = BKE_screen_find_area_xy(screen, SPACE_TYPE_ANY, event->xy);
   wmDropBox *drop = nullptr;
 
-  if (drag->drop_state.free_disabled_info) {
-    MEM_SAFE_FREE(drag->drop_state.disabled_info);
-  }
-  drag->drop_state.disabled_info = nullptr;
+  drag->drop_state.disabled_info = std::nullopt;
 
   if (area) {
     ARegion *region = BKE_area_find_region_xy(area, RGN_TYPE_ANY, event->xy);
@@ -1121,7 +1117,7 @@ static void wm_drag_draw_tooltip(bContext *C, wmWindow *win, wmDrag *drag, const
   int padding = 4 * UI_SCALE_FAC;
   blender::StringRef tooltip = drag->drop_state.tooltip;
   const bool has_disabled_info = drag->drop_state.disabled_info &&
-                                 drag->drop_state.disabled_info[0];
+                                 drag->drop_state.disabled_info.value()[0];
   if (tooltip.is_empty() && !has_disabled_info) {
     return;
   }
@@ -1178,7 +1174,7 @@ static void wm_drag_draw_tooltip(bContext *C, wmWindow *win, wmDrag *drag, const
     wm_drop_operator_draw(tooltip, x, y);
   }
   else if (has_disabled_info) {
-    wm_drop_redalert_draw(drag->drop_state.disabled_info, x, y);
+    wm_drop_redalert_draw(*drag->drop_state.disabled_info, x, y);
   }
 }
 

commit 782b374fa835487640fa0384272e15b6496efbf5

Fix #151287: Regression: Crash when dragging local asset into a catalog
Caused by a4f0005149aad17296c5e0cde92f99710147de9c
Crashed due to missing nullptr check

Pull Request: https://projects.blender.org/blender/blender/pulls/151289

source/blender/editors/interface/interface_dropboxes.cc
@@ -44,8 +44,9 @@ static bool ui_view_drop_poll(bContext *C, wmDrag *drag, const wmEvent *event)
   const char *disabled_info = "";
   const bool can_drop = drop_target->can_drop(*drag, &disabled_info);
 
-  drag->drop_state.disabled_info = disabled_info;
-
+  if (disabled_info) {
+    drag->drop_state.disabled_info = disabled_info;
+  }
   return can_drop;
 }
 

source/blender/editors/space_file/asset_catalog_tree_view.cc
@@ -522,7 +522,6 @@ bool AssetCatalogDropTarget::has_droppable_asset(const wmDrag &drag, const char 
 {
   const ListBase *asset_drags = WM_drag_asset_list_get(&drag);
 
-  *r_disabled_hint = nullptr;
   /* There needs to be at least one asset from the current file. */
   LISTBASE_FOREACH (const wmDragAssetListItem *, asset_item, asset_drags) {
     if (!asset_item->is_external) {