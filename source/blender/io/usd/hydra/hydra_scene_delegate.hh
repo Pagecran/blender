Pagecran/blender

commit 86171ce9b8e7e08e8a34ac407e2f058b325e9372

Cleanup: USD: Remove unnecessary includes
Remove some unnecessary includes and make use of more appropriate
forward declarations or other includes where applicable.

Pull Request: https://projects.blender.org/blender/blender/pulls/151560

source/blender/io/usd/hydra/hydra_scene_delegate.hh
@@ -4,10 +4,7 @@
 
 #pragma once
 
-#include <pxr/base/gf/vec2f.h>
 #include <pxr/imaging/hd/sceneDelegate.h>
-
-#include "DEG_depsgraph.hh"
 
 #include "CLG_log.h"
 
@@ -20,7 +17,11 @@
 #include "world.hh"
 
 struct Depsgraph;
+struct ID;
 struct Main;
+struct Material;
+struct Object;
+struct ParticleSystem;
 struct Scene;
 struct View3D;
 

source/blender/io/usd/hydra/mesh.cc
@@ -2,8 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include <pxr/base/gf/vec2f.h>
-#include <pxr/base/tf/staticTokens.h>
+#include <pxr/base/tf/token.h>
 #include <pxr/imaging/hd/tokens.h>
 
 #include "BLI_array_utils.hh"
@@ -11,7 +10,6 @@
 #include "BLI_vector_set.hh"
 
 #include "BKE_attribute.hh"
-#include "BKE_customdata.hh"
 #include "BKE_material.hh"
 #include "BKE_mesh.hh"
 

source/blender/io/usd/hydra/curves.hh
@@ -4,17 +4,19 @@
 
 #pragma once
 
-#include <pxr/base/vt/array.h>
+#include <pxr/base/vt/types.h>
 #include <pxr/imaging/hd/sceneDelegate.h>
 
 #include "BLI_set.hh"
 
-#include "DNA_particle_types.h"
-
 #include "material.hh"
 #include "object.hh"
 
+struct ParticleSystem;
+
 namespace blender::io::hydra {
+
+class HydraSceneDelegate;
 
 class CurvesData : public ObjectData {
  protected:

source/blender/io/usd/hydra/id.hh
@@ -8,9 +8,9 @@
 #include <pxr/base/vt/value.h>
 #include <pxr/usd/sdf/path.h>
 
-#include "DNA_ID.h"
-
 #include "BLI_hash.hh"
+
+struct ID;
 
 template<> struct blender::DefaultHash<pxr::SdfPath> {
   uint64_t operator()(const pxr::SdfPath &value) const

source/blender/io/usd/hydra/instancer.cc
@@ -4,16 +4,14 @@
 
 #include "instancer.hh"
 
-#include <pxr/base/gf/vec2f.h>
-#include <pxr/imaging/hd/light.h>
+#include <pxr/base/vt/types.h>
+#include <pxr/base/vt/value.h>
 
 #include "BKE_duplilist.hh"
 #include "BKE_particle.h"
 
 #include "BLI_listbase.h"
 #include "BLI_string.h"
-
-#include "DEG_depsgraph_query.hh"
 
 #include "DNA_particle_types.h"
 

source/blender/io/usd/hydra/light.cc
@@ -10,9 +10,7 @@
 
 #include "DNA_light_types.h"
 
-#include "IMB_colormanagement.hh"
-
-#include "BLI_math_rotation.h"
+#include "BLI_math_constants.h"
 
 #include "hydra_scene_delegate.hh"
 

source/blender/io/usd/hydra/light.hh
@@ -5,7 +5,6 @@
 #pragma once
 
 #include <pxr/base/tf/hashmap.h>
-#include <pxr/usd/sdf/assetPath.h>
 #include <pxr/usd/sdf/path.h>
 
 #include "BKE_light.h"

source/blender/io/usd/hydra/material.cc
@@ -5,9 +5,6 @@
 #include "material.hh"
 #include "usd_private.hh"
 
-#include <Python.h>
-#include <unicodeobject.h>
-
 #include <pxr/base/tf/stringUtils.h>
 #include <pxr/imaging/hd/material.h>
 #include <pxr/imaging/hd/renderDelegate.h>

commit 47b44163e7d7c1526557e3272498450c6d430133

Fix broken build in USD Hydra code on linux clang/mold.
Caused by 86171ce9b8.

source/blender/io/usd/hydra/hydra_scene_delegate.hh
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <pxr/base/gf/vec2f.h>
 #include <pxr/imaging/hd/sceneDelegate.h>
 
 #include "CLG_log.h"

source/blender/io/usd/hydra/mesh.cc
@@ -2,6 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include <pxr/base/gf/vec2f.h>
 #include <pxr/base/tf/token.h>
 #include <pxr/imaging/hd/tokens.h>
 