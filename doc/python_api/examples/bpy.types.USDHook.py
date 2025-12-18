Pagecran/blender

commit db88381e7533858a8c13db98b7262f8809fd8d69

Docs: Include `get_prim_map()` in `bpy.types.USDHook` example code
Provide an example of using the `get_prim_map()` in the existing USD
Hook sample.

Related: blender/blender@0df5d8220b407fc6929302f1ca7040dd66476be7

Pull Request: https://projects.blender.org/blender/blender/pulls/146866

doc/python_api/examples/bpy.types.USDHook.py
@@ -245,8 +245,28 @@ class USDHookExample(bpy.types.USDHook):
 
     @staticmethod
     def on_import(import_context):
-        """ Create a text object to display the stage's custom data.
+        """Inspect the imported stage & objects to set some custom data
         """
+
+        ###########################################################
+        # Store some USD metadata on each imported data block.
+        ###########################################################
+        prim_map = import_context.get_prim_map()
+
+        # Store prim path as a string on each data block created.
+        for prim_path, data_blocks in prim_map.items():
+
+            # Type hints for prim map.
+            prim_path: Sdf.Path
+            data_blocks: list[bpy.types.ID]
+
+            # Loop over mapped data blocks to store some metadata.
+            for data_block in data_blocks:
+                data_block["prim_path"] = str(prim_path)
+
+        ###########################################################
+        # Create a text object to display the stage's custom data.
+        ###########################################################
         stage = import_context.get_stage()
 
         if stage is None:

commit 35785eeee2fb818cea3a0917dd8d6d3f5a25f359

Fix #151742: Docs: Fix Typo in `bpy.types.USDHook` code
Changes to example code introduced with db88381e753385 contains a typo,
using `data block` instead of the standard `data-block`.

This changes ensures consistency with the spelling of `data-block` with
other API example files.

Pull Request: https://projects.blender.org/blender/blender/pulls/151850

doc/python_api/examples/bpy.types.USDHook.py
@@ -251,18 +251,18 @@ class USDHookExample(bpy.types.USDHook):
         """
 
         ###########################################################
-        # Store some USD metadata on each imported data block.
+        # Store some USD metadata on each imported data-block.
         ###########################################################
         prim_map = import_context.get_prim_map()
 
-        # Store prim path as a string on each data block created.
+        # Store prim path as a string on each data-block created.
         for prim_path, data_blocks in prim_map.items():
 
             # Type hints for prim map.
             prim_path: Sdf.Path
             data_blocks: list[bpy.types.ID]
 
-            # Loop over mapped data blocks to store some metadata.
+            # Loop over mapped data-blocks to store some metadata.
             for data_block in data_blocks:
                 data_block["prim_path"] = str(prim_path)
 