Pagecran/blender

commit 4fe5946db3feece991211f0feb32160fedf11fd9

Assets: Disable editing catalogs in essentials asset library
Allowing this doesn't really make sense. Essentials assets cannot be
assigned to different catalogs anyway (without opening the source .blend
files, which users shouldn't do and will be reset in new Blender
versions). The fact that this was possible before was probably an
oversight, I don't think this was ever a conscious decision.

Pull Request: https://projects.blender.org/blender/blender/pulls/151176

source/blender/asset_system/intern/asset_catalog.cc
@@ -33,9 +33,11 @@ namespace blender::asset_system {
 
 const CatalogFilePath AssetCatalogService::DEFAULT_CATALOG_FILENAME = "blender_assets.cats.txt";
 
-AssetCatalogService::AssetCatalogService(const CatalogFilePath &asset_library_root)
+AssetCatalogService::AssetCatalogService(const CatalogFilePath &asset_library_root,
+                                         std::optional<read_only_tag> read_only_tag)
     : catalog_collection_(std::make_unique<AssetCatalogCollection>()),
-      asset_library_root_(asset_library_root)
+      asset_library_root_(asset_library_root),
+      is_read_only_(read_only_tag ? true : false)
 {
 }
 

source/blender/asset_system/intern/asset_library.cc
@@ -226,11 +226,15 @@ void AS_asset_library_essential_import_method_update()
 
 namespace blender::asset_system {
 
-AssetLibrary::AssetLibrary(eAssetLibraryType library_type, StringRef name, StringRef root_path)
+AssetLibrary::AssetLibrary(
+    eAssetLibraryType library_type,
+    StringRef name,
+    StringRef root_path,
+    std::optional<AssetCatalogService::read_only_tag> catalogs_read_only_tag)
     : library_type_(library_type),
       name_(name),
       root_path_(std::make_shared<std::string>(utils::normalize_directory_path(root_path))),
-      catalog_service_(std::make_unique<AssetCatalogService>(*root_path_))
+      catalog_service_(std::make_unique<AssetCatalogService>(*root_path_, catalogs_read_only_tag))
 {
 }
 

source/blender/asset_system/AS_asset_catalog.hh
@@ -61,7 +61,8 @@ class AssetCatalogService {
 
   struct read_only_tag {};
 
-  explicit AssetCatalogService(const CatalogFilePath &asset_library_root = {});
+  explicit AssetCatalogService(const CatalogFilePath &asset_library_root = {},
+                               std::optional<read_only_tag> read_only_tag = std::nullopt);
   explicit AssetCatalogService(read_only_tag);
 
   /**

source/blender/asset_system/AS_asset_library.hh
@@ -108,7 +108,11 @@ class AssetLibrary {
    *              #ASSET_LIBRARY_CUSTOM ones.
    * \param root_path: If this is an asset library on disk, the top-level directory path.
    */
-  AssetLibrary(eAssetLibraryType library_type, StringRef name = "", StringRef root_path = "");
+  AssetLibrary(
+      eAssetLibraryType library_type,
+      StringRef name = "",
+      StringRef root_path = "",
+      std::optional<AssetCatalogService::read_only_tag> catalogs_read_only_tag = std::nullopt);
   virtual ~AssetLibrary();
 
   /**

source/blender/asset_system/intern/library_types/essentials_library.cc
@@ -20,7 +20,8 @@ namespace blender::asset_system {
 EssentialsAssetLibrary::EssentialsAssetLibrary()
     : OnDiskAssetLibrary(ASSET_LIBRARY_ESSENTIALS,
                          {},
-                         utils::normalize_directory_path(essentials_directory_path()))
+                         utils::normalize_directory_path(essentials_directory_path()),
+                         AssetCatalogService::read_only_tag{})
 {
   import_method_ = ASSET_IMPORT_PACK;
   if (U.experimental.no_data_block_packing) {

source/blender/asset_system/intern/library_types/on_disk_library.cc
@@ -10,10 +10,12 @@
 
 namespace blender::asset_system {
 
-OnDiskAssetLibrary::OnDiskAssetLibrary(eAssetLibraryType library_type,
-                                       StringRef name,
-                                       StringRef root_path)
-    : AssetLibrary(library_type, name, root_path)
+OnDiskAssetLibrary::OnDiskAssetLibrary(
+    eAssetLibraryType library_type,
+    StringRef name,
+    StringRef root_path,
+    std::optional<AssetCatalogService::read_only_tag> catalogs_read_only_tag)
+    : AssetLibrary(library_type, name, root_path, catalogs_read_only_tag)
 {
   this->on_blend_save_handler_register();
 }

source/blender/asset_system/intern/library_types/on_disk_library.hh
@@ -14,9 +14,11 @@ namespace blender::asset_system {
 
 class OnDiskAssetLibrary : public AssetLibrary {
  public:
-  OnDiskAssetLibrary(eAssetLibraryType library_type,
-                     StringRef name = "",
-                     StringRef root_path = "");
+  OnDiskAssetLibrary(
+      eAssetLibraryType library_type,
+      StringRef name = "",
+      StringRef root_path = "",
+      std::optional<AssetCatalogService::read_only_tag> catalogs_read_only_tag = std::nullopt);
 
   std::optional<AssetLibraryReference> library_reference() const override;
   void refresh_catalogs() override;

commit 0bc28ee2a50431286842c155f95c681d49d4bcf9

Fix: Failing (harmless) assert on file save after recent commit
Saving a file would throw a failing assert since 4fe5946db3.

The file-save handler to write the catalog definition file didn't
account for the case where asset libraries are read-only. It would
attempt to write the catalog definition file anyway.

source/blender/asset_system/intern/asset_catalog.cc
@@ -469,7 +469,12 @@ bool AssetCatalogService::is_catalog_known_with_unsaved_changes(const CatalogID 
 
 bool AssetCatalogService::write_to_disk(const CatalogFilePath &blend_file_path)
 {
+  /* The caller should probably check this somewhat earlier and properly disable whatever operation
+   * triggers the writing. */
   BLI_assert(!is_read_only_);
+  if (is_read_only_) {
+    return false;
+  }
 
   if (!this->write_to_disk_ex(blend_file_path)) {
     return false;

source/blender/asset_system/intern/asset_library.cc
@@ -388,7 +388,7 @@ void AssetLibrary::on_blend_save_post(Main *bmain,
                                       PointerRNA ** /*pointers*/,
                                       const int /*num_pointers*/)
 {
-  if (save_catalogs_when_file_is_saved) {
+  if (save_catalogs_when_file_is_saved && !this->catalog_service().is_read_only()) {
     this->catalog_service().write_to_disk(bmain->filepath);
   }
 }