Pagecran/blender

commit 799a34fef4f32b90207fab533aef0661a412ac64

Refactor: Simplify shapkey copy function
No functional changes intended.

This simplifies the process of copying the shapekey data.

* `cp_cu_key` was removed. This was only ever called with the range `{0, tot}`
meaning it always copied all the nurbs segments anyway. Because of that all
vertices of either bpoint or beztriple were always copied. This was replaced
to a call to `copy_key_float3_range` which does not need to differentiate
between bpoint and beztriple.
* all calls to `copy_key` except for 1 were replaced with calls to `copy_key_float3_range`
* `copy_key` was converted into `copy_key_float3_weighted` to hint at what's
so special about this function. It was also greatly simplified.

Pull Request: https://projects.blender.org/blender/blender/pulls/151407

source/blender/blenkernel/intern/key.cc
@@ -571,14 +571,6 @@ static void flerp(const int tot,
   }
 }
 
-static void rel_flerp(
-    const int tot, float *in, const float *ref, const float *out, const float fac)
-{
-  for (int a = 0; a < tot; a++) {
-    in[a] -= fac * (ref[a] - out[a]);
-  }
-}
-
 static char *key_block_get_data(Key *key, KeyBlock *actkb, KeyBlock *kb, char **freedata)
 {
   if (kb == actkb) {
@@ -650,213 +642,6 @@ static bool key_pointer_size(
 }
 
 /**
- * Copy the shapekey of `active_keyblock` into the output array of `r_target`.
- */
-static void copy_key(const int start,
-                     int end,
-                     const int vertex_count,
-                     char *r_target,
-                     Key *key,
-                     KeyBlock *active_keyblock,
-                     KeyBlock *kb,
-                     float *weights,
-                     const int mode)
-{
-  int ofs[32];
-  /* Currently always 0, in future key_pointer_size may assign. */
-  ofs[1] = 0;
-
-  int pointer_size = 0;
-  int step;
-  if (!key_pointer_size(key, mode, &pointer_size, &ofs[0], &step)) {
-    return;
-  }
-
-  end = std::min(end, vertex_count);
-
-  bool flagflo = false;
-  /* I (christoph) don't know what those variables do but they are only used if
-   * `flagflo` is set to true. */
-  float ktot = 0.0, kd = 0.0;
-  if (vertex_count != kb->totelem) {
-    ktot = 0.0;
-    flagflo = true;
-    if (kb->totelem) {
-      kd = kb->totelem / float(vertex_count);
-    }
-    else {
-      return;
-    }
-  }
-
-  char *free_k1, *free_kref;
-  char *k1 = key_block_get_data(key, active_keyblock, kb, &free_k1);
-  char *kref = key_block_get_data(key, active_keyblock, key->refkey, &free_kref);
-
-  /* This exception is needed curves with multiple splines. */
-  if (start != 0) {
-
-    r_target += pointer_size * start;
-
-    if (flagflo) {
-      ktot += start * kd;
-      const int ktot_floor = int(floor(ktot));
-      if (ktot_floor) {
-        ktot -= ktot_floor;
-        k1 += ktot_floor * key->elemsize;
-      }
-    }
-    else {
-      k1 += start * key->elemsize;
-    }
-  }
-
-  char elemstr[8];
-  if (mode == KEY_MODE_BEZTRIPLE) {
-    elemstr[0] = 1;
-    elemstr[1] = IPO_BEZTRIPLE;
-    elemstr[2] = 0;
-  }
-
-  /* Just do it here, not above! */
-  const int elemsize = key->elemsize * step;
-
-  for (int a = start; a < end; a += step) {
-    char *cp = key->elemstr;
-    if (mode == KEY_MODE_BEZTRIPLE) {
-      cp = elemstr;
-    }
-
-    int *ofsp = ofs;
-
-    while (cp[0]) {
-
-      switch (cp[1]) {
-        case IPO_FLOAT:
-          if (weights) {
-            memcpy(r_target, kref, sizeof(float[KEYELEM_FLOAT_LEN_COORD]));
-            if (*weights != 0.0f) {
-              rel_flerp(KEYELEM_FLOAT_LEN_COORD,
-                        (float *)r_target,
-                        (float *)kref,
-                        (float *)k1,
-                        *weights);
-            }
-            weights++;
-          }
-          else {
-            memcpy(r_target, k1, sizeof(float[KEYELEM_FLOAT_LEN_COORD]));
-          }
-          break;
-        case IPO_BPOINT:
-          memcpy(r_target, k1, sizeof(float[KEYELEM_FLOAT_LEN_BPOINT]));
-          break;
-        case IPO_BEZTRIPLE:
-          memcpy(r_target, k1, sizeof(float[KEYELEM_FLOAT_LEN_BEZTRIPLE]));
-          break;
-        default:
-          BLI_assert_unreachable();
-          if (free_k1) {
-            MEM_freeN(free_k1);
-          }
-          if (free_kref) {
-            MEM_freeN(free_kref);
-          }
-          BLI_assert_msg(0, "invalid 'cp[1]'");
-          return;
-      }
-
-      r_target += *ofsp;
-      cp += 2;
-      ofsp++;
-    }
-
-    /* Are we going to be nasty? */
-    if (flagflo) {
-      ktot += kd;
-      while (ktot >= 1.0f) {
-        ktot -= 1.0f;
-        k1 += elemsize;
-        kref += elemsize;
-      }
-    }
-    else {
-      k1 += elemsize;
-      kref += elemsize;
-    }
-  }
-
-  if (free_k1) {
-    MEM_freeN(free_k1);
-  }
-  if (free_kref) {
-    MEM_freeN(free_kref);
-  }
-}
-
-static void cp_cu_key(Curve *cu,
-                      Key *key,
-                      KeyBlock *actkb,
-                      KeyBlock *kb,
-                      const int start,
-                      int end,
-                      char *out,
-                      const int tot)
-{
-  Nurb *nu;
-  int a, step, a1, a2;
-
-  for (a = 0, nu = static_cast<Nurb *>(cu->nurb.first); nu; nu = nu->next, a += step) {
-    if (nu->bp) {
-      step = KEYELEM_ELEM_LEN_BPOINT * nu->pntsu * nu->pntsv;
-
-      a1 = max_ii(a, start);
-      a2 = min_ii(a + step, end);
-
-      if (a1 < a2) {
-        copy_key(a1, a2, tot, out, key, actkb, kb, nullptr, KEY_MODE_BPOINT);
-      }
-    }
-    else if (nu->bezt) {
-      step = KEYELEM_ELEM_LEN_BEZTRIPLE * nu->pntsu;
-
-      /* Exception because keys prefer to work with complete blocks. */
-      a1 = max_ii(a, start);
-      a2 = min_ii(a + step, end);
-
-      if (a1 < a2) {
-        copy_key(a1, a2, tot, out, key, actkb, kb, nullptr, KEY_MODE_BEZTRIPLE);
-      }
-    }
-    else {
-      step = 0;
-    }
-  }
-}
-
-/**
- * Copy a subset of the given shapekey `source` into `r_target`.
- *
- * \param range is the range of vertices to copy. Each index is considered to be a float3.
- */
-static void copy_key_float3_range(const blender::IndexRange range,
-                                  Key *key,
-                                  KeyBlock *source,
-                                  float *r_target)
-{
-  char *free_keyblock_data;
-  float *keyblock_data = reinterpret_cast<float *>(
-      key_block_get_data(key, source, key->refkey, &free_keyblock_data));
-
-  memcpy(
-      &r_target[range.start()], &keyblock_data[range.start()], range.size() * 3 * sizeof(float));
-
-  if (free_keyblock_data) {
-    MEM_freeN(free_keyblock_data);
-  }
-}
-
-/**
  * Move the point in `r_targets` along the vector of ab by a factor of `weight`.
  *
  * \param start_index points to the x value in the flat float array. Indices of +1 and +2 from this
@@ -868,6 +653,77 @@ static void add_weighted_vector(
   r_target[start_index + 0] += weight * (b[start_index + 0] - a[start_index + 0]);
   r_target[start_index + 1] += weight * (b[start_index + 1] - a[start_index + 1]);
   r_target[start_index + 2] += weight * (b[start_index + 2] - a[start_index + 2]);
+}
+
+/**
+ * Copy a subset of the given shapekey `source` into `r_target`.
+ *
+ * \param range is the range of vertices to copy. Each index is considered to be a float3.
+ */
+static void copy_key_float3_range(const blender::IndexRange range,
+                                  Key *key,
+                                  KeyBlock *active_keyblock,
+                                  KeyBlock *source,
+                                  float *r_target)
+{
+  char *free_keyblock_data;
+  float *keyblock_data = reinterpret_cast<float *>(
+      key_block_get_data(key, active_keyblock, source, &free_keyblock_data));
+
+  memcpy(
+      &r_target[range.start()], &keyblock_data[range.start()], range.size() * 3 * sizeof(float));
+
+  if (free_keyblock_data) {
+    MEM_freeN(free_keyblock_data);
+  }
+}
+
+/**
+ * Copy the shapekey of `source` into the output array of `r_target`.
+ *
+ * \param weights is a float array of size `vertex_count`. It determines how much of `source` is
+ * blended into the result. The base for it is the reference key. If this is passed as a nullptr,
+ * `source` is copied at full weight.
+ */
+static void copy_key_float3_weighted(const int vertex_count,
+                                     Key *key,
+                                     KeyBlock *active_keyblock,
+                                     KeyBlock *source,
+                                     const float *weights,
+                                     float *r_target)
+{
+  if (!weights) {
+    copy_key_float3_range({0, vertex_count}, key, active_keyblock, source, r_target);
+    return;
+  }
+
+  if (vertex_count != source->totelem) {
+    /* There was a system in place before that worked with keys that only have partial data. I
+     * (christoph) removed that since there is no known case for that. */
+    BLI_assert_unreachable();
+    return;
+  }
+
+  char *free_source_data, *free_refkey_data;
+  const float *source_data = reinterpret_cast<float *>(
+      key_block_get_data(key, active_keyblock, source, &free_source_data));
+  const float *refkey_data = reinterpret_cast<float *>(
+      key_block_get_data(key, active_keyblock, key->refkey, &free_refkey_data));
+
+  for (int i = 0; i < vertex_count; i++) {
+    const int vector_index = i * 3;
+    memcpy(&r_target[vector_index], &refkey_data[vector_index], 3 * sizeof(float));
+    if (weights[i] != 0.0f) {
+      add_weighted_vector(vector_index, weights[i], refkey_data, source_data, r_target);
+    }
+  }
+
+  if (free_source_data) {
+    MEM_freeN(free_source_data);
+  }
+  if (free_refkey_data) {
+    MEM_freeN(free_refkey_data);
+  }
 }
 
 /**
@@ -889,7 +745,7 @@ static void key_evaluate_relative_float3(Key *key,
                                          float *target_data)
 {
   /* Creates the basis values of the reference key in target_data. */
-  copy_key_float3_range(range, key, key->refkey, target_data);
+  copy_key_float3_range(range, key, active_keyblock, key->refkey, target_data);
 
   int keyblock_index = 0;
   LISTBASE_FOREACH_INDEX (KeyBlock *, kb, &key->block, keyblock_index) {
@@ -1347,7 +1203,7 @@ static void do_mesh_key(Object *ob, Key *key, char *out, const int tot)
       do_key(0, tot, tot, out, key, actkb, k, t, KEY_MODE_DUMMY);
     }
     else {
-      copy_key(0, tot, tot, out, key, actkb, k[2], nullptr, KEY_MODE_DUMMY);
+      copy_key_float3_range({0, tot}, key, actkb, k[2], reinterpret_cast<float *>(out));
     }
   }
 }
@@ -1412,7 +1268,7 @@ static void do_curve_key(Object *ob, Key *key, char *out, const int tot)
       do_cu_key(cu, key, actkb, k, t, out, tot);
     }
     else {
-      cp_cu_key(cu, key, actkb, k[2], 0, tot, out, tot);
+      copy_key_float3_range({0, tot}, key, actkb, k[2], reinterpret_cast<float *>(out));
     }
   }
 }
@@ -1440,7 +1296,7 @@ static void do_latt_key(Object *ob, Key *key, char *out, const int tot)
       do_key(0, tot, tot, out, key, actkb, k, t, KEY_MODE_DUMMY);
     }
     else {
-      copy_key(0, tot, tot, out, key, actkb, k[2], nullptr, KEY_MODE_DUMMY);
+      copy_key_float3_range({0, tot}, key, actkb, k[2], reinterpret_cast<float *>(out));
     }
   }
 
@@ -1517,16 +1373,15 @@ float *BKE_key_evaluate_object_ex(
     }
 
     if (OB_TYPE_SUPPORT_VGROUP(ob->type)) {
-      float *weights = get_weights_array(ob, kb->vgroup, nullptr);
-
-      copy_key(0, tot, tot, out, key, actkb, kb, weights, 0);
+      const float *weights = get_weights_array(ob, kb->vgroup, nullptr);
+      copy_key_float3_weighted(tot, key, actkb, kb, weights, reinterpret_cast<float *>(out));
 
       if (weights) {
         MEM_freeN(weights);
       }
     }
     else if (ELEM(ob->type, OB_CURVES_LEGACY, OB_SURF)) {
-      cp_cu_key(static_cast<Curve *>(ob->data), key, actkb, kb, 0, tot, out, tot);
+      copy_key_float3_range({0, tot}, key, actkb, kb, reinterpret_cast<float *>(out));
     }
   }
   else {

commit a9326db36973b49cf50e3a5c6fef34ae895482cf

Fix: Shapekey kernel code with unequal element counts
Most of this patch are tests for the kernel code, but this also fixes a bug introduced with
799a34fef4f32b90207fab533aef0661a412ac64 where copying a
shapekey would write out of the bounds of the data array if the element count
is not equal. The reason this is kept together is that it is impossible to
get Blender into a state where the element count of the mesh and the key
are not equal (outside of unit tests), but that is the only way to trigger this issue.

This is relevant for #151472
in which I want to refactor the code without breaking the behavior.

Pull Request: https://projects.blender.org/blender/blender/pulls/151765

source/blender/blenkernel/CMakeLists.txt
@@ -804,6 +804,7 @@ if(WITH_GTESTS)
     intern/idprop_test.cc
     intern/image_partial_update_test.cc
     intern/image_test.cc
+    intern/key_test.cc
     intern/lattice_deform_test.cc
     intern/layer_test.cc
     intern/lib_id_remapper_test.cc

source/blender/blenkernel/intern/key.cc
@@ -625,11 +625,27 @@ static void add_weighted_vector(
 static void copy_key_float3(
     const int vertex_count, Key *key, KeyBlock *active_keyblock, KeyBlock *source, float *r_target)
 {
+  if (vertex_count == 0 || source->totelem == 0) {
+    return;
+  }
   char *free_keyblock_data;
   float *keyblock_data = reinterpret_cast<float *>(
       key_block_get_data(key, active_keyblock, source, &free_keyblock_data));
 
-  memcpy(r_target, keyblock_data, vertex_count * 3 * sizeof(float));
+  if (vertex_count == source->totelem) {
+    memcpy(r_target, keyblock_data, vertex_count * 3 * sizeof(float));
+  }
+  else {
+    /* In case of KeyBlocks that have a different number of elements than the original data.
+     * Maintained for backwards compatibility even though this state should not be reachable
+     * through normal interactions with Blender. */
+    const float step_rate = source->totelem / float(vertex_count);
+    for (int i = 0; i < vertex_count; i++) {
+      /* Rounding down to avoid exceeding the bounds. */
+      const int source_index = int(step_rate * i);
+      memcpy(&r_target[i * 3], &keyblock_data[source_index * 3], 3 * sizeof(float));
+    }
+  }
 
   if (free_keyblock_data) {
     MEM_freeN(free_keyblock_data);

source/blender/blenkernel/intern/key_test.cc
@@ -1,0 +1,366 @@
+/* SPDX-FileCopyrightText: 2025 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "BKE_idtype.hh"
+#include "BKE_key.hh"
+#include "BKE_main.hh"
+#include "BKE_mesh.h"
+#include "BKE_mesh.hh"
+#include "BKE_object.hh"
+
+#include "DNA_object_types.h"
+
+#include "testing/testing.h"
+
+#include "MEM_guardedalloc.h"
+namespace blender {
+
+class ShapekeyTest : public testing::Test {
+ public:
+  Main *bmain = nullptr;
+  Object *ob = nullptr;
+  Mesh *mesh = nullptr;
+
+  static void SetUpTestSuite()
+  {
+    BKE_idtype_init();
+  }
+
+  void SetUp() override
+  {
+    bmain = BKE_main_new();
+    ob = BKE_object_add_only_object(bmain, OB_MESH, "Object");
+    mesh = BKE_mesh_add(bmain, "Test Mesh");
+    ob->data = &mesh->id;
+
+    mesh->verts_num = 4;
+    bke::mesh_ensure_required_data_layers(*mesh);
+    /* Shapekeys don't affect edges and faces so they can be ignored here. */
+    Vector<blender::float3> verts = {
+        {0, 0, 0},
+        {1, 0, 0},
+        {1, 1, 0},
+        {0, 1, 0},
+    };
+    mesh->vert_positions_for_write().copy_from(verts);
+  }
+
+  void TearDown() override
+  {
+    BKE_main_free(bmain);
+  }
+};
+
+namespace bke::tests {
+
+/* Test that creating a shapekey from a mesh has the correct data. */
+TEST_F(ShapekeyTest, mesh_key_creation)
+{
+  Key *key = BKE_key_add(bmain, &mesh->id);
+  ASSERT_EQ(key->from, &mesh->id);
+  /* Assignment to the mesh does not happen automatically by adding it. */
+  mesh->key = key;
+  EXPECT_EQ(BKE_key_from_object(ob), key);
+  KeyBlock *base = BKE_keyblock_add(key, "base");
+  /* This should be set automatically after adding the first key. */
+  ASSERT_EQ(key->refkey, base);
+  /* The elemsize stores how many bytes one element has (vertex in this case). */
+  ASSERT_EQ(key->elemsize, sizeof(float[3]));
+  /* Adding the keyblock does not actually allocate any data for it. */
+  EXPECT_EQ(base->data, nullptr);
+  BKE_keyblock_convert_from_mesh(mesh, key, base);
+  ASSERT_NE(base->data, nullptr);
+  EXPECT_EQ(base->totelem, 4);
+  float3 *data = reinterpret_cast<float3 *>(base->data);
+  Array<float3> expected = {
+      {0, 0, 0},
+      {1, 0, 0},
+      {1, 1, 0},
+      {0, 1, 0},
+  };
+  EXPECT_EQ_ARRAY(&expected[0], data, 4);
+}
+
+TEST_F(ShapekeyTest, mesh_key_evaluation_relative)
+{
+  Key *key = BKE_key_add(bmain, &mesh->id);
+  mesh->key = key;
+  key->type = KEY_RELATIVE;
+  KeyBlock *base = BKE_keyblock_add(key, "base");
+  BKE_keyblock_convert_from_mesh(mesh, key, base);
+
+  KeyBlock *key1 = BKE_keyblock_add(key, "one");
+  BKE_keyblock_convert_from_mesh(mesh, key, key1);
+  float3 *key1_data = reinterpret_cast<float3 *>(key1->data);
+  key1_data[0] = {1, 1, 1};
+
+  key1->curval = 1.0;
+  int totelem = 0;
+  float3 *ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  ASSERT_NE(ob_eval, nullptr);
+  ASSERT_EQ(totelem, mesh->verts_num);
+  Array<float3> expected = {
+      {1, 1, 1},
+      {1, 0, 0},
+      {1, 1, 0},
+      {0, 1, 0},
+  };
+  EXPECT_EQ_ARRAY(&expected[0], ob_eval, 4);
+  MEM_freeN(ob_eval);
+
+  KeyBlock *key2 = BKE_keyblock_add(key, "two");
+  BKE_keyblock_convert_from_mesh(mesh, key, key2);
+  float3 *key2_data = reinterpret_cast<float3 *>(key2->data);
+  key2_data[0] = {2, 2, 2};
+  key2_data[1] = {-1, -1, -1};
+
+  key2->curval = 1.0;
+  ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  expected = {
+      {3, 3, 3},
+      {-1, -1, -1},
+      {1, 1, 0},
+      {0, 1, 0},
+  };
+  EXPECT_EQ_ARRAY(&expected[0], ob_eval, 4);
+  MEM_freeN(ob_eval);
+
+  /* Blend in halfway. */
+  key2->curval = 0.5;
+  ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  expected = {
+      {2, 2, 2},
+      {0, -0.5, -0.5},
+      {1, 1, 0},
+      {0, 1, 0},
+  };
+  EXPECT_EQ_ARRAY(&expected[0], ob_eval, 4);
+  MEM_freeN(ob_eval);
+
+  /* Blend in double. */
+  key2->curval = 2.0;
+  ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  expected = {
+      {5, 5, 5},
+      {-3, -2, -2},
+      {1, 1, 0},
+      {0, 1, 0},
+  };
+  EXPECT_EQ_ARRAY(&expected[0], ob_eval, 4);
+  MEM_freeN(ob_eval);
+}
+
+TEST_F(ShapekeyTest, mesh_key_evaluation_absolute)
+{
+  Key *key = BKE_key_add(bmain, &mesh->id);
+  mesh->key = key;
+  key->type = KEY_NORMAL;
+  KeyBlock *base = BKE_keyblock_add(key, "base");
+  BKE_keyblock_convert_from_mesh(mesh, key, base);
+
+  KeyBlock *key1 = BKE_keyblock_add(key, "one");
+  key1->pos = 1.0;
+  BKE_keyblock_convert_from_mesh(mesh, key, key1);
+  float3 *key1_data = reinterpret_cast<float3 *>(key1->data);
+  key1_data[1] = {1, 1, 1};
+
+  KeyBlock *key2 = BKE_keyblock_add(key, "two");
+  key2->pos = 2.0;
+  BKE_keyblock_convert_from_mesh(mesh, key, key2);
+  float3 *key2_data = reinterpret_cast<float3 *>(key2->data);
+  key2_data[1] = {0, 0, 0};
+  key2_data[2] = {5, 5, 5};
+
+  /* At 0 this should be the base. */
+  key->ctime = 0.0;
+  int totelem = 0;
+  float3 *ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  Array<float3> expected = {
+      {0, 0, 0},
+      {1, 0, 0},
+      {1, 1, 0},
+      {0, 1, 0},
+  };
+  EXPECT_EQ_ARRAY(&expected[0], ob_eval, 4);
+  MEM_freeN(ob_eval);
+
+  /* At 1 this should be at key1. */
+  key->ctime = 100.0;
+  ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  expected = {
+      {0, 0, 0},
+      {1, 1, 1},
+      {1, 1, 0},
+      {0, 1, 0},
+  };
+  EXPECT_EQ_ARRAY(&expected[0], ob_eval, 4);
+  MEM_freeN(ob_eval);
+
+  /* At 2 this should be at key2. */
+  key->ctime = 200.0;
+  ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  expected = {
+      {0, 0, 0},
+      {0, 0, 0},
+      {5, 5, 5},
+      {0, 1, 0},
+  };
+  EXPECT_EQ_ARRAY(&expected[0], ob_eval, 4);
+  MEM_freeN(ob_eval);
+
+  /* This should be a linear blend between key1 and key2; */
+  key->ctime = 150.0;
+  ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  expected = {
+      {0, 0, 0},
+      {0.5, 0.5, 0.5},
+      {3, 3, 2.5},
+      {0, 1, 0},
+  };
+  EXPECT_EQ_ARRAY(&expected[0], ob_eval, 4);
+  EXPECT_NEAR(3.0, 0.5 * key1_data[2][0] + 0.5 * key2_data[2][0], 0.001);
+  MEM_freeN(ob_eval);
+}
+
+/* For historical reasons, keyblocks can end up having an element count that does not match the
+ * element count of the source data (unequal vertex count in the case of meshes). This is not
+ * supported in relative evaluation, so that should ignore the shape key, regardless of its weight.
+ */
+TEST_F(ShapekeyTest, mesh_key_evaluation_relative_uneqal_element_count)
+{
+  Key *key = BKE_key_add(bmain, &mesh->id);
+  mesh->key = key;
+  key->type = KEY_RELATIVE;
+  KeyBlock *base = BKE_keyblock_add(key, "base");
+  BKE_keyblock_convert_from_mesh(mesh, key, base);
+
+  KeyBlock *key1 = BKE_keyblock_add(key, "one");
+  ASSERT_EQ(mesh->verts_num, 4);
+  ASSERT_EQ(key->elemsize, sizeof(float[3]));
+  /* The mesh has 4 vertices, but this shapekey will only have 3. */
+  constexpr int SHAPEKEY_VERTEX_COUNT = 3;
+  float3 *key1_data = reinterpret_cast<float3 *>(
+      MEM_malloc_arrayN(size_t(SHAPEKEY_VERTEX_COUNT), size_t(key->elemsize), __func__));
+  key1->data = key1_data;
+  key1->totelem = SHAPEKEY_VERTEX_COUNT;
+  key1_data[1] = {5, 5, 5};
+
+  key1->curval = 1.0;
+  int totelem = 0;
+  float3 *ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  /* Despite having the key at full influence, it should do nothing since the element count does
+   * not match. */
+  Array<float3> expected = {
+      {0, 0, 0},
+      {1, 0, 0},
+      {1, 1, 0},
+      {0, 1, 0},
+  };
+  EXPECT_EQ_ARRAY(&expected[0], ob_eval, 4);
+  MEM_freeN(ob_eval);
+}
+
+/* Same as mesh_key_evaluation_relative_uneqal_element_count but with absolute shapekeys. This is
+ * somewhat supported but there is no known way to get Blender into such a state. */
+TEST_F(ShapekeyTest, mesh_key_evaluation_absolute_uneqal_element_count)
+{
+  Key *key = BKE_key_add(bmain, &mesh->id);
+  mesh->key = key;
+  key->type = KEY_NORMAL;
+  KeyBlock *base = BKE_keyblock_add(key, "base");
+  BKE_keyblock_convert_from_mesh(mesh, key, base);
+  float3 *base_data = reinterpret_cast<float3 *>(base->data);
+
+  KeyBlock *key1 = BKE_keyblock_add(key, "one");
+  ASSERT_EQ(mesh->verts_num, 4);
+  ASSERT_EQ(key->elemsize, 12);
+  /* The mesh has 4 vertices, but this shapekey will only have 2. */
+  float3 *key1_data = reinterpret_cast<float3 *>(
+      MEM_malloc_arrayN(size_t(2), size_t(key->elemsize), __func__));
+  key1->data = key1_data;
+  key1->totelem = 2;
+  key1_data[0] = {1, 0, 0};
+  key1_data[1] = {2, 0, 0};
+
+  key1->pos = 1.0;
+  key->ctime = 100.0;
+
+  int totelem = 0;
+  float3 *ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  ASSERT_EQ(totelem, 4);
+  /* Vertices are skipped in this case. */
+  Array<float3> expected = {
+      {1, 0, 0},
+      {1, 0, 0},
+      {2, 0, 0},
+      {2, 0, 0},
+  };
+  EXPECT_NEAR_ARRAY_ND(&expected[0], ob_eval, 4, 3, 0.001);
+  MEM_freeN(ob_eval);
+
+  /* Causing blending with the base key. */
+  key->ctime = 95.0;
+
+  totelem = 0;
+  ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  ASSERT_EQ(totelem, 4);
+  expected = {
+      {0.95, 0, 0},
+      {1, 0, 0},
+      {1.95, 0.05, 0},
+      {1.9, 0.05, 0},
+  };
+  EXPECT_NEAR_ARRAY_ND(&expected[0], ob_eval, 4, 3, 0.001);
+  MEM_freeN(ob_eval);
+
+  MEM_freeN(key1_data);
+
+  key->ctime = 100.0;
+  /* Testing with more vertices than the mesh. */
+  key1_data = reinterpret_cast<float3 *>(
+      MEM_malloc_arrayN(size_t(8), size_t(key->elemsize), __func__));
+  key1->data = key1_data;
+  key1->totelem = 8;
+  key1_data[0] = {1, 0, 0};
+  key1_data[1] = {2, 0, 0};
+  key1_data[2] = {3, 0, 0};
+  key1_data[3] = {4, 0, 0};
+  key1_data[4] = {5, 0, 0};
+  key1_data[5] = {6, 0, 0};
+  key1_data[6] = {7, 0, 0};
+  key1_data[7] = {8, 0, 0};
+
+  totelem = 0;
+  ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  ASSERT_EQ(totelem, 4);
+  /* The evaluation ignores any vertices that are extra. */
+  expected = {
+      {1, 0, 0},
+      {3, 0, 0},
+      {5, 0, 0},
+      {7, 0, 0},
+  };
+  EXPECT_NEAR_ARRAY_ND(&expected[0], ob_eval, 4, 3, 0.001);
+  MEM_freeN(ob_eval);
+
+  /* Causing blending with the base key. */
+  key->ctime = 95.0;
+
+  totelem = 0;
+  ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  ASSERT_EQ(totelem, 4);
+  expected = {
+      {0.95, 0, 0},
+      {2.9, 0, 0},
+      {4.8, 0.05, 0},
+      {6.65, 0.05, 0},
+  };
+  EXPECT_NEAR_ARRAY_ND(&expected[0], ob_eval, 4, 3, 0.001);
+  /* The values should be a linear interpolation between base and key1 at 95%. */
+  EXPECT_NEAR(6.65, 0.95 * key1_data[6][0] + 0.05 * base_data[3][0], 0.001);
+  MEM_freeN(ob_eval);
+}
+
+}  // namespace bke::tests
+}  // namespace blender