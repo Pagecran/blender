847c0f300f72205e568d3d0979b5f98cbeaf9f2f
Pascal André <pascalandr@gmail.com>
2026-01-08 15:40:44 +0100

    Update build_msbuild.cmd

--- build_files/windows/build_msbuild.cmd
+++ build_files/windows/build_msbuild.cmd
@@ -1,15 +1,22 @@
 if "%NOBUILD%"=="1" goto EOF
 echo %TIME% > %BUILD_DIR%\buildtime.txt
+
+if exist "%BUILD_DIR%\Blender.slnx" (
+	set SOLUTION_FILE=%BUILD_DIR%\Blender.slnx
+) else (
+	set SOLUTION_FILE=%BUILD_DIR%\Blender.sln
+)
+
 msbuild ^
-	%BUILD_DIR%\Blender.sln ^
+	%SOLUTION_FILE% ^
 	/target:build ^
 	/property:Configuration=%BUILD_TYPE% ^
-	/maxcpucount:2 ^
+	/maxcpucount ^
 	/verbosity:minimal ^
 	/p:platform=%MSBUILD_PLATFORM% ^
-	/flp:Summary;Verbosity=minimal;LogFile=%BUILD_DIR%\Build.log 
+	/flp:Summary;Verbosity=minimal;LogFile=%BUILD_DIR%\Build.log
 	if errorlevel 1 (
-		echo Error during build, see %BUILD_DIR%\Build.log for details 
+		echo Error during build, see %BUILD_DIR%\Build.log for details
 		exit /b 1
 	)
 
@@ -17,7 +24,7 @@ msbuild ^
 	%BUILD_DIR%\INSTALL.vcxproj ^
 	/property:Configuration=%BUILD_TYPE% ^
 	/verbosity:minimal ^
-	/p:platform=%MSBUILD_PLATFORM% 
+	/p:platform=%MSBUILD_PLATFORM%
 	if errorlevel 1 (
 		echo Error during install phase
 		exit /b 1
