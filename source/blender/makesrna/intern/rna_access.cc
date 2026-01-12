Pagecran/blender

commit 43bba3fc5b51d4e1ff1cc48520318079a7a600e2

RNA: add callback to support dynamic property ui description
This PR adds a new `PropertyRNA::description_func` property similar to the
`PropertyRNA::name_func` added as part of PR #147137. This allows dynamically
getting a UI description for the property.

This is useful for displaying correct tooltips for node sockets which are added
as properties in UI panels. Without this, they use the hardcoded description
"Input value used for unconnected socket" which is not helpful. This patch shows
the correct description as specified in the node socket.

Pull Request: https://projects.blender.org/blender/blender/pulls/150403

source/blender/makesrna/intern/rna_access.cc
@@ -2235,13 +2235,23 @@ const char *RNA_property_ui_name_raw(const PropertyRNA *prop, const PointerRNA *
   return rna_ensure_property_name(prop);
 }
 
-const char *RNA_property_ui_description(const PropertyRNA *prop)
+const char *RNA_property_ui_description(const PropertyRNA *prop, const PointerRNA *ptr)
 {
-  return TIP_(rna_ensure_property_description(prop));
+  if (ptr && prop->magic == RNA_MAGIC && prop->ui_description_func) {
+    if (const char *description = prop->ui_description_func(ptr, prop, true)) {
+      return description;
+    }
+  }
+  return CTX_IFACE_(RNA_property_translation_context(prop), rna_ensure_property_description(prop));
 }
 
-const char *RNA_property_ui_description_raw(const PropertyRNA *prop)
+const char *RNA_property_ui_description_raw(const PropertyRNA *prop, const PointerRNA *ptr)
 {
+  if (ptr && prop->magic == RNA_MAGIC && prop->ui_description_func) {
+    if (const char *description = prop->ui_description_func(ptr, prop, false)) {
+      return description;
+    }
+  }
   return rna_ensure_property_description(prop);
 }
 

source/blender/editors/include/ED_node.hh
@@ -151,4 +151,6 @@ void node_tree_interface_draw(bContext &C, ui::Layout &layout, bNodeTree &tree);
 
 const char *node_socket_get_label(const bNodeSocket *socket, const char *panel_label = nullptr);
 
+const char *node_socket_get_description(const bNodeSocket *socket);
+
 }  // namespace blender::ed::space_node

source/blender/editors/interface/interface.cc
@@ -4888,7 +4888,7 @@ static uiBut *ui_def_but_rna(uiBlock *block,
   }
 
   if (!tip && proptype != PROP_ENUM) {
-    tip = RNA_property_ui_description(prop);
+    tip = RNA_property_ui_description(prop, ptr);
   }
 
   float step = -1.0f;
@@ -6826,7 +6826,7 @@ std::string UI_but_string_get_tooltip_label(const uiBut &but)
 std::string UI_but_string_get_rna_label(uiBut &but)
 {
   if (but.rnaprop) {
-    return RNA_property_ui_name(but.rnaprop);
+    return RNA_property_ui_name(but.rnaprop, &but.rnapoin);
   }
   if (but.optype) {
     PointerRNA *opptr = UI_but_operator_ptr_ensure(&but);
@@ -6878,7 +6878,7 @@ std::string UI_but_string_get_tooltip(bContext &C, uiBut &but)
 std::string UI_but_string_get_rna_tooltip(bContext &C, uiBut &but)
 {
   if (but.rnaprop) {
-    const char *t = RNA_property_ui_description(but.rnaprop);
+    const char *t = RNA_property_ui_description(but.rnaprop, &but.rnapoin);
     if (t && t[0]) {
       return t;
     }

source/blender/editors/space_node/node_draw.cc
@@ -479,6 +479,21 @@ const char *node_socket_get_label(const bNodeSocket *socket, const char *panel_l
   return translated_socket_label;
 }
 
+const char *node_socket_get_description(const bNodeSocket *socket)
+{
+  if (socket->runtime->declaration == nullptr) {
+    if (socket->description[0]) {
+      return socket->description;
+    }
+    return nullptr;
+  }
+  const nodes::SocketDeclaration &socket_decl = *socket->runtime->declaration;
+  if (!socket_decl.description.empty()) {
+    return socket_decl.description.c_str();
+  }
+  return nullptr;
+}
+
 static void draw_socket_layout(TreeDrawContext &tree_draw_ctx,
                                const bContext &C,
                                uiLayout &layout,

source/blender/makesrna/RNA_access.hh
@@ -284,8 +284,9 @@ bool RNA_property_string_is_utf8(PropertyRNA *prop);
 
 const char *RNA_property_ui_name(const PropertyRNA *prop, const PointerRNA *ptr = nullptr);
 const char *RNA_property_ui_name_raw(const PropertyRNA *prop, const PointerRNA *ptr = nullptr);
-const char *RNA_property_ui_description(const PropertyRNA *prop);
-const char *RNA_property_ui_description_raw(const PropertyRNA *prop);
+const char *RNA_property_ui_description(const PropertyRNA *prop, const PointerRNA *ptr = nullptr);
+const char *RNA_property_ui_description_raw(const PropertyRNA *prop,
+                                            const PointerRNA *ptr = nullptr);
 const char *RNA_property_translation_context(const PropertyRNA *prop);
 int RNA_property_ui_icon(const PropertyRNA *prop);
 

source/blender/makesrna/RNA_define.hh
@@ -494,6 +494,7 @@ void RNA_def_property_string_default(PropertyRNA *prop, const char *value);
 
 void RNA_def_property_ui_text(PropertyRNA *prop, const char *name, const char *description);
 void RNA_def_property_ui_name_func(PropertyRNA *prop, const char *name_func);
+void RNA_def_property_ui_description_func(PropertyRNA *prop, const char *description_func);
 
 void RNA_def_property_deprecated(PropertyRNA *prop,
                                  const char *note,

source/blender/makesrna/intern/makesrna.cc
@@ -4382,7 +4382,7 @@ static void rna_generate_property(FILE *f, StructRNA *srna, const char *nest, Pr
           prop->arraylength[2],
           prop->totarraylength);
   fprintf(f,
-          "\t%s%s, %d, %s, %s, %s, %s, %s, %s,\n",
+          "\t%s%s, %d, %s, %s, %s, %s, %s, %s, %s,\n",
           /* NOTE: void cast is needed to quiet function cast warning in C++. */
           (prop->flag & PROP_CONTEXT_UPDATE) ? "(UpdateFunc)(void *)" : "",
           rna_function_string(prop->update),
@@ -4390,6 +4390,7 @@ static void rna_generate_property(FILE *f, StructRNA *srna, const char *nest, Pr
           rna_function_string(prop->editable),
           rna_function_string(prop->itemeditable),
           rna_function_string(prop->ui_name_func),
+          rna_function_string(prop->ui_description_func),
           rna_function_string(prop->override_diff),
           rna_function_string(prop->override_store),
           rna_function_string(prop->override_apply));

source/blender/makesrna/intern/rna_define.cc
@@ -3236,6 +3236,18 @@ void RNA_def_property_ui_name_func(PropertyRNA *prop, const char *name_func)
   }
 }
 
+void RNA_def_property_ui_description_func(PropertyRNA *prop, const char *description_func)
+{
+  if (!DefRNA.preprocess) {
+    CLOG_ERROR(&LOG, "only during preprocessing.");
+    return;
+  }
+
+  if (description_func) {
+    prop->ui_description_func = (PropUINameFunc)description_func;
+  }
+}
+
 void RNA_def_property_update(PropertyRNA *prop, int noteflag, const char *func)
 {
   if (!DefRNA.preprocess) {

commit 97eec1de88f1e93bc6f44112c08ebe2fa228a2d3

Fix #152537: Tooltips are translated with wrong translation option
Caused by 43bba3fc5b51d4e1ff1cc48520318079a7a600e2 .
Wrong macro was being used for the description translation.

Pull Request: https://projects.blender.org/blender/blender/pulls/152557

source/blender/makesrna/intern/rna_access.cc
@@ -2232,7 +2232,7 @@ const char *RNA_property_ui_description(const PropertyRNA *prop, const PointerRN
       return description;
     }
   }
-  return CTX_IFACE_(RNA_property_translation_context(prop), rna_ensure_property_description(prop));
+  return CTX_TIP_(RNA_property_translation_context(prop), rna_ensure_property_description(prop));
 }
 
 const char *RNA_property_ui_description_raw(const PropertyRNA *prop, const PointerRNA *ptr)