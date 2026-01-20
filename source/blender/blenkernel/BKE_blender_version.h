569074bea9f4c124ceda11288f87b3127bdb6e40
Pascal André <pascalandr@gmail.com>
2026-01-20 12:58:25 +0100

    Add BLENDER_VERSION_BUILD for Windows executable versioning

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -23,6 +23,8 @@ namespace blender {
 #define BLENDER_VERSION 501
 /** Blender patch version for bug-fix releases. */
 #define BLENDER_VERSION_PATCH 0
+/** Blender build number for Pagecran Studio releases. */
+#define BLENDER_VERSION_BUILD 1
 /** Blender release cycle stage: alpha/beta/rc/release. */
 #define BLENDER_VERSION_CYCLE release
 /** Blender release type suffix. LTS or blank. */
