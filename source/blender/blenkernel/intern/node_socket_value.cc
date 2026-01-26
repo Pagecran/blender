Pagecran/blender

commit e73e3d68336e922dbac77a491f99e302cedbeb2c

Geometry Nodes: initial support for counting memory usage of socket value variant
This is necessary to be able to properly use `SocketValueVariant` in a cache
that has a memory-bound. Note that this does not attempt to count exact memory
usage (which is generally not really possible), but it at least tries to cover
the large arrays.

Pull Request: https://projects.blender.org/blender/blender/pulls/153413

source/blender/blenkernel/intern/node_socket_value.cc
@@ -21,6 +21,7 @@
 #include "BLI_color.hh"
 #include "BLI_math_rotation_types.hh"
 #include "BLI_math_vector_types.hh"
+#include "BLI_memory_counter.hh"
 
 #include "FN_field.hh"
 
@@ -680,6 +681,46 @@ bool SocketValueVariant::valid_for_socket(eNodeSocketDatatype socket_type) const
     return false;
   }
   return socket_type_ == socket_type;
+}
+
+void SocketValueVariant::count_memory(MemoryCounter &memory) const
+{
+  switch (kind_) {
+    case Kind::None: {
+      break;
+    }
+    case Kind::Single: {
+      const GPointer value = this->get_single_ptr();
+      const CPPType &cpp_type = *value.type();
+      memory.add(cpp_type.size);
+      if (cpp_type.is<GeometrySet>()) {
+        const GeometrySet &geometry = *value.get<GeometrySet>();
+        geometry.count_memory(memory);
+      }
+      if (cpp_type.is<nodes::BundlePtr>()) {
+        const nodes::BundlePtr &bundle_ptr = *value.get<nodes::BundlePtr>();
+        if (bundle_ptr) {
+          bundle_ptr->count_memory(memory);
+        }
+      }
+      break;
+    }
+    case Kind::Field: {
+      break;
+    }
+    case Kind::Grid: {
+      if (const GVolumeGrid &grid = value_.get<GVolumeGrid>()) {
+        grid->count_memory(memory);
+      }
+      break;
+    }
+    case Kind::List: {
+      if (const nodes::ListPtr &list = value_.get<nodes::ListPtr>()) {
+        list->count_memory(memory);
+      }
+      break;
+    }
+  }
 }
 
 #define INSTANTIATE(TYPE) \

source/blender/blenkernel/BKE_node_socket_value.hh
@@ -12,6 +12,7 @@
 
 #include "BLI_any.hh"
 #include "BLI_generic_pointer.hh"
+#include "BLI_memory_counter_fwd.hh"
 
 #include "BKE_node_socket_value_fwd.hh"
 
@@ -199,6 +200,8 @@ class SocketValueVariant {
    * caller is responsible to construct the value in the returned memory before it is used.
    */
   void *allocate_single(eNodeSocketDatatype socket_type);
+
+  void count_memory(MemoryCounter &memory) const;
 
   friend std::ostream &operator<<(std::ostream &stream, const SocketValueVariant &value_variant);
 

source/blender/nodes/NOD_geometry_nodes_bundle.hh
@@ -5,8 +5,10 @@
 #pragma once
 
 #include "BKE_node.hh"
-
 #include "BKE_node_socket_value.hh"
+
+#include "BLI_memory_counter_fwd.hh"
+
 #include "NOD_geometry_nodes_bundle_fwd.hh"
 #include "NOD_geometry_nodes_values.hh"
 
@@ -106,6 +108,8 @@ class Bundle : public ImplicitSharingMixin {
   BundlePtr copy() const;
 
   void delete_self() override;
+
+  void count_memory(MemoryCounter &memory) const;
 
   /** Create the combined path by inserting '/' between each element. */
   static std::string combine_path(const Span<StringRef> path);

source/blender/nodes/NOD_geometry_nodes_list.hh
@@ -8,6 +8,7 @@
 
 #include "BLI_generic_pointer.hh"
 #include "BLI_generic_virtual_array.hh"
+#include "BLI_memory_counter_fwd.hh"
 
 #include "NOD_geometry_nodes_list_fwd.hh"
 
@@ -23,6 +24,8 @@ class List : public ImplicitSharingMixin {
     static ArrayData ForDefaultValue(const CPPType &type, int64_t size);
     static ArrayData ForConstructed(const CPPType &type, int64_t size);
     static ArrayData ForUninitialized(const CPPType &type, int64_t size);
+
+    void count_memory(MemoryCounter &memory, const CPPType &type, const int64_t size) const;
   };
 
   class SingleData {
@@ -31,6 +34,8 @@ class List : public ImplicitSharingMixin {
     ImplicitSharingPtr<> sharing_info;
     static SingleData ForValue(const GPointer &value);
     static SingleData ForDefaultValue(const CPPType &type);
+
+    void count_memory(MemoryCounter &memory, const CPPType &type) const;
   };
 
   using DataVariant = std::variant<ArrayData, SingleData>;
@@ -60,6 +65,8 @@ class List : public ImplicitSharingMixin {
   /** Access the list as virtual array. */
   GVArray varray() const;
   template<typename T> VArray<T> varray() const;
+
+  void count_memory(MemoryCounter &memory) const;
 };
 
 inline const List::DataVariant &List::data() const

source/blender/nodes/intern/geometry_nodes_bundle.cc
@@ -275,6 +275,15 @@ void Bundle::delete_self()
   MEM_delete(this);
 }
 
+void Bundle::count_memory(MemoryCounter &memory) const
+{
+  for (const auto &item : items_.items()) {
+    if (const auto *socket_value = std::get_if<BundleItemSocketValue>(&item.value.value)) {
+      socket_value->value.count_memory(memory);
+    }
+  }
+}
+
 NodeSocketInterfaceStructureType get_structure_type_for_bundle_signature(
     const bNodeSocket &socket,
     const NodeSocketInterfaceStructureType stored_structure_type,

source/blender/nodes/intern/geometry_nodes_list.cc
@@ -2,6 +2,8 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "BLI_memory_counter.hh"
+
 #include "NOD_geometry_nodes_list.hh"
 
 namespace blender::nodes {
@@ -135,6 +137,30 @@ GVArray List::varray() const
   }
   BLI_assert_unreachable();
   return {};
+}
+
+void List::count_memory(MemoryCounter &memory) const
+{
+  if (const auto *array_data = std::get_if<ArrayData>(&data_)) {
+    array_data->count_memory(memory, cpp_type_, size_);
+    return;
+  }
+  if (const auto *single_data = std::get_if<SingleData>(&data_)) {
+    single_data->count_memory(memory, cpp_type_);
+    return;
+  }
+}
+
+void List::ArrayData::count_memory(MemoryCounter &memory,
+                                   const CPPType &type,
+                                   const int64_t size) const
+{
+  memory.add_shared(this->sharing_info.get(), type.size * size);
+}
+
+void List::SingleData::count_memory(MemoryCounter &memory, const CPPType &type) const
+{
+  memory.add(type.size);
 }
 
 }  // namespace blender::nodes

commit 35d4d2b59aadbc7636d41ad43525e5f4eaf9d617

Fix: Build error in lite build after recent commit
Caused by e73e3d68336e922dbac77a491f99e302cedbeb2c

source/blender/blenkernel/intern/node_socket_value.cc
@@ -709,9 +709,11 @@ void SocketValueVariant::count_memory(MemoryCounter &memory) const
       break;
     }
     case Kind::Grid: {
+#ifdef WITH_OPENVDB
       if (const GVolumeGrid &grid = value_.get<GVolumeGrid>()) {
         grid->count_memory(memory);
       }
+#endif
       break;
     }
     case Kind::List: {