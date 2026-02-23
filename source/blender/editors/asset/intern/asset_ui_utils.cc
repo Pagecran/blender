Pagecran/blender

commit ab2aa2512f0783651556e8c06d85e22f9c3c17bd

Assets: Option to disable asset libraries in the Preferences
Adds a checkbox to each asset library in the Preferences to control if it is
enabled or not. A disabled asset library will not be available in asset library
selectors, and its assets will not show up in any asset listings.

This has been a long requested feature, since not all assets and asset
libraries are relevant in every context/project, and irrelevant ones can get in
the way. It's also important for online asset libraries support, so users can
control which asset libraries they want to keep in sync with the online source,
see #134495.

Pull Request: https://projects.blender.org/blender/blender/pulls/138150

source/blender/editors/asset/intern/asset_ui_utils.cc
@@ -14,6 +14,7 @@
 #include "BKE_preferences.h"
 #include "BKE_preview_image.hh"
 
+#include "BLI_listbase.h"
 #include "BLI_path_utils.hh"
 
 #include "BLT_translation.hh"
@@ -110,6 +111,31 @@ AssetLibraryReference get_asset_library_ref_from_opptr(PointerRNA &ptr)
 {
   const int enum_value = RNA_enum_get(&ptr, "asset_library_reference");
   return asset::library_reference_from_enum_value(enum_value);
+}
+
+std::optional<AssetLibraryReference> get_user_library_ref_for_save(
+    const asset_system::AssetLibrary *preferred_library)
+{
+  std::optional<AssetLibraryReference> preferred_library_ref =
+      preferred_library ? preferred_library->library_reference() : std::nullopt;
+  BLI_assert(bool(preferred_library_ref));
+
+  if (preferred_library_ref &&
+      !ELEM(preferred_library_ref->type, ASSET_LIBRARY_ALL, ASSET_LIBRARY_ESSENTIALS))
+  {
+    return preferred_library_ref;
+  }
+
+  /* Fallback to the first enabled user library. */
+  for (const bUserAssetLibrary &asset_library : U.asset_libraries) {
+    if (asset_library.flag & ASSET_LIBRARY_DISABLED) {
+      continue;
+    }
+    return asset::user_library_to_library_ref(asset_library);
+  }
+
+  /* No enabled user asset library found. */
+  return {};
 }
 
 void visit_library_catalogs_catalog_for_search(

scripts/startup/bl_ui/space_userpref.py
@@ -1792,7 +1792,10 @@ class USERPREF_PT_file_paths_asset_libraries(FilePathsPanel, Panel):
 class USERPREF_UL_asset_libraries(UIList):
     def draw_item(self, _context, layout, _data, item, _icon, _active_data, _active_propname, _index):
         asset_library = item
-        layout.prop(asset_library, "name", text="", emboss=False)
+
+        row = layout.row(align=True)
+        row.prop(asset_library, "enabled", text="")
+        row.prop(asset_library, "name", text="", emboss=False)
 
 
 class USERPREF_UL_extension_repos(UIList):

source/blender/asset_system/AS_asset_library.hh
@@ -118,8 +118,8 @@ class AssetLibrary {
   virtual ~AssetLibrary();
 
   /**
-   * Execute \a fn for every asset library that is loaded. The asset library is passed to the
-   * \a fn call.
+   * Execute \a fn for every asset library that is loaded and enabled. The asset library is passed
+   * to the \a fn call.
    *
    * \param include_all_library: When true, \a fn will also be executed for the "All" asset
    *   library. This is just a combination of the other ones, so usually iterating over it is
@@ -200,6 +200,7 @@ class AssetLibrary {
   virtual void refresh_catalogs();
 };
 
+/** Get all asset library references which are enabled and for which the directory exists. */
 Vector<AssetLibraryReference> all_valid_asset_library_refs();
 
 AssetLibraryReference all_library_reference();

source/blender/asset_system/intern/asset_library.cc
@@ -442,6 +442,9 @@ Vector<AssetLibraryReference> all_valid_asset_library_refs()
   }
 
   for (const auto [i, asset_library] : U.asset_libraries.enumerate()) {
+    if (asset_library.flag & ASSET_LIBRARY_DISABLED) {
+      continue;
+    }
     if (!BLI_is_dir(asset_library.dirpath)) {
       continue;
     }

source/blender/asset_system/intern/asset_library_service.cc
@@ -612,7 +612,9 @@ void AssetLibraryService::foreach_loaded_asset_library(FunctionRef<void(AssetLib
   }
 
   for (const auto &asset_lib_uptr : on_disk_libraries_.values()) {
-    fn(*asset_lib_uptr);
+    if (asset_lib_uptr->is_enabled()) {
+      fn(*asset_lib_uptr);
+    }
   }
 }
 

source/blender/asset_system/intern/library_types/on_disk_library.cc
@@ -40,4 +40,9 @@ void OnDiskAssetLibrary::refresh_catalogs()
   this->catalog_service().reload_catalogs();
 }
 
+bool OnDiskAssetLibrary::is_enabled() const
+{
+  return true;
+}
+
 }  // namespace blender::asset_system

source/blender/asset_system/intern/library_types/on_disk_library.hh
@@ -22,6 +22,8 @@ class OnDiskAssetLibrary : public AssetLibrary {
 
   std::optional<AssetLibraryReference> library_reference() const override;
   void refresh_catalogs() override;
+
+  virtual bool is_enabled() const;
 };
 
 }  // namespace blender::asset_system

source/blender/asset_system/intern/library_types/preferences_on_disk_library.cc
@@ -40,4 +40,19 @@ std::optional<AssetLibraryReference> PreferencesOnDiskAssetLibrary::library_refe
   return {};
 }
 
+bool PreferencesOnDiskAssetLibrary::is_enabled() const
+{
+  for (const bUserAssetLibrary &asset_library : U.asset_libraries) {
+    if (!BLI_is_dir(asset_library.dirpath)) {
+      continue;
+    }
+
+    if (BLI_path_cmp_normalized(asset_library.dirpath, this->root_path().c_str()) == 0) {
+      return (asset_library.flag & ASSET_LIBRARY_DISABLED) == 0;
+    }
+  }
+
+  return false;
+}
+
 }  // namespace blender::asset_system

commit ac5063554aa382215196dfc7020048ee856242c1

Fix: Incorrect assert in asset utility function
`preferred_library` is allowed to be null (it is by default), in which
case the assert would fail incorrectly.

Found by Brecht's LLM review, see:
https://projects.blender.org/brecht/paste/src/branch/main/llm_review/log_2026_w04.md#assets-option-to-disable-asset-libraries-in-the-preferences

Issue introduced in ab2aa2512f.

source/blender/editors/asset/intern/asset_ui_utils.cc
@@ -118,7 +118,7 @@ std::optional<AssetLibraryReference> get_user_library_ref_for_save(
 {
   std::optional<AssetLibraryReference> preferred_library_ref =
       preferred_library ? preferred_library->library_reference() : std::nullopt;
-  BLI_assert(bool(preferred_library_ref));
+  BLI_assert(!preferred_library || bool(preferred_library_ref));
 
   if (preferred_library_ref &&
       !ELEM(preferred_library_ref->type, ASSET_LIBRARY_ALL, ASSET_LIBRARY_ESSENTIALS))