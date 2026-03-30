Pagecran/blender

commit 927cfc6d172f49191cd492f091a91fa17ff93b19

Fix: EEVEE: Camera::sync
We have faced several issues and regressions related to the handling of
the projection matrix and render region in EEVEE.

There were several issues at play here:
1. Viewports with invalid parameters, like 0 FoV or ortho scale,
   generating projection matrices with Infs or NaNs.
2. Viewports where the projection matrix has not been generated from
   CameraParams (XR), and therefore can't use the BKE_camera functions
   to recompute the projection matrix.
3. Wrong detection of these cases, and misunderstanding of the
   workarounds.

This started with #143946, where, to work around the first issue, we
disabled projection matrix re-computation in EEVEE for these.
However, this introduced the third issue, since it was applying the
same workaround to viewport navigation (where it's not needed) and XR,
without taking render-region into account.
Also, the projection matrix that EEVEE receives was degenerate anyway,
so it didn't prevent other issues down the line.

So, after this one, a series of half-fixes and regressions due to a
misunderstanding of the issue has followed:
#144724, #151923, #153039, #148422, and #154095 (report).

What this PR does is:
- Sanitize CameraParams, so no degenerate matrices are created at all.
  This allows us to always follow the regular render path outside XR.
- Disable overscan and projection matrix-recomputation for XR by
  checking directly for the DRWContext::Mode, so we don't apply the
  workaround in any other case.

If other issues arise, we should fix them at that level. If there are
cases where we can't somehow prevent receiving wrong matrices, we
should detect them at draw_context.cc and fully skip the render,
since we can't render anything meaningful anyway.

Fix #154095.

Pull Request: https://projects.blender.org/blender/blender/pulls/154214

source/blender/draw/engines/eevee/eevee_camera.cc
@@ -76,8 +76,8 @@ void Camera::init()
   float overscan = 0.0f;
   if ((inst_.scene->eevee.flag & SCE_EEVEE_OVERSCAN) && (inst_.drw_view || inst_.render)) {
     overscan = inst_.scene->eevee.overscan / 100.0f;
-    if (inst_.drw_view && (inst_.rv3d->dist == 0.0f || v3d_camera_params_get().lens == 0.0f)) {
-      /* In these cases we need to use the v3d winmat as-is. */
+    if (inst_.is_xr()) {
+      /* In XR we need to use the v3d winmat as-is. */
       overscan = 0.0f;
     }
   }
@@ -137,9 +137,14 @@ void Camera::sync()
     data.viewmat = inst_.drw_view->viewmat();
     data.viewinv = inst_.drw_view->viewinv();
 
-    CameraParams params = v3d_camera_params_get();
+    if (inst_.is_xr()) {
+      /* In XR we need to use the v3d winmat as-is. */
+      data.winmat = inst_.drw_view->winmat();
+      data.wininv = inst_.drw_view->wininv();
+    }
+    else {
+      CameraParams params = v3d_camera_params_get();
 
-    if (inst_.rv3d->dist > 0.0f && params.lens > 0.0f) {
       BKE_camera_params_compute_viewplane(&params, UNPACK2(display_extent), 1.0f, 1.0f);
 
       BLI_assert(BLI_rctf_size_x(&params.viewplane) > 0.0f);
@@ -153,21 +158,6 @@ void Camera::sync()
                                      params.viewplane,
                                      overscan_,
                                      data.winmat.ptr());
-    }
-    else {
-      /* Can happen for the case of XR or if `rv3d->dist == 0`.
-       * In this case the produced winmat is degenerate. So just revert to the input matrix. */
-      data.winmat = inst_.drw_view->winmat();
-      if (!camera_eval) {
-        /* Apply the render region, but only for non-camera views. See #153033. */
-        /* FIXME(@pragma37): This is still broken with Camera View + Render Region + Fly/Walk
-         * Navigation. Untangle this whole walk/fly navigation projection matrix mess. */
-        float2 film_center = float2(film_offset) + float2(film_extent) / 2.0f;
-        float2 uv_offset = float2(0.5f) - (film_center / float2(display_extent));
-        data.winmat = math::projection::translate(data.winmat, uv_offset * 2.0f);
-        data.winmat = math::from_scale<float4x4>(float4(1.0f / data.uv_scale, 1.0f, 1.0f)) *
-                      data.winmat;
-      }
     }
   }
   else if (inst_.render) {
@@ -192,14 +182,6 @@ void Camera::sync()
     data.viewmat = float4x4::identity();
     data.viewinv = float4x4::identity();
     data.winmat = math::projection::perspective(-0.1f, 0.1f, -0.1f, 0.1f, 0.1f, 1.0f);
-  }
-
-  /* Compute a part of the frustum planes. In some cases (#134320, #148258)
-   * the window matrix becomes degenerate during render or draw_view.
-   * Simply fall back to something we can render with. */
-  float bottom = (-data.winmat[3][1] - 1.0f) / data.winmat[1][1];
-  if (std::isnan(bottom) || std::isinf(std::abs(bottom))) {
-    data.winmat = math::projection::orthographic(0.01f, 0.01f, 0.01f, 0.01f, -1000.0f, +1000.0f);
   }
 
   data.wininv = math::invert(data.winmat);

source/blender/draw/engines/eevee/eevee_instance.hh
@@ -312,6 +312,11 @@ class Instance : public DrawEngine {
     return is_light_bake;
   }
 
+  bool is_xr() const
+  {
+    return draw_ctx && draw_ctx->mode == DRWContext::VIEWPORT_XR;
+  }
+
   bool overlays_enabled() const
   {
     return overlays_enabled_;

source/blender/blenkernel/intern/camera.cc
@@ -403,6 +403,11 @@ void BKE_camera_params_from_object(CameraParams *params, const Object *cam_ob)
   else {
     params->lens = 35.0f;
   }
+
+  /* Ensure it's possible to compute a valid projection matrix. */
+  params->lens = math::max(params->lens, 1e-9f);
+  params->ortho_scale = math::max(params->ortho_scale, 1e-9f);
+  params->clip_end = math::max(params->clip_end, params->clip_start + 1e-3f);
 }
 
 void BKE_camera_params_from_view3d(CameraParams *params,
@@ -447,6 +452,11 @@ void BKE_camera_params_from_view3d(CameraParams *params,
     /* perspective view */
     params->zoom = CAMERA_PARAM_ZOOM_INIT_PERSP;
   }
+
+  /* Ensure it's possible to compute a valid projection matrix. */
+  params->lens = math::max(params->lens, 1e-9f);
+  params->ortho_scale = math::max(params->ortho_scale, 1e-9f);
+  params->clip_end = math::max(params->clip_end, params->clip_start + 1e-3f);
 }
 
 void BKE_camera_params_compute_viewplane(

commit 56352b520ccf27f93a3b1362574e69381375ff24

Fix #156370: Broken render in Offscreen.draw_view3d()
Regression from 927cfc6d172f49191cd492f091a91fa17ff93b19.
(See that commit message for insight into previous related issues)

Some offscreen render paths have the same issue as XR of relying on a
custom projection matrix.
`ED_view3d_draw_offscreen`"flagged" the v3d as such, by setting the
lens to 0, but we no longer check that.

This replaces that workaround with a dedicated `V3D_CUSTOM_MATRIX` flag
and updates EEVEE to check it.

Pull Request: https://projects.blender.org/blender/blender/pulls/156389

source/blender/draw/engines/eevee/eevee_camera.cc
@@ -76,8 +76,9 @@ void Camera::init()
   float overscan = 0.0f;
   if ((inst_.scene->eevee.flag & SCE_EEVEE_OVERSCAN) && (inst_.drw_view || inst_.render)) {
     overscan = inst_.scene->eevee.overscan / 100.0f;
-    if (inst_.is_xr()) {
-      /* In XR we need to use the v3d winmat as-is. */
+    if (inst_.is_custom_matrix()) {
+      /* If using a custom matrix (XR and some offscreen render paths)
+       * we need to use the v3d winmat as-is. */
       overscan = 0.0f;
     }
   }
@@ -137,8 +138,9 @@ void Camera::sync()
     data.viewmat = inst_.drw_view->viewmat();
     data.viewinv = inst_.drw_view->viewinv();
 
-    if (inst_.is_xr()) {
-      /* In XR we need to use the v3d winmat as-is. */
+    if (inst_.is_custom_matrix()) {
+      /* If using a custom matrix (XR and some offscreen render paths)
+       * we need to use the v3d winmat as-is. */
       data.winmat = inst_.drw_view->winmat();
       data.wininv = inst_.drw_view->wininv();
     }

source/blender/draw/engines/eevee/eevee_instance.hh
@@ -24,6 +24,7 @@
 
 #include "DNA_lightprobe_types.h"
 
+#include "DNA_view3d_types.h"
 #include "DRW_render.hh"
 
 #include "eevee_ambient_occlusion.hh"
@@ -314,9 +315,10 @@ class Instance : public DrawEngine {
     return is_light_bake;
   }
 
-  bool is_xr() const
+  bool is_custom_matrix() const
   {
-    return draw_ctx && draw_ctx->mode == DRWContext::VIEWPORT_XR;
+    return (v3d && v3d->flag & V3D_CUSTOM_MATRIX) ||
+           (draw_ctx && draw_ctx->mode == DRWContext::VIEWPORT_XR);
   }
 
   bool overlays_enabled() const

source/blender/editors/space_view3d/view3d_draw.cc
@@ -1871,7 +1871,7 @@ void ED_view3d_draw_offscreen(Depsgraph *depsgraph,
     /* #View3D */
     eDrawType v3d_shading_type;
     Object *v3d_camera;
-    float v3d_lens;
+    short v3d_flag;
 
     /* #Region */
     int region_winx, region_winy;
@@ -1887,7 +1887,7 @@ void ED_view3d_draw_offscreen(Depsgraph *depsgraph,
   } orig{};
   orig.v3d_shading_type = eDrawType(v3d->shading.type);
   orig.v3d_camera = v3d->camera;
-  orig.v3d_lens = v3d->lens;
+  orig.v3d_flag = v3d->flag;
   orig.region_winx = region->winx;
   orig.region_winy = region->winy;
   orig.region_winrct = region->winrct;
@@ -1918,16 +1918,6 @@ void ED_view3d_draw_offscreen(Depsgraph *depsgraph,
     BKE_image_free_anim_gputextures(G.main);
   }
 
-  if (viewmat) {
-    /* WORKAROUND: Disable camera view to avoid EEVEE being confused and try to
-     * get the projection matrix from the camera.
-     * Set the `lens` parameter to 0 to make EEVEE prefer the `winmat` from the rv3d instead of
-     * trying to rederive it. Note that this produces incorrect result with over-scan. */
-    rv3d->persp = (winmat[3][3] == 0.0f) ? RV3D_PERSP : RV3D_ORTHO;
-    v3d->camera = nullptr;
-    v3d->lens = 0.0f;
-  }
-
   GPU_matrix_push_projection();
   GPU_matrix_identity_set();
   GPU_matrix_push();
@@ -1940,6 +1930,15 @@ void ED_view3d_draw_offscreen(Depsgraph *depsgraph,
   }
   else {
     view3d_main_region_setup_offscreen(depsgraph, scene, v3d, region, viewmat, winmat);
+  }
+
+  if (viewmat || winmat) {
+    /* Now that rv3d data has been updated taking into account viewmat and winmat,
+     * we can remove the camera and flag the view as using custom matrices,
+     * to ensure engines don't recompute them. */
+    v3d->camera = nullptr;
+    v3d->flag |= V3D_CUSTOM_MATRIX;
+    rv3d->persp = (winmat[3][3] == 0.0f) ? RV3D_PERSP : RV3D_ORTHO;
   }
 
   if (viewport) {
@@ -1978,7 +1977,7 @@ void ED_view3d_draw_offscreen(Depsgraph *depsgraph,
 
   v3d->shading.type = orig.v3d_shading_type;
   v3d->camera = orig.v3d_camera;
-  v3d->lens = orig.v3d_lens;
+  v3d->flag = orig.v3d_flag;
 
   G.f &= ~G_FLAG_RENDER_VIEWPORT;
 }

source/blender/makesdna/DNA_view3d_types.h
@@ -54,6 +54,7 @@ enum {
   V3D_FLAG_UNUSED_2 = 1 << 3, /* cleared */
   V3D_XR_SESSION_MIRROR = 1 << 4,
   V3D_XR_SESSION_SURFACE = 1 << 5,
+  V3D_CUSTOM_MATRIX = 1 << 6,
 
   V3D_FLAG_UNUSED_10 = 1 << 10, /* cleared */
   V3D_SELECT_OUTLINE = 1 << 11,