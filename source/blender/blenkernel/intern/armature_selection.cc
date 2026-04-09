Pagecran/blender

commit a09d0cfd8ccecb57248c6ed96ba25900eab82e0f

Anim: Move pose bone selection state to bPoseChannel
Move the selection flag for pose bones, from the (edit)bone to the
pose bone.

Previously having multiple instances of armatures in pose mode at the
same time caused issues because selecting a pose bone on one armature
would automatically select it on all instances of it.

This is now fixed since the selection state is stored on the pose bone
(Object level) Doing so breaks API compatibility with 4.5 since the
RNA property on the Bone no longer affects the pose bone. Instead,
there is a new property on the pose bone for that.

Due to this change, some runtime flags for the transform system also
had to be moved to the pose bone. This is due to the fact that these
flags are used by the transform system to pass information between
functions. If we keep the flag at the bone level, this wouldn't work
with armature instances. See `bPoseChannelRuntimeFlag`

Fixes #117892

Pull Request: https://projects.blender.org/blender/blender/pulls/146102

source/blender/blenkernel/intern/armature_selection.cc
@@ -13,6 +13,7 @@
 #include "BLI_listbase.h"
 
 #include "DNA_armature_types.h"
+#include "DNA_object_types.h"
 
 namespace blender::bke {
 
@@ -35,6 +36,7 @@ void find_selected_bones__visit_bone(const bArmature *armature,
     find_selected_bones__visit_bone(armature, callback, result, child_bone);
   }
 }
+
 }  // namespace
 
 SelectedBonesResult BKE_armature_find_selected_bones(const bArmature *armature,
@@ -55,6 +57,21 @@ BoneNameSet BKE_armature_find_selected_bone_names(const bArmature *armature)
   /* Iterate over the selected bones to fill the set of bone names. */
   auto callback = [&](Bone *bone) { selected_bone_names.add(bone->name); };
   BKE_armature_find_selected_bones(armature, callback);
+  return selected_bone_names;
+}
+
+BoneNameSet BKE_pose_channel_find_selected_names(const Object *object)
+{
+  if (!object->pose) {
+    return {};
+  }
+
+  BoneNameSet selected_bone_names;
+  LISTBASE_FOREACH (bPoseChannel *, pose_bone, &object->pose->chanbase) {
+    if (pose_bone->flag & POSE_SELECTED) {
+      selected_bone_names.add(pose_bone->name);
+    }
+  }
   return selected_bone_names;
 }
 

source/blender/animrig/ANIM_armature.hh
@@ -48,7 +48,7 @@ inline bool bone_is_selected(const bArmature *armature, const Bone *bone)
 
 inline bool bone_is_selected(const bArmature *armature, const bPoseChannel *pchan)
 {
-  return (pchan->bone->flag & BONE_SELECTED) && bone_is_visible(armature, pchan);
+  return (pchan->flag & POSE_SELECTED) && bone_is_visible(armature, pchan);
 }
 
 inline bool bone_is_selected(const bArmature *armature, const EditBone *ebone)
@@ -74,4 +74,12 @@ void pose_bone_descendent_iterator(bPose &pose,
                                    bPoseChannel &pose_bone,
                                    FunctionRef<void(bPoseChannel &child_bone)> callback);
 
+/**
+ * Iterates all descendents of the given pose bone depth first. The traversal for a branch is
+ * stopped if the callback returns false. Returns true if the iteration completed or false if it
+ * was stopped before visiting all bones.
+ */
+bool pose_bone_descendent_depth_iterator(bPose &pose,
+                                         bPoseChannel &pose_bone,
+                                         FunctionRef<bool(bPoseChannel &child_bone)> callback);
 }  // namespace blender::animrig

source/blender/animrig/intern/armature.cc
@@ -40,4 +40,37 @@ void pose_bone_descendent_iterator(bPose &pose,
   }
 };
 
+static bool pose_depth_iterator_recursive(bPose &pose,
+                                          bPoseChannel &pose_bone,
+                                          FunctionRef<bool(bPoseChannel &child_bone)> callback)
+{
+  if (!callback(pose_bone)) {
+    return false;
+  }
+  if (!pose_bone.bone) {
+    BLI_assert_unreachable();
+    return false;
+  }
+  bool success = true;
+  LISTBASE_FOREACH (Bone *, child_bone, &pose_bone.bone->childbase) {
+    bPoseChannel *child_pose_bone = BKE_pose_channel_find_name(&pose, child_bone->name);
+    if (!child_pose_bone) {
+      BLI_assert_unreachable();
+      success = false;
+      continue;
+    }
+    success &= pose_depth_iterator_recursive(pose, *child_pose_bone, callback);
+  }
+  return success;
+}
+
+bool pose_bone_descendent_depth_iterator(bPose &pose,
+                                         bPoseChannel &pose_bone,
+                                         FunctionRef<bool(bPoseChannel &child_bone)> callback)
+{
+  /* Needed for fast name lookups. */
+  BKE_pose_channels_hash_ensure(&pose);
+  return pose_depth_iterator_recursive(pose, pose_bone, callback);
+}
+
 }  // namespace blender::animrig

source/blender/animrig/intern/pose.cc
@@ -67,9 +67,8 @@ void pose_apply(Object *ob,
     return;
   }
 
-  const bArmature *armature = static_cast<bArmature *>(ob->data);
   const blender::bke::BoneNameSet selected_bone_names =
-      blender::bke::BKE_armature_find_selected_bone_names(armature);
+      blender::bke::BKE_pose_channel_find_selected_names(ob);
 
   /* Mute all FCurves that are not associated with selected bones. This separates the concept of
    * bone selection from the FCurve evaluation code. */
@@ -128,7 +127,7 @@ bool any_bone_selected(const blender::Span<const Object *> objects)
       continue;
     }
     LISTBASE_FOREACH (bPoseChannel *, pose_bone, &obj->pose->chanbase) {
-      if (pose_bone->bone->flag & BONE_SELECTED) {
+      if (pose_bone->flag & POSE_SELECTED) {
         return true;
       }
     }

source/blender/animrig/intern/pose_test.cc
@@ -229,8 +229,8 @@ TEST_F(PoseTest, apply_action_blend_single_slot)
   bone_a->loc[0] = 0.0;
   bone_b->loc[1] = 0.0;
 
-  bone_a->bone->flag |= BONE_SELECTED;
-  bone_b->bone->flag &= ~BONE_SELECTED;
+  bone_a->flag |= POSE_SELECTED;
+  bone_b->flag &= ~POSE_SELECTED;
 
   /* This should only affect the selected bone. */
   blender::animrig::pose_apply_action_blend(
@@ -265,7 +265,7 @@ TEST_F(PoseTest, apply_action_multiple_objects)
       arm_a_bone_a, arm_a_bone_b, arm_b_bone_a, arm_b_bone_b};
 
   for (bPoseChannel *pose_bone : all_bones) {
-    pose_bone->bone->flag &= ~BONE_SELECTED;
+    pose_bone->flag &= ~POSE_SELECTED;
     pose_bone->loc[0] = 0.0;
     pose_bone->loc[1] = 0.0;
   }
@@ -285,7 +285,7 @@ TEST_F(PoseTest, apply_action_multiple_objects)
     pose_bone->loc[1] = 0.0;
   }
 
-  arm_a_bone_a->bone->flag |= BONE_SELECTED;
+  arm_a_bone_a->flag |= POSE_SELECTED;
 
   blender::animrig::pose_apply_action(
       {obj_armature_a, obj_armature_b}, *pose_action, &eval_context, 1.0);
@@ -301,8 +301,8 @@ TEST_F(PoseTest, apply_action_multiple_objects)
     pose_bone->loc[1] = 0.0;
   }
 
-  arm_a_bone_a->bone->flag |= BONE_SELECTED;
-  arm_b_bone_a->bone->flag |= BONE_SELECTED;
+  arm_a_bone_a->flag |= POSE_SELECTED;
+  arm_b_bone_a->flag |= POSE_SELECTED;
 
   blender::animrig::pose_apply_action(
       {obj_armature_a, obj_armature_b}, *pose_action, &eval_context, 1.0);
@@ -332,9 +332,9 @@ TEST_F(PoseTest, apply_action_multiple_objects)
     pose_bone->loc[1] = 0.0;
   }
 
-  arm_a_bone_a->bone->flag |= BONE_SELECTED;
-  arm_a_bone_b->bone->flag |= BONE_SELECTED;
-  arm_b_bone_a->bone->flag |= BONE_SELECTED;
+  arm_a_bone_a->flag |= POSE_SELECTED;
+  arm_a_bone_b->flag |= POSE_SELECTED;
+  arm_b_bone_a->flag |= POSE_SELECTED;
 
   blender::animrig::pose_apply_action(
       {obj_armature_a, obj_armature_b}, *pose_action, &eval_context, 1.0);
@@ -364,7 +364,7 @@ TEST_F(PoseTest, apply_action_multiple_objects_single_slot)
       arm_a_bone_a, arm_a_bone_b, arm_b_bone_a, arm_b_bone_b};
 
   for (bPoseChannel *pose_bone : all_bones) {
-    pose_bone->bone->flag &= ~BONE_SELECTED;
+    pose_bone->flag &= ~POSE_SELECTED;
     pose_bone->loc[0] = 0.0;
     pose_bone->loc[1] = 0.0;
   }

source/blender/blenkernel/BKE_action.hh
@@ -371,7 +371,7 @@ void BKE_pose_rest(bPose *pose, bool selected_bones_only);
  */
 void BKE_pose_tag_recalc(Main *bmain, bPose *pose) ATTR_NONNULL(1, 2);
 
-void BKE_pose_blend_write(BlendWriter *writer, bPose *pose, bArmature *arm) ATTR_NONNULL(1, 2, 3);
+void BKE_pose_blend_write(BlendWriter *writer, bPose *pose) ATTR_NONNULL(1, 2);
 void BKE_pose_blend_read_data(BlendDataReader *reader, ID *id_owner, bPose *pose)
     ATTR_NONNULL(1, 2);
 void BKE_pose_blend_read_after_liblink(BlendLibReader *reader, Object *ob, bPose *pose)

source/blender/blenkernel/BKE_armature.hh
@@ -574,7 +574,7 @@ void BKE_pchan_bbone_deform_segment_index(const bPoseChannel *pchan,
        _pchan = _pchan->next) \
   { \
     if (blender::animrig::bone_is_visible(((bArmature *)(_ob)->data), _pchan) && \
-        ((_pchan)->bone->flag & BONE_SELECTED)) \
+        ((_pchan)->flag & POSE_SELECTED)) \
     {
 #define FOREACH_PCHAN_SELECTED_IN_OBJECT_END \
   } \
@@ -692,4 +692,5 @@ using BoneNameSet = blender::Set<std::string>;
  */
 BoneNameSet BKE_armature_find_selected_bone_names(const bArmature *armature);
 
+BoneNameSet BKE_pose_channel_find_selected_names(const Object *object);
 };  // namespace blender::bke

source/blender/blenkernel/intern/action.cc
@@ -1879,8 +1879,7 @@ void BKE_pose_rest(bPose *pose, bool selected_bones_only)
   memset(pose->cyclic_offset, 0, sizeof(pose->cyclic_offset));
 
   LISTBASE_FOREACH (bPoseChannel *, pchan, &pose->chanbase) {
-    if (selected_bones_only && pchan->bone != nullptr && (pchan->bone->flag & BONE_SELECTED) == 0)
-    {
+    if (selected_bones_only && pchan->bone != nullptr && (pchan->flag & POSE_SELECTED) == 0) {
       continue;
     }
     zero_v3(pchan->loc);
@@ -2089,10 +2088,10 @@ void BKE_pose_check_uids_unique_and_report(const bPose *pose)
   BLI_gset_free(used_uids, nullptr);
 }
 
-void BKE_pose_blend_write(BlendWriter *writer, bPose *pose, bArmature *arm)
+void BKE_pose_blend_write(BlendWriter *writer, bPose *pose)
 {
 #ifndef __GNUC__
-  BLI_assert(pose != nullptr && arm != nullptr);
+  BLI_assert(pose != nullptr);
 #endif
 
   /* Write channels */
@@ -2109,17 +2108,6 @@ void BKE_pose_blend_write(BlendWriter *writer, bPose *pose, bArmature *arm)
     BKE_constraint_blend_write(writer, &chan->constraints);
 
     animviz_motionpath_blend_write(writer, chan->mpath);
-
-    /* Prevent crashes with auto-save,
-     * when a bone duplicated in edit-mode has not yet been assigned to its pose-channel.
-     * Also needed with memundo, in some cases we can store a step before pose has been
-     * properly rebuilt from previous undo step. */
-    Bone *bone = (pose->flag & POSE_RECALC) ? BKE_armature_find_bone_name(arm, chan->name) :
-                                              chan->bone;
-    if (bone != nullptr) {
-      /* gets restored on read, for library armatures */
-      chan->selectflag = bone->flag & BONE_SELECTED;
-    }
 
     BLO_write_struct(writer, bPoseChannel, chan);
   }
@@ -2219,11 +2207,6 @@ void BKE_pose_blend_read_after_liblink(BlendLibReader *reader, Object *ob, bPose
 
     if (UNLIKELY(pchan->bone == nullptr)) {
       rebuild = true;
-    }
-    else if (!ID_IS_LINKED(ob) && ID_IS_LINKED(arm)) {
-      /* local pose selection copied to armature, bit hackish */
-      pchan->bone->flag &= ~BONE_SELECTED;
-      pchan->bone->flag |= pchan->selectflag;
     }
 
     /* At some point in history, bones could have an armature object as custom shape, which caused

commit 6e1c48b74c26e2ba2546caabbeb8f949c1c7ef19

Fix #156995: Pose library applies to bones hidden by collection
Caused by a09d0cfd8ccecb57248c6ed96ba25900eab82e0f

The above commit added a new function
`BKE_pose_channel_find_selected_names`.
Compared to its counterpart `BKE_armature_find_selected_bone_names` it
didn't check for collection visibility when determining which bones
should count as selected. Since that function is used when applying
pose assets, it would now also apply to bones hidden by a collection.

The fix is to use `animrig::bone_is_selected` which does check collection visibility.

Pull Request: https://projects.blender.org/blender/blender/pulls/157039

source/blender/blenkernel/intern/armature_selection.cc
@@ -67,8 +67,11 @@ BoneNameSet BKE_pose_channel_find_selected_names(const Object *object)
   }
 
   BoneNameSet selected_bone_names;
+  const bArmature *armature = BKE_armature_from_object(const_cast<Object *>(object));
+  /* Since there is a pose, there should be an armature. */
+  BLI_assert(armature);
   for (bPoseChannel &pose_bone : object->pose->chanbase) {
-    if (pose_bone.flag & POSE_SELECTED) {
+    if (animrig::bone_is_selected(armature, &pose_bone)) {
       selected_bone_names.add(pose_bone.name);
     }
   }