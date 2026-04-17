zNightlord/Bforartists

commit 6f0fe9329ccc6bffff91f7655b0672ce7aa5b596

Fix #156892: EEVEE: Regression in max allowed number of samplers
Regression from 01a6ab5cb0.

Allow as many slots as the GPU Backend supports.
SlotAllocator now tracks the first 32 slots individually, and just
increases by one slot at a time afterward (since those are assumed to
be material textures) up to the max number allowed by the backend.

Pull Request: https://projects.blender.org/blender/blender/pulls/156934

source/blender/draw/engines/eevee/eevee_shader.cc
@@ -24,6 +24,7 @@
 
 #include "BLI_assert.h"
 #include "BLI_math_bits.h"
+#include <fmt/format.h>
 
 namespace blender::eevee {
 
@@ -589,9 +590,14 @@ gpu::Shader *ShaderModule::static_shader_get(eShaderType shader_type)
 
 /* Helper class to get free sampler slots for materials. */
 class SlotAllocator {
-  uint64_t available_samplers_ = ~uint64_t(0u);
-  uint32_t available_vertex_id_ = ~uint32_t(0u);
+  /* Assumes slots reserved from ShaderCreateInfos are always below 32. */
+  uint32_t available_samplers_ = ~uint32_t(0u);
+  /* But some backends may allow more samplers that we can use for material textures.
+   * These slots are just increased linearly. */
+  int total_requested_samplers_ = 0;
   bool sampler_overflow_ = false;
+
+  uint32_t available_vertex_id_ = ~uint32_t(0u);
   bool vertex_id_overflow_ = false;
 
   Set<std::string> visited_infos;
@@ -610,17 +616,17 @@ class SlotAllocator {
     }
     for (const ShaderCreateInfo::Resource &res : info.pass_resources_) {
       if (res.bind_type == ShaderCreateInfo::Resource::SAMPLER) {
-        available_samplers_ &= ~(uint64_t(1) << res.slot);
+        available_samplers_ &= ~(uint32_t(1) << res.slot);
       }
     }
     for (const ShaderCreateInfo::Resource &res : info.batch_resources_) {
       if (res.bind_type == ShaderCreateInfo::Resource::SAMPLER) {
-        available_samplers_ &= ~(uint64_t(1) << res.slot);
+        available_samplers_ &= ~(uint32_t(1) << res.slot);
       }
     }
     for (const ShaderCreateInfo::Resource &res : info.geometry_resources_) {
       if (res.bind_type == ShaderCreateInfo::Resource::SAMPLER) {
-        available_samplers_ &= ~(uint64_t(1) << res.slot);
+        available_samplers_ &= ~(uint32_t(1) << res.slot);
       }
     }
 
@@ -630,11 +636,18 @@ class SlotAllocator {
       /** WATCH: Recursive. */
       reserve_slots(*info);
     }
+
+    total_requested_samplers_ = count_bits_uint64(uint64_t(~available_samplers_));
   }
 
   bool sampler_overflow() const
   {
     return sampler_overflow_;
+  }
+
+  int requested_sampler_count() const
+  {
+    return total_requested_samplers_;
   }
 
   bool vertex_id_overflow() const
@@ -644,12 +657,15 @@ class SlotAllocator {
 
   int get_next_sampler()
   {
-    if (available_samplers_ == 0) {
+    int next_sampler = available_samplers_ == 0 ? total_requested_samplers_++ :
+                                                  bitscan_forward_clear_uint(&available_samplers_);
+    if (next_sampler >= GPU_max_textures()) {
       /* Should result in compilation failure. */
       sampler_overflow_ = true;
       return -1;
     }
-    return bitscan_forward_clear_uint64(&available_samplers_);
+
+    return next_sampler;
   }
 
   void set_vertex_input(int index)
@@ -1282,9 +1298,11 @@ void ShaderModule::material_create_info_amend(GPUMaterial *gpumat, GPUCodegenOut
   /* Make shaders that have as too many samplers fail compilation and have correct error
    * report instead of raising an error. */
   if (slots.sampler_overflow()) {
-    /* We ran out of binding slots. Many systems inside the GPU backend assume a max amount of 64
-     * samplers. */
-    std::cerr << "Error: EEVEE: Material " << material_name << " uses too many samplers."
+    /* We ran out of binding slots. */
+    std::cerr << fmt::format("Error: EEVEE: Material {} uses too many samplers. ({}/{})",
+                             material_name,
+                             slots.requested_sampler_count(),
+                             GPU_max_textures())
               << std::endl;
     /* Avoid assert in ShaderCreateInfo::finalize. */
     info.batch_resources_.clear();

Pagecran/blender

commit b14eea04ecbc8082f48c715773607a730982cbed

Fix: EEVEE SlotAllocator
Regressions from 6f0fe9329c and 01a6ab5cb0.

Fix total_requested_samplers_ and avoid assigning material texture
slots twice.

Pull Request: https://projects.blender.org/blender/blender/pulls/157422

source/blender/draw/engines/eevee/eevee_shader.cc
@@ -602,9 +602,8 @@ class SlotAllocator {
 
   Set<std::string> visited_infos;
 
- public:
-  /** WATCH: Recursive. */
-  void reserve_slots(const gpu::shader::ShaderCreateInfo &info)
+  void reserve_slots_recursive(const gpu::shader::ShaderCreateInfo &info,
+                               bool is_gpumat_info = true)
   {
     if (!visited_infos.add_overwrite(info.name_)) {
       /* Avoid infinite recursion or visiting an info more than once. */
@@ -619,14 +618,18 @@ class SlotAllocator {
         available_samplers_ &= ~(uint32_t(1) << res.slot);
       }
     }
-    for (const ShaderCreateInfo::Resource &res : info.batch_resources_) {
+    for (const ShaderCreateInfo::Resource &res : info.geometry_resources_) {
       if (res.bind_type == ShaderCreateInfo::Resource::SAMPLER) {
         available_samplers_ &= ~(uint32_t(1) << res.slot);
       }
     }
-    for (const ShaderCreateInfo::Resource &res : info.geometry_resources_) {
-      if (res.bind_type == ShaderCreateInfo::Resource::SAMPLER) {
-        available_samplers_ &= ~(uint32_t(1) << res.slot);
+    if (!is_gpumat_info) {
+      /* Don't assign slots for material textures until all the internal engine slots are reserved,
+       * they may overlap with our internal slots. */
+      for (const ShaderCreateInfo::Resource &res : info.batch_resources_) {
+        if (res.bind_type == ShaderCreateInfo::Resource::SAMPLER) {
+          available_samplers_ &= ~(uint32_t(1) << res.slot);
+        }
       }
     }
 
@@ -634,10 +637,22 @@ class SlotAllocator {
       const ShaderCreateInfo *info = reinterpret_cast<const ShaderCreateInfo *>(
           GPU_shader_create_info_get(info_name.c_str()));
       /** WATCH: Recursive. */
-      reserve_slots(*info);
+      reserve_slots_recursive(*info, false);
     }
+  }
 
+ public:
+  void reserve_slots(gpu::shader::ShaderCreateInfo &info)
+  {
+    reserve_slots_recursive(info);
     total_requested_samplers_ = count_bits_uint64(uint64_t(~available_samplers_));
+
+    for (auto &resource : info.batch_resources_) {
+      if (resource.bind_type == gpu::shader::ShaderCreateInfo::Resource::BindType::SAMPLER) {
+        /* Assign non overlapping slots for material textures. */
+        resource.slot = get_next_sampler();
+      }
+    }
   }
 
   bool sampler_overflow() const
@@ -657,8 +672,9 @@ class SlotAllocator {
 
   int get_next_sampler()
   {
-    int next_sampler = available_samplers_ == 0 ? total_requested_samplers_++ :
+    int next_sampler = available_samplers_ == 0 ? total_requested_samplers_ :
                                                   bitscan_forward_clear_uint(&available_samplers_);
+    total_requested_samplers_++;
     if (next_sampler >= GPU_max_textures()) {
       /* Should result in compilation failure. */
       sampler_overflow_ = true;
@@ -887,12 +903,6 @@ void ShaderModule::material_create_info_amend(GPUMaterial *gpumat, GPUCodegenOut
 
   SlotAllocator slots = add_pipeline_create_info(
       info, pipeline_type, geometry_type, use_shader_to_rgba);
-
-  for (auto &resource : info.batch_resources_) {
-    if (resource.bind_type == ShaderCreateInfo::Resource::BindType::SAMPLER) {
-      resource.slot = slots.get_next_sampler();
-    }
-  }
 
   if (GPU_material_flag_get(gpumat, GPU_MATFLAG_SHADER_TO_RGBA)) {
     info.define("MAT_SHADER_TO_RGBA");