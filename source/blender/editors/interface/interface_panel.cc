Pagecran/blender

commit fffd68ff370ff2a847db18fd267299eb505968c0

UI: Panel Category Tab Icons
This PR allows a "Compact" view of sidebar tabs that can show an
optional icon or an abbreviated name if an icon is not defined. The tab
well is slightly wider in Compact view and will show up to two
unrotated characters. Just one if a complex language (like Chinese,
Japanese, or Korean) or symbol or emoji. While in this mode it also
shows "instant" tooltips that display the full category name.

Pull Request: https://projects.blender.org/blender/blender/pulls/154985

source/blender/editors/interface/interface_panel.cc
@@ -1472,8 +1472,12 @@ void panel_category_tabs_draw_all(ARegion *region, const char *category_id_activ
 
   is_alpha = (region->overlap && (theme_col_back[3] != 255));
 
-  BLF_enable(fontid, BLF_ROTATION);
-  BLF_rotation(fontid, is_left ? M_PI_2 : -M_PI_2);
+  const bool compact = U.uiflag2 & USER_UIFLAG2_PANEL_TABS_COMPACT;
+  if (!compact) {
+    BLF_enable(fontid, BLF_ROTATION);
+    BLF_rotation(fontid, is_left ? M_PI_2 : -M_PI_2);
+  }
+
   fontscale(&fstyle_points, aspect);
   BLF_size(fontid, fstyle_points * UI_SCALE_FAC);
 
@@ -1489,7 +1493,8 @@ void panel_category_tabs_draw_all(ARegion *region, const char *category_id_activ
     const char *category_id = pc_dyn.idname;
     const char *category_id_draw = IFACE_(category_id);
     const int category_width = round_fl_to_int(
-        BLF_width(fontid, category_id_draw, BLF_DRAW_STR_DUMMY_MAX));
+        compact ? 10.5 * UI_SCALE_FAC * zoom :
+                  BLF_width(fontid, category_id_draw, BLF_DRAW_STR_DUMMY_MAX));
 
     rct->xmin = rct_xmin;
     rct->xmax = rct_xmax;
@@ -1617,31 +1622,83 @@ void panel_category_tabs_draw_all(ARegion *region, const char *category_id_activ
     }
 
     /* Tab titles. */
-
-    /* Offset toward the middle of the rect. */
-    const int text_v_ofs = round_fl_to_int(float(rct_xmax - rct_xmin) * 0.5f);
-    /* Offset down as the font size increases. */
-    const int text_size_offset = round_fl_to_int(fstyle_points * UI_SCALE_FAC * 0.35f);
-
-    BLF_position(fontid,
-                 is_left ? rct->xmax - text_v_ofs + text_size_offset :
-                           rct->xmin + text_v_ofs - text_size_offset,
-                 is_left ? rct->ymin + tab_v_pad_text : rct->ymax - tab_v_pad_text,
-                 0.0f);
     BLF_color3ubv(fontid, is_active ? theme_col_tab_text_sel : theme_col_tab_text);
 
-    if (fstyle->shadow) {
-      BLF_enable(fontid, BLF_SHADOW);
-      const float shadow_color[4] = {
-          fstyle->shadowcolor, fstyle->shadowcolor, fstyle->shadowcolor, fstyle->shadowalpha};
-      BLF_shadow(fontid, FontShadowType(fstyle->shadow), shadow_color);
-      BLF_shadow_offset(fontid, fstyle->shadx, fstyle->shady);
+    if (compact) {
+      if (pc_dyn.icon != ICON_NONE) {
+        const float icon_size = 16.0f * UI_SCALE_FAC * zoom;
+        const float ofs_x = float(rct_xmax - rct_xmin - icon_size) / 2.0f;
+        const float ofs_y = float(rct->ymax - rct->ymin - icon_size) / 2.0f;
+        icon_draw_ex(float(rct_xmin) + ofs_x,
+                     float(rct->ymin) + ofs_y,
+                     pc_dyn.icon,
+                     aspect / UI_SCALE_FAC,
+                     0.9f,
+                     0.0f,
+                     nullptr,
+                     false,
+                     nullptr,
+                     false);
+        BLF_size(fontid, fstyle_points * UI_SCALE_FAC);
+      }
+      else {
+        std::string title;
+        int char_offset1 = BLI_str_utf8_offset_from_index(category_id_draw, category_draw_len, 1);
+        if (char_offset1 > 2) {
+          /* only a single complex character, symbol, or emoji.*/
+          title = std::string(category_id_draw, char_offset1);
+        }
+        else {
+          int char_offset2 = BLI_str_utf8_offset_from_index(
+              category_id_draw, category_draw_len, 2);
+          char *space = BLI_strcasestr(category_id_draw, " ");
+          if (char_offset2 == 2 && isupper(category_id_draw[1])) {
+            /* First two characters are latin with second uppercase. */
+            title = std::string(category_id_draw, char_offset2);
+          }
+          else if (space && category_draw_len > (space - category_id_draw)) {
+            /* First characters from each of the first two words. */
+            title = std::string(category_id_draw, char_offset1) + std::string(space + 1, 1);
+          }
+          else {
+            /* First two characters of a single word. */
+            title = std::string(category_id_draw, char_offset2);
+          }
+        }
+
+        float width;
+        float height;
+        BLF_width_and_height(fontid, title.c_str(), title.size(), &width, &height);
+        const float ofs_x = float(rct_xmax - rct_xmin - width) / 2.0f;
+        const float ofs_y = float(rct->ymax - rct->ymin - height) / 2.0f;
+        BLF_position(fontid, rct->xmin + ofs_x, rct->ymin + ofs_y, 0.0f);
+        BLF_draw(fontid, title.c_str(), title.size());
+      }
     }
+    else {
+      /* Offset toward the middle of the rect. */
+      const int text_v_ofs = round_fl_to_int(float(rct_xmax - rct_xmin) * 0.5f);
+      /* Offset down as the font size increases. */
+      const int text_size_offset = round_fl_to_int(fstyle_points * UI_SCALE_FAC * 0.35f);
 
-    BLF_draw(fontid, category_id_draw, category_draw_len);
+      BLF_position(fontid,
+                   is_left ? rct->xmax - text_v_ofs + text_size_offset :
+                             rct->xmin + text_v_ofs - text_size_offset,
+                   is_left ? rct->ymin + tab_v_pad_text : rct->ymax - tab_v_pad_text,
+                   0.0f);
+      if (fstyle->shadow) {
+        BLF_enable(fontid, BLF_SHADOW);
+        const float shadow_color[4] = {
+            fstyle->shadowcolor, fstyle->shadowcolor, fstyle->shadowcolor, fstyle->shadowalpha};
+        BLF_shadow(fontid, FontShadowType(fstyle->shadow), shadow_color);
+        BLF_shadow_offset(fontid, fstyle->shadx, fstyle->shady);
+      }
 
-    if (fstyle->shadow) {
-      BLF_disable(fontid, BLF_SHADOW);
+      BLF_draw(fontid, category_id_draw, category_draw_len);
+
+      if (fstyle->shadow) {
+        BLF_disable(fontid, BLF_SHADOW);
+      }
     }
 
     GPU_blend(GPU_BLEND_NONE);
@@ -2490,12 +2547,13 @@ static PanelCategoryDyn *panel_categories_find_mouse_over(ARegion *region, const
   return nullptr;
 }
 
-void panel_category_add(ARegion *region, const char *name)
+void panel_category_add(ARegion *region, const char *name, int icon)
 {
   PanelCategoryDyn *pc_dyn = MEM_new<PanelCategoryDyn>(__func__);
   BLI_addtail(&region->runtime->panels_category, pc_dyn);
 
   STRNCPY_UTF8(pc_dyn->idname, name);
+  pc_dyn->icon = icon;
 
   /* 'pc_dyn->rect' must be set on draw. */
 }
@@ -2562,6 +2620,35 @@ static int handle_panel_category_cycling(const wmEvent *event,
   return WM_UI_HANDLER_CONTINUE;
 }
 
+static ARegion *WM_panel_category_tooltip_init(bContext *C,
+                                               ARegion *region,
+                                               int * /*r_pass*/,
+                                               double * /*pass_delay*/,
+                                               bool * /*r_exit_on_event*/)
+{
+  BLI_assert(BKE_regiontype_uses_category_tabs(region->runtime->type));
+
+  bScreen *screen = CTX_wm_screen(C);
+  const int mouse_x = screen->tool_tip->event_xy[0];
+  const int mouse_y = screen->tool_tip->event_xy[1];
+  PanelCategoryDyn *tab = nullptr;
+  for (PanelCategoryDyn &ptd : region->runtime->panels_category) {
+    if (BLI_rcti_isect_pt(&ptd.rect, mouse_x - region->winrct.xmin, mouse_y - region->winrct.ymin))
+    {
+      tab = &ptd;
+      break;
+    }
+  }
+
+  if (tab) {
+    const int x = region->winrct.xmin + tab->rect.xmin;
+    const int y = region->winrct.ymin + tab->rect.ymin;
+    return ui::tooltip_create_from_panel_category(C, TIP_(tab->idname), x, y);
+  }
+
+  return nullptr;
+}
+
 static void panel_region_width_set(ARegion *region, const float aspect, int unscaled_size)
 {
   const float size_new = unscaled_size / aspect;
@@ -2586,13 +2673,8 @@ int handler_panel_region(bContext *C,
                          ARegion *region,
                          const Button *active_but)
 {
-  /* Mouse-move events are handled by separate handlers for dragging and drag collapsing. */
-  if (ISMOUSE_MOTION(event->type)) {
-    return WM_UI_HANDLER_CONTINUE;
-  }
-
-  /* We only use KM_PRESS events in this function, so it's simpler to return early. */
-  if (event->val != KM_PRESS) {
+  /* Handle release of dragged panel in separate handlers. */
+  if (event->type == LEFTMOUSE && event->val == KM_RELEASE) {
     return WM_UI_HANDLER_CONTINUE;
   }
 
@@ -2605,7 +2687,7 @@ int handler_panel_region(bContext *C,
 
   /* Handle category tabs. */
   if (panel_category_tabs_is_visible(region)) {
-    if (event->type == LEFTMOUSE) {
+    if (event->type == LEFTMOUSE && event->val == KM_PRESS) {
       PanelCategoryDyn *pc_dyn = panel_categories_find_mouse_over(region, event);
       if (pc_dyn) {
         const bool already_active = STREQ(pc_dyn->idname,
@@ -2645,10 +2727,24 @@ int handler_panel_region(bContext *C,
              ELEM(event->type, WHEELUPMOUSE, WHEELDOWNMOUSE))
     {
       /* Cycle tabs. */
+      WM_tooltip_clear(C, CTX_wm_window(C));
       retval = handle_panel_category_cycling(event, region, active_but);
     }
     if (event->type == EVT_PADPERIOD) {
+      WM_tooltip_clear(C, CTX_wm_window(C));
       retval = panel_category_show_active_tab(region, event->xy);
+    }
+    else if (event->type == MOUSEMOVE && U.flag & USER_TOOLTIPS &&
+             U.uiflag2 & USER_UIFLAG2_PANEL_TABS_COMPACT)
+    {
+      PanelCategoryDyn *pc_dyn = panel_categories_find_mouse_over(region, event);
+      if (pc_dyn) {
+        WM_tooltip_timer_init_ex(
+            C, CTX_wm_window(C), CTX_wm_area(C), region, WM_panel_category_tooltip_init, 0.0f);
+      }
+      else {
+        WM_tooltip_clear(C, CTX_wm_window(C));
+      }
     }
   }
 
@@ -2696,7 +2792,15 @@ int handler_panel_region(bContext *C,
       continue;
     }
 
-    if (has_panel_header && mouse_state == PANEL_MOUSE_INSIDE_HEADER) {
+    if (has_panel_header && event->type == RIGHTMOUSE) {
+      if (ELEM(mouse_state, PANEL_MOUSE_INSIDE_HEADER, PANEL_MOUSE_INSIDE_CONTENT)) {
+        retval = WM_UI_HANDLER_BREAK;
+        popup_context_menu_for_panel(C, region, block.panel);
+        break;
+      }
+    }
+
+    if ((has_panel_header && mouse_state == PANEL_MOUSE_INSIDE_HEADER)) {
       /* All mouse clicks inside panel headers should return in break. */
       if (ELEM(event->type, EVT_RETKEY, EVT_PADENTER, LEFTMOUSE)) {
         retval = WM_UI_HANDLER_BREAK;
@@ -2715,6 +2819,11 @@ int handler_panel_region(bContext *C,
         handle_layout_panel_header(C, &block, mx, my, event->type);
       }
     }
+  }
+
+  if (retval == WM_UI_HANDLER_CONTINUE && event->type == RIGHTMOUSE) {
+    retval = WM_UI_HANDLER_BREAK;
+    popup_context_menu_for_panel(C, region, nullptr);
   }
 
   return retval;

source/blender/blenkernel/BKE_screen.hh
@@ -355,6 +355,7 @@ struct PanelType {
   char label[BKE_ST_MAXNAME];
   /** For panel tooltip. */
   const char *description;
+  int icon;
   char translation_context[BKE_ST_MAXNAME];
   /** For buttons window. */
   char context[BKE_ST_MAXNAME];

source/blender/editors/include/UI_interface_c.hh
@@ -440,10 +440,11 @@ enum {
 /** Total width of Toolbar showing one icon column. */
 #define UI_TOOLBAR_WIDTH UI_TOOLBAR_MARGIN + UI_TOOLBAR_COLUMN
 
-#define UI_PANEL_CATEGORY_MARGIN_WIDTH (U.widget_unit * 1.0f)
+#define UI_PANEL_CATEGORY_MARGIN_WIDTH \
+  (((U.uiflag2 & USER_UIFLAG2_PANEL_TABS_COMPACT) ? 1.4f : 1.0f) * U.widget_unit)
 
 /* Minimum width for a panel showing only category tabs. */
-#define UI_PANEL_CATEGORY_MIN_WIDTH 26.0f
+#define UI_PANEL_CATEGORY_MIN_WIDTH ((U.uiflag2 & USER_UIFLAG2_PANEL_TABS_COMPACT) ? 32.0f : 26.0f)
 /* Minimum width for a panel showing content and category tabs. */
 #define UI_PANEL_CATEGORY_MIN_SNAP_WIDTH 90.0f
 
@@ -2254,7 +2255,7 @@ bool panel_can_be_pinned(const Panel *panel);
 
 bool panel_category_is_visible(const ARegion *region);
 bool panel_category_tabs_is_visible(const ARegion *region);
-void panel_category_add(ARegion *region, const char *name);
+void panel_category_add(ARegion *region, const char *name, int icon = 0);
 PanelCategoryDyn *panel_category_find(const ARegion *region, const char *idname);
 int panel_category_index_find(ARegion *region, const char *idname);
 PanelCategoryStack *panel_category_active_find(ARegion *region, const char *idname);
@@ -3050,6 +3051,11 @@ ARegion *tooltip_create_from_button_or_extra_icon(bContext *C,
                                                   ButtonExtraOpIcon *extra_icon,
                                                   bool is_quick_tip);
 ARegion *tooltip_create_from_gizmo(bContext *C, wmGizmo *gz);
+ARegion *tooltip_create_from_panel_category(bContext *C,
+                                            const std::string &category_name,
+                                            const int x,
+                                            const int y);
+
 void tooltip_free(bContext *C, bScreen *screen, ARegion *region);
 
 /**

source/blender/editors/interface/interface_context_menu.cc
@@ -1307,19 +1307,16 @@ void popup_context_menu_for_panel(bContext *C, ARegion *region, Panel *panel)
   if (!any_item_visible) {
     return;
   }
-  if (panel->type->parent != nullptr) {
-    return;
-  }
-  if (!panel_can_be_pinned(panel)) {
+  if (panel && panel->type->parent != nullptr) {
     return;
   }
 
   PointerRNA ptr = RNA_pointer_create_discrete(&screen->id, RNA_Panel, panel);
 
-  PopupMenu *pup = popup_menu_begin(C, IFACE_("Panel"), ICON_NONE);
+  PopupMenu *pup = popup_menu_begin(C, IFACE_("Sidebar"), ICON_NONE);
   Layout &layout = *popup_menu_layout(pup);
 
-  if (has_panel_category) {
+  if (has_panel_category && panel && panel_can_be_pinned(panel)) {
     char tmpstr[80];
     SNPRINTF_UTF8(tmpstr, "%s" UI_SEP_CHAR_S "%s", IFACE_("Pin"), IFACE_("Shift Left Mouse"));
     layout.prop(&ptr, "use_pin", UI_ITEM_NONE, tmpstr, ICON_NONE);
@@ -1330,7 +1327,12 @@ void popup_context_menu_for_panel(bContext *C, ARegion *region, Panel *panel)
       Button *but = block->buttons_ptrs.last().get();
       but->flag |= BUT_HAS_SEP_CHAR;
     }
+    layout.separator();
   }
+
+  PointerRNA prefs_ptr = RNA_pointer_create_discrete(nullptr, RNA_PreferencesSystem, &U);
+  layout.prop(&prefs_ptr, "show_panel_tabs_compact", UI_ITEM_NONE, "Compact Tabs", ICON_NONE);
+
   popup_menu_end(C, pup);
 }
 

source/blender/editors/interface/regions/interface_region_tooltip.cc
@@ -1791,6 +1791,22 @@ ARegion *tooltip_create_from_gizmo(bContext *C, wmGizmo *gz)
   return tooltip_create_with_data(C, std::move(data), init_position, nullptr);
 }
 
+ARegion *tooltip_create_from_panel_category(bContext *C,
+                                            const std::string &category_name,
+                                            const int x,
+                                            const int y)
+{
+  std::unique_ptr<TooltipData> data = std::make_unique<TooltipData>();
+  tooltip_text_field_add(*data, category_name, {}, TIP_STYLE_HEADER, TIP_LC_VALUE, false);
+  const float init_position[2] = {float(x) + 61.0f * UI_SCALE_FAC,
+                                  float(y) + 32.0f * UI_SCALE_FAC};
+  const rcti overlap_rect_fl = {x - int(25.0f * UI_SCALE_FAC),
+                                x + int(28.0f * UI_SCALE_FAC),
+                                y - int(31.0f * UI_SCALE_FAC),
+                                y + int(round(20.7f * UI_SCALE_FAC))};
+  return tooltip_create_with_data(C, std::move(data), init_position, &overlap_rect_fl);
+}
+
 static void tooltip_from_image(Image &ima, TooltipData &data)
 {
   if (ima.filepath[0]) {

source/blender/editors/screen/area.cc
@@ -3273,7 +3273,7 @@ static const char *region_panels_collect_categories(ARegion *region,
     PanelType *pt = static_cast<PanelType *>(pt_link->link);
     if (pt->category[0]) {
       if (!ui::panel_category_find(region, pt->category)) {
-        ui::panel_category_add(region, pt->category);
+        ui::panel_category_add(region, pt->category, pt->icon);
       }
     }
   }

source/blender/makesdna/DNA_screen_types.h
@@ -307,6 +307,7 @@ struct Panel {
 struct PanelCategoryDyn {
   struct PanelCategoryDyn *next = nullptr, *prev = nullptr;
   char idname[64] = "";
+  int icon = 0;
   rcti rect = {};
 };
 

source/blender/makesdna/DNA_userdef_types.h
@@ -175,6 +175,7 @@ enum eUserpref_UI_Flag2 {
   USER_UIFLAG2_UNUSED_2 = (1 << 2),
   USER_UIFLAG2_UNUSED_3 = (1 << 3), /* dirty */
   USER_UIFLAG2_SHOW_ONLINE_ASSETS = (1 << 4),
+  USER_UIFLAG2_PANEL_TABS_COMPACT = (1 << 5),
 };
 
 /** #UserDef.gpu_flag */

commit e32be5807433568f12815d615acfaafaa134547e

Fix #157626: Regression: Context menu doesn't work in panels
Caused by fffd68ff370ff2a847db18fd267299eb505968c0
Add extra check around `popup_context_menu_for_panel` for categories tab
context menu. Make sure mouse is over the caegories using
`panel_categories_find_mouse_over` when RMB is pressed.

Pull Request: https://projects.blender.org/blender/blender/pulls/157632

source/blender/editors/interface/interface_panel.cc
@@ -2746,6 +2746,11 @@ int handler_panel_region(bContext *C,
         WM_tooltip_clear(C, CTX_wm_window(C));
       }
     }
+    else if ((event->type == RIGHTMOUSE) && panel_categories_find_mouse_over(region, event)) {
+      BLI_assert(retval == WM_UI_HANDLER_CONTINUE);
+      retval = WM_UI_HANDLER_BREAK;
+      popup_context_menu_for_panel(C, region, nullptr);
+    }
   }
 
   if (retval == WM_UI_HANDLER_BREAK) {
@@ -2819,11 +2824,6 @@ int handler_panel_region(bContext *C,
         handle_layout_panel_header(C, &block, mx, my, event->type);
       }
     }
-  }
-
-  if (retval == WM_UI_HANDLER_CONTINUE && event->type == RIGHTMOUSE) {
-    retval = WM_UI_HANDLER_BREAK;
-    popup_context_menu_for_panel(C, region, nullptr);
   }
 
   return retval;