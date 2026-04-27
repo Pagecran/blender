Pagecran/blender

commit f6a41b596bec94cd41a5e11a219f825e39f20e81

Fix #154760: Geometry Nodes: incorrect socket usage for warning node
This implements the correct socket usage taking into account that it
matters whether the show output is connected or not.

Pull Request: https://projects.blender.org/blender/blender/pulls/155116

source/blender/nodes/intern/socket_usage_inference.cc
@@ -279,6 +279,10 @@ class SocketUsageInferencerImpl {
         this->usage_task__input__capture_attribute_node(socket);
         break;
       }
+      case GEO_NODE_WARNING: {
+        this->usage_task__input__warning_node(socket);
+        break;
+      }
       case SH_NODE_OUTPUT_AOV:
       case SH_NODE_OUTPUT_LIGHT:
       case SH_NODE_OUTPUT_WORLD:
@@ -460,6 +464,48 @@ class SocketUsageInferencerImpl {
         socket, {&node->output_socket(socket->index())}, {}, socket.context);
   }
 
+  void usage_task__input__warning_node(const SocketInContext &socket)
+  {
+    const NodeInContext node = socket.owner_node();
+    const SocketInContext show_input_socket = node.input_socket(0);
+    const SocketInContext message_input_socket = node.input_socket(1);
+    const SocketInContext show_output_socket = node.output_socket(0);
+    if (show_output_socket->is_directly_linked()) {
+      if (socket == show_input_socket) {
+        this->usage_task__with_dependent_sockets(
+            show_input_socket, {&*show_output_socket}, {}, socket.context);
+      }
+      if (socket == message_input_socket) {
+        this->usage_task__with_dependent_sockets(
+            message_input_socket, {&*show_output_socket}, {&*show_input_socket}, socket.context);
+      }
+      return;
+    }
+    const ComputeContext *context = node.context;
+    const bool is_in_zone = !dynamic_cast<const bke::GroupNodeComputeContext *>(context);
+    if (is_in_zone) {
+      /* Warning nodes where the output is not linked must not be in a zone. */
+      all_socket_usages_.add_new(socket, false);
+      return;
+    }
+    const bNode *output_node = node->owner_tree().group_output_node();
+    if (!output_node) {
+      all_socket_usages_.add_new(socket, false);
+      return;
+    }
+    const Span<const bNodeSocket *> group_output_sockets = output_node->input_sockets().drop_back(
+        1);
+    /* The warning is used if any of the group outputs is used. */
+    if (socket == show_input_socket) {
+      this->usage_task__with_dependent_sockets(
+          show_input_socket, group_output_sockets, {}, context);
+    }
+    if (socket == message_input_socket) {
+      this->usage_task__with_dependent_sockets(
+          message_input_socket, group_output_sockets, {&*show_input_socket}, context);
+    }
+  }
+
   void usage_task__input__enable_output(const SocketInContext &socket)
   {
     const NodeInContext node = socket.owner_node();
@@ -569,7 +615,7 @@ class SocketUsageInferencerImpl {
       this->push_usage_task(next_unknown_socket);
       return;
     }
-    if (!any_output_used) {
+    if (!any_output_used && !dependent_outputs.is_empty()) {
       all_socket_usages_.add_new(socket, false);
       return;
     }

commit a613dd2ff6cbce93cae94e91627883cdba16f83b

Fix #157253: socket usage inferencing regression
This issue was introduced in f6a41b596b which accidentally changed the behavior
for non-warning nodes. The change to `usage_task__with_dependent_sockets` was
not valid and broke what the method was supposed to do.

Pull Request: https://projects.blender.org/blender/blender/pulls/157887

source/blender/nodes/intern/socket_usage_inference.cc
@@ -485,11 +485,14 @@ class SocketUsageInferencerImpl {
     const bool is_top_level = context == nullptr;
     if (is_top_level) {
       if (socket == show_input_socket) {
-        this->usage_task__with_dependent_sockets(show_input_socket, {}, {}, context);
+        /* At the top level, the show input is always used. */
+        all_socket_usages_.add_new(socket, true);
       }
       if (socket == message_input_socket) {
-        this->usage_task__with_dependent_sockets(
-            message_input_socket, {}, {&*show_input_socket}, context);
+        /* The message input is used if the show input is true. */
+        const InferenceValue show_inference_value = this->get_socket_value(show_input_socket);
+        const bool show_value = show_inference_value.get_if_primitive<bool>().value_or(true);
+        all_socket_usages_.add_new(socket, show_value);
       }
       return;
     }
@@ -592,17 +595,17 @@ class SocketUsageInferencerImpl {
 
   /**
    * Utility that handles simple cases where a socket is used if any of its dependent sockets is
-   * used.
+   * used and all of the boolean condition inputs are true.
    */
   void usage_task__with_dependent_sockets(const SocketInContext &socket,
-                                          const Span<const bNodeSocket *> dependent_outputs,
+                                          const Span<const bNodeSocket *> dependent_sockets,
                                           const Span<const bNodeSocket *> condition_inputs,
                                           const ComputeContext *dependent_socket_context)
   {
     /* Check if any of the dependent outputs are used. */
     SocketInContext next_unknown_socket;
     bool any_output_used = false;
-    for (const bNodeSocket *dependent_socket_ptr : dependent_outputs) {
+    for (const bNodeSocket *dependent_socket_ptr : dependent_sockets) {
       const SocketInContext dependent_socket{dependent_socket_context, dependent_socket_ptr};
       const std::optional<bool> is_used = all_socket_usages_.lookup_try(dependent_socket);
       if (!is_used.has_value()) {
@@ -626,7 +629,7 @@ class SocketUsageInferencerImpl {
       this->push_usage_task(next_unknown_socket);
       return;
     }
-    if (!any_output_used && !dependent_outputs.is_empty()) {
+    if (!any_output_used) {
       all_socket_usages_.add_new(socket, false);
       return;
     }