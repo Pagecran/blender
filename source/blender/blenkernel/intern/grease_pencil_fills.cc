Pagecran/blender

commit 51aa14a93cc1c994c196b15ead5304484ab34fd5

Cleanup: Curves: Deduplicate a few IndexMask operations
- Reuse `indices_for_type` in Grease Pencil draw extraction
- Move "point to curve" selection interpolation to blenkernel
    - Reuse this in a few places
    - Use BitVector instead of Array<bool>

Pull Request: https://projects.blender.org/blender/blender/pulls/154824

source/blender/blenkernel/intern/grease_pencil_fills.cc
@@ -118,21 +118,8 @@ IndexMask selected_mask_to_fills(const IndexMask &selected_mask,
     }
     BLI_assert(domain == AttrDomain::Point);
 
-    Array<bool> selected_points(curves.points_num());
-    selected_mask.to_bools(selected_points);
-
-    const IndexMask selected_curves = IndexMask::from_predicate(
-        curves.curves_range(),
-        memory,
-        [&](const int curve_i) {
-          const IndexRange points = points_by_curve[curve_i];
-          const Span<bool> selected_curve_points = selected_points.as_span().slice(points);
-          return std::any_of(selected_curve_points.begin(),
-                             selected_curve_points.end(),
-                             [](const bool value) { return value; });
-        },
-        exec_mode::grain_size(512));
-
+    const IndexMask selected_curves = curves::point_to_curve_selection(
+        points_by_curve, curves.curves_range(), memory);
     return curves::curve_to_point_selection(points_by_curve, selected_curves, memory);
   }
 
@@ -140,20 +127,8 @@ IndexMask selected_mask_to_fills(const IndexMask &selected_mask,
   Array<bool> src_selected_curves(curves.curves_num());
 
   if (domain == AttrDomain::Point) {
-    Array<bool> selected_points(curves.points_num());
-    selected_mask.to_bools(selected_points);
-
-    const IndexMask selected_curves = IndexMask::from_predicate(
-        curves.curves_range(),
-        memory,
-        [&](const int curve_i) {
-          const IndexRange points = points_by_curve[curve_i];
-          const Span<bool> selected_curve_points = selected_points.as_span().slice(points);
-          return std::any_of(selected_curve_points.begin(),
-                             selected_curve_points.end(),
-                             [](const bool value) { return value; });
-        },
-        exec_mode::grain_size(512));
+    const IndexMask selected_curves = curves::point_to_curve_selection(
+        points_by_curve, curves.curves_range(), memory);
 
     selected_curves.foreach_index([&](const int64_t curve_i) {
       const int fill_id = fill_ids[curve_i];

source/blender/blenkernel/BKE_curves_utils.hh
@@ -469,6 +469,13 @@ IndexMask curve_to_point_selection(OffsetIndices<int> points_by_curve,
                                    const IndexMask &curve_selection,
                                    LinearAllocator<> &memory);
 
+/**
+ * Create a mask for all curves that have at least one point in the point mask.
+ */
+IndexMask point_to_curve_selection(OffsetIndices<int> points_by_curve,
+                                   const IndexMask &point_mask,
+                                   LinearAllocator<> &memory);
+
 IndexMask curve_type_point_selection(const bke::CurvesGeometry &curves,
                                      CurveType curve_type,
                                      LinearAllocator<> &memory);

source/blender/blenkernel/intern/curves_utils.cc
@@ -10,8 +10,24 @@
 #include "BKE_customdata.hh"
 
 #include "BLI_array_utils.hh"
+#include "BLI_bit_span_ops.hh"
+#include "BLI_bit_vector.hh"
 
 namespace blender::bke::curves {
+
+IndexMask point_to_curve_selection(OffsetIndices<int> points_by_curve,
+                                   const IndexMask &point_mask,
+                                   LinearAllocator<> &memory)
+{
+  BitVector<1024> selected(points_by_curve.total_size());
+  point_mask.to_bits(selected);
+
+  return IndexMask::from_predicate(points_by_curve.index_range(), memory, [&](const int curve_i) {
+    const IndexRange points = points_by_curve[curve_i];
+    const BitSpan curve_bits = bits::to_best_bit_span(selected).slice(points);
+    return bits::any_bit_set(curve_bits);
+  });
+}
 
 IndexMask curve_to_point_selection(OffsetIndices<int> points_by_curve,
                                    const IndexMask &curve_selection,

source/blender/draw/intern/draw_cache_impl_grease_pencil.cc
@@ -10,6 +10,7 @@
 
 #include "BKE_attribute.hh"
 #include "BKE_curves.hh"
+#include "BKE_curves_utils.hh"
 #include "BKE_grease_pencil.h"
 #include "BKE_grease_pencil.hh"
 #include "BKE_grease_pencil_fills.hh"
@@ -487,10 +488,11 @@ static IndexMask grease_pencil_get_visible_nurbs_curves(Object &object,
       ed::greasepencil::retrieve_editable_and_selected_strokes(
           object, drawing, layer_index, memory);
 
-  const VArray<int8_t> types = curves.curve_types();
-  return IndexMask::from_predicate(selected_editable_strokes, memory, [&](const int64_t curve_i) {
-    return types[curve_i] == CURVE_TYPE_NURBS;
-  });
+  return bke::curves::indices_for_type(curves.curve_types(),
+                                       curves.curve_type_counts(),
+                                       CURVE_TYPE_NURBS,
+                                       selected_editable_strokes,
+                                       memory);
 }
 
 static IndexMask grease_pencil_get_visible_non_nurbs_curves(

source/blender/editors/curves/intern/curves_masks.cc
@@ -15,25 +15,6 @@
 
 namespace blender::ed::curves {
 
-IndexMask curve_mask_from_points(const bke::CurvesGeometry &curves,
-                                 const IndexMask &point_mask,
-                                 IndexMaskMemory &memory)
-{
-  Array<bool> selected(curves.points_num());
-  point_mask.to_bools(selected);
-
-  const OffsetIndices points_by_curve = curves.points_by_curve();
-  return IndexMask::from_predicate(curves.curves_range(), memory, [&](const int curve_i) {
-    const IndexRange points = points_by_curve[curve_i];
-    for (const int point_i : points) {
-      if (selected[point_i]) {
-        return true;
-      }
-    }
-    return false;
-  });
-}
-
 IndexMask end_points(const bke::CurvesGeometry &curves,
                      const IndexMask &curves_mask,
                      const int amount_start,

source/blender/editors/grease_pencil/intern/grease_pencil_select.cc
@@ -9,6 +9,7 @@
 #include "BKE_attribute.hh"
 #include "BKE_context.hh"
 #include "BKE_curves.hh"
+#include "BKE_curves_utils.hh"
 #include "BKE_grease_pencil.hh"
 #include "BKE_grease_pencil_fills.hh"
 #include "BKE_object.hh"
@@ -153,10 +154,10 @@ bool apply_mask_as_segment_selection(bke::CurvesGeometry &curves,
   }
   IndexMaskMemory memory;
 
-  const IndexMask changed_curve_mask = ed::curves::curve_mask_from_points(
-      curves, point_selection_mask, memory);
-
   const OffsetIndices points_by_curve = curves.points_by_curve();
+  const IndexMask changed_curve_mask = bke::curves::point_to_curve_selection(
+      points_by_curve, point_selection_mask, memory);
+
   const Span<float2> screen_space_positions = tree_data.start_positions.as_span().slice(
       tree_data_range);
 

source/blender/editors/include/ED_curves.hh
@@ -268,13 +268,6 @@ void CURVES_OT_separate(wmOperatorType *ot);
  * \{ */
 
 /**
- * Create a mask for all curves that have at least one point in the point mask.
- */
-IndexMask curve_mask_from_points(const bke::CurvesGeometry &curves,
-                                 const IndexMask &point_mask,
-                                 IndexMaskMemory &memory);
-
-/**
  * Return a mask of all the end points in the curves.
  * \param curves_mask: (optional) The curves that should be used in the resulting point mask.
  * \param amount_start: The amount of points to mask from the front.

commit 30d4d7a6a4e687f5eedc6049f1f26e521424a735

Fix: Grease Pencil: Selection Curve mode selects wrong curves
This fixes a problem that some curves would become selected when
entering `Curve Selection Mode` without having any points selected.

This was caused by 51aa14a93cc1c994c196b15ead5304484ab34fd5.

Pull Request: https://projects.blender.org/blender/blender/pulls/158622

source/blender/blenkernel/intern/grease_pencil_fills.cc
@@ -129,7 +129,7 @@ IndexMask selected_mask_to_fills(const IndexMask &selected_mask,
     BLI_assert(domain == AttrDomain::Point);
 
     const IndexMask selected_curves = curves::point_to_curve_selection(
-        points_by_curve, curves.curves_range(), memory);
+        points_by_curve, selected_mask, memory);
     return curves::curve_to_point_selection(points_by_curve, selected_curves, memory);
   }
 
@@ -138,7 +138,7 @@ IndexMask selected_mask_to_fills(const IndexMask &selected_mask,
 
   if (domain == AttrDomain::Point) {
     const IndexMask selected_curves = curves::point_to_curve_selection(
-        points_by_curve, curves.curves_range(), memory);
+        points_by_curve, selected_mask, memory);
 
     selected_curves.foreach_index([&](const int64_t curve_i) {
       const int fill_id = fill_ids[curve_i];