Pagecran/blender

commit 7c2ff868e3cc9d2605fa88a77af8f366816665a5

Assets: Main remote asset library commit (Experimental Feature)
Part of the remote asset libraries project, see:
https://projects.blender.org/blender/blender/issues/134495

New functionality:
- Automatic daily updating of the asset library listing
- Loading and displaying remote asset libraries in the asset browser and
  asset shelf
- Filter flag for online assets in the asset browser and asset shelf
- Operator to download assets, available in context menu
- Prefetch handler to download assets when dragging is invoked
- "Downloading Asset..." hint when dragging assets being downloaded

Further notes:
- Assets that are still being downloaded cannot be dropped currently
  (releasing the mouse button does nothing). We might change this but
  the planned proper solution is some kind of "placeholder" data-block
  that gets replaced with the downloaded data-block once available.
- Downloading is done in Python (already committed), which then "pings"
  the asset system for status updates. This happens via the window
  manager currently, we might move this to a more dedicated asset system
  entry point.
- Asset/file browser previews for online assets use the UI preview
  loading system, not the asset/file browser one. Supporting online
  previews in the latter turned out tricky, plus it's redundant anyway.
  Asset/file browsers should switch to the UI system at some point.

A bunch of more isolated code for remote asset library support was
already committed, see PR for more context. This keeps this commit
smaller and more manageable. And a bunch of more general code was
already committed to `main` throughout the project.

Co-authored-by: Sybren A. Stüvel <sybren@blender.org>

Pull Request: https://projects.blender.org/blender/blender/pulls/153284

source/blender/editors/asset/intern/asset_list.cc
@@ -20,6 +20,7 @@
 
 #include "BKE_context.hh"
 #include "BKE_main.hh"
+#include "BKE_preferences.h"
 #include "BKE_screen.hh"
 
 #include "BLI_listbase.h"
@@ -41,6 +42,9 @@
 #include "ED_screen.hh"
 
 #include "asset_library_reference.hh"
+
+/* TODO somehow update online asset status after downloaded by subscribing to
+ * #WM_MSG_TYPE_REMOTE_DOWNLOADER messages. */
 
 namespace blender::ed::asset::list {
 
@@ -92,6 +96,7 @@ class AssetList : NonCopyable {
 
   static bool listen(const wmNotifier &notifier);
 
+  void ensure_updated();
   void setup();
   void fetch(const bContext &C);
   void ensure_blocking(const bContext &C);
@@ -121,17 +126,6 @@ void AssetList::setup()
   /* TODO pass options properly. */
   filelist_setrecursion(files, FILE_SELECT_MAX_RECURSIONS);
   filelist_setsorting(files, FILE_SORT_ASSET_CATALOG, false);
-  filelist_setlibrary(files, &library_ref_);
-  filelist_setfilter_options(
-      files,
-      true,
-      true,
-      true, /* Just always hide parent, prefer to not add an extra user option for this. */
-      FILE_TYPE_BLENDERLIB,
-      FILTER_ID_ALL,
-      true,
-      "",
-      "");
 
   const bool use_asset_indexer = !USER_DEVELOPER_TOOL_TEST(&U, no_asset_indexing);
   filelist_setindexer(files, use_asset_indexer ? &index::file_indexer_asset : &file_indexer_noop);
@@ -141,6 +135,26 @@ void AssetList::setup()
     STRNCPY(dirpath, asset_lib_path.c_str());
   }
   filelist_setdir(files, dirpath);
+}
+
+void AssetList::ensure_updated()
+{
+  FileList *files = filelist_;
+
+  const bool show_online_assets = (U.uiflag2 & USER_UIFLAG2_SHOW_ONLINE_ASSETS) != 0;
+  filelist_setlibrary(files, &library_ref_);
+  filelist_setfilter_options(
+      files,
+      true,
+      true,
+      true, /* Just always hide parent, prefer to not add an extra user option for this. */
+      FILE_TYPE_BLENDERLIB,
+      FILTER_ID_ALL,
+      true,
+      (U.uiflag2 & USER_UIFLAG2_SHOW_ONLINE_ASSETS) == 0,
+      "",
+      "");
+  filelist_set_asset_include_online(files, show_online_assets);
 }
 
 void AssetList::fetch(const bContext &C)
@@ -346,8 +360,14 @@ static std::optional<eFileSelectType> asset_library_reference_to_fileselect_type
     case ASSET_LIBRARY_ALL:
       return FILE_ASSET_LIBRARY_ALL;
     case ASSET_LIBRARY_ESSENTIALS:
-    case ASSET_LIBRARY_CUSTOM:
+    case ASSET_LIBRARY_CUSTOM: {
+      const bUserAssetLibrary *user_library = BKE_preferences_asset_library_find_index(
+          &U, library_reference.custom_library_index);
+      if (user_library->flag & ASSET_LIBRARY_USE_REMOTE_URL) {
+        return FILE_ASSET_LIBRARY_REMOTE;
+      }
       return FILE_ASSET_LIBRARY;
+    }
     case ASSET_LIBRARY_LOCAL:
       return FILE_MAIN_ASSET;
   }
@@ -407,6 +427,8 @@ void storage_fetch(const AssetLibraryReference *library_reference, const bContex
   }
 
   auto [list, is_new] = ensure_list_storage(*library_reference, *filesel_type);
+  list.ensure_updated();
+
   if (is_new || list.needs_refetch()) {
     list.setup();
     list.fetch(*C);
@@ -422,6 +444,8 @@ void storage_fetch_blocking(const AssetLibraryReference &library_reference, cons
   }
 
   auto [list, is_new] = ensure_list_storage(library_reference, *filesel_type);
+  list.ensure_updated();
+
   if (is_new || list.needs_refetch()) {
     list.setup();
     list.ensure_blocking(C);

source/blender/editors/asset/intern/asset_shelf.cc
@@ -371,6 +371,31 @@ void region_message_subscribe(const wmRegionMessageSubscribeParams *params)
   msg_sub_value_region_tag_redraw.notify = ED_region_do_msg_notify_tag_redraw;
   WM_msg_subscribe_rna_prop(
       mbus, &workspace->id, workspace, WorkSpace, tools, &msg_sub_value_region_tag_redraw);
+
+  {
+    wmMsgSubscribeValue msg_sub_value_region_clear_remote_libraries{};
+    msg_sub_value_region_clear_remote_libraries.owner = region;
+    msg_sub_value_region_clear_remote_libraries.user_data = region;
+    msg_sub_value_region_clear_remote_libraries.notify = [](/* Follow wmMsgNotifyFn spec */
+                                                            bContext *C,
+                                                            wmMsgSubscribeKey * /*msg_key*/,
+                                                            wmMsgSubscribeValue *msg_val) {
+      ARegion *region = static_cast<ARegion *>(msg_val->owner);
+      RegionAssetShelf *shelf_regiondata = RegionAssetShelf::get_from_asset_shelf_region(*region);
+      AssetShelf *active_shelf = shelf_regiondata->active_shelf;
+      if (blender::asset_system::is_or_contains_remote_libraries(
+              active_shelf->settings.asset_library_reference))
+      {
+        asset::list::clear(&active_shelf->settings.asset_library_reference, C);
+      }
+    };
+    WM_msg_subscribe_rna_prop(mbus,
+                              nullptr,
+                              &U,
+                              PreferencesSystem,
+                              use_online_access,
+                              &msg_sub_value_region_clear_remote_libraries);
+  }
 }
 
 void region_init(wmWindowManager *wm, ARegion *region)

scripts/addons_core/bl_pkg/__init__.py
@@ -17,6 +17,9 @@ bl_info = {
     "category": "System",
 }
 
+from pathlib import Path
+from typing import TYPE_CHECKING
+
 if "bpy" in locals():
     # This doesn't need to be inline because sub-modules aren't imported into the global name-space.
     # The check for `bpy` ensures this is always assigned before use.
@@ -32,6 +35,20 @@ from bpy.props import (
     CollectionProperty,
     StringProperty,
 )
+
+
+# Only import submodules here when necessary for type checking.
+# At runtime, the module is imported only when it's actually used.
+if TYPE_CHECKING:
+    from _bpy_internal.assets.remote_library_listing import listing_downloader
+
+    type _RemoteAssetListingDownloader = listing_downloader.RemoteAssetListingDownloader
+else:
+    type _RemoteAssetListingDownloader = object
+
+
+# Auto-refresh remote asset libraries once per day.
+REMOTE_ASSET_LIBS_AUTOSYNC_PERIOD_SEC = 3600 * 24
 
 
 # -----------------------------------------------------------------------------
@@ -400,6 +417,126 @@ def repos_to_notify():
         ))
 
     return repos_notify
+
+
+# -----------------------------------------------------------------------------
+# Remote Asset Libraries
+
+_downloaders: list[_RemoteAssetListingDownloader] = []
+
+
+# Called directly from C++ code.
+def remote_asset_library_sync(
+    asset_library_url: str,
+    asset_library_local_path: Path,
+    only_if_older_than_sec=0,
+) -> None:
+    """Download the remote asset library listing."""
+
+    # Ignore in background mode, as that should trigger these updates explicitly.
+    if bpy.app.background:
+        return
+
+    # Only download remote libraries.
+    if not asset_library_url:
+        return
+
+    # Only download over HTTP.
+    supported_schemas = ("http://", "https://")
+    if not any(asset_library_url.startswith(schema) for schema in supported_schemas):
+        print("  skipping {!r}, can only handle {!s}".format(asset_library_url, ", ".join(supported_schemas)))
+        return
+
+    # Refuse to download if online access is turned off.
+    if not bpy.app.online_access:
+        print("  skipping {!r}, online access is not allowed,".format(asset_library_url))
+        return
+
+    from _bpy_internal.assets.remote_library_listing import listing_downloader
+
+    # Check if the download should happen at all.
+    if only_if_older_than_sec and listing_downloader.is_more_recent_than(
+            asset_library_local_path, only_if_older_than_sec):
+        return
+
+    # Only actually start downloading if no other Blender is already syncing
+    # this asset library.
+    from _bpy_internal.assets.remote_library_listing import sync_mutex
+    if not sync_mutex.mutex_lock(asset_library_local_path):
+        print("  skipping {!r}, another Blender is already syncing this asset library,".format(asset_library_url))
+        return
+
+    # Communicate to the asset system that we started loading a library. It will let asset browsers
+    # and other UIs displaying this library indicate that loading is ongoing then, until finished.
+    bpy.types.WindowManager.asset_library_status_begin_loading(asset_library_url)
+
+    # Create the downloader and start downloading.
+    downloader = listing_downloader.RemoteAssetListingDownloader(
+        asset_library_url,
+        asset_library_local_path,
+        on_update_callback=_remote_asset_library_sync_update,
+        on_done_callback=_remote_asset_library_sync_done,
+        on_metafiles_done_callback=_remote_asset_library_sync_metafiles_done,
+        on_page_done_callback=_remote_asset_library_sync_new_page_done,
+    )
+    downloader.download_and_process()
+
+    # Just to keep the Python object referenced:
+    _downloaders.append(downloader)
+
+
+def _remote_asset_library_sync_done(downloader: _RemoteAssetListingDownloader) -> None:
+    """Called when the downloading of hte remote asset listing is done.
+
+    Here "done" does not imply "successful", as cancellations, network errors,
+    or other issues can cause things to abort. In that case, this function is
+    still called.
+    """
+    from _bpy_internal.assets.remote_library_listing import sync_mutex
+    from _bpy_internal.assets.remote_library_listing.listing_downloader import DownloadStatus
+    from bpy.types import WindowManager
+
+    try:
+        _downloaders.remove(downloader)
+
+        match downloader.status:
+            case DownloadStatus.LOADING:
+                print("Unexpected: `on_done_callback` called while downloader status is loading")
+            case DownloadStatus.FINISHED_SUCCESSFULLY:
+                WindowManager.asset_library_status_finished_loading(downloader.remote_url)
+            case DownloadStatus.FAILED:
+                WindowManager.asset_library_status_failed_loading(
+                    downloader.remote_url, message=downloader.error_message)
+    finally:
+        sync_mutex.mutex_unlock(downloader.local_path)
+
+
+def _remote_asset_library_sync_update(downloader: _RemoteAssetListingDownloader) -> None:
+    from _bpy_internal.assets.remote_library_listing.listing_downloader import DownloadStatus
+
+    # Only call `asset_library_status_ping_still_loading()` if the loading is still going on.
+    if downloader.status == DownloadStatus.LOADING:
+        bpy.types.WindowManager.asset_library_status_ping_still_loading(downloader.remote_url)
+
+
+def _remote_asset_library_sync_metafiles_done(downloader: _RemoteAssetListingDownloader) -> None:
+    bpy.types.WindowManager.asset_library_status_ping_metafiles_in_place(downloader.remote_url)
+
+
+def _remote_asset_library_sync_new_page_done(downloader: _RemoteAssetListingDownloader) -> None:
+    bpy.types.WindowManager.asset_library_status_ping_loaded_new_pages(downloader.remote_url)
+
+
+def _remote_asset_library_sync_all_periodic():
+    """Periodically download remote asset library listings."""
+    if not bpy.app.online_access:
+        return
+    if not bpy.context.preferences.experimental.use_remote_asset_libraries:
+        return
+
+    for asset_lib in bpy.context.preferences.filepaths.asset_libraries:
+        remote_asset_library_sync(asset_lib.remote_url, Path(asset_lib.path),
+                                  only_if_older_than_sec=REMOTE_ASSET_LIBS_AUTOSYNC_PERIOD_SEC)
 
 
 # -----------------------------------------------------------------------------
@@ -798,6 +935,7 @@ def register():
         if prefs.view.show_extensions_updates:
             from . import bl_extension_notify
             bl_extension_notify.update_non_blocking(repos_fn=repos_to_notify)
+        _remote_asset_library_sync_all_periodic()
 
 
 def unregister():

scripts/modules/_bpy_internal/assets/remote_library_listing/asset_downloader.py
@@ -119,8 +119,7 @@ def download_preview(
         age_in_seconds = time.time() - stat.st_mtime
         if age_in_seconds < PREVIEW_DOWNLOAD_AGE_THRESHOLD_SEC:
             # The local file is still fresh, just pretend we just downloaded it.
-            wm = bpy.context.window_manager
-            wm.asset_library_status_ping_loaded_new_preview(str(dst_filepath))
+            bpy.types.WindowManager.asset_library_status_ping_loaded_new_preview(str(dst_filepath))
             return
 
     try:
@@ -147,8 +146,7 @@ def _asset_download_done(
     _http_req_descr: http_dl.RequestDescription,
     _preview_local_path: Path,
 ) -> None:
-    wm = bpy.context.window_manager
-    wm.asset_library_status_ping_loaded_new_assets(downloader.remote_url)
+    bpy.types.WindowManager.asset_library_status_ping_loaded_new_assets(downloader.remote_url)
 
 
 def _preview_download_done(
@@ -175,8 +173,7 @@ def _preview_download_done(
     preview_local_path.touch()
 
     # Poke Blender so it knows there's a thumbnail update.
-    wm = bpy.context.window_manager
-    wm.asset_library_status_ping_loaded_new_preview(str(preview_local_path))
+    bpy.types.WindowManager.asset_library_status_ping_loaded_new_preview(str(preview_local_path))
 
 
 def downloader_status(asset_library_url: str) -> DownloadStatus:

scripts/startup/bl_ui/asset_shelf.py
@@ -40,6 +40,9 @@ class ASSETSHELF_PT_filter(Panel):
         layout = self.layout
         prefs = context.preferences
 
+        # Filter option stored in the Preferences.
+        layout.prop(prefs.view, "show_online_assets", text="Online Assets")
+
 
 classes = (
     ASSETSHELF_PT_display,

scripts/startup/bl_ui/space_filebrowser.py
@@ -642,6 +642,8 @@ class ASSETBROWSER_PT_filter(asset_utils.AssetBrowserPanel, Panel):
                     row.label(icon=filter_id.bl_rna.properties[identifier].icon)
                     row.prop(filter_id, identifier, toggle=False)
 
+        layout.prop(params, "show_online_assets", text="Online Assets")
+
 
 class AssetBrowserMenu:
     @classmethod
@@ -860,6 +862,7 @@ class ASSETBROWSER_MT_context_menu(AssetBrowserMenu, Menu):
         layout = self.layout
         st = context.space_data
         params = st.params
+        asset = context.asset
 
         layout.operator("asset.library_refresh", icon='FILE_REFRESH')
 
@@ -869,6 +872,11 @@ class ASSETBROWSER_MT_context_menu(AssetBrowserMenu, Menu):
         sub.operator_context = 'EXEC_DEFAULT'
         sub.operator("asset.clear", text="Clear Asset").set_fake_user = False
         sub.operator("asset.clear", text="Clear Asset (Set Fake User)").set_fake_user = True
+
+        if asset and asset.is_online:
+            layout.separator()
+
+            layout.operator("asset.assets_download")
 
         layout.separator()
 

source/blender/asset_system/AS_asset_library.hh
@@ -12,6 +12,7 @@
 #include <optional>
 
 #include "AS_asset_catalog.hh"
+#include "AS_asset_representation.hh" /* For URLWithHash. */
 
 #include "DNA_asset_types.h"
 
@@ -56,10 +57,10 @@ class AssetLibrary {
   /**
    * AssetStorage for assets (better said their representations) that are considered to be part of
    * this library. Assets are not automatically loaded into this when loading an asset library.
-   * Assets have to be loaded externally and added to this storage via #add_external_asset() or
-   * #add_local_id_asset(). So this really is arbitrary storage as far as #AssetLibrary is
-   * concerned (allowing the API user to manage partial library storage and partial loading, so
-   * only relevant parts of a library are kept in memory).
+   * Assets have to be loaded externally and added to this storage via
+   * #add_external_on_disk_asset() or #add_local_id_asset(). So this really is arbitrary storage as
+   * far as #AssetLibrary is concerned (allowing the API user to manage partial library storage and
+   * partial loading, so only relevant parts of a library are kept in memory).
    *
    * For now, multiple parts of Blender just keep adding their own assets to this storage. E.g.
    * multiple asset browsers might load multiple representations for the same asset into this.
@@ -158,14 +159,23 @@ class AssetLibrary {
    *         reference stored to be able to call #remove_asset(). This would be dangling once the
    *         asset library is destructed, so a weak pointer should be used to reference it.
    */
-  std::weak_ptr<AssetRepresentation> add_external_asset(StringRef relative_asset_path,
-                                                        StringRef name,
-                                                        int id_type,
-                                                        std::unique_ptr<AssetMetaData> metadata);
-  /** See #AssetLibrary::add_external_asset(). */
+  std::weak_ptr<AssetRepresentation> add_external_on_disk_asset(
+      StringRef relative_asset_path,
+      StringRef name,
+      int id_type,
+      std::unique_ptr<AssetMetaData> metadata);
+  /** See #AssetLibrary::add_external_on_disk_asset(). Use this for assets that are not available
+   * on disk, and part of an online asset library. */
+  std::weak_ptr<AssetRepresentation> add_external_online_asset(
+      StringRef relative_asset_path,
+      StringRef name,
+      int id_type,
+      std::unique_ptr<AssetMetaData> metadata,
+      OnlineAssetInfo online_info);
+  /** See #AssetLibrary::add_external_on_disk_asset(). */
   std::weak_ptr<AssetRepresentation> add_local_id_asset(ID &id);
   /**
-   * Remove an asset from the library that was added using #add_external_asset() or
+   * Remove an asset from the library that was added using #add_external_on_disk_asset() or
    * #add_local_id_asset(). Can usually be expected to be constant time complexity (worst case may
    * differ).
    * \note This is safe to call if \a asset is freed (dangling reference), will not perform any
@@ -215,6 +225,14 @@ Vector<AssetLibraryReference> all_valid_asset_library_refs();
 AssetLibraryReference all_library_reference();
 AssetLibraryReference current_file_library_reference();
 void all_library_reload_catalogs_if_dirty();
+
+/**
+ * Return whether this is a remote asset library, or contains remote assets.
+ *
+ * The All and Essentials libraries can (now resp. in the future) have a mixture of local & remote
+ * assets.
+ */
+bool is_or_contains_remote_libraries(const AssetLibraryReference &reference);
 
 }  // namespace asset_system
 

source/blender/asset_system/AS_asset_representation.hh
@@ -27,14 +27,17 @@
 namespace blender {
 
 struct AssetMetaData;
+struct bContext;
 struct ID;
 struct PreviewImage;
+struct ReportList;
 
 namespace asset_system {
 
 class AssetLibrary;
 struct OnlineAssetInfo;
 struct OnlineAssetFile;
+struct URLWithHash;
 
 class AssetRepresentation : NonCopyable, NonMovable {
   /** Pointer back to the asset library that owns this asset representation. */
@@ -60,12 +63,26 @@ class AssetRepresentation : NonCopyable, NonMovable {
   friend class AssetLibrary;
 
  public:
-  /** Constructs an asset representation for an external ID. The asset will not be editable. */
+  /**
+   * Constructs an asset representation for an external ID stored on disk. The asset will not be
+   * editable.
+   *
+   * For online assets, use the version with #online_info below.
+   */
   AssetRepresentation(StringRef relative_asset_path,
                       StringRef name,
                       int id_type,
                       std::unique_ptr<AssetMetaData> metadata,
                       AssetLibrary &owner_asset_library);
+  /**
+   * Constructs an asset representation for an external ID stored online (requiring download).
+   */
+  AssetRepresentation(StringRef relative_asset_path,
+                      StringRef name,
+                      int id_type,
+                      std::unique_ptr<AssetMetaData> metadata,
+                      AssetLibrary &owner_asset_library,
+                      OnlineAssetInfo online_info);
   /**
    * Constructs an asset representation for an ID stored in the current file. This makes the asset
    * local and fully editable.
@@ -87,8 +104,10 @@ class AssetRepresentation : NonCopyable, NonMovable {
    * to the preview but doesn't actually load it. To load it, attach its
    * #PreviewImageRuntime::icon_id to a UI button (UI loads it asynchronously then) or call
    * #BKE_previewimg_ensure() (not asynchronous).
+   *
+   * For online assets this triggers downloading of the preview.
    */
-  void ensure_previewable();
+  void ensure_previewable(const bContext &C, ReportList *reports = nullptr);
   /**
    * Get the preview of this asset.
    *

commit 25212701cd9efa7706b9f2add8fcc431ca9cd21f

Assets: Better handle deleted libraries in asset shelves
An asset library may be deleted in the Preferences while asset shelves
are displaying it. The behavior there wasn't well defined, and some
glitches could happen or Blender could crash even.

Now asset shelves will show the following custom library in the list
instead, and cleanly fall back to the "All" library if there is none.

Also fixes a crash reported by the Blender Studio. The crash would
happen when switching to a workspace with an asset shelf showing the
deleted library (would have to be the last library in the Preferences
for the crash to happen). This is because 7c2ff868e3c introduced a
lookup that can fail, without handling the failure case. A simple
null-check would fix the crash itself, but the library selector in the
asset shelf would just fall back to displaying the "Current File"
library, while no assets would actually be displayed, not even from the
current file.

Implementation wise, the sanitization happens via explicit calls to
`settings_ensure_valid_library_ref()`. This isn't too nice since it can
be forgotten and has to be done on redraws which is usually redundant,
but we're lacking a good way to "subscribe" to library deletion. The
explicit sanitizing is entirely handled internally in the asset shelf
code and it's cheap to do, so it should be okay.

Pull Request: https://projects.blender.org/blender/blender/pulls/158897

source/blender/editors/asset/ED_asset_list.hh
@@ -51,12 +51,17 @@ void iterate(const AssetLibraryReference &library_reference, AssetListIterFn fn)
  * \see: #storage_fetch_blocking for a blocking version.
  * \warning: Asset list reading involves an #AS_asset_library_load() call which may reload asset
  *           library data like catalogs (invalidating pointers). Refer to its warning for details.
+ * \warning: The caller is responsible for ensuring \a library_reference is valid so that it
+ *           doesn't reference a deleted asset library. Otherwise an empty list may be loaded and
+ *           there are assertions to catch the case. But it's unclear what library choice is being
+ *           shown to the user, and if that matches the empty library that will be presented.
  */
 void storage_fetch(const AssetLibraryReference *library_reference, const bContext *C);
 /**
  * Invoke asset list reading, guaranteed to execute on the same thread.
  *
- * \see #storage_fetch for an asynchronous version.
+ * \see #storage_fetch for an asynchronous version. Its warning on \a library_reference applies
+ *      here too.
  */
 void storage_fetch_blocking(const AssetLibraryReference &library_reference, const bContext &C);
 bool is_loaded(const AssetLibraryReference *library_reference);

source/blender/editors/asset/intern/asset_list.cc
@@ -379,6 +379,12 @@ static std::optional<eFileSelectType> asset_library_reference_to_fileselect_type
     case ASSET_LIBRARY_CUSTOM: {
       const bUserAssetLibrary *user_library = BKE_preferences_asset_library_find_index(
           &U, library_reference.custom_library_index);
+      if (!user_library) {
+        /* The caller should make sure the passed library reference is valid. */
+        BLI_assert_unreachable();
+        return std::nullopt;
+      }
+
       if (user_library->flag & ASSET_LIBRARY_USE_REMOTE_URL) {
         return FILE_ASSET_LIBRARY_REMOTE;
       }

source/blender/editors/asset/intern/asset_shelf.cc
@@ -567,10 +567,12 @@ void region_layout(const bContext *C, ARegion *region)
       shelf_regiondata,
       "Region-data should've been created by a previously called `region_on_poll_success()`.");
 
-  const AssetShelf *active_shelf = shelf_regiondata->active_shelf;
+  AssetShelf *active_shelf = shelf_regiondata->active_shelf;
   if (!active_shelf) {
     return;
   }
+
+  settings_ensure_valid_library_ref(active_shelf->settings);
 
   ui::Block *block = block_begin(C, region, __func__, ui::EmbossType::Emboss);
 
@@ -772,11 +774,8 @@ int context(const bContext *C, const char *member, bContextDataResult *result)
     if (!active_shelf) {
       return CTX_RESULT_NO_DATA;
     }
-
-    CTX_data_pointer_set(result,
-                         &screen->id,
-                         RNA_AssetLibraryReference,
-                         &active_shelf->settings.asset_library_reference);
+    AssetLibraryReference &library_ref = settings_ensure_valid_library_ref(active_shelf->settings);
+    CTX_data_pointer_set(result, &screen->id, RNA_AssetLibraryReference, &library_ref);
     return CTX_RESULT_OK;
   }
 

source/blender/editors/asset/intern/asset_shelf.hh
@@ -63,6 +63,15 @@ void regiondata_blend_read_data(BlendDataReader *reader, RegionAssetShelf **shel
 void settings_blend_write(BlendWriter *writer, const AssetShelfSettings &settings);
 void settings_blend_read_data(BlendDataReader *reader, AssetShelfSettings &settings);
 
+/**
+ * Important: Must be called before #AssetShelfSettings.asset_library_reference is used. It will
+ * make sure to fall back to the "All" library if the reference refers to a deleted library. An
+ * invalid reference would make loading the asset listing fail.
+ *
+ * The library reference in \a settings will be updated and returned (for convenience).
+ */
+AssetLibraryReference &settings_ensure_valid_library_ref(AssetShelfSettings &settings);
+
 void settings_set_active_catalog(AssetShelfSettings &settings,
                                  const asset_system::AssetCatalogPath &path);
 void settings_set_all_catalog_active(AssetShelfSettings &settings);

source/blender/editors/asset/intern/asset_shelf_catalog_selector.cc
@@ -203,6 +203,8 @@ static void catalog_selector_panel_draw(const bContext *C, Panel *panel)
     return;
   }
 
+  settings_ensure_valid_library_ref(shelf->settings);
+
   ui::Layout &layout = *panel->layout;
 
   library_selector_draw(C, layout, *shelf);

source/blender/editors/asset/intern/asset_shelf_popover.cc
@@ -237,6 +237,8 @@ static void popover_panel_draw(const bContext *C, Panel *panel)
     return;
   }
 
+  settings_ensure_valid_library_ref(shelf->settings);
+
   bScreen *screen = CTX_wm_screen(C);
   PointerRNA library_ref_ptr = RNA_pointer_create_discrete(
       &screen->id, RNA_AssetLibraryReference, &shelf->settings.asset_library_reference);

source/blender/editors/asset/intern/asset_shelf_settings.cc
@@ -9,6 +9,7 @@
  */
 
 #include "AS_asset_catalog_path.hh"
+#include "AS_asset_library.hh"
 
 #include "DNA_defs.h"
 #include "DNA_screen_types.h"
@@ -86,6 +87,23 @@ void settings_blend_read_data(BlendDataReader *reader, AssetShelfSettings &setti
 {
   BKE_asset_catalog_path_list_blend_read_data(reader, settings.enabled_catalog_paths);
   BLO_read_string(reader, &settings.active_catalog_path);
+}
+
+AssetLibraryReference &settings_ensure_valid_library_ref(AssetShelfSettings &settings)
+{
+  if (settings.asset_library_reference.type != ASSET_LIBRARY_CUSTOM) {
+    /* Nothing to validate, all good. */
+    return settings.asset_library_reference;
+  }
+
+  const bUserAssetLibrary *user_library = BKE_preferences_asset_library_find_index(
+      &U, settings.asset_library_reference.custom_library_index);
+
+  /* If the library wasn't found, fall back to the "All" library. */
+  if (!user_library) {
+    settings.asset_library_reference = asset_system::all_library_reference();
+  }
+  return settings.asset_library_reference;
 }
 
 void settings_set_active_catalog(AssetShelfSettings &settings,