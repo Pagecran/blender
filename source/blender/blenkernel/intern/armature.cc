Pagecran/blender

commit b6a84566b9f8baad2d9316081a21c57b7df51d08

Fix  #141841: Removing a bone does not remove its drivers
When removing a bone in edit mode, the drivers on
the pose bone stuck around. Thus when creating
a bone with the same name, the drivers would appear again.

This is now fixed by calling `BKE_animdata_drivers_remove_for_rna_struct`
when leaving edit mode.
The reason this call is not deeper in the code is because there is a
valid use case for freeing a bone without touching its animdata.
One example for that in Blender is mirroring where the bones on
one side are freed. It would be unexpected to lose drivers from mirroring.
This is also why the drivers are only removed when leaving edit mode.
That way it can be consistent between using the api and editing the
armature via the UI.

Pull Request: https://projects.blender.org/blender/blender/pulls/154571

source/blender/blenkernel/intern/armature.cc
@@ -36,9 +36,12 @@
 #include "DNA_object_types.h"
 #include "DNA_scene_types.h"
 
+#include "RNA_prototypes.hh"
+
 #include "BKE_action.hh"
 #include "BKE_anim_data.hh"
 #include "BKE_anim_visualization.h"
+#include "BKE_animsys.h"
 #include "BKE_armature.hh"
 #include "BKE_constraint.h"
 #include "BKE_curve.hh"
@@ -2863,10 +2866,13 @@ void BKE_pchan_rebuild_bbone_handles(bPose *pose, bPoseChannel *pchan)
   pchan->bbone_next = pose_channel_find_bone(pose, pchan->bone->bbone_next);
 }
 
-void BKE_pose_channels_clear_with_null_bone(bPose *pose, const bool do_id_user)
+void BKE_pose_channels_clear_with_null_bone(Object *armature_ob, const bool do_id_user)
 {
+  BLI_assert(armature_ob->pose);
+  bPose *pose = armature_ob->pose;
   for (bPoseChannel &pchan : pose->chanbase.items_mutable()) {
     if (pchan.bone == nullptr) {
+      BKE_animdata_drivers_remove_for_rna_struct(armature_ob->id, *RNA_PoseBone, &pchan);
       BKE_pose_channel_free_ex(&pchan, do_id_user);
       BKE_pose_channels_hash_free(pose);
       BLI_freelinkN(&pose->chanbase, &pchan);
@@ -2899,7 +2905,7 @@ void BKE_pose_rebuild(Main *bmain, Object *ob, bArmature *arm, const bool do_id_
   }
 
   /* and a check for garbage */
-  BKE_pose_channels_clear_with_null_bone(pose, do_id_user);
+  BKE_pose_channels_clear_with_null_bone(ob, do_id_user);
 
   BKE_pose_channels_hash_ensure(pose);
 

source/blender/blenkernel/BKE_armature.hh
@@ -251,7 +251,7 @@ void BKE_pose_remap_bone_pointers(bArmature *armature, bPose *pose);
  * Update the links for the B-Bone handles from Bone data.
  */
 void BKE_pchan_rebuild_bbone_handles(bPose *pose, bPoseChannel *pchan);
-void BKE_pose_channels_clear_with_null_bone(bPose *pose, bool do_id_user);
+void BKE_pose_channels_clear_with_null_bone(Object *armature_ob, bool do_id_user);
 /**
  * Only after leave edit-mode, duplicating, validating older files, library syncing.
  *

source/blender/editors/object/object_edit.cc
@@ -681,7 +681,7 @@ static bool editmode_load_free_ex(Main *bmain,
         /* Don't keep unused pose channels created by duplicating bones
          * which may have been deleted/undone, see: #87631. */
         if (obedit->pose != nullptr) {
-          BKE_pose_channels_clear_with_null_bone(obedit->pose, true);
+          BKE_pose_channels_clear_with_null_bone(obedit, true);
         }
       }
     }

tests/python/bl_animation_drivers.py
@@ -261,6 +261,47 @@ class SubDataDriverRemovalTest(AbstractEmptyDriverTest, unittest.TestCase):
         self.assertEqual(len(shape_key_id.animation_data.drivers), 0,
                          "Removing the shape key should remove any driver on it")
 
+    def test_remove_bone(self):
+        arm = bpy.data.armatures.new('Armature')
+        arm_ob = bpy.data.objects.new('ArmObject', arm)
+        bpy.context.scene.collection.objects.link(arm_ob)
+        bpy.context.view_layer.objects.active = arm_ob
+
+        bpy.ops.object.mode_set(mode='EDIT')
+        ebone = arm.edit_bones.new(name="test")
+        ebone.tail = (1, 0, 0)
+        ebone = arm.edit_bones.new(name="keep_driver")
+        ebone.head = (0, 1, 0)
+        ebone.tail = (1, 1, 0)
+
+        bpy.ops.object.mode_set(mode='POSE')
+        pose_bone = arm_ob.pose.bones["test"]
+        pose_bone.driver_add("location", 0)
+        constraint = pose_bone.constraints.new('LIMIT_DISTANCE')
+        constraint.name = "test"
+        constraint.driver_add("distance")
+
+        pose_bone = arm_ob.pose.bones["keep_driver"]
+        pose_bone.driver_add("location", 1)
+        self.assertEqual(len(arm_ob.animation_data.drivers), 3)
+
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.context.evaluated_depsgraph_get()
+        arm.edit_bones.remove(arm.edit_bones["test"])
+        arm.edit_bones.remove(arm.edit_bones["keep_driver"])
+        self.assertEqual(
+            len(
+                arm_ob.animation_data.drivers),
+            3,
+            "Drivers should only be removed once leaving edit mode. "
+            "This allows replacing a bone by deleting it and creating a bone with the same name")
+        # Drivers for this bone will not be removed, because it was re-created before leaving edit mode.
+        ebone = arm.edit_bones.new(name="keep_driver")
+        ebone.tail = (1, 0, 0)
+        bpy.ops.object.mode_set(mode='POSE')
+        self.assertEqual(len(arm_ob.animation_data.drivers), 1,
+                         "Removing the bone should remove the driver on it and on its constraint")
+
 
 def main():
     global args

commit 521db930d5cd1c4169f96e3c2acb4f3b2b4018b1

Fix #158665: Crash when running python script to rename and create edit bones
Caused by b6a84566b9f8baad2d9316081a21c57b7df51d08

With the above commit, drivers are removed if their pose bone no longer
exists upon leaving edit mode.

The crash of the report was caused by the following chain of events.
* script goes into edit mode
* gets an edit bone
* renames it
* creates a new edit bone
* that triggers a depsgraph event
* which in turn calls `BKE_pose_rebuild` on the cow ID
* that in turn calls `BKE_pose_channels_clear_with_null_bone`
* which then cannot find the `Bone` for the `bPoseChannel` because the
name hasn't propagated correctly (see `ED_armature_bone_rename`. Tldr.
The `Bone` is **not renamed** in edit mode, but the `bPoseChannel` and
`EditBone` are)
* which causes the call to `BKE_animdata_drivers_remove_for_rna_struct`
removing the driver, *only on the cow copy*!
* However the depsgraph node for the driver is still there, and it has
stored its index into the cached `driver_array` of `AnimData`
* Because of that we get a segfault for reading out of the array bounds.

The fix is to not remove drivers when we are evaluating depsgraph data.
Luckily the `do_id_data` variable already tells us if that is the case.

Pull Request: https://projects.blender.org/blender/blender/pulls/158793

source/blender/blenkernel/intern/armature.cc
@@ -2928,7 +2928,11 @@ void BKE_pose_channels_clear_with_null_bone(Object *armature_ob, const bool do_i
   for (bPoseChannel &pchan : pose->chanbase.items_mutable()) {
     Bone *bone = pchan.bone_get(*armature_ob);
     if (bone == nullptr) {
-      BKE_animdata_drivers_remove_for_rna_struct(armature_ob->id, *RNA_PoseBone, &pchan);
+      /* If `do_id_user` is false, we are working with copy on write data in which case we should
+       * not be deleting any drivers of missing bones. See #158665.  */
+      if (do_id_user) {
+        BKE_animdata_drivers_remove_for_rna_struct(armature_ob->id, *RNA_PoseBone, &pchan);
+      }
       BKE_pose_channel_free_ex(&pchan, do_id_user);
       BKE_pose_channels_hash_free(pose);
       BLI_freelinkN(&pose->chanbase, &pchan);

source/blender/depsgraph/intern/eval/deg_eval_copy_on_write.cc
@@ -672,6 +672,10 @@ void update_animation_data_after_copy(const ID *id_orig, ID *id_cow)
   AnimData *anim_data_cow = BKE_animdata_from_id(id_cow);
   BLI_assert(anim_data_cow != nullptr);
   update_nla_tracks_orig_pointers(&anim_data_orig->nla_tracks, &anim_data_cow->nla_tracks);
+  /* If the driver count on the evaluated ID is different, we will get a crash when trying to
+   * evaluate the drivers because it will read an FCurve out of Array bounds. See #158665. */
+  BLI_assert(BLI_listbase_count(&anim_data_orig->drivers) ==
+             BLI_listbase_count(&anim_data_cow->drivers));
 }
 
 /* Do some special treatment of data transfer from original ID to its