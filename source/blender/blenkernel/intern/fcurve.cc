Pagecran/blender

commit 8870a2cdd6fd665c7fe912e643194ae209730207

Anim: Graph Editor - Improve feedback adding F-Curve modifiers
So far Cycles/smooth modifier can only be added when modifier stack is
empty. They are required to be first in stack as they expect original
data. This PR will allow appending/reordering these modifier
irrespective of their position in stack. When not first, set
`FMODIFIER_FLAG_DISABLED` to disable the evaluation, added new function
`BKE_fmodifier_ensure_flag` to handle this. Also color the name button
red and show error message in panel body that the modifier is disabled.

Alternative for !155237

Design task: #155193

See PR description for video

Pull Request: https://projects.blender.org/blender/blender/pulls/156802

source/blender/blenkernel/intern/fcurve.cc
@@ -191,6 +191,15 @@ void BKE_fmodifier_name_set(FModifier *fcm, const char *name)
                  sizeof(fcm->name));
 }
 
+void BKE_fmodifier_ensure_flag(ListBaseT<FModifier> *modifiers)
+{
+  for (FModifier &fcm : *modifiers) {
+    if (get_fmodifier_typeinfo(fcm.type)->requires_flag & FMI_REQUIRES_ORIGINAL_DATA) {
+      SET_FLAG_FROM_TEST(fcm.flag, &fcm != modifiers->first, FMODIFIER_FLAG_DISABLED);
+    }
+  }
+}
+
 void BKE_fcurve_foreach_id(FCurve *fcu, LibraryForeachIDData *data)
 {
   ChannelDriver *driver = fcu->driver;

source/blender/blenkernel/BKE_fcurve.hh
@@ -245,6 +245,10 @@ void BKE_fcurve_rnapath_set(FCurve &fcu, StringRef rna_path);
 void BKE_fmodifier_name_set(FModifier *fcm, const char *name);
 
 /**
+ * Disable modifiers that requires original data and are not first in the stack.
+ */
+void BKE_fmodifier_ensure_flag(ListBaseT<FModifier> *modifiers);
+/**
  * Callback used by lib_query to walk over all ID usages
  * (mimics `foreach_id` callback of #IDTypeInfo structure).
  *

source/blender/blenkernel/intern/fmodifier.cc
@@ -1188,16 +1188,6 @@ FModifier *add_fmodifier(ListBaseT<FModifier> *modifiers, int type, FCurve *owne
     return nullptr;
   }
 
-  /* special checks for whether modifier can be added */
-  if ((modifiers->first) && (fmi->requires_flag & FMI_REQUIRES_ORIGINAL_DATA)) {
-    /* Modifiers requiring original data must be first in stack, so for now, don't add if it can't
-     * be. */
-    /* TODO: perhaps there is some better way, but for now, */
-    CLOG_ERROR(
-        &LOG, "Cannot add '%s' modifier to F-Curve, as it can only be first in stack.", fmi->name);
-    return nullptr;
-  }
-
   /* add modifier itself */
   fcm = MEM_new<FModifier>("F-Curve Modifier");
   fcm->type = eFModifier_Types(type);
@@ -1205,6 +1195,8 @@ FModifier *add_fmodifier(ListBaseT<FModifier> *modifiers, int type, FCurve *owne
   fcm->curve = owner_fcu;
   fcm->influence = 1.0f;
   BLI_addtail(modifiers, fcm);
+
+  BKE_fmodifier_ensure_flag(modifiers);
 
   /* Set modifier name and make sure it is unique. */
   BKE_fmodifier_name_set(fcm, "");
@@ -1317,6 +1309,7 @@ bool remove_fmodifier(ListBaseT<FModifier> *modifiers, FModifier *fcm)
       BKE_fcurve_handles_recalc(*update_fcu);
     }
 
+    BKE_fmodifier_ensure_flag(modifiers);
     return true;
   }
 

source/blender/editors/animation/fmodifier_ui.cc
@@ -105,23 +105,8 @@ static void fmodifier_reorder(bContext *C, Panel *panel, int new_index)
   ID *owner_id;
   PointerRNA *ptr = fmodifier_get_pointers(nullptr, panel, &owner_id);
   FModifier *fcm = static_cast<FModifier *>(ptr->data);
-  const FModifierTypeInfo *fmi = get_fmodifier_typeinfo(fcm->type);
-
-  /* Cycles modifier has to be the first, so make sure it's kept that way. */
-  if (fmi->requires_flag & FMI_REQUIRES_ORIGINAL_DATA) {
-    WM_global_report(RPT_ERROR, "Modifier requires original data");
-    return;
-  }
 
   ListBaseT<FModifier> *modifiers = fmodifier_list_space_specific(C);
-
-  /* Again, make sure we don't move a modifier before a cycles modifier. */
-  FModifier *fcm_first = static_cast<FModifier *>(modifiers->first);
-  const FModifierTypeInfo *fmi_first = get_fmodifier_typeinfo(fcm_first->type);
-  if (fmi_first->requires_flag & FMI_REQUIRES_ORIGINAL_DATA && new_index == 0) {
-    WM_global_report(RPT_ERROR, "Modifier requires original data");
-    return;
-  }
 
   int current_index = BLI_findindex(modifiers, fcm);
   BLI_assert(current_index >= 0);
@@ -134,6 +119,8 @@ static void fmodifier_reorder(bContext *C, Panel *panel, int new_index)
 
   /* Move the FModifier in the list. */
   BLI_listbase_link_move(modifiers, fcm, new_index - current_index);
+
+  BKE_fmodifier_ensure_flag(modifiers);
 
   ED_undo_push(C, "Reorder F-Curve Modifier");
 
@@ -310,6 +297,9 @@ static void fmodifier_panel_header(const bContext *C, Panel *panel)
 
   /* Checkbox for 'active' status (for now). */
   sub->prop(ptr, "active", ui::ITEM_R_ICON_ONLY, "", ICON_NONE);
+  if (fcm->flag & FMODIFIER_FLAG_DISABLED) {
+    sub->red_alert_set(true);
+  }
 
   /* Name. */
   if (fmi) {
@@ -489,9 +479,15 @@ static void cycles_panel_draw(const bContext *C, Panel *panel)
   ui::Layout &layout = *panel->layout;
 
   PointerRNA *ptr = fmodifier_get_pointers(C, panel, nullptr);
+  const FModifier *fcm = static_cast<FModifier *>(ptr->data);
 
   layout.use_property_split_set(true);
   layout.use_property_decorate_set(false);
+
+  if (fcm->flag & FMODIFIER_FLAG_DISABLED) {
+    layout.label("Modifier disabled because it is not the first modifier in the stack.",
+                 ICON_ERROR);
+  }
 
   /* Before. */
   ui::Layout *col = &layout.column(false);
@@ -872,9 +868,15 @@ static void smooth_panel_draw(const bContext *C, Panel *panel)
   ui::Layout &layout = *panel->layout;
 
   PointerRNA *ptr = fmodifier_get_pointers(C, panel, nullptr);
+  const FModifier *fcm = static_cast<FModifier *>(ptr->data);
 
   layout.use_property_split_set(true);
   layout.use_property_decorate_set(false);
+
+  if (fcm->flag & FMODIFIER_FLAG_DISABLED) {
+    layout.label("Modifier disabled because it is not the first modifier in the stack.",
+                 ICON_ERROR);
+  }
 
   ui::Layout &col = layout.column(false);
   col.prop(ptr, "sigma", UI_ITEM_NONE, std::nullopt, ICON_NONE);

commit 1a5a83baf3ffcdc50efa1915e4aa849671a59229

Fix: Check whether Fmodifiertypeinfo is valid
If an F-Curve modifier has an invalid or unknown type,
`get_fmodifier_typeinfo` returns a null pointer. Dereferencing it
without checking causes a crash. Added a check for null typeinfo.

This issue was introduced in 8870a2cdd6f.

Pull Request: https://projects.blender.org/blender/blender/pulls/159201

source/blender/blenkernel/intern/fcurve.cc
@@ -194,7 +194,8 @@ void BKE_fmodifier_name_set(FModifier *fcm, const char *name)
 void BKE_fmodifier_ensure_flag(ListBaseT<FModifier> *modifiers)
 {
   for (FModifier &fcm : *modifiers) {
-    if (get_fmodifier_typeinfo(fcm.type)->requires_flag & FMI_REQUIRES_ORIGINAL_DATA) {
+    const FModifierTypeInfo *fmi = get_fmodifier_typeinfo(fcm.type);
+    if (fmi && fmi->requires_flag & FMI_REQUIRES_ORIGINAL_DATA) {
       SET_FLAG_FROM_TEST(fcm.flag, &fcm != modifiers->first, FMODIFIER_FLAG_DISABLED);
     }
   }