b26c3a1733380aa40a0c889fce934ee62fe7cb6d
Pascal André <pascalandr@gmail.com>
2026-06-10 22:43:07 +0200

    Update BKE_blender_version.h
    build 6

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -26,7 +26,7 @@ namespace blender {
 #define BLENDER_VERSION_PATCH 1
 
 /** Blender build number for Pagecran Studio releases. */
-#define BLENDER_VERSION_BUILD 4
+#define BLENDER_VERSION_BUILD 6
 
 /** Blender release cycle stage: alpha/beta/rc/release. */
 #define BLENDER_VERSION_CYCLE release
