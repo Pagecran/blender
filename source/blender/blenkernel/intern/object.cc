Pagecran/blender

commit 0ee66b616554f2d23c77762764c1cc10c12b1be9

Rigging: Add Head/Tail slider for object-bone parenting
When parenting objects to bones using the `Bone` Parent Type, the existing code positions the object relative to the *tail* of the bone, something that has been [pointed out](https://blender.stackexchange.com/questions/295201/why-does-parenting-to-a-bone-put-the-object-at-the-bone-tail) as being very unintuitive and unexpected.

The options to mitigate this were:

* Add an Empty with a `Copy Transforms` constraint pointing to the bone, then parent the child to the Empty. (convoluted, increases scene complexity).
* Manually add a local transform to the child to position it at the head. (if bone length is changed, the child transform must also be manually changed to match).

Instead of the above, this adds a `Head/Tail` slider when using the `Bone` Parent Type to allow the user to position the child along the length of the bone (same interface as the _Track To_ constraint and others).

~~In addition, this changes the default behavior: **the default position is now the bone's head**. I believe this makes sense since the constraints that use the Head/Tail slider default to the head, and just intuitively, the bone's head is the most obvious choice.~~ (This is no longer the case, the default behavior is unchanged).

Backwards compatibility is maintained by version migration logic.

<video src="attachments/eaf60892-894a-4003-ba1c-24b515a818fc" title="Screencast_20260427_191224.webm" controls></video>

Co-authored-by: Colin Basnett <5035660+cmbasnett@users.noreply.github.com>
Pull Request: https://projects.blender.org/blender/blender/pulls/157900

source/blender/blenkernel/intern/object.cc
@@ -3228,10 +3228,10 @@ static void ob_parbone(const Object *ob, const Object *par, float r_mat[4][4])
   }
   else {
     copy_m4_m4(r_mat, pchan->pose_mat);
-
-    /* but for backwards compatibility, the child has to move to the tail */
     copy_v3_v3(vec, r_mat[1]);
-    mul_v3_fl(vec, pchan_bone->length);
+    if (ob->parent_bone_head_tail_factor != 0.0f) {
+      mul_v3_fl(vec, pchan_bone->length * ob->parent_bone_head_tail_factor);
+    }
     add_v3_v3(r_mat[3], vec);
   }
 }

scripts/startup/bl_ui/properties_object.py
@@ -147,6 +147,7 @@ class OBJECT_PT_relations(ObjectButtonsPanel, Panel):
         parent = ob.parent
         if parent and ob.parent_type == 'BONE' and parent.type == 'ARMATURE':
             sub.prop_search(ob, "parent_bone", parent.data, "bones")
+            sub.prop(ob, "parent_bone_head_tail_factor", text="Head/Tail")
         elif ob.parent_type == 'VERTEX':
             col.prop(ob, "parent_vertices", text="Parent Vertex", index=0)
             sub.prop(ob, "use_parent_final_indices")

source/blender/blenloader/intern/versioning_520.cc
@@ -726,6 +726,12 @@ void blo_do_versions_520(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
     FOREACH_NODETREE_END;
   }
 
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 502, 35)) {
+    for (Object &object : bmain->objects) {
+      object.parent_bone_head_tail_factor = 1.0;
+    }
+  }
+
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.

source/blender/makesdna/DNA_object_types.h
@@ -480,6 +480,8 @@ struct Object {
   /** String describing sub-object info. */
   char parsubstr[/*MAX_NAME*/ 64] = "";
   struct Object *parent = nullptr, *track = nullptr;
+  float parent_bone_head_tail_factor = 1.0f;
+  char _pad4[4] = {};
   /* Proxy pointer are deprecated, only kept for conversion to liboverrides. */
   DNA_DEPRECATED struct Object *proxy = nullptr;
   DNA_DEPRECATED struct Object *proxy_group = nullptr;

source/blender/makesrna/intern/rna_object.cc
@@ -3058,6 +3058,14 @@ static void rna_def_object(BlenderRNA *brna)
       prop, "Parent Bone", "Name of parent bone in case of a bone parenting relation");
   RNA_def_property_update(prop, NC_OBJECT | ND_DRAW, "rna_Object_dependency_update");
 
+  prop = RNA_def_property(srna, "parent_bone_head_tail_factor", PROP_FLOAT, PROP_FACTOR);
+  RNA_def_property_float_sdna(prop, nullptr, "parent_bone_head_tail_factor");
+  RNA_def_property_float_default(prop, 1.0f);
+  RNA_def_property_range(prop, 0.0f, 1.0f);
+  RNA_def_property_ui_range(prop, 0.0, 1.0, 1, 2);
+  RNA_def_property_ui_text(prop, "Parent Bone Head/Tail", "Position along the length of bone");
+  RNA_def_property_update(prop, NC_OBJECT | ND_DRAW, "rna_Object_internal_update");
+
   prop = RNA_def_property(srna, "use_parent_final_indices", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "transflag", OB_PARENT_USE_FINAL_INDICES);
   RNA_def_property_ui_text(

commit 5a71a2e5a599b5521556ef2c4b4d368f992dc815

Fix #159865: Parenting an object to a pose bone results in a jump
Caused by 0ee66b616554f2d23c77762764c1cc10c12b1be9

That commit introduced a feature to choose between parenting to the
head or the tail of a bone. The default value was 1 meaning it would
parent to the tail which is consistent with 5.1.
However when calculating the parent inverse matrix
(`BKE_object_calc_parent`), a temporary object is created which has
that value at 0.

The fix is to copy the `parent_bone_head_tail_factor` value from the
original object. That makes the inverse calculations work for any
values of `parent_bone_head_tail_factor` at the point of parenting.

Pull Request: https://projects.blender.org/blender/blender/pulls/159866

source/blender/blenkernel/intern/object.cc
@@ -3566,6 +3566,7 @@ float4x4 BKE_object_calc_parent(Depsgraph *depsgraph, Scene *scene, Object *ob)
   workob.par1 = ob->par1;
   workob.par2 = ob->par2;
   workob.par3 = ob->par3;
+  workob.parent_bone_head_tail_factor = ob->parent_bone_head_tail_factor;
 
   /* The effects of constraints should NOT be included in the parent-inverse matrix. Constraints
    * are supposed to be applied after the object's local loc/rot/scale. If the (inverted) effect of